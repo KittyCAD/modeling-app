16b6ef3a087ac06950e475507187fd824b314ede
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2023-01-17 16:09:41 +1100

    refactor internal fns

--- src/lang/sketch.ts
+++ src/lang/sketch.ts
@@ -32,38 +32,29 @@ function getCoordsFromPaths(paths: Path[], index = 0): Coords2d {
   return [0, 0]
 }
 
-export const sketchFns = {
-  // base: (
-  //   programMemory: ProgramMemory,
-  //   name: string = '',
-  //   sourceRange: SourceRange,
-  //   ...args: any[]
-  // ): PathReturn => {
-  //   if ((programMemory?._sketch?.length || 0) > 0) {
-  //     throw new Error('Base can only be called once')
-  //   }
-  //   const [x, y] = args as [number, number]
-  //   let from: [number, number] = [x, y]
-  //   const geo = baseGeo({ from: [x, y, 0] })
-  //   const newPath: Path = {
-  //     type: 'base',
-  //     from,
-  //     sourceRange,
-  //     geo,
-  //   }
-  //   return {
-  //     programMemory: {
-  //       ...programMemory,
-  //       _sketch: [...(programMemory?._sketch || []), newPath],
-  //     },
-  //     currentPath: newPath,
-  //   }
-  // },
-  close: (
-    programMemory: ProgramMemory,
-    name: string = '',
-    sourceRange: SourceRange
-  ): PathReturn => {
+interface InternalFirstArg {
+  programMemory: ProgramMemory
+  name?: string
+  sourceRange: SourceRange
+}
+
+type SketchFn = (internals: InternalFirstArg, ...args: any[]) => PathReturn
+
+export type SketchFnNames = 'close' | 'lineTo'
+
+type InternalFn = (internals: InternalFirstArg, ...args: any[]) => any
+
+export type InternalFnNames =
+  | 'extrude'
+  | 'translate'
+  | 'transform'
+  | 'getExtrudeWallTransform'
+  | 'rx'
+  | 'ry'
+  | 'rz'
+
+export const sketchFns: { [key in SketchFnNames]: SketchFn } = {
+  close: ({ programMemory, name = '', sourceRange }) => {
     const firstPath = programMemory?._sketch?.[0] as Path
 
     let from = getCoordsFromPaths(
@@ -110,12 +101,7 @@ export const sketchFns = {
       currentPath: newPath,
     }
   },
-  lineTo: (
-    programMemory: ProgramMemory,
-    name: string = '',
-    sourceRange: SourceRange,
-    ...args: any[]
-  ): PathReturn => {
+  lineTo: ({ programMemory, name = '', sourceRange }, ...args) => {
     const [x, y] = args
     if (!programMemory._sketch) {
       throw new Error('No sketch to draw on')
@@ -155,100 +141,12 @@ export const sketchFns = {
       currentPath,
     }
   },
-  rx: rotateOnAxis([1, 0, 0]),
-  ry: rotateOnAxis([0, 1, 0]),
-  rz: rotateOnAxis([0, 0, 1]),
-  extrude: (
-    programMemory: ProgramMemory,
-    name: string = '',
-    sourceRange: SourceRange,
-    length: number,
-    sketchVal: SketchGroup
-  ): ExtrudeGroup => {
-    const getSketchGeo = (sketchVal: SketchGroup): SketchGroup => {
-      return sketchVal
-    }
-
-    const sketch = getSketchGeo(sketchVal)
-    const { position, rotation } = sketchVal
-
-    const extrudeSurfaces: ExtrudeSurface[] = []
-    const extrusionDirection = clockwiseSign(
-      sketch.value.map((line) => line.to)
-    )
-    sketch.value.map((line, index) => {
-      if (line.type === 'toPoint') {
-        let from: [number, number] = line.from
-        const to = line.to
-        const {
-          geo,
-          position: facePosition,
-          rotation: faceRotation,
-        } = extrudeGeo({
-          from: [from[0], from[1], 0],
-          to: [to[0], to[1], 0],
-          length,
-          extrusionDirection,
-        })
-        const groupQuaternion = new Quaternion(...rotation)
-        const currentWallQuat = new Quaternion(...faceRotation)
-        const unifiedQuit = new Quaternion().multiplyQuaternions(
-          currentWallQuat,
-          groupQuaternion.clone().invert()
-        )
-
-        const facePositionVector = new Vector3(...facePosition)
-        facePositionVector.applyQuaternion(groupQuaternion.clone())
-        const unifiedPosition = new Vector3().addVectors(
-          facePositionVector,
-          new Vector3(...position)
-        )
-        const surface: ExtrudeSurface = {
-          type: 'extrudePlane',
-          position: unifiedPosition.toArray() as Position,
-          rotation: unifiedQuit.toArray() as Rotation,
-          __geoMeta: {
-            geo,
-            sourceRange: line.__geoMeta.sourceRange,
-            pathToNode: line.__geoMeta.pathToNode,
-          },
-        }
-        line.name && (surface.name = line.name)
-        extrudeSurfaces.push(surface)
-      }
-    })
-    return {
-      type: 'extrudeGroup',
-      value: extrudeSurfaces,
-      height: length,
-      position,
-      rotation,
-      __meta: [
-        {
-          sourceRange,
-          pathToNode: [], // TODO
-        },
-        {
-          sourceRange: sketchVal.__meta[0].sourceRange,
-          pathToNode: sketchVal.__meta[0].pathToNode,
-        },
-      ],
-    }
-  },
-  translate,
-  transform,
-  getExtrudeWallTransform,
 }
 
 function rotateOnAxis<T extends SketchGroup | ExtrudeGroup>(
   axisMultiplier: [number, number, number]
-) {
-  return (
-    programMemory: ProgramMemory,
-    sourceRange: SourceRange,
-    rotationD: number,
-    sketch: T
-  ): T => {
+): InternalFn {
+  return ({ sourceRange }, rotationD: number, sketch: T): T => {
     const rotationR = rotationD * (Math.PI / 180)
     const rotateVec = new Vector3(...axisMultiplier)
     const quaternion = new Quaternion()
@@ -275,12 +173,85 @@ function rotateOnAxis<T extends SketchGroup | ExtrudeGroup>(
   }
 }
 
-function translate<T extends SketchGroup | ExtrudeGroup>(
-  programMemory: ProgramMemory,
-  sourceRange: SourceRange,
+const extrude: InternalFn = (
+  { sourceRange },
+  length: number,
+  sketchVal: SketchGroup
+): ExtrudeGroup => {
+  const getSketchGeo = (sketchVal: SketchGroup): SketchGroup => {
+    return sketchVal
+  }
+
+  const sketch = getSketchGeo(sketchVal)
+  const { position, rotation } = sketchVal
+
+  const extrudeSurfaces: ExtrudeSurface[] = []
+  const extrusionDirection = clockwiseSign(sketch.value.map((line) => line.to))
+  sketch.value.map((line, index) => {
+    if (line.type === 'toPoint') {
+      let from: [number, number] = line.from
+      const to = line.to
+      const {
+        geo,
+        position: facePosition,
+        rotation: faceRotation,
+      } = extrudeGeo({
+        from: [from[0], from[1], 0],
+        to: [to[0], to[1], 0],
+        length,
+        extrusionDirection,
+      })
+      const groupQuaternion = new Quaternion(...rotation)
+      const currentWallQuat = new Quaternion(...faceRotation)
+      const unifiedQuit = new Quaternion().multiplyQuaternions(
+        currentWallQuat,
+        groupQuaternion.clone().invert()
+      )
+
+      const facePositionVector = new Vector3(...facePosition)
+      facePositionVector.applyQuaternion(groupQuaternion.clone())
+      const unifiedPosition = new Vector3().addVectors(
+        facePositionVector,
+        new Vector3(...position)
+      )
+      const surface: ExtrudeSurface = {
+        type: 'extrudePlane',
+        position: unifiedPosition.toArray() as Position,
+        rotation: unifiedQuit.toArray() as Rotation,
+        __geoMeta: {
+          geo,
+          sourceRange: line.__geoMeta.sourceRange,
+          pathToNode: line.__geoMeta.pathToNode,
+        },
+      }
+      line.name && (surface.name = line.name)
+      extrudeSurfaces.push(surface)
+    }
+  })
+  return {
+    type: 'extrudeGroup',
+    value: extrudeSurfaces,
+    height: length,
+    position,
+    rotation,
+    __meta: [
+      {
+        sourceRange,
+        pathToNode: [], // TODO
+      },
+      {
+        sourceRange: sketchVal.__meta[0].sourceRange,
+        pathToNode: sketchVal.__meta[0].pathToNode,
+      },
+    ],
+  }
+}
+
+const translate: InternalFn = <T extends SketchGroup | ExtrudeGroup>(
+  { sourceRange }: InternalFirstArg,
   vec3: [number, number, number],
   sketch: T
-): T {
+): T => {
   const oldPosition = new Vector3(...sketch.position)
   const newPosition = oldPosition.add(new Vector3(...vec3))
   return {
@@ -296,15 +267,14 @@ function translate<T extends SketchGroup | ExtrudeGroup>(
   }
 }
 
-function transform<T extends SketchGroup | ExtrudeGroup>(
-  programMemory: ProgramMemory,
-  sourceRange: SourceRange,
+const transform: InternalFn = <T extends SketchGroup | ExtrudeGroup>(
+  { sourceRange }: InternalFirstArg,
   transformInfo: {
     position: Position
     quaternion: Rotation
   },
   sketch: T
-): T {
+): T => {
   const quaternionToApply = new Quaternion(...transformInfo.quaternion)
   const newQuaternion = new Quaternion(...sketch.rotation).multiply(
     quaternionToApply.invert()
@@ -328,21 +298,30 @@ function transform<T extends SketchGroup | ExtrudeGroup>(
   }
 }
 
-function getExtrudeWallTransform(
-  programMemory: ProgramMemory,
-  sourceRange: SourceRange,
+const getExtrudeWallTransform: InternalFn = (
+  _,
   pathName: string,
   extrudeGroup: ExtrudeGroup
 ): {
   position: Position
   quaternion: Rotation
-} {
+} => {
   const path = extrudeGroup.value.find((path) => path.name === pathName)
   if (!path) throw new Error(`Could not find path with name ${pathName}`)
   return {
     position: path.position,
     quaternion: path.rotation,
   }
+}
+
+export const internalFns: { [key in InternalFnNames]: InternalFn } = {
+  rx: rotateOnAxis([1, 0, 0]),
+  ry: rotateOnAxis([0, 1, 0]),
+  rz: rotateOnAxis([0, 0, 1]),
+  extrude,
+  translate,
+  transform,
+  getExtrudeWallTransform,
 }
 
 function clockwiseSign(points: [number, number][]): number {
