dba05ea3d8bdd5ed3d7d65c2effd847bbecb5f6e
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2023-02-13 17:11:51 +1100

    it pget vercel to build

--- src/lang/rust.ts
+++ src/lang/rust.ts
@@ -6,6 +6,6 @@ const url =
     : window.location.origin.includes('localhost')
     ? 'http://127.0.0.1:3000'
     : window.location.origin
-const fullUrl = url + '/wasm/wasm_lib_bg.wasm'
+const fullUrl = url + '/wasm_lib_bg.wasm'
 console.log('fullUrl', fullUrl)
 export const initPromise = init(fullUrl)
