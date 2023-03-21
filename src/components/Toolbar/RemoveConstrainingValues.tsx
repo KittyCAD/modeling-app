49b5b3cc2cc13ff6ecddd177334638a847e1e075
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2023-03-22 02:56:56 +1100

    Add 'remove constraining values' UI

---
+++ src/components/Toolbar/RemoveConstrainingValues.tsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react'
+import { toolTips, useStore } from '../../useStore'
+import { Value } from '../../lang/abstractSyntaxTree'
+import {
+  getNodePathFromSourceRange,
+  getNodeFromPath,
+} from '../../lang/queryAst'
+import {
+  TransformInfo,
+  getRemoveConstraintsTransforms,
+  transformAstSketchLines,
+} from '../../lang/std/sketchcombos'
+
+export const RemoveConstrainingValues = () => {
+  const { guiMode, selectionRanges, ast, programMemory, updateAst } = useStore(
+    (s) => ({
+      guiMode: s.guiMode,
+      ast: s.ast,
+      updateAst: s.updateAst,
+      selectionRanges: s.selectionRanges,
+      programMemory: s.programMemory,
+    })
+  )
+  const [enableHorz, setEnableHorz] = useState(false)
+  const [transformInfos, setTransformInfos] = useState<TransformInfo[]>()
+  useEffect(() => {
+    if (!ast) return
+    const paths = selectionRanges.map((selectionRange) =>
+      getNodePathFromSourceRange(ast, selectionRange)
+    )
+    const nodes = paths.map(
+      (pathToNode) => getNodeFromPath<Value>(ast, pathToNode).node
+    )
+    const isAllTooltips = nodes.every(
+      (node) =>
+        node?.type === 'CallExpression' &&
+        toolTips.includes(node.callee.name as any)
+    )
+
+    const theTransforms = getRemoveConstraintsTransforms(
+      selectionRanges,
+      ast,
+      'removeConstrainingValues'
+    )
+    setTransformInfos(theTransforms)
+
+    const _enableHorz = isAllTooltips && theTransforms.every(Boolean)
+    setEnableHorz(_enableHorz)
+  }, [guiMode, selectionRanges])
+  if (guiMode.mode !== 'sketch') return null
+
+  return (
+    <button
+      onClick={() =>
+        transformInfos &&
+        ast &&
+        updateAst(
+          transformAstSketchLines({
+            ast,
+            selectionRanges,
+            transformInfos,
+            programMemory,
+            referenceSegName: '',
+          })?.modifiedAst
+        )
+      }
+      className={`border m-1 px-1 rounded text-xs ${
+        enableHorz ? 'bg-gray-50 text-gray-800' : 'bg-gray-200 text-gray-400'
+      }`}
+      disabled={!enableHorz}
+      title="yo dawg"
+    >
+      RemoveConstrainingValues
+    </button>
+  )
+}
