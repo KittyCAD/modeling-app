5e1d620b3bbef9a1ab89958951ba57a07c462141
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2023-04-01 10:06:10 +1100

    update button text

--- src/components/SetVarNameModal.tsx
+++ src/components/SetVarNameModal.tsx
@@ -73,7 +73,7 @@ export const SetVarNameModal = ({
                       })
                     }
                   >
-                    Add constraining value
+                    Add variable
                   </button>
                 </div>
               </Dialog.Panel>
