32a98107a5383ee736ad816bfff24f8d3ac88f05
Frank Johnson <frankjohnson1993@gmail.com>
2023-07-12 10:55:52 -0400

    Rough-in of AppHeader

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,23 @@
+import { Toolbar } from "../Toolbar"
+
+export const AppHeader = ({ children }: React.PropsWithChildren) => {
+    return (
+        <header className="py-1 px-5 bg-chalkboard-10 border-b border-chalkboard-30 flex justify-between">
+            <button onClick={() => alert('A project menu pane is coming soon!')}>
+                <img src="/kitt-arcade-winking.svg" alt="KittyCAD App" className="h-9 w-auto" />
+                <span className="sr-only">KittyCAD App</span>
+            </button>
+            {/* Toolbar if the context deems it */}
+            <div className="max-w-4xl">
+                <Toolbar />
+            </div>
+            {/* If there are children, show them, otherwise... */}
+            {children || (
+                // TODO: If signed out, show the token paste field
+
+                // If signed in, show the account avatar
+                <button onClick={() => alert("An account menu pane is coming soon!")}>Account</button>
+            )}
+        </header>
+    )
+}
