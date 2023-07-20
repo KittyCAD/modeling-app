369770557a296bb938b722c75de6c616c0f16957
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2023-07-21 09:07:42 +1000

    more tweaks to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,18 @@
-import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import eslint from 'vite-plugin-eslint'
 
-export default defineConfig({
-    server: {
-        open: true,
-        port: 3000,
-    },
-    build: {
-      outDir: 'build',
-    },
+export default {
+  server: {
+    open: true,
+    port: 3000,
+  },
+  build: {
+    outDir: 'build',
+  },
   plugins: [
     react(),
     viteTsconfigPaths(),
     eslint(),
   ],
-  test: {
-    globals: true,
-  }
-})
+}
