ded40352d39aab0464fb255889b73a17d19b2290
Frank Johnson <frankjohnson1993@gmail.com>
2023-07-20 12:51:21 -0400

    Add Toggle component

---
+++ src/components/Toggle/Toggle.tsx
@@ -0,0 +1,29 @@
+import styles from './Toggle.module.css'
+
+interface ToggleProps {
+    className?: string
+    offLabel: string
+    onLabel: string
+    name: string
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+    checked: boolean
+}
+
+export const Toggle = ({
+    className = "",
+    offLabel,
+    onLabel,
+    name = "",
+    onChange,
+    checked,
+}: ToggleProps) => {
+    return (
+        <label className={`${styles.toggle} ${className}`}>
+            {offLabel}
+            <input type="checkbox" name={name} id={name}
+                checked={checked} onChange={onChange} />
+            <span></span>
+            {onLabel}
+        </label>
+    )
+}
