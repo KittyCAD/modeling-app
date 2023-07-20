c84ef387ef0d70bc7d73b29b4b79e4009570b9f1
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2023-07-21 07:37:41 +1000

    clean up deps

--- vite.config.ts
+++ vite.config.ts
@@ -16,9 +16,4 @@ export default defineConfig({
     viteTsconfigPaths(),
     eslint(),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-  }
 })
