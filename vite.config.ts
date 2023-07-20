b89faa4a28dd2430703318a54c9a6735a2f7de9c
Frank Noirot <frank@kittycad.io>
2023-07-20 19:25:04 -0400

    Migrate from CRA to Vite (#170)
    * Basic CRA to Vite conversion
    
    * Restore ESLint support
    
    * Remove semicolons from vite config
    
    * Add vite client types to tsconfig
    
    * Migrate to Vitest for testing (not working on Mac)
    
    * some test progress (#175)
    
    * some test progress
    
    * something maybe working
    
    * remove local lib
    
    * small clean up
    
    * tweaks
    
    * fix dependency
    
    * clean up deps
    
    * remove vitest import
    
    * vitest config is needed even though we're not using vitest
    
    * more tweaks to vite config
    
    ---------
    
    Co-authored-by: Kurt Hutten <k.hutten@protonmail.ch>

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import react from '@vitejs/plugin-react'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+import eslint from 'vite-plugin-eslint'
+
+export default {
+  server: {
+    open: true,
+    port: 3000,
+  },
+  build: {
+    outDir: 'build',
+  },
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    eslint(),
+  ],
+}
