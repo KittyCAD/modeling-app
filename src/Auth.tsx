1a020f4e9a9bcc9b8ca72c704f5c63c1da876444
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2023-07-21 14:14:56 +1000

    remove auth

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -4,28 +4,7 @@ import withBaseUrl from './lib/withBaseURL'
 import App from './App'
 
 export const Auth = () => {
-  const { data: user } = useSWR(withBaseUrl('/user'), fetcher) as any
-  const isLocalHost =
-    typeof window !== 'undefined' && window.location.hostname === 'localhost'
 
-  if (!user && !isLocalHost) {
-    return (
-      <>
-        <div className=" bg-gray-800 p-1 px-4 rounded-r-lg pointer-events-auto flex items-center">
-          <a
-            className="font-bold mr-2 text-purple-400"
-            rel="noopener noreferrer"
-            target={'_self'}
-            href={`https://dev.kittycad.io/signin?callbackUrl=${encodeURIComponent(
-              typeof window !== 'undefined' && window.location.href
-            )}`}
-          >
-            Sign in
-          </a>
-        </div>
-      </>
-    )
-  }
 
   return <App />
 }
