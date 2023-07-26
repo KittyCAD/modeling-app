a57a66f4021bfdc2f01734929ba883a9272ce150
Adam Chalmers <adam.s.chalmers@gmail.com>
2023-07-26 12:55:15 -0500

    Run yarn prettier on jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,17 @@
-import type {Config} from 'jest';
+import type { Config } from 'jest'
 
 const config: Config = {
   testEnvironment: 'jsdom',
   preset: 'ts-jest/presets/js-with-ts',
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
-    "^.+\\.(js|jsx)$": "babel-jest",
+    '^.+\\.(js|jsx)$': 'babel-jest',
   },
-  transformIgnorePatterns: [
-    "//node_modules/(?!(allotment|@tauri-apps/api)/)",
-  ],
+  transformIgnorePatterns: ['//node_modules/(?!(allotment|@tauri-apps/api)/)'],
   moduleNameMapper: {
     '^allotment$': 'allotment/dist/legacy',
   },
   setupFilesAfterEnv: ['./src/setupTests.ts'],
-};
+}
 
-export default config;
+export default config
