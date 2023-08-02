c29e1efc0c3f0bb01e31b43cb8a14355e198be9d
Frank Johnson <frankjohnson1993@gmail.com>
2023-08-02 12:14:47 -0400

    Polish up DebugPanel

--- src/components/DebugPanel.tsx
+++ src/components/DebugPanel.tsx
@@ -1,4 +1,4 @@
-import { PanelHeader } from '../components/PanelHeader'
+import { CollapsiblePanel } from './CollapsiblePanel'
 import { useStore } from '../useStore'
 import { v4 as uuidv4 } from 'uuid'
 import { EngineCommand } from '../lang/std/engineConnection'
@@ -21,61 +21,78 @@ export const DebugPanel = () => {
   })
   if (!sketchModeCmd) return null
   return (
-    <div>
-      <PanelHeader title="Debug" />
-      <Xyz onChange={setSketchModeCmd} pointKey="origin" data={sketchModeCmd} />
-      <Xyz onChange={setSketchModeCmd} pointKey="x_axis" data={sketchModeCmd} />
-      <Xyz onChange={setSketchModeCmd} pointKey="y_axis" data={sketchModeCmd} />
-      <div className="flex">
-        <div className="pr-4">distance_to_plane</div>
-        <input
-          className="w-16"
-          type="number"
-          value={sketchModeCmd.distance_to_plane}
-          onChange={({ target }) => {
-            setSketchModeCmd({
-              ...sketchModeCmd,
-              distance_to_plane: Number(target.value),
+    <CollapsiblePanel
+      title="Debug"
+      className="!absolute !h-auto bottom-5 right-5"
+      open
+    >
+      <section className="p-4 flex flex-col gap-4">
+        <Xyz
+          onChange={setSketchModeCmd}
+          pointKey="origin"
+          data={sketchModeCmd}
+        />
+        <Xyz
+          onChange={setSketchModeCmd}
+          pointKey="x_axis"
+          data={sketchModeCmd}
+        />
+        <Xyz
+          onChange={setSketchModeCmd}
+          pointKey="y_axis"
+          data={sketchModeCmd}
+        />
+        <div className="flex">
+          <div className="pr-4">distance_to_plane</div>
+          <input
+            className="w-16 dark:bg-chalkboard-90"
+            type="number"
+            value={sketchModeCmd.distance_to_plane}
+            onChange={({ target }) => {
+              setSketchModeCmd({
+                ...sketchModeCmd,
+                distance_to_plane: Number(target.value),
+              })
+            }}
+          />
+          <div className="pr-4">ortho</div>
+          <input
+            className="w-16"
+            type="checkbox"
+            checked={sketchModeCmd.ortho}
+            onChange={(a) => {
+              console.log(a, (a as any).checked)
+              setSketchModeCmd({
+                ...sketchModeCmd,
+                ortho: a.target.checked,
+              })
+            }}
+          />
+        </div>
+        <ActionButton
+          onClick={() => {
+            engineCommandManager?.sendSceneCommand({
+              type: 'ModelingCmdReq',
+              cmd: {
+                DefaultCameraEnableSketchMode: sketchModeCmd,
+              },
+              cmd_id: uuidv4(),
+              file_id: uuidv4(),
             })
           }}
-        />
-        <div className="pr-4">ortho</div>
-        <input
-          className="w-16"
-          type="checkbox"
-          checked={sketchModeCmd.ortho}
-          onChange={(a) => {
-            console.log(a, (a as any).checked)
-            setSketchModeCmd({
-              ...sketchModeCmd,
-              ortho: a.target.checked,
-            })
+          className="hover:border-succeed-50"
+          icon={{
+            icon: faCheck,
+            bgClassName:
+              'bg-succeed-80 group-hover:bg-succeed-70 hover:bg-succeed-70',
+            iconClassName:
+              'text-succeed-20 group-hover:text-succeed-10 hover:text-succeed-10',
           }}
-        />
-      </div>
-      <ActionButton
-        onClick={() => {
-          engineCommandManager?.sendSceneCommand({
-            type: 'ModelingCmdReq',
-            cmd: {
-              DefaultCameraEnableSketchMode: sketchModeCmd,
-            },
-            cmd_id: uuidv4(),
-            file_id: uuidv4(),
-          })
-        }}
-        className="hover:border-succeed-50"
-        icon={{
-          icon: faCheck,
-          bgClassName:
-            'bg-succeed-80 group-hover:bg-succeed-70 hover:bg-succeed-70',
-          iconClassName:
-            'text-succeed-20 group-hover:text-succeed-10 hover:text-succeed-10',
-        }}
-      >
-        Send sketch mode command
-      </ActionButton>
-    </div>
+        >
+          Send sketch mode command
+        </ActionButton>
+      </section>
+    </CollapsiblePanel>
   )
 }
 
@@ -97,7 +114,7 @@ const Xyz = ({
           <div key={axis} className="flex">
             <div className="w-4">{axis}</div>
             <input
-              className="w-16"
+              className="w-16 dark:bg-chalkboard-90"
               type="number"
               value={val}
               onChange={({ target }) => {
