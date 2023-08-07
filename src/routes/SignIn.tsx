aa71232c5cf40a6924c0f28cb1cb3e77a6cde931
Frank Johnson <frankjohnson1993@gmail.com>
2023-08-07 12:51:35 -0400

    Have separate SITE_URL env from API_URL

--- src/routes/SignIn.tsx
+++ src/routes/SignIn.tsx
@@ -4,7 +4,7 @@ import { isTauri } from '../lib/isTauri'
 import { useStore } from '../useStore'
 import { invoke } from '@tauri-apps/api/tauri'
 import { useNavigate } from 'react-router-dom'
-import { VITE_KC_API_BASE_URL } from '../env'
+import { VITE_KC_SITE_BASE_URL } from '../env'
 
 const SignIn = () => {
   const navigate = useNavigate()
@@ -69,7 +69,7 @@ const SignIn = () => {
         ) : (
           <ActionButton
             Element="link"
-            to={`${VITE_KC_API_BASE_URL}/signin?callbackUrl=${encodeURIComponent(
+            to={`${VITE_KC_SITE_BASE_URL}/signin?callbackUrl=${encodeURIComponent(
               typeof window !== 'undefined' &&
                 window.location.href.replace('signin', '')
             )}`}
