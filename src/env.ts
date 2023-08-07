6e14dbaf7719429fe70d4ab3970786b90d7d880e
Frank Noirot <frank@kittycad.io>
2023-08-07 13:07:40 -0400

    Have separate SITE_URL env from API_URL (#234)

--- src/env.ts
+++ src/env.ts
@@ -9,3 +9,4 @@ export const VITE_KC_API_WS_MODELING_URL = import.meta.env
   .VITE_KC_API_WS_MODELING_URL
 // @ts-ignore
 export const VITE_KC_API_BASE_URL = import.meta.env.VITE_KC_API_BASE_URL
+export const VITE_KC_SITE_BASE_URL = import.meta.env.VITE_KC_SITE_BASE_URL
