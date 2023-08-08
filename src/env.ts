d98974e3a3a1184287a551830e0b1016d942454c
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2023-08-08 10:40:15 +1000

    remove unneeded @ts-ignore

--- src/env.ts
+++ src/env.ts
@@ -4,11 +4,8 @@
 // i.e. const VITE_MY_VAR = import.meta.env.VITE_MY_VAR
 // Maybe this file should be generated in a GHA from .env.development?
 
-// @ts-ignore
 export const VITE_KC_API_WS_MODELING_URL = import.meta.env
   .VITE_KC_API_WS_MODELING_URL
-// @ts-ignore
 export const VITE_KC_API_BASE_URL = import.meta.env.VITE_KC_API_BASE_URL
 export const VITE_KC_SITE_BASE_URL = import.meta.env.VITE_KC_SITE_BASE_URL
-
 export const TEST = import.meta.env.TEST
