bebbcb6fe29c98f06305cb83da291a27b54d00d0
Frank Johnson <frankjohnson1993@gmail.com>
2023-08-17 18:16:09 -0400

    Selectable commands in command bar

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -6,11 +6,10 @@ import { ActionIcon } from './ActionIcon'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import Fuse from 'fuse.js'
 
-const defaultActions = [
-  { type: 'navigate.home' },
-  { type: 'navigate.settings' },
-  { type: 'navigate.file' },
-]
+const defaultActions = '_'
+  .repeat(13)
+  .split('')
+  .map((_, i) => ({ type: `action-${i}` }))
 
 const ActionBar = ({
   actions = defaultActions,
@@ -22,9 +21,11 @@ const ActionBar = ({
   const [selectedAction, setSelectedAction] = useState()
   const [query, setQuery] = useState('')
 
-  const fuse = new Fuse(actions, {})
+  const fuse = new Fuse(actions, { keys: ['type'] })
 
-  const filteredActions = query === '' ? actions : fuse.search(query)
+  const filteredActions = query
+    ? fuse.search(query)
+    : actions.map((a) => ({ item: a }))
 
   return (
     <Dialog
@@ -33,27 +34,32 @@ const ActionBar = ({
       className="relative z-50"
     >
       <Dialog.Backdrop className="fixed inset-0 bg-chalkboard-10/70 dark:bg-chalkboard-110/50" />
-      <div className="fixed inset-0 flex items-center justify-center p-4">
-        <Dialog.Panel className="rounded-sm relative p-2 pr-4 bg-chalkboard-10 dark:bg-chalkboard-100 border dark:border-chalkboard-60 max-w-xl shadow-lg w-full flex gap-2">
-          <ActionIcon icon={faSearch} size="lg" />
+      <div className="fixed inset-0 flex items-center justify-center">
+        <Dialog.Panel className="rounded-sm relative p-2 bg-chalkboard-10 dark:bg-chalkboard-100 border dark:border-chalkboard-70 max-w-xl w-full shadow-lg">
           <Combobox value={selectedAction} onChange={setSelectedAction}>
-            <Combobox.Input
-              onChange={(event) => setQuery(event.target.value)}
-              className="bg-transparent focus:outline-none"
-            />
-            <Combobox.Options>
-              {filteredActions.map((actionOrResult) => {
-                const action = (
-                  'item' in actionOrResult
-                    ? actionOrResult.item
-                    : actionOrResult
-                ) as BaseActionObject
-                return (
-                  <Combobox.Option key={action.type} value={action}>
-                    {action.type}
-                  </Combobox.Option>
-                )
-              })}
+            <div className="flex gap-2 items-center">
+              <ActionIcon icon={faSearch} size="xl" />
+              <Combobox.Input
+                onChange={(event) => setQuery(event.target.value)}
+                className="bg-transparent focus:outline-none"
+                onKeyDown={(event) =>
+                  event.metaKey && event.key === 'k' && setIsOpen(false)
+                }
+                displayValue={(action: { item: BaseActionObject }) =>
+                  action.item.type
+                }
+              />
+            </div>
+            <Combobox.Options className="max-h-96 overflow-y-auto">
+              {filteredActions.map((actionResult) => (
+                <Combobox.Option
+                  key={actionResult.item.type}
+                  value={actionResult}
+                  className="ui-active:bg-liquid-10 dark:ui-active:bg-liquid-90 py-1 px-2"
+                >
+                  {actionResult.item.type}
+                </Combobox.Option>
+              ))}
             </Combobox.Options>
           </Combobox>
         </Dialog.Panel>
--- src/components/ActionIcon.tsx
+++ src/components/ActionIcon.tsx
@@ -8,7 +8,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const iconSizes = {
   sm: 12,
   md: 14.4,
-  lg: 18,
+  lg: 20,
+  xl: 28,
 }
 
 export interface ActionIconProps extends React.PropsWithChildren {
@@ -28,7 +29,7 @@ export const ActionIcon = ({
   return (
     <div
       className={
-        'p-1 w-fit inline-grid place-content-center ' +
+        `p-${size === 'xl' ? '2' : '1'} w-fit inline-grid place-content-center ` +
         (bgClassName ||
           'bg-chalkboard-100 group-hover:bg-chalkboard-90 hover:bg-chalkboard-90 dark:bg-liquid-20 dark:group-hover:bg-liquid-10 dark:hover:bg-liquid-10')
       }
@@ -40,7 +41,7 @@ export const ActionIcon = ({
           height={iconSizes[size]}
           className={
             iconClassName ||
-            'text-liquid-20 group-hover:text-liquid-10 hover:text-liquid-10 dark:text-liquid-100 dark:group-hover:text-liquid-100 dark:hover:text-liquid-100'
+            'text-liquid-20 h-auto group-hover:text-liquid-10 hover:text-liquid-10 dark:text-liquid-100 dark:group-hover:text-liquid-100 dark:hover:text-liquid-100'
           }
         />
       )}
