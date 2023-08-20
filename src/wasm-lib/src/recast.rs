4f63c0e53631ad4fa4be8c707b8e96f5123d3ad3
Jess Frazelle <github@jessfraz.com>
2023-08-19 22:47:11 -0700

    cleanup
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/wasm-lib/src/recast.rs
+++ src/wasm-lib/src/recast.rs
@@ -400,14 +400,6 @@ pub fn recast_function(expression: FunctionExpression) -> String {
     )
 }
 
-#[wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
 // wasm_bindgen wrapper for recast
 // test for this function and by extension the recaster are done in javascript land src/lang/recast.test.ts
 #[wasm_bindgen]
