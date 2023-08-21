08154d7a1531fac66a6c9886c186583512949171
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2023-08-21 13:18:21 +1000

    use createActorContext

--- src/hooks/useAuthMachine.tsx
+++ src/hooks/useAuthMachine.tsx
@@ -1,19 +1,10 @@
-import { InterpreterFrom, State, Sender } from 'xstate'
-import { useInterpret, useActor, useSelector } from '@xstate/react'
-import { createContext, useContext } from 'react'
+import { createActorContext } from '@xstate/react'
 import { useNavigate } from 'react-router-dom'
 import { paths } from '../Router'
-import {
-  authMachine,
-  TOKEN_PERSIST_KEY,
-  UserContext,
-  Events,
-} from '../lib/authMachine'
+import { authMachine, TOKEN_PERSIST_KEY } from '../lib/authMachine'
 import withBaseUrl from '../lib/withBaseURL'
 
-export const GlobalStateContext = createContext({
-  authMachine: {} as InterpreterFrom<typeof authMachine>,
-})
+export const AuthMachineContext = createActorContext(authMachine)
 
 export const GlobalStateProvider = ({
   children,
@@ -21,33 +12,35 @@ export const GlobalStateProvider = ({
   children: React.ReactNode
 }) => {
   const navigate = useNavigate()
-  const _authMachine = useInterpret(authMachine, {
-    actions: {
-      goToSignInPage: () => {
-        navigate(paths.SIGN_IN)
-        logout()
-      },
-      goToIndexPage: () => navigate(paths.INDEX),
-    },
-  })
-
   return (
-    <GlobalStateContext.Provider value={{ authMachine: _authMachine }}>
+    <AuthMachineContext.Provider
+      machine={() =>
+        authMachine.withConfig({
+          actions: {
+            goToSignInPage: () => {
+              navigate(paths.SIGN_IN)
+              logout()
+            },
+            goToIndexPage: () => navigate(paths.INDEX),
+          },
+        })
+      }
+    >
       {children}
-    </GlobalStateContext.Provider>
+    </AuthMachineContext.Provider>
   )
 }
 
 export function useAuthMachine<T>(
-  selector?: (state: State<UserContext, Events>) => T
-): [T, Sender<Events>] {
-  const globalServices = useContext(GlobalStateContext)
-  const [_, send] = useActor(globalServices.authMachine)
-  const defaultFn = () => null as T
-  const selection = useSelector(
-    globalServices.authMachine,
-    selector || defaultFn
-  )
+  selector: (
+    state: Parameters<Parameters<typeof AuthMachineContext.useSelector>[0]>[0]
+  ) => T = () => null as T
+): [T, ReturnType<typeof AuthMachineContext.useActor>[1]] {
+  // useActor api normally `[state, send] = useActor`
+  // we're only interested in stend because of the selector
+  const send = AuthMachineContext.useActor()[1]
+
+  const selection = AuthMachineContext.useSelector(selector)
   return [selection, send]
 }
 
