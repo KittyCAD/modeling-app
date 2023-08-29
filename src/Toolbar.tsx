32f79c98f84eff78fe76c4efabf23291cf01a6d6
Frank Johnson <frankjohnson1993@gmail.com>
2023-08-29 12:27:50 -0400

    Fix up light mode of basic bar

--- src/Toolbar.tsx
+++ src/Toolbar.tsx
@@ -35,14 +35,14 @@ export const Toolbar = () => {
   }))
 
   return (
-    <Popover className='relative'>
-      <div className="flex gap-4 items-center rounded-full border border-cool-100/50 bg-cool-120/50">
-        <span className="self-stretch flex items-center px-4 py-1 bg-cool-90/50 text-cool-30 rounded-l-full text-sm font-bold">
+    <Popover className="relative">
+      <div className="flex gap-4 items-center rounded-full border border-cool-20/30 dark:border-cool-100/50 bg-cool-10/50 dark:bg-cool-120/50">
+        <span className="self-stretch flex items-center px-4 py-1 bg-cool-20/50 dark:bg-cool-90/50 text-cool-100 dark:text-cool-30 rounded-l-full text-sm font-bold">
           {guiMode.mode === 'sketch' ? '2D' : '3D'}
         </span>
         <menu className="flex flex-1 gap-2 py-0.5">
           <ActionButton
-            Element='button'
+            Element="button"
             icon={{ icon: faPen, size: 'sm' }}
             onClick={() => {
               setGuiMode({
@@ -50,22 +50,20 @@ export const Toolbar = () => {
                 sketchMode: 'selectFace',
               })
             }}
-            className='py-0 px-0.5'
+            className="py-0 px-0.5 bg-chalkboard-10 dark:bg-chalkboard-100"
           >
             Start Sketch
           </ActionButton>
         </menu>
-        <Popover.Button
-        className="self-stretch m-0 flex items-center px-4 py-1 bg-cool-90/50 hover:bg-cool-90 dark:hover:bg-cool-90 text-cool-30 rounded-r-full border-none text-sm font-bold"
-        >
+        <Popover.Button className="self-stretch m-0 flex items-center px-4 py-1 bg-cool-20/50 dark:bg-cool-90/50 hover:bg-cool-20 dark:hover:bg-cool-90 text-cool-100 dark:text-cool-30 rounded-r-full border-none text-sm font-bold">
           <FontAwesomeIcon icon={faSearch} />
         </Popover.Button>
       </div>
-      <Popover.Overlay className="fixed inset-0 bg-chalkboard-110/30 dark:bg-chalkboard-110/50" />
-      <Popover.Panel className='absolute top-0 w-screen max-w-xl left-1/2 -translate-x-1/2 bg-chalkboard-10 dark:bg-chalkboard-100 p-5 rounded border border-chalkboard-20/30 dark:border-chalkboard-70/50'>
-          Open!
-          <Popover.Button>Close</Popover.Button>
-        </Popover.Panel>
+      <Popover.Overlay className="fixed inset-0 bg-chalkboard-110/20 dark:bg-chalkboard-110/50" />
+      <Popover.Panel className="absolute top-0 w-screen max-w-xl left-1/2 -translate-x-1/2 bg-chalkboard-10 dark:bg-chalkboard-100 p-5 rounded border border-chalkboard-20/30 dark:border-chalkboard-70/50">
+        Open!
+        <Popover.Button>Close</Popover.Button>
+      </Popover.Panel>
       {/* // <div>
     //   {guiMode.mode === 'default' && (
     //     <button
