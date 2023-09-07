cee74085493496cce3f5d26469f18d40c65f0304
Frank Noirot <frank@kittycad.io>
2023-09-07 15:12:35 -0400

    Fix formatting on generated type file

--- src/machines/settingsMachine.typegen.ts
+++ src/machines/settingsMachine.typegen.ts
@@ -1,35 +1,52 @@
+// This file was automatically generated. Edits will be overwritten
 
-  // This file was automatically generated. Edits will be overwritten
-
-  export interface Typegen0 {
-        '@@xstate/typegen': true;
-        internalEvents: {
-          "xstate.init": { type: "xstate.init" };
-        };
-        invokeSrcNameMap: {
-          
-        };
-        missingImplementations: {
-          actions: "toastSuccess";
-          delays: never;
-          guards: never;
-          services: never;
-        };
-        eventsCausingActions: {
-          "persistSettings": "Set Base Unit" | "Set Camera Controls" | "Set Default Directory" | "Set Default Project Name" | "Set Onboarding Status" | "Set Text Wrapping" | "Set Theme" | "Set Unit System" | "Toggle Debug Panel";
-"setThemeClass": "Set Base Unit" | "Set Camera Controls" | "Set Default Directory" | "Set Default Project Name" | "Set Onboarding Status" | "Set Text Wrapping" | "Set Theme" | "Set Unit System" | "Toggle Debug Panel" | "xstate.init";
-"toastSuccess": "Set Base Unit" | "Set Camera Controls" | "Set Default Directory" | "Set Default Project Name" | "Set Text Wrapping" | "Set Theme" | "Set Unit System" | "Toggle Debug Panel";
-        };
-        eventsCausingDelays: {
-          
-        };
-        eventsCausingGuards: {
-          
-        };
-        eventsCausingServices: {
-          
-        };
-        matchesStates: "idle";
-        tags: never;
-      }
-  
+export interface Typegen0 {
+  '@@xstate/typegen': true
+  internalEvents: {
+    'xstate.init': { type: 'xstate.init' }
+  }
+  invokeSrcNameMap: {}
+  missingImplementations: {
+    actions: 'toastSuccess'
+    delays: never
+    guards: never
+    services: never
+  }
+  eventsCausingActions: {
+    persistSettings:
+      | 'Set Base Unit'
+      | 'Set Camera Controls'
+      | 'Set Default Directory'
+      | 'Set Default Project Name'
+      | 'Set Onboarding Status'
+      | 'Set Text Wrapping'
+      | 'Set Theme'
+      | 'Set Unit System'
+      | 'Toggle Debug Panel'
+    setThemeClass:
+      | 'Set Base Unit'
+      | 'Set Camera Controls'
+      | 'Set Default Directory'
+      | 'Set Default Project Name'
+      | 'Set Onboarding Status'
+      | 'Set Text Wrapping'
+      | 'Set Theme'
+      | 'Set Unit System'
+      | 'Toggle Debug Panel'
+      | 'xstate.init'
+    toastSuccess:
+      | 'Set Base Unit'
+      | 'Set Camera Controls'
+      | 'Set Default Directory'
+      | 'Set Default Project Name'
+      | 'Set Text Wrapping'
+      | 'Set Theme'
+      | 'Set Unit System'
+      | 'Toggle Debug Panel'
+  }
+  eventsCausingDelays: {}
+  eventsCausingGuards: {}
+  eventsCausingServices: {}
+  matchesStates: 'idle'
+  tags: never
+}
