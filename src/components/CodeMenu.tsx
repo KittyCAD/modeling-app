c5f5e4b462ba0137c28a54c3dfa45b20b10dd762
Frank Noirot <frank@kittycad.io>
2023-09-08 14:11:10 -0400

    Style tweaks

--- src/components/CodeMenu.tsx
+++ src/components/CodeMenu.tsx
@@ -25,14 +25,14 @@ export const CodeMenu = ({ children }: PropsWithChildren) => {
           <ActionIcon
             icon={faListDots}
             bgClassName={
-              'bg-chalkboard-30 dark:bg-chalkboard-110 group-open:bg-chalkboard-80 group-open:dark:bg-chalkboard-90 rounded'
+              'bg-chalkboard-20 dark:bg-chalkboard-110 hover:bg-chalkboard-30 hover:dark:bg-chalkboard-90 ui-active:bg-chalkboard-80 ui-active:dark:bg-chalkboard-90  rounded'
             }
             iconClassName={
-              'text-chalkboard-90 dark:text-chalkboard-40 group-open:text-liquid-10'
+              'text-chalkboard-90 dark:text-chalkboard-40 hover:text-liquid-10'
             }
           />
         </Menu.Button>
-        <Menu.Items className="absolute right-0 left-auto w-64 flex flex-col gap-2 align-stretch px-0 py-1 bg-chalkboard-10 dark:bg-chalkboard-90 rounded shadow-lg border border-solid border-chalkboard-30/50 dark:border-chalkboard-80/50">
+        <Menu.Items className="absolute right-0 left-auto w-64 flex flex-col gap-1 divide-y divide-chalkboard-20 dark:divide-chalkboard-70 align-stretch px-0 py-1 bg-chalkboard-10 dark:bg-chalkboard-90 rounded shadow-lg border border-solid border-chalkboard-30/50 dark:border-chalkboard-80/50">
           <Menu.Item>
             <button onClick={() => formatCode()} className={styles.button}>
               <span>Format code</span>
