4ff652e8ea68ed1265e7b2f95f07e1f267477494
Frank Noirot <frank@kittycad.io>
2023-09-08 15:45:19 -0400

    Move convert to variable into code menu
    Signed off by Frank Noirot <frank@kittycad.io>

--- src/components/CodeMenu.tsx
+++ src/components/CodeMenu.tsx
@@ -4,11 +4,14 @@ import { faEllipsis } from '@fortawesome/free-solid-svg-icons'
 import { ActionIcon } from './ActionIcon'
 import { useStore } from 'useStore'
 import styles from './CodeMenu.module.css'
+import { useConvertToVariable } from 'hooks/useToolbarGuards'
 
 export const CodeMenu = ({ children }: PropsWithChildren) => {
   const { formatCode } = useStore((s) => ({
     formatCode: s.formatCode,
   }))
+  const { enable: convertToVarEnabled, handleClick: handleConvertToVarClick } =
+    useConvertToVariable()
 
   return (
     <Menu>
@@ -27,9 +30,7 @@ export const CodeMenu = ({ children }: PropsWithChildren) => {
             bgClassName={
               'bg-chalkboard-20 dark:bg-chalkboard-110 hover:bg-chalkboard-30 hover:dark:bg-chalkboard-90 ui-active:bg-chalkboard-80 ui-active:dark:bg-chalkboard-90  rounded'
             }
-            iconClassName={
-              'text-chalkboard-90 dark:text-chalkboard-40'
-            }
+            iconClassName={'text-chalkboard-90 dark:text-chalkboard-40'}
           />
         </Menu.Button>
         <Menu.Items className="absolute right-0 left-auto w-64 flex flex-col gap-1 divide-y divide-chalkboard-20 dark:divide-chalkboard-70 align-stretch px-0 py-1 bg-chalkboard-10 dark:bg-chalkboard-90 rounded shadow-lg border border-solid border-chalkboard-30/50 dark:border-chalkboard-80/50">
@@ -39,6 +40,16 @@ export const CodeMenu = ({ children }: PropsWithChildren) => {
               <small>Alt + Shift + F</small>
             </button>
           </Menu.Item>
+          {convertToVarEnabled && (
+            <Menu.Item>
+              <button
+                onClick={handleConvertToVarClick}
+                className={styles.button}
+              >
+                <span>Convert to Variable</span>
+              </button>
+            </Menu.Item>
+          )}
         </Menu.Items>
       </div>
     </Menu>
