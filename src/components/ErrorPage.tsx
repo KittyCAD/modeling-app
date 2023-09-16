ff52595789fc4cf75b11118cf011fb37aeb03b8e
Frank Noirot <frank@kittycad.io>
2023-09-15 20:36:02 -0400

    Improve error page to actually show error

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,8 +1,16 @@
+import { useRouteError } from 'react-router-dom'
+
 export const ErrorPage = () => {
+  let error = useRouteError()
+
+  console.error('error', error)
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">404</h1>
-      <p className="text-2xl font-bold">Page not found</p>
+      <section className="max-w-full xl:max-w-4xl mx-auto">
+        <h1 className="text-4xl mb-8 font-bold">An unexpected error occurred</h1>
+        <p>{String(error)}</p>
+      </section>
     </div>
   )
 }
