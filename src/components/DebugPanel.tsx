6ab84444667027c06216690e62824e695fe1c43a
Pierre Jacquier <pierrejacquier39@gmail.com>
2023-09-19 20:55:07 -0400

    Fmt

--- src/components/DebugPanel.tsx
+++ src/components/DebugPanel.tsx
@@ -30,7 +30,9 @@ export const DebugPanel = ({ className, ...props }: CollapsiblePanelProps) => {
   return (
     <CollapsiblePanel
       {...props}
-      className={'!absolute overflow-hidden !h-auto bottom-5 right-5 ' + className}
+      className={
+        '!absolute overflow-hidden !h-auto bottom-5 right-5 ' + className
+      }
       // header height, top-5, and bottom-5
       style={{ maxHeight: 'calc(100% - 3rem - 1.25rem - 1.25rem)' }}
     >
