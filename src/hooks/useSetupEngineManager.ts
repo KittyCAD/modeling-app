c1080cee86f10ecd0a7120360ef8188b307a04c2
Jess Frazelle <github@jessfraz.com>
2023-09-25 17:36:34 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/hooks/useSetupEngineManager.ts
+++ src/hooks/useSetupEngineManager.ts
@@ -2,18 +2,25 @@ import { useLayoutEffect } from 'react'
 import { _executor } from '../lang/executor'
 import { useStore } from '../useStore'
 import { engineCommandManager } from '../lang/std/engineConnection'
+import { debounce } from 'debounce'
 
 export function useSetupEngineManager(
   streamRef: React.RefObject<HTMLDivElement>,
   token?: string
 ) {
-  const { setMediaStream, setIsStreamReady, setStreamDimensions, executeCode } =
-    useStore((s) => ({
-      setMediaStream: s.setMediaStream,
-      setIsStreamReady: s.setIsStreamReady,
-      setStreamDimensions: s.setStreamDimensions,
-      executeCode: s.executeCode,
-    }))
+  const {
+    setMediaStream,
+    setIsStreamReady,
+    setStreamDimensions,
+    executeCode,
+    streamDimensions,
+  } = useStore((s) => ({
+    setMediaStream: s.setMediaStream,
+    setIsStreamReady: s.setIsStreamReady,
+    setStreamDimensions: s.setStreamDimensions,
+    executeCode: s.executeCode,
+    streamDimensions: s.streamDimensions,
+  }))
 
   const streamWidth = streamRef?.current?.offsetWidth
   const streamHeight = streamRef?.current?.offsetHeight
@@ -26,20 +33,31 @@ export function useSetupEngineManager(
   useLayoutEffect(() => {
     // Load the engine command manager once with the initial width and height,
     // then we do not want to reload it.
-    engineCommandManager.start({
-      setMediaStream,
-      setIsStreamReady,
-      width: quadWidth,
-      height: quadHeight,
-      token,
-    })
-    engineCommandManager.waitForReady.then(() => {
-      executeCode()
-    })
-    setStreamDimensions({
-      streamWidth: quadWidth,
-      streamHeight: quadHeight,
-    })
+    if (
+      streamDimensions.streamWidth !== quadWidth ||
+      streamDimensions.streamHeight !== quadHeight
+    ) {
+      debounce(
+        () => {
+          engineCommandManager.start({
+            setMediaStream,
+            setIsStreamReady,
+            width: quadWidth,
+            height: quadHeight,
+            token,
+          })
+          engineCommandManager.waitForReady.then(() => {
+            executeCode()
+          })
+          setStreamDimensions({
+            streamWidth: quadWidth,
+            streamHeight: quadHeight,
+          })
+        },
+        500,
+        true
+      )()
+    }
     return () => {}
   }, [quadWidth, quadHeight])
 }
