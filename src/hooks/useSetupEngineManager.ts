a363c46764d2d7bbea145aa62fd4847df1e34591
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2023-09-26 11:43:21 +1000

    handle resize effect

--- src/hooks/useSetupEngineManager.ts
+++ src/hooks/useSetupEngineManager.ts
@@ -1,8 +1,8 @@
-import { useLayoutEffect } from 'react'
+import { useLayoutEffect, useEffect, useRef } from 'react'
 import { _executor } from '../lang/executor'
 import { useStore } from '../useStore'
 import { engineCommandManager } from '../lang/std/engineConnection'
-import { debounce } from 'debounce'
+import { deferExecution } from 'lib/utils'
 
 export function useSetupEngineManager(
   streamRef: React.RefObject<HTMLDivElement>,
@@ -25,39 +25,66 @@ export function useSetupEngineManager(
   const streamWidth = streamRef?.current?.offsetWidth
   const streamHeight = streamRef?.current?.offsetHeight
 
-  const width = streamWidth ? streamWidth : 0
-  const quadWidth = Math.round(width / 4) * 4
-  const height = streamHeight ? streamHeight : 0
-  const quadHeight = Math.round(height / 4) * 4
+  const hasSetNonZeroDimensions = useRef<boolean>(false)
 
   useLayoutEffect(() => {
     // Load the engine command manager once with the initial width and height,
     // then we do not want to reload it.
-    if (
-      streamDimensions.streamWidth !== quadWidth ||
-      streamDimensions.streamHeight !== quadHeight
-    ) {
-      debounce(
-        () => {
-          engineCommandManager.start({
-            setMediaStream,
-            setIsStreamReady,
-            width: quadWidth,
-            height: quadHeight,
-            token,
-          })
-          engineCommandManager.waitForReady.then(() => {
-            executeCode()
-          })
-          setStreamDimensions({
-            streamWidth: quadWidth,
-            streamHeight: quadHeight,
-          })
-        },
-        500,
-        true
-      )()
+    const { width: quadWidth, height: quadHeight } = getDimensions(
+      streamWidth,
+      streamHeight
+    )
+    if (!hasSetNonZeroDimensions.current && quadHeight && quadWidth) {
+      engineCommandManager.start({
+        setMediaStream,
+        setIsStreamReady,
+        width: quadWidth,
+        height: quadHeight,
+        token,
+      })
+      engineCommandManager.waitForReady.then(() => {
+        executeCode()
+      })
+      setStreamDimensions({
+        streamWidth: quadWidth,
+        streamHeight: quadHeight,
+      })
+      hasSetNonZeroDimensions.current = true
     }
-    return () => {}
-  }, [quadWidth, quadHeight])
+  }, [streamRef?.current?.offsetWidth, streamRef?.current?.offsetHeight])
+
+  useEffect(() => {
+    const handleResize = deferExecution(() => {
+      const { width, height } = getDimensions(
+        streamRef?.current?.offsetWidth,
+        streamRef?.current?.offsetHeight
+      )
+      if (
+        streamDimensions.streamWidth !== width ||
+        streamDimensions.streamHeight !== height
+      ) {
+        engineCommandManager.handleResize({
+          streamWidth: width,
+          streamHeight: height,
+        })
+        setStreamDimensions({
+          streamWidth: width,
+          streamHeight: height,
+        })
+      }
+    }, 500)
+
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+}
+
+function getDimensions(streamWidth?: number, streamHeight?: number) {
+  const width = streamWidth ? streamWidth : 0
+  const quadWidth = Math.round(width / 4) * 4
+  const height = streamHeight ? streamHeight : 0
+  const quadHeight = Math.round(height / 4) * 4
+  return { width: quadWidth, height: quadHeight }
 }
