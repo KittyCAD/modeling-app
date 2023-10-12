1a331c41097813c18cc617e6d359466a103f0d32
Jess Frazelle <github@jessfraz.com>
2023-10-12 16:04:03 -0700

    updates;
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/wasm-lib/kcl/src/engine/conn.rs
+++ src/wasm-lib/kcl/src/engine/conn.rs
@@ -140,7 +140,6 @@ impl EngineManager for EngineConnection {
         source_range: crate::executor::SourceRange,
         cmd: kittycad::types::ModelingCmd,
     ) -> Result<OkWebSocketResponseData, KclError> {
-        println!("{:?}", cmd);
         let (tx, rx) = oneshot::channel();
 
         // Send the request to the engine, via the actor.
