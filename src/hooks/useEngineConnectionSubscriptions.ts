63fc9549ff05ee2567f088fe64ae6c3421554737
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2023-10-16 16:56:02 +1100

    extract selection event into selections.ts

--- src/hooks/useEngineConnectionSubscriptions.ts
+++ src/hooks/useEngineConnectionSubscriptions.ts
@@ -4,6 +4,7 @@ import { engineCommandManager } from '../lang/std/engineConnection'
 import { useModelingContext } from './useModelingContext'
 import { v4 as uuidv4 } from 'uuid'
 import { SourceRange } from 'lang/wasm'
+import { getEventForSelectWithPoint } from 'lib/selections'
 
 export function useEngineConnectionSubscriptions() {
   const { setHighlightRange, highlightRange } = useStore((s) => ({
@@ -37,63 +38,11 @@ export function useEngineConnectionSubscriptions() {
     })
     const unSubClick = engineCommandManager.subscribeTo({
       event: 'select_with_point',
-      callback: ({ data }) => {
-        if (!data?.entity_id) {
-          send({
-            type: 'Set selection',
-            data: { selectionType: 'singleCodeCursor' },
-          })
-          return
-        }
-        const sourceRange =
-          engineCommandManager.artifactMap[data.entity_id]?.range
-        if (engineCommandManager.artifactMap[data.entity_id]) {
-          send({
-            type: 'Set selection',
-            data: {
-              selectionType: 'singleCodeCursor',
-              selection: { range: sourceRange, type: 'default' },
-            },
-          })
-        } else {
-          // selected a vertex
-          engineCommandManager
-            .sendSceneCommand({
-              type: 'modeling_cmd_req',
-              cmd_id: uuidv4(),
-              cmd: {
-                type: 'path_get_curve_uuids_for_vertices',
-                vertex_ids: [data.entity_id],
-                path_id: context.sketchEnginePathId,
-              },
-            })
-            .then((res) => {
-              const curveIds = res?.data?.data?.curve_ids
-              const ranges: RangeAndId[] = curveIds
-                .map(
-                  (id: string): RangeAndId => ({
-                    id,
-                    range: engineCommandManager.artifactMap[id].range,
-                  })
-                )
-                .sort((a: RangeAndId, b: RangeAndId) => a.range[0] - b.range[0])
-              // default to the head of the curve selected
-              const _sourceRange = ranges?.[0].range
-              const artifact = engineCommandManager.artifactMap[ranges?.[0]?.id]
-              if (artifact.type === 'result') {
-                artifact.headVertexId = data.entity_id
-              }
-              send({
-                type: 'Set selection',
-                data: {
-                  selectionType: 'singleCodeCursor',
-                  // line-end is used to indicate that headVertexId should be sent to the engine as "selected"
-                  // not the whole curve
-                  selection: { range: _sourceRange, type: 'line-end' },
-                },
-              })
-            })
-        }
+      callback: async (engineEvent) => {
+        const event = await getEventForSelectWithPoint(engineEvent, {
+          sketchEnginePathId: context.sketchEnginePathId,
+        })
+        send(event)
       },
     })
     return () => {
--- src/lib/selections.ts
+++ src/lib/selections.ts
@@ -1,4 +1,8 @@
+import { Models } from '@kittycad/lib'
+import { engineCommandManager } from 'lang/std/engineConnection'
 import { SourceRange } from 'lang/wasm'
+import { ModelingEvent } from 'machines/modelingMachine'
+import { v4 as uuidv4 } from 'uuid'
 
 export type Axis = 'y-axis' | 'x-axis' | 'z-axis'
 
@@ -22,4 +26,70 @@ export type Selections = {
 
 export interface SelectionRangeTypeMap {
   [key: number]: Selection['type']
+}
+
+interface RangeAndId {
+  id: string
+  range: SourceRange
+}
+
+export async function getEventForSelectWithPoint(
+  {
+    data,
+  }: Extract<
+    Models['OkModelingCmdResponse_type'],
+    { type: 'select_with_point' }
+  >,
+  { sketchEnginePathId }: { sketchEnginePathId: string }
+): Promise<ModelingEvent> {
+  if (!data?.entity_id) {
+    return {
+      type: 'Set selection',
+      data: { selectionType: 'singleCodeCursor' },
+    }
+  }
+  const sourceRange = engineCommandManager.artifactMap[data.entity_id]?.range
+  if (engineCommandManager.artifactMap[data.entity_id]) {
+    return {
+      type: 'Set selection',
+      data: {
+        selectionType: 'singleCodeCursor',
+        selection: { range: sourceRange, type: 'default' },
+      },
+    }
+  }
+  // selected a vertex
+  const res = await engineCommandManager.sendSceneCommand({
+    type: 'modeling_cmd_req',
+    cmd_id: uuidv4(),
+    cmd: {
+      type: 'path_get_curve_uuids_for_vertices',
+      vertex_ids: [data.entity_id],
+      path_id: sketchEnginePathId,
+    },
+  })
+  const curveIds = res?.data?.data?.curve_ids
+  const ranges: RangeAndId[] = curveIds
+    .map(
+      (id: string): RangeAndId => ({
+        id,
+        range: engineCommandManager.artifactMap[id].range,
+      })
+    )
+    .sort((a: RangeAndId, b: RangeAndId) => a.range[0] - b.range[0])
+  // default to the head of the curve selected
+  const _sourceRange = ranges?.[0].range
+  const artifact = engineCommandManager.artifactMap[ranges?.[0]?.id]
+  if (artifact.type === 'result') {
+    artifact.headVertexId = data.entity_id
+  }
+  return {
+    type: 'Set selection',
+    data: {
+      selectionType: 'singleCodeCursor',
+      // line-end is used to indicate that headVertexId should be sent to the engine as "selected"
+      // not the whole curve
+      selection: { range: _sourceRange, type: 'line-end' },
+    },
+  }
 }
