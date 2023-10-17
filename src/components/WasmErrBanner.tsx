4f0cb08f245f140222ce6aaafe7dc0ca3139213e
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2023-10-17 11:49:20 +1100

    fmt

--- src/components/WasmErrBanner.tsx
+++ src/components/WasmErrBanner.tsx
@@ -36,22 +36,26 @@ export function WasmErrBanner() {
           />
         </div>
         <p>
-        <a
+          <a
             href="https://webassembly.org/"
             rel="noopener noreferrer"
             target="_blank"
             className="text-warn-80 dark:text-warn-80 dark:hover:text-warn-70 underline"
           >
             WASM or web assembly
-          </a> is core part of how our app works. It might because you OS is not up-to-date.
-          If you're able to update your OS to a later version try and see if that fixes the issue. If not create an issue on <a
+          </a>{' '}
+          is core part of how our app works. It might because you OS is not
+          up-to-date. If you're able to update your OS to a later version, try
+          that. If not create an issue on{' '}
+          <a
             href="https://github.com/KittyCAD/modeling-app"
             rel="noopener noreferrer"
             target="_blank"
             className="text-warn-80 dark:text-warn-80 dark:hover:text-warn-70 underline"
           >
             our Github
-          </a>.
+          </a>
+          .
         </p>
       </Dialog.Panel>
     </Dialog>
