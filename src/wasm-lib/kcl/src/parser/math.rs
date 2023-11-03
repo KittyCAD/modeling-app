7c22bac638225ce57a0333ca5b931eddd3f8ba2c
Adam Chalmers <adam.chalmers@kittycad.io>
2023-11-03 13:30:19 -0500

    Remove unwraps from binary expression algorithm (#992)

--- src/wasm-lib/kcl/src/parser/math.rs
+++ src/wasm-lib/kcl/src/parser/math.rs
@@ -1,20 +1,33 @@
-use crate::ast::types::{BinaryExpression, BinaryOperator, BinaryPart};
+use crate::{
+    ast::types::{BinaryExpression, BinaryOperator, BinaryPart},
+    errors::{KclError, KclErrorDetails},
+    executor::SourceRange,
+};
 
 /// Parses a list of tokens (in infix order, i.e. as the user typed them)
 /// into a binary expression tree.
-pub fn parse(infix_tokens: Vec<BinaryExpressionToken>) -> BinaryExpression {
+pub fn parse(infix_tokens: Vec<BinaryExpressionToken>) -> Result<BinaryExpression, KclError> {
     let rpn = postfix(infix_tokens);
     evaluate(rpn)
 }
 
 /// Parses a list of tokens (in postfix order) into a binary expression tree.
-fn evaluate(rpn: Vec<BinaryExpressionToken>) -> BinaryExpression {
-    let mut operand_stack = Vec::new();
+fn evaluate(rpn: Vec<BinaryExpressionToken>) -> Result<BinaryExpression, KclError> {
+    let source_ranges = source_range(&rpn);
+    let mut operand_stack: Vec<BinaryPart> = Vec::new();
+    let e = KclError::Internal(KclErrorDetails {
+        source_ranges,
+        message: "error parsing binary math expressions".to_owned(),
+    });
     for item in rpn {
         let expr = match item {
             BinaryExpressionToken::Operator(operator) => {
-                let right: BinaryPart = operand_stack.pop().unwrap();
-                let left = operand_stack.pop().unwrap();
+                let Some(right) = operand_stack.pop() else {
+                    return Err(e);
+                };
+                let Some(left) = operand_stack.pop() else {
+                    return Err(e);
+                };
                 BinaryPart::BinaryExpression(Box::new(BinaryExpression {
                     start: left.start(),
                     end: right.end(),
@@ -27,10 +40,26 @@ fn evaluate(rpn: Vec<BinaryExpressionToken>) -> BinaryExpression {
         };
         operand_stack.push(expr)
     }
-    if let BinaryPart::BinaryExpression(expr) = operand_stack.pop().unwrap() {
-        *expr
+    if let Some(BinaryPart::BinaryExpression(expr)) = operand_stack.pop() {
+        Ok(*expr)
     } else {
-        panic!("Last expression was not a binary expression")
+        // If this branch is used, the evaluation algorithm has a bug and must be fixed.
+        // This is a programmer error, not a user error.
+        Err(e)
+    }
+}
+
+fn source_range(tokens: &[BinaryExpressionToken]) -> Vec<SourceRange> {
+    let sources: Vec<_> = tokens
+        .iter()
+        .filter_map(|op| match op {
+            BinaryExpressionToken::Operator(_) => None,
+            BinaryExpressionToken::Operand(o) => Some((o.start(), o.end())),
+        })
+        .collect();
+    match (sources.first(), sources.last()) {
+        (Some((start, _)), Some((_, end))) => vec![SourceRange([*start, *end])],
+        _ => Vec::new(),
     }
 }
 
@@ -46,15 +75,16 @@ fn postfix(infix: Vec<BinaryExpressionToken>) -> Vec<BinaryExpressionToken> {
                 //     there is an operator o2 at the top of the operator stack which is not a left parenthesis,
                 //     and (o2 has greater precedence than o1 or (o1 and o2 have the same precedence and o1 is left-associative))
                 // )
-                while operator_stack
-                    .last()
-                    .map(|o2| {
-                        (o2.precedence() > o1.precedence())
-                            || o1.precedence() == o2.precedence() && o1.associativity().is_left()
-                    })
-                    .unwrap_or(false)
-                {
-                    output.push(BinaryExpressionToken::Operator(operator_stack.pop().unwrap()));
+                // pop o2 from the operator stack into the output queue
+                while let Some(o2) = operator_stack.pop() {
+                    if (o2.precedence() > o1.precedence())
+                        || o1.precedence() == o2.precedence() && o1.associativity().is_left()
+                    {
+                        output.push(BinaryExpressionToken::Operator(o2));
+                    } else {
+                        operator_stack.push(o2);
+                        break;
+                    }
                 }
                 operator_stack.push(o1);
             }
@@ -127,8 +157,7 @@ mod tests {
         ];
         for infix_input in tests {
             let rpn = postfix(infix_input);
-            let tree = evaluate(rpn);
-            dbg!(tree);
+            let _tree = evaluate(rpn).unwrap();
         }
     }
 }
