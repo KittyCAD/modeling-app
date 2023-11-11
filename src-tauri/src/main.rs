5f605d6d1e9af5fef23d4bc030cf530197d7d609
Pierre Jacquier <pierrejacquier39@gmail.com>
2023-11-11 16:30:29 -0500

    rust int

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::io::Read;
 use std::fs;
+use std::io::Read;
 
 use anyhow::Result;
 use oauth2::TokenResponse;
