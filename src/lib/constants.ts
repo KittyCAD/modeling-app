b030e8ff3f6ec9cb0d881bb016fc34fbaafb2385
Frank Noirot <frank@kittycad.io>
2024-02-06 23:20:36 -0500

    Replace Vite define with Vite plugin

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = 'Modeling App'
-// This uses a Vite define, set in vite.config.ts
+// This uses a Vite plugin, set in vite.config.ts
 // to inject the version from package.json
-export const APP_VERSION = __APP_VERSION__
+export const APP_VERSION = import.meta.env.PACKAGE_VERSION || 'unknown'
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import eslint from 'vite-plugin-eslint'
 import dns from 'dns'
-import { defineConfig, configDefaults } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config'
+import version from 'vite-plugin-package-version'
 
 // Only needed because we run Node < 17 
 // and we want to open `localhost` not `127.0.0.1` on server start
@@ -30,11 +31,8 @@ const config = defineConfig({
     react(),
     viteTsconfigPaths(),
     eslint(),
-  ],
-  define: {
-    // Allows us to use `__APP_VERSION__` in our code
-    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
-  },
+    version(),
+  ]
 })
 
 export default config
