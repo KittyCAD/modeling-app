459a49029cac9b68ce4211d2b8386f86ca3df51c
Frank Noirot <frank@kittycad.io>
2024-02-16 08:24:05 -0500

    Switch permissions to APPCONFIG from APPDATA + revert test changes

--- e2e/tauri/specs/auth.e2e.ts
+++ e2e/tauri/specs/auth.e2e.ts
@@ -65,10 +65,8 @@ describe('ZMA (Tauri, Linux)', () => {
     const settingsButton = await $('[data-testid="settings-button"]')
     await click(settingsButton)
 
-    // The default directory is now "" until the initial project directory gets
-    // initialized. @franknoirot is not sure how to await until that happens.
-    // const defaultDirInput = await $('[data-testid="default-directory-input"]')
-    // expect(await defaultDirInput.getValue()).toEqual(defaultDir)
+    const defaultDirInput = await $('[data-testid="default-directory-input"]')
+    expect(await defaultDirInput.getValue()).toEqual(defaultDir)
 
     const nameInput = await $('[data-testid="name-input"]')
     expect(await nameInput.getValue()).toEqual('project-$nnn')
