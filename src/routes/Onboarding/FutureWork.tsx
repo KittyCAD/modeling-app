cad64fb7802b5b5d5064ffc4857b2624d05b32d8
Frank Noirot <frank@zoo.dev>
2024-03-08 17:59:14 -0500

    Update onboarding based on team feedback (#1665)
    * Make onboarding links underlined and a different color
    
    * Override underlined links that shouldn't be
    
    * Update "KittyCAD" to "Zoo" camera controls, copy edits
    
    * Copy edits through CodeEditor step, add counter to onboarding
    
    * Notes through ParametricModeling step
    
    * Notes through UserMenu step
    
    * Add three-dots icon, separate Zoo logo link from project button
    
    * Feedback through end of onboarding
    
    * A snapshot a day keeps the bugs away! 📷🐛 (OS: ubuntu)
    
    * Fix ESLint warnings
    
    * Revert cameraControls to 'KittyCAD', ensure we get type errors if that isn't correct in the future
    
    * fmt
    
    * Missed a reverted "Zoo" cameraControls item
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/routes/Onboarding/FutureWork.tsx
+++ src/routes/Onboarding/FutureWork.tsx
@@ -4,6 +4,7 @@ import { bracket } from 'lib/exampleKcl'
 import { kclManager } from 'lang/KclSingleton'
 import { useModelingContext } from 'hooks/useModelingContext'
 import { APP_NAME } from 'lib/constants'
+import { onboardingPaths } from './paths'
 
 export default function FutureWork() {
   const { send } = useModelingContext()
@@ -28,16 +29,39 @@ export default function FutureWork() {
         <p className="my-4">
           We have curves, cuts, and many more CAD features coming soon. We want
           your feedback on this user interface, and we want to know what
-          features you want to see next. Please message us in the Discord server
-          and open issues on GitHub.
+          features you want to see next. Please message us in{' '}
+          <a
+            href="https://discord.gg/JQEpHR7Nt2"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            our Discord server
+          </a>
+          and{' '}
+          <a
+            href="https://github.com/KittyCAD/modeling-app/issues/new/choose"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            open issues on GitHub
+          </a>
+          .
         </p>
         <p className="my-4">
-          If you make anything with the app we'd love to see it! Thank you for
-          taking time to try out {APP_NAME}, and build the future of hardware
-          design with us.
+          If you make anything with the app we'd love to see it, feel free to{' '}
+          <a
+            href="https://twitter.com/zoodotdev"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            tag us on X
+          </a>
+          ! Thank you for taking time to try out {APP_NAME}, and build the
+          future of hardware design with us.
         </p>
         <p className="my-4">💚 The Zoo Team</p>
         <OnboardingButtons
+          currentSlug={onboardingPaths.FUTURE_WORK}
           className="mt-6"
           dismiss={dismiss}
           next={dismiss}
