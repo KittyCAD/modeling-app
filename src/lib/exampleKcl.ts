ee1606bdf252cee003811922652e0655eb766db6
jgomez720 <114548659+jgomez720@users.noreply.github.com>
2024-03-15 22:18:51 -0700

    adding comment to kcl

--- src/lib/exampleKcl.ts
+++ src/lib/exampleKcl.ts
@@ -8,7 +8,8 @@ const distance = 12 // inches
 const M = 12 * 300 / 2 // Moment experienced at fixed end of bracket
 const FOS = 2 // Factor of safety of 2
 const shelfMountL = 8 // The length of the bracket holding up the shelf is 6 inches
-const wallMountL = 8 // the length of the bracket 
+const wallMountL = 8 // the length of the bracket
+
 
 // Calculate the thickness off the allowable bending stress and factor of safety
 const thickness = sqrt(6 * M * FOS / (width * sigmaAllow))
@@ -16,21 +17,22 @@ const thickness = sqrt(6 * M * FOS / (width * sigmaAllow))
 // 0.25 inch fillet radius
 const filletR = 0.25
 
+// Sketch the bracket and extrude with fillets
 const bracket = startSketchOn('XY')
   |> startProfileAt([0, 0], %)
   |> line([0, wallMountL], %, 'outerEdge')
   |> line([-shelfMountL, 0], %)
   |> line([0, -thickness], %)
-  |> line([shelfMountL-thickness, 0], %, 'innerEdge')
-  |> line([0, -wallMountL+thickness], %)
+  |> line([shelfMountL - thickness, 0], %, 'innerEdge')
+  |> line([0, -wallMountL + thickness], %)
   |> close(%)
   |> extrude(width, %)
   |> fillet({
-    radius: filletR,
-    tags: [getNextAdjacentEdge('innerEdge', %)],
-  }, %)
+       radius: filletR,
+       tags: [getNextAdjacentEdge('innerEdge', %)]
+     }, %)
   |> fillet({
-    radius: filletR + thickness,
-    tags: [getNextAdjacentEdge('outerEdge', %)],
-  }, %)
+       radius: filletR + thickness,
+       tags: [getNextAdjacentEdge('outerEdge', %)]
+     }, %)
 `
