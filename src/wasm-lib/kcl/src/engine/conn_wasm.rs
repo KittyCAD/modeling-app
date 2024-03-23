bd70d1b06049712bf1f5be249df350f52f6164f7
Jess Frazelle <github@jessfraz.com>
2024-03-22 19:58:02 -0700

    port to wasm
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/wasm-lib/kcl/src/engine/conn_wasm.rs
+++ src/wasm-lib/kcl/src/engine/conn_wasm.rs
@@ -1,12 +1,15 @@
 //! Functions for setting up our WebSocket and WebRTC connections for communications with the
 //! engine.
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use anyhow::Result;
-use kittycad::types::WebSocketRequest;
+use kittycad::types::{OkWebSocketResponseData, WebSocketRequest};
 use wasm_bindgen::prelude::*;
 
-use crate::errors::{KclError, KclErrorDetails};
+use crate::{
+    engine::is_cmd_with_return_values,
+    errors::{KclError, KclErrorDetails},
+};
 
 #[wasm_bindgen(module = "/../../lang/std/engineConnection.ts")]
 extern "C" {
@@ -25,6 +28,7 @@ extern "C" {
 #[derive(Debug, Clone)]
 pub struct EngineConnection {
     manager: Arc<EngineCommandManager>,
+    batch: Arc<Mutex<Vec<WebSocketRequest>>>,
 }
 
 // Safety: WebAssembly will only ever run in a single-threaded context.
@@ -35,6 +39,7 @@ impl EngineConnection {
     pub async fn new(manager: EngineCommandManager) -> Result<EngineConnection, JsValue> {
         Ok(EngineConnection {
             manager: Arc::new(manager),
+            batch: Arc::new(Mutex::new(Vec::new())),
         })
     }
 }
@@ -43,18 +48,76 @@ impl EngineConnection {
 impl crate::engine::EngineManager for EngineConnection {
     async fn send_modeling_cmd(
         &self,
+        flush_batch: bool,
         id: uuid::Uuid,
         source_range: crate::executor::SourceRange,
         cmd: kittycad::types::ModelingCmd,
     ) -> Result<kittycad::types::OkWebSocketResponseData, KclError> {
+        let req = WebSocketRequest::ModelingCmdReq {
+            cmd: cmd.clone(),
+            cmd_id: id,
+        };
+
+        if !flush_batch {
+            self.batch.lock().unwrap().push(req);
+        }
+
+        // If the batch only has this one command that expects a return value,
+        // fire it right away, or if we want to flush batch queue.
+        let is_sending = (is_cmd_with_return_values(&cmd) && self.batch.lock().unwrap().len() == 1)
+            || flush_batch
+            || is_cmd_with_return_values(&cmd);
+
+        // Return a fake modeling_request empty response.
+        if !is_sending {
+            return Ok(OkWebSocketResponseData::Modeling {
+                modeling_response: kittycad::types::OkModelingCmdResponse::Empty {},
+            });
+        }
+
+        let batched_requests = WebSocketRequest::ModelingCmdBatchReq {
+            requests: self.batch.lock().unwrap().iter().fold(vec![], |mut acc, val| {
+                let WebSocketRequest::ModelingCmdReq { cmd, cmd_id } = val else {
+                    return acc;
+                };
+                acc.push(kittycad::types::ModelingCmdReq {
+                    cmd: cmd.clone(),
+                    cmd_id: *cmd_id,
+                });
+                acc
+            }),
+            batch_id: uuid::Uuid::new_v4(),
+        };
+
+        let final_req = if self.batch.lock().unwrap().len() == 1 {
+            // We can unwrap here because we know the batch has only one element.
+            self.batch.lock().unwrap().first().unwrap().clone()
+        } else {
+            batched_requests
+        };
+
+        // Throw away the old batch queue.
+        self.batch.lock().unwrap().clear();
+
+        // We pop off the responses to cleanup our mappings.
+        let id_final = match final_req {
+            WebSocketRequest::ModelingCmdBatchReq { requests: _, batch_id } => batch_id,
+            WebSocketRequest::ModelingCmdReq { cmd: _, cmd_id } => cmd_id,
+            _ => {
+                return Err(KclError::Engine(KclErrorDetails {
+                    message: format!("The final request is not a modeling command: {:?}", final_req),
+                    source_ranges: vec![source_range],
+                }));
+            }
+        };
+
         let source_range_str = serde_json::to_string(&source_range).map_err(|e| {
             KclError::Engine(KclErrorDetails {
                 message: format!("Failed to serialize source range: {:?}", e),
                 source_ranges: vec![source_range],
             })
         })?;
-        let ws_msg = WebSocketRequest::ModelingCmdReq { cmd, cmd_id: id };
-        let cmd_str = serde_json::to_string(&ws_msg).map_err(|e| {
+        let cmd_str = serde_json::to_string(&final_req).map_err(|e| {
             KclError::Engine(KclErrorDetails {
                 message: format!("Failed to serialize modeling command: {:?}", e),
                 source_ranges: vec![source_range],
@@ -63,7 +126,7 @@ impl crate::engine::EngineManager for EngineConnection {
 
         let promise = self
             .manager
-            .send_modeling_cmd_from_wasm(id.to_string(), source_range_str, cmd_str)
+            .send_modeling_cmd_from_wasm(id_final.to_string(), source_range_str, cmd_str)
             .map_err(|e| {
                 KclError::Engine(KclErrorDetails {
                     message: e.to_string().into(),
