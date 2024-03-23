9bf5ece43939bdb919348a6f875a513a40580dee
Jess Frazelle <github@jessfraz.com>
2024-03-22 18:38:45 -0700

    push up mods
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/wasm-lib/tests/modify/main.rs
+++ src/wasm-lib/tests/modify/main.rs
@@ -25,7 +25,11 @@ async fn setup(code: &str, name: &str) -> Result<(ExecutorContext, Program, uuid
     let token = std::env::var("KITTYCAD_API_TOKEN").expect("KITTYCAD_API_TOKEN not set");
 
     // Create the client.
-    let client = kittycad::Client::new_from_reqwest(token, http_client, ws_client);
+    let mut client = kittycad::Client::new_from_reqwest(token, http_client, ws_client);
+    // Set a local engine address if it's set.
+    if let Ok(addr) = std::env::var("LOCAL_ENGINE_ADDR") {
+        client.set_base_url(addr);
+    }
 
     let ws = client
         .modeling()
