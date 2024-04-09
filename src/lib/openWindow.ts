b2ec5af02fb23ea032828a5169ef8402e25b7e57
Jess Frazelle <github@jessfraz.com>
2024-04-09 16:28:39 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/lib/openWindow.ts
+++ src/lib/openWindow.ts
@@ -1,5 +1,5 @@
 import { isTauri } from 'lib/isTauri'
-import { open as tauriOpen } from '@tauri-apps/api/shell'
+import { open as tauriOpen } from '@tauri-apps/plugin-shell'
 
 // Open a new browser window tauri style or browser style.
 export default async function openWindow(url: string) {
