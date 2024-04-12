6887ab58e4f50bf48a42ac4fbf23f02212539497
49lf <ircsurfer33@gmail.com>
2024-04-12 16:38:47 -0400

    Fix build errors

--- src/lang/std/engineConnection.ts
+++ src/lang/std/engineConnection.ts
@@ -1205,7 +1205,7 @@ export class EngineCommandManager extends EventTarget {
 
         // When the EngineConnection starts a connection, we want to register
         // callbacks into the WebSocket/PeerConnection.
-        engineConnection.websocket?.addEventListener('message', (event) => {
+        engineConnection.websocket?.addEventListener('message', ((event) => {
           if (event.data instanceof ArrayBuffer) {
             // If the data is an ArrayBuffer, it's  the result of an export command,
             // because in all other cases we send JSON strings. But in the case of
@@ -1236,9 +1236,7 @@ export class EngineCommandManager extends EventTarget {
               this.handleFailedModelingCommand(message.request_id, message)
             }
           }
-        )
-      }) as EventListener
-    )
+      }) as EventListener)
 
     this.engineConnection.addEventListener(EngineConnectionEvents.NewTrack, (({
       detail: { mediaStream },
@@ -1255,7 +1253,9 @@ export class EngineCommandManager extends EventTarget {
     }) as EventListener)
 
     this.engineConnection?.connect()
+  }))
   }
+
   handleResize({
     streamWidth,
     streamHeight,
