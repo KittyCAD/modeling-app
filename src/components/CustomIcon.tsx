64d402eec8a53e690e1e615328ff3f00795846e3
Frank Noirot <frank@kittycad.io>
2024-04-16 11:47:17 -0400

    Make fallback menu button look better

--- src/components/CustomIcon.tsx
+++ src/components/CustomIcon.tsx
@@ -219,6 +219,16 @@ const CustomIconMap = {
       />
     </svg>
   ),
+  menu: (
+    <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M15 6H5V5H15V6ZM15 10.5H5V9.5H15V10.5ZM5 15H15V14H5V15Z"
+        fill="currentColor"
+      />
+    </svg>
+  ),
   move: (
     <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
