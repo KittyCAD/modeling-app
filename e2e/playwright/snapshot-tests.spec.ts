03a6becd45fffd1020026300a04941116050a7bb
Jess Frazelle <github@jessfraz.com>
2024-04-17 18:47:48 -0700

    combine init into one
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- e2e/playwright/snapshot-tests.spec.ts
+++ e2e/playwright/snapshot-tests.spec.ts
@@ -340,7 +340,17 @@ test('extrude on each default plane should be stable', async ({
   page,
   context,
 }) => {
-  await context.addInitScript(async () => {
+  const u = getUtils(page)
+  const makeCode = (plane = 'XY') => `const part001 = startSketchOn('${plane}')
+  |> startProfileAt([7.00, 4.40], %)
+  |> line([6.60, -0.20], %)
+  |> line([2.80, 5.00], %)
+  |> line([-5.60, 4.40], %)
+  |> line([-5.40, -3.80], %)
+  |> close(%)
+  |> extrude(10.00, %)
+`
+  await context.addInitScript(async (code) => {
     localStorage.setItem(
       'SETTINGS_PERSIST_KEY',
       JSON.stringify({
@@ -355,18 +365,6 @@ test('extrude on each default plane should be stable', async ({
         unitSystem: 'imperial',
       })
     )
-  })
-  const u = getUtils(page)
-  const makeCode = (plane = 'XY') => `const part001 = startSketchOn('${plane}')
-  |> startProfileAt([7.00, 4.40], %)
-  |> line([6.60, -0.20], %)
-  |> line([2.80, 5.00], %)
-  |> line([-5.60, 4.40], %)
-  |> line([-5.40, -3.80], %)
-  |> close(%)
-  |> extrude(10.00, %)
-`
-  await context.addInitScript(async (code) => {
     localStorage.setItem('persistCode', code)
   }, makeCode('XY'))
   await page.setViewportSize({ width: 1200, height: 500 })
