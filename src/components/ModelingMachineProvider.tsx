222f9d6e4e40bdb1ea1cd9d6c48cf520381b4aee
Frank Noirot <frank@kittycad.io>
2024-04-24 15:14:20 -0400

    Make it impossible to crash app while extruding

--- src/components/ModelingMachineProvider.tsx
+++ src/components/ModelingMachineProvider.tsx
@@ -267,10 +267,12 @@ export const ModelingMachineProvider = ({
         'has valid extrude selection': ({ selectionRanges }) => {
           // A user can begin extruding if they either have 1+ faces selected or nothing selected
           // TODO: I believe this guard only allows for extruding a single face at a time
-          if (selectionRanges.codeBasedSelections.length < 1) return false
           const isPipe = isSketchPipe(selectionRanges)
 
-          if (isSelectionLastLine(selectionRanges, codeManager.code))
+          if (
+            selectionRanges.codeBasedSelections.length === 0 ||
+            isSelectionLastLine(selectionRanges, codeManager.code)
+          )
             return true
           if (!isPipe) return false
 
