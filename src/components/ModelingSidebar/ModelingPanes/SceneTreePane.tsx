31aabfa7bec6dda1e9b98b5bb6759617a70060e2
Frank Noirot <frank@kittycad.io>
2024-05-17 13:53:31 -0400

    Add artifactMap to SceneTreePane until a better representation exists

--- src/components/ModelingSidebar/ModelingPanes/SceneTreePane.tsx
+++ src/components/ModelingSidebar/ModelingPanes/SceneTreePane.tsx
@@ -1,9 +1,26 @@
+import { useResolvedTheme } from 'hooks/useResolvedTheme'
+import { engineCommandManager } from 'lib/singletons'
+import ReactJson from 'react-json-view'
+
 export const SceneTreePane = () => {
+  const theme = useResolvedTheme()
+  const { artifactMap } = engineCommandManager
   return (
     <div className="h-full relative">
       <div className="absolute inset-0 p-2 flex flex-col items-start">
         <div className="overflow-auto h-full w-full pb-12">
-          <h1>Waddup?</h1>
+          <ReactJson
+            src={artifactMap}
+            collapsed={1}
+            collapseStringsAfterLength={60}
+            enableClipboard={false}
+            displayDataTypes={false}
+            displayObjectSize={true}
+            indentWidth={2}
+            quotesOnKeys={false}
+            name={false}
+            theme={theme === 'light' ? 'rjv-default' : 'monokai'}
+          />
         </div>
       </div>
     </div>
