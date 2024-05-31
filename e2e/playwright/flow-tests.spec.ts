fb30528256a59ae29f8a40d4488cc73eeaf26c0d
max-mrgrsk <margorskyi@gmail.com>
2024-05-31 22:11:54 +0200

    increase timeout time

--- e2e/playwright/flow-tests.spec.ts
+++ e2e/playwright/flow-tests.spec.ts
@@ -4453,7 +4453,7 @@ test.describe('Testing Gizmo', () => {
       await page.mouse.move(clickPosition.x, clickPosition.y)
       await page.waitForTimeout(100)
       await page.mouse.click(clickPosition.x, clickPosition.y)
-      await page.waitForTimeout(100)
+      await page.waitForTimeout(300) // wait for camera to move and update debug values
 
       await Promise.all([
         // position
