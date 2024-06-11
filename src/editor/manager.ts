5613473f9ddc889228ec129646242cc51b215069
Paul Tagliamonte <paul@zoo.dev>
2024-06-11 13:45:59 -0400

    == vs ===

--- src/editor/manager.ts
+++ src/editor/manager.ts
@@ -10,7 +10,7 @@ import { addLineHighlight } from './highlightextension'
 import { forEachDiagnostic, setDiagnostics, Diagnostic } from '@codemirror/lint'
 
 function diagnosticIsEqual(d1: Diagnostic, d2: Diagnostic): boolean {
-  return d1.start == d2.start && d1.end == d2.end && d1.message == d2.message
+  return d1.start === d2.start && d1.end === d2.end && d1.message === d2.message
 }
 
 export default class EditorManager {
