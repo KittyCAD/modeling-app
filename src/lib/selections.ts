570fd827ed68e5bbd0fb3fcf9d602dbecd3acecd
Kurt Hutten <k.hutten@protonmail.ch>
2024-06-18 16:08:41 +1000

    fix zoom issues with sketch mode (#2664)
    * cam stuff start
    
    * more progress
    
    * mostly done
    
    * fix snapshot tests
    
    * A snapshot a day keeps the bugs away! 📷🐛 (OS: ubuntu)
    
    * fix
    
    * fix ubuntu
    
    * more tweaks fixes
    
    * add test
    
    * more FE fixes
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/lib/selections.ts
+++ src/lib/selections.ts
@@ -540,7 +540,7 @@ function codeToIdSelections(
     .filter(Boolean) as any
 }
 
-export function sendSelectEventToEngine(
+export async function sendSelectEventToEngine(
   e: MouseEvent | React.MouseEvent<HTMLDivElement, MouseEvent>,
   el: HTMLVideoElement,
   streamDimensions: { streamWidth: number; streamHeight: number }
@@ -551,7 +551,7 @@ export function sendSelectEventToEngine(
     el,
     ...streamDimensions,
   })
-  const result: Promise<Models['SelectWithPoint_type']> = engineCommandManager
+  const result: Models['SelectWithPoint_type'] = await engineCommandManager
     .sendSceneCommand({
       type: 'modeling_cmd_req',
       cmd: {
