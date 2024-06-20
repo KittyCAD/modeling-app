cfa9cde5c607cd5d5bb0b8e4361a1e9c32d8e056
Jess Frazelle <github@jessfraz.com>
2024-06-19 18:29:49 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/wasm-lib/kcl/src/lib.rs
+++ src/wasm-lib/kcl/src/lib.rs
@@ -4,6 +4,13 @@
 //! the standard library implementation, a LSP implementation, generator for the docs, and more.
 #![recursion_limit = "1024"]
 
+macro_rules! println {
+    ($($rest:tt)*) => {
+        #[cfg(not(feature = "disable-println"))]
+        std::println!($($rest)*)
+    }
+}
+
 pub mod ast;
 pub mod coredump;
 pub mod docs;
