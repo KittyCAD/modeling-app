cf1d0ca2adf0a1d84decc16e7cd35d544d7033cc
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2024-07-02 14:00:07 +1000

    fmt

--- e2e/playwright/flow-tests.spec.ts
+++ e2e/playwright/flow-tests.spec.ts
@@ -6413,7 +6413,7 @@ test('Basic default modeling and sketch hotkeys work', async ({ page }) => {
   // Start a sketch
   await page.keyboard.press('s')
   await page.waitForTimeout(2000)
-  await page.mouse.move(800, 300,{steps:5})
+  await page.mouse.move(800, 300, { steps: 5 })
   await page.mouse.click(800, 300)
   await page.waitForTimeout(2000)
   await expect(lineButton).toHaveAttribute('aria-pressed', 'true', {
