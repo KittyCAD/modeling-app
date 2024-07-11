843fcdcbd79e332f230cffff2d96877e59516a8f
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2024-07-12 09:45:19 +1000

    fmt

--- src/AppState.tsx
+++ src/AppState.tsx
@@ -53,7 +53,9 @@ const AppStreamContext = createContext<AppStream>({
 export const useAppStream = () => useContext(AppStreamContext)
 
 export const AppStreamProvider = ({ children }: { children: ReactNode }) => {
-  const [mediaStream, setMediaStream] = useState<MediaStream>(undefined as unknown as MediaStream,)
+  const [mediaStream, setMediaStream] = useState<MediaStream>(
+    undefined as unknown as MediaStream
+  )
 
   return (
     <AppStreamContext.Provider
