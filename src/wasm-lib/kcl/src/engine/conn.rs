060886dea0d889be79d265d035c17f6014d70809
Paul Tagliamonte <paul@zoo.dev>
2024-07-18 13:37:41 -0400

    Remove the comment too
    Signed-off-by: Paul Tagliamonte <paul@zoo.dev>

--- src/wasm-lib/kcl/src/engine/conn.rs
+++ src/wasm-lib/kcl/src/engine/conn.rs
@@ -161,13 +161,6 @@ impl EngineConnection {
     }
 
     pub async fn new(ws: reqwest::Upgraded) -> Result<EngineConnection> {
-        // allowing the field_reassign_with_default lint here because the
-        // defaults for this object don't match the type defaults. We want
-        // to inherent the default config
-        //
-        // See the `impl Default for WebSocketConfig` in
-        // `tungstenite/protocol/mod.rs`
-
         let wsconfig = tokio_tungstenite::tungstenite::protocol::WebSocketConfig {
             // 4294967296 bytes, which is around 4.2 GB.
             max_message_size: Some(0x100000000),
