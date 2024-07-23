8dd8baffa1d79a77599c712406dbd9c12d0e7e3c
Ryan Rosello <goneby30+github@gmail.com>
2024-07-24 07:06:47 +1000

    set playwright actionTimeout to 15 seconds

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
+    actionTimeout: 15000,
   },
 
   /* Configure projects for major browsers */
