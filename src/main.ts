c4196b18c6bed9e105e51dbe3caeede226c1bf79
49lf <ircsurfer33@gmail.com>
2024-07-23 10:39:35 -0400

    Get electron building something at all

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { app, BrowserWindow } from 'electron'
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.whenReady().then(() => {
+  createWindow()
+})
