686a89d4ea884b85f29afb577db7ab502c208332
Jonathan Tran <jonnytran@gmail.com>
2024-07-23 12:48:41 -0400

    Fix to preserve types using structuredClone

--- src/lang/util.ts
+++ src/lang/util.ts
@@ -38,7 +38,7 @@ export function updatePathToNodeFromMap(
   oldPath: PathToNode,
   pathToNodeMap: { [key: number]: PathToNode }
 ): PathToNode {
-  const updatedPathToNode = JSON.parse(JSON.stringify(oldPath))
+  const updatedPathToNode = structuredClone(oldPath)
   let max = 0
   Object.values(pathToNodeMap).forEach((path) => {
     const index = Number(path[1][0])
