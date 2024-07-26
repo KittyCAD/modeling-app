927cc7713a8145421b9209e49104a91d108266a6
Paul R. Tagliamonte <paul@zoo.dev>
2024-07-26 10:22:19 -0400

    Add in an `assert` family of functions
    This will check values at runtime, allowing for users to ensure computed
    values do not violate static constraints.
    
    Signed-off-by: Paul R. Tagliamonte <paul@zoo.dev>

---
+++ src/wasm-lib/kcl/src/std/assert.rs
@@ -0,0 +1,121 @@
+//! Standard library assert functions.
+
+use anyhow::Result;
+use derive_docs::stdlib;
+use schemars::JsonSchema;
+
+use crate::{
+    errors::{KclError, KclErrorDetails},
+    executor::MemoryItem,
+    std::Args,
+};
+
+async fn _assert(value: bool, message: &str, args: &Args) -> Result<(), KclError> {
+    if !value {
+        return Err(KclError::Type(KclErrorDetails {
+            message: format!("assert failed: {}", message),
+            source_ranges: vec![args.source_range],
+        }));
+    }
+    Ok(())
+}
+
+/// Check that the provided value is true, or raise a [KclError]
+/// with the provided description.
+pub async fn assert(args: Args) -> Result<MemoryItem, KclError> {
+    let (data, description): (bool, String) = args.get_data()?;
+    args.make_user_val_from_f64(inner_assert(data, &description, args.clone()).await?)
+}
+
+/// Check a value at runtime, and raise an error if the argument provided
+/// is false.
+///
+/// ```no_run
+/// const myVar = true
+/// assert(myVar, "should always be true")
+/// ```
+#[stdlib {
+    name = "assert",
+}]
+async fn inner_assert(data: bool, message: &str, args: Args) -> Result<f64, KclError> {
+    _assert(data, message, &args).await?;
+    Ok(0.0)
+}
+
+pub async fn assert_lt(args: Args) -> Result<MemoryItem, KclError> {
+    let (left, right, description): (f64, f64, String) = args.get_data()?;
+    args.make_user_val_from_f64(inner_assert_lt(left, right, &description, args.clone()).await?)
+}
+
+/// Check that a numerical value is less than to another at runtime,
+/// otherwise raise an error.
+///
+/// ```no_run
+/// assertLessThan(1, 2, "1 is less than 2")
+/// ```
+#[stdlib {
+    name = "assertLessThan",
+}]
+async fn inner_assert_lt(left: f64, right: f64, message: &str, args: Args) -> Result<f64, KclError> {
+    _assert(left < right, message, &args).await?;
+    Ok(0.0)
+}
+
+pub async fn assert_gt(args: Args) -> Result<MemoryItem, KclError> {
+    let (left, right, description): (f64, f64, String) = args.get_data()?;
+    args.make_user_val_from_f64(inner_assert_gt(left, right, &description, args.clone()).await?)
+}
+
+/// Check that a numerical value is greater than another at runtime,
+/// otherwise raise an error.
+///
+/// ```no_run
+/// assertGreaterThan(2, 1, "2 is greater than 1")
+/// ```
+#[stdlib {
+    name = "assertGreaterThan",
+}]
+async fn inner_assert_gt(left: f64, right: f64, message: &str, args: Args) -> Result<f64, KclError> {
+    _assert(left > right, message, &args).await?;
+    Ok(0.0)
+}
+
+pub async fn assert_lte(args: Args) -> Result<MemoryItem, KclError> {
+    let (left, right, description): (f64, f64, String) = args.get_data()?;
+    args.make_user_val_from_f64(inner_assert_lte(left, right, &description, args.clone()).await?)
+}
+
+/// Check that a numerical value is less than or equal to another at runtime,
+/// otherwise raise an error.
+///
+/// ```no_run
+/// assertLessThanOrEq(1, 2, "1 is less than 2")
+/// assertLessThanOrEq(1, 1, "1 is equal to 1")
+/// ```
+#[stdlib {
+    name = "assertLessThanOrEq",
+}]
+async fn inner_assert_lte(left: f64, right: f64, message: &str, args: Args) -> Result<f64, KclError> {
+    _assert(left <= right, message, &args).await?;
+    Ok(0.0)
+}
+
+pub async fn assert_gte(args: Args) -> Result<MemoryItem, KclError> {
+    let (left, right, description): (f64, f64, String) = args.get_data()?;
+    args.make_user_val_from_f64(inner_assert_gte(left, right, &description, args.clone()).await?)
+}
+
+/// Check that a numerical value is greater than or equal to another at runtime,
+/// otherwise raise an error.
+///
+/// ```no_run
+/// assertGreaterThanOrEq(2, 1, "2 is greater than 1")
+/// assertGreaterThanOrEq(1, 1, "1 is equal to 1")
+/// ```
+#[stdlib {
+    name = "assertGreaterThanOrEq",
+}]
+async fn inner_assert_gte(left: f64, right: f64, message: &str, args: Args) -> Result<f64, KclError> {
+    _assert(left >= right, message, &args).await?;
+    Ok(0.0)
+}
