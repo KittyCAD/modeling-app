0434b0e0d85932866760f16b7706bc33ef771f3c
Ryan Rosello <goneby30+github@gmail.com>
2024-07-26 21:14:18 +1000

    enable screenshot

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,6 +32,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
