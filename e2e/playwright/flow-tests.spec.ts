3455913e28783e531cf8601db41fe109e1c0519c
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2024-08-01 16:44:36 +1000

    fix 'Engine disconnect & reconnect in sketch mode' test

--- e2e/playwright/flow-tests.spec.ts
+++ e2e/playwright/flow-tests.spec.ts
@@ -7290,15 +7290,15 @@ test.describe('Test network and connection issues', () => {
       .toHaveText(`const sketch001 = startSketchOn('XZ')
     |> startProfileAt(${commonPoints.startAt}, %)
     |> line([${commonPoints.num1}, 0], %)
-    |> line([-9.16, 8.81], %)`)
+    |> line([-1.21, -0.41], %)`)
     await page.waitForTimeout(100)
     await page.mouse.click(startXPx, 500 - PUR * 20)
     await expect(page.locator('.cm-content'))
       .toHaveText(`const sketch001 = startSketchOn('XZ')
     |> startProfileAt(${commonPoints.startAt}, %)
     |> line([${commonPoints.num1}, 0], %)
-    |> line([-9.16, 8.81], %)
-    |> line([-5.28, 0], %)`)
+    |> line([-1.21, -0.41], %)
+    |> line([-2.41, 0], %)`)
 
     // Unequip line tool
     await page.keyboard.press('Escape')
