6773dbe7ff4dd0f1a9ab4dec255c8af9aadc1d45
ryanrosello-og <50574915+ryanrosello-og@users.noreply.github.com>
2024-08-08 09:19:07 +1000

    set Playwright actionTimeout to 15 seconds (#3115)
    * set playwright actionTimeout to 15 seconds
    
    * enabled screenshot on failure and html reporter to easily see individual test duration
    
    * extend timeout when waiting for console
    
    * removed duplicated html reporter
    
    ---------
    
    Co-authored-by: Kurt Hutten <k.hutten@protonmail.ch>
    Co-authored-by: ryanrosello-og <ry@zoo.dev>

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,6 +32,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
+    actionTimeout: 15000,
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
