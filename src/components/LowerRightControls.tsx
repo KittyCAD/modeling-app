77dbf2ae872084d51b58d556079392a4f6bb6c36
Lee <lee@Lees-Mac-mini.local>
2024-08-07 20:18:23 -0400

    fmt

--- src/components/LowerRightControls.tsx
+++ src/components/LowerRightControls.tsx
@@ -88,7 +88,7 @@ export function LowerRightControls({
         <Link
           to={
             location.pathname.includes(paths.FILE)
-              ? (filePath + paths.SETTINGS + '?tab=project')
+              ? filePath + paths.SETTINGS + '?tab=project'
               : paths.HOME + paths.SETTINGS
           }
         >
