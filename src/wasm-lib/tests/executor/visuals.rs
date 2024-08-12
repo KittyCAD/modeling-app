2557bbdfe8f0c95620dac26921bec4aafd6d08ab
Adam Chalmers <adam.chalmers@zoo.dev>
2024-08-12 16:46:17 -0500

    Use a macro to make KCL snapshot tests easier
    Now the process for adding a new KCL test is:
    
     - Write a new .kcl file in the `tests/inputs` directory,
       e.g. `tests/inputs/foo.kcl`
     - Run `just new-test foo`
     - Commit changes and open a PR

---
+++ src/wasm-lib/tests/executor/visuals.rs
@@ -0,0 +1,48 @@
+macro_rules! kcl_input {
+    ($file:literal) => {
+        include_str!(concat!("inputs/", $file, ".kcl"))
+    };
+}
+
+macro_rules! kcl_test {
+    ($file:literal, $test_name:ident) => {
+        #[tokio::test(flavor = "multi_thread")]
+        async fn $test_name() {
+            let code = kcl_input!($file);
+
+            let result = super::execute_and_snapshot(code, kcl_lib::settings::types::UnitLength::Mm)
+                .await
+                .unwrap();
+            super::assert_out($file, &result);
+        }
+    };
+}
+
+kcl_test!("sketch_on_face", kcl_test_sketch_on_face);
+kcl_test!("basic_fillet_cube_start", kcl_test_basic_fillet_cube_start);
+kcl_test!(
+    "basic_fillet_cube_next_adjacent",
+    kcl_test_basic_fillet_cube_next_adjacent
+);
+kcl_test!(
+    "basic_fillet_cube_previous_adjacent",
+    kcl_test_basic_fillet_cube_previous_adjacent
+);
+kcl_test!("basic_fillet_cube_end", kcl_test_basic_fillet_cube_end);
+kcl_test!(
+    "basic_fillet_cube_close_opposite",
+    kcl_test_basic_fillet_cube_close_opposite
+);
+kcl_test!("sketch_on_face_end", kcl_test_sketch_on_face_end);
+kcl_test!("sketch_on_face_start", kcl_test_sketch_on_face_start);
+kcl_test!(
+    "sketch_on_face_end_negative_extrude",
+    kcl_test_sketch_on_face_end_negative_extrude
+);
+kcl_test!("mike_stress_test", kcl_test_mike_stress_test);
+kcl_test!("pentagon_fillet_sugar", kcl_test_pentagon_fillet_sugar);
+kcl_test!("pipe_as_arg", kcl_test_pipe_as_arg);
+kcl_test!("computed_var", kcl_test_computed_var);
+kcl_test!("lego", kcl_test_lego);
+kcl_test!("riddle_small", kcl_test_riddle_small);
+kcl_test!("tan_arc_x_line", kcl_test_tan_arc_x_line);
