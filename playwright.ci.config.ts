11bdaac4e097c6f5f1fba39fea1297cbe7c28d86
Ryan Rosello <goneby30+github@gmail.com>
2024-08-13 21:20:12 +1000

    enable list reporter

--- playwright.ci.config.ts
+++ playwright.ci.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['dot'],
+    ['list'],
     ['json', { outputFile: './test-results/report.json' }],
     ['html'],
   ],
