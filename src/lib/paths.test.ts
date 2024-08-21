01964ac4182d91f444b702d0cabed039947a59be
Adam Sunderland <iterion@gmail.com>
2024-08-21 11:44:19 -0400

    More paths cleanup & some tests

---
+++ src/lib/paths.test.ts
@@ -0,0 +1,55 @@
+import { parseProjectRoute } from './paths'
+import * as path from 'path'
+describe('testing parseProjectRoute', () => {
+  it('should parse a project as a subpath of project dir', async () => {
+    global.window = {electron:  { path: path }}
+    let config = {
+      settings: {
+        app: {
+          project_directory: '/home/somebody/projects',
+        },
+      },
+    }
+    const route = '/home/somebody/projects/project'
+    expect(await parseProjectRoute(config, route, true)).toEqual({
+      projectName: 'project',
+      projectPath: route,
+      currentFileName: null,
+      currentFilePath: null,
+    })
+  })
+  it('should parse a project as the project dir', async () => {
+    global.window = {electron:  { path: path }}
+    let config = {
+      settings: {
+        app: {
+          project_directory: '/home/somebody/projects',
+        },
+      },
+    }
+    const route = '/home/somebody/projects'
+    expect(await parseProjectRoute(config, route, true)).toEqual({
+      projectName: null,
+      projectPath: route,
+      currentFileName: null,
+      currentFilePath: null,
+    })
+  })
+  it('should parse a project with file in the project dir', async () => {
+    global.window = {electron:  { path: path }}
+    let config = {
+      settings: {
+        app: {
+          project_directory: '/home/somebody/projects',
+        },
+      },
+    }
+    const route = "/home/somebody/projects/assembly/main.kcl"
+    expect(await parseProjectRoute(config, route, true)).toEqual({
+      projectName: "assembly",
+      projectPath: "/home/somebody/projects/assembly",
+      currentFileName: "main.kcl",
+      currentFilePath: route,
+    })
+  })
+})
