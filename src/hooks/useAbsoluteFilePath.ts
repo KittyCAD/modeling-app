0991b01457b63640ba5f9f5718e2cb571ac0687e
49lf <ircsurfer33@gmail.com>
2024-08-21 13:57:32 -0400

    Revert; everything's working because of Adam S.'s commits

--- src/hooks/useAbsoluteFilePath.ts
+++ src/hooks/useAbsoluteFilePath.ts
@@ -1,18 +1,15 @@
 import { type IndexLoaderData } from 'lib/types'
 import { BROWSER_PATH, PATHS } from 'lib/paths'
 import { useRouteLoaderData } from 'react-router-dom'
-import { isDesktop } from 'lib/isDesktop'
 
 export function useAbsoluteFilePath() {
   const routeData = useRouteLoaderData(PATHS.FILE) as IndexLoaderData
-
-  const sep = isDesktop() ? window.electron.sep : '/'
 
   return (
     PATHS.FILE +
     '/' +
     encodeURIComponent(
-      routeData?.file?.path + sep + routeData?.file?.name || BROWSER_PATH
+      routeData?.file?.path || BROWSER_PATH
     )
   )
 }
