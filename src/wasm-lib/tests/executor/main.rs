3543c5f0e75c0e005899445795753460e5c70c32
Jess Frazelle <jessfraz@users.noreply.github.com>
2024-08-20 18:57:23 -0700

    Update tests after ben merge of fillet working w shell (#3586)
    * fixes
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * bump all the things
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    ---------
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/wasm-lib/tests/executor/main.rs
+++ src/wasm-lib/tests/executor/main.rs
@@ -2185,15 +2185,3 @@ someFunction('INVALID')
         r#"semantic: KclErrorDetails { source_ranges: [SourceRange([89, 114]), SourceRange([126, 155]), SourceRange([159, 182])], message: "Argument at index 0 was supposed to be type kcl_lib::std::sketch::SketchData but found string (text)" }"#
     );
 }
-
-#[tokio::test(flavor = "multi_thread")]
-async fn kcl_test_fillet_and_shell() {
-    let code = kcl_input!("fillet-and-shell");
-
-    let result = execute_and_snapshot(code, UnitLength::Mm).await;
-    assert!(result.is_err());
-    assert_eq!(
-        result.err().unwrap().to_string(),
-        r#"engine: KclErrorDetails { source_ranges: [SourceRange([2004, 2065])], message: "Modeling command failed: [ApiError { error_code: InternalEngine, message: \"Invalid brep after shell operation\" }]" }"#
-    );
-}
