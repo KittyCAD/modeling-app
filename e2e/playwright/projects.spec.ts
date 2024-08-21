1eebdd17de5e4c6cdb8915336b3afe83379555cf
Adam Sunderland <iterion@gmail.com>
2024-08-21 13:00:27 -0400

    Remove sleep

--- e2e/playwright/projects.spec.ts
+++ e2e/playwright/projects.spec.ts
@@ -1676,7 +1676,6 @@ test.describe('Renaming in the file tree', () => {
       })
 
       await test.step('Rename the folder', async () => {
-        await page.waitForTimeout(60000)
         await folderToRename.click({ button: 'right' })
         await expect(renameMenuItem).toBeVisible()
         await renameMenuItem.click()
