dbfc1fca1947606b321e70233a6e2a4e6f958b6b
49lf <ircsurfer33@gmail.com>
2024-08-21 11:26:38 -0400

    Use agnostic sep

--- src/hooks/useAbsoluteFilePath.ts
+++ src/hooks/useAbsoluteFilePath.ts
@@ -1,15 +1,18 @@
 import { type IndexLoaderData } from 'lib/types'
 import { BROWSER_PATH, PATHS } from 'lib/paths'
 import { useRouteLoaderData } from 'react-router-dom'
+import { isDesktop } from 'lib/isDesktop'
 
 export function useAbsoluteFilePath() {
   const routeData = useRouteLoaderData(PATHS.FILE) as IndexLoaderData
+
+  const sep = isDesktop() ? window.electron.sep : '/'
 
   return (
     PATHS.FILE +
     '/' +
     encodeURIComponent(
-      routeData?.file?.path + '/' + routeData?.file?.name || BROWSER_PATH
+      routeData?.file?.path + sep + routeData?.file?.name || BROWSER_PATH
     )
   )
 }
