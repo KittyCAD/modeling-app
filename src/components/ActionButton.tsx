bbdca7421e2997feed37434b5590b5f86a852395
Frank Noirot <frank@zoo.dev>
2024-08-29 14:21:42 -0400

    New sign-in page and signed-in on-load animation (#3684)
    * Basic sign-in page layout
    
    * Better responsive styling
    
    * Add wipe animation
    
    * Fix mobile button styling
    
    * Add juicy on-load animation to logo in app header
    
    * Make video card a link to the sample's code
    
    * Fix video URL on bundled desktop app, add "open default browser" behavior to links
    
    * Revert "Add juicy on-load animation to logo in app header"
    
    This reverts commit c167569d7e8578584dbea4368724c6d5d3948466.
    
    ---------
    
    Co-authored-by: 49fl <ircsurfer33@gmail.com>

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -42,7 +42,13 @@ export type ActionButtonProps =
 
 export const ActionButton = forwardRef((props: ActionButtonProps, ref) => {
   const classNames = `action-button p-0 m-0 group mono text-xs leading-none flex items-center gap-2 rounded-sm border-solid border border-chalkboard-30 hover:border-chalkboard-40 enabled:dark:border-chalkboard-70 dark:hover:border-chalkboard-60 dark:bg-chalkboard-90/50 text-chalkboard-100 dark:text-chalkboard-10 ${
-    props.iconStart ? (props.iconEnd ? 'px-0' : 'pr-2') : 'px-2'
+    props.iconStart
+      ? props.iconEnd
+        ? 'px-0'
+        : 'pr-2'
+      : props.iconEnd
+      ? 'px-2'
+      : 'pl-2'
   } ${props.className ? props.className : ''}`
 
   switch (props.Element) {
