0926e838553e88c8fcd35ba531fa7e80b2c6a773
Pierre Jacquier <pierrejacquier39@gmail.com>
2024-08-29 05:34:45 -0400

    WIP macOS sign and notarize

---
+++ notarize.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    tool: 'notarytool',
+    teamId: process.env.APPLE_TEAM_ID,
+    appBundleId: 'dev.zoo.modeling-app',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_PASSWORD,
+  });
+};
