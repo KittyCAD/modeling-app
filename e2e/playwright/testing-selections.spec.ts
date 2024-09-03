49c8fc4c97dc8a92d5fd70cd47bc88f932d81c6b
Frank Noirot <frank@kittycad.io>
2024-09-03 18:35:09 -0400

    Update bracket example code and some test colors that broke

--- e2e/playwright/testing-selections.spec.ts
+++ e2e/playwright/testing-selections.spec.ts
@@ -773,9 +773,9 @@ const extrude001 = extrude(50, sketch001)
 
     await page.waitForTimeout(1000)
 
-    let noHoverColor: [number, number, number] = [82, 82, 82]
-    let hoverColor: [number, number, number] = [116, 116, 116]
-    let selectColor: [number, number, number] = [144, 148, 97]
+    let noHoverColor: [number, number, number] = [92, 92, 92]
+    let hoverColor: [number, number, number] = [127, 127, 127]
+    let selectColor: [number, number, number] = [155, 155, 105]
 
     const extrudeWall = { x: 670, y: 275 }
     const extrudeText = `line([170.36, -121.61], %, $seg01)`
@@ -816,9 +816,9 @@ const extrude001 = extrude(50, sketch001)
     await expect(page.getByTestId('hover-highlight')).not.toBeVisible()
 
     // because of shading, color is not exact everywhere on the face
-    noHoverColor = [104, 104, 104]
-    hoverColor = [134, 134, 134]
-    selectColor = [158, 162, 110]
+    noHoverColor = [115, 115, 115]
+    hoverColor = [145, 145, 145]
+    selectColor = [168, 168, 120]
 
     await expect(await u.getGreatestPixDiff(cap, noHoverColor)).toBeLessThan(6)
     await page.mouse.move(cap.x, cap.y)
