697bff6c7c57f4b49b24dd29fcbcbd055f2694d7
Frank Noirot <frank@kittycad.io>
2024-09-03 20:48:22 -0400

    Relax color expectations a little bit, as they can be different on web and electron

--- e2e/playwright/testing-selections.spec.ts
+++ e2e/playwright/testing-selections.spec.ts
@@ -787,7 +787,7 @@ const extrude001 = extrude(50, sketch001)
 
     await expect
       .poll(() => u.getGreatestPixDiff(extrudeWall, noHoverColor))
-      .toBeLessThan(5)
+      .toBeLessThan(15)
     await page.mouse.move(nothing.x, nothing.y)
     await page.waitForTimeout(100)
     await page.mouse.move(extrudeWall.x, extrudeWall.y)
@@ -798,18 +798,18 @@ const extrude001 = extrude(50, sketch001)
     await page.waitForTimeout(200)
     await expect(
       await u.getGreatestPixDiff(extrudeWall, hoverColor)
-    ).toBeLessThan(6)
+    ).toBeLessThan(15)
     await page.mouse.click(extrudeWall.x, extrudeWall.y)
     await expect(page.locator('.cm-activeLine')).toHaveText(`|> ${extrudeText}`)
     await page.waitForTimeout(200)
     await expect(
       await u.getGreatestPixDiff(extrudeWall, selectColor)
-    ).toBeLessThan(6)
+    ).toBeLessThan(15)
     await page.waitForTimeout(1000)
     // check color stays there, i.e. not overridden (this was a bug previously)
     await expect(
       await u.getGreatestPixDiff(extrudeWall, selectColor)
-    ).toBeLessThan(6)
+    ).toBeLessThan(15)
 
     await page.mouse.move(nothing.x, nothing.y)
     await page.waitForTimeout(300)
@@ -820,21 +820,21 @@ const extrude001 = extrude(50, sketch001)
     hoverColor = [145, 145, 145]
     selectColor = [168, 168, 120]
 
-    await expect(await u.getGreatestPixDiff(cap, noHoverColor)).toBeLessThan(6)
+    await expect(await u.getGreatestPixDiff(cap, noHoverColor)).toBeLessThan(15)
     await page.mouse.move(cap.x, cap.y)
     await expect(page.getByTestId('hover-highlight').first()).toBeVisible()
     await expect(page.getByTestId('hover-highlight').first()).toContainText(
       removeAfterFirstParenthesis(capText)
     )
     await page.waitForTimeout(200)
-    await expect(await u.getGreatestPixDiff(cap, hoverColor)).toBeLessThan(6)
+    await expect(await u.getGreatestPixDiff(cap, hoverColor)).toBeLessThan(15)
     await page.mouse.click(cap.x, cap.y)
     await expect(page.locator('.cm-activeLine')).toHaveText(`|> ${capText}`)
     await page.waitForTimeout(200)
-    await expect(await u.getGreatestPixDiff(cap, selectColor)).toBeLessThan(6)
+    await expect(await u.getGreatestPixDiff(cap, selectColor)).toBeLessThan(15)
     await page.waitForTimeout(1000)
     // check color stays there, i.e. not overridden (this was a bug previously)
-    await expect(await u.getGreatestPixDiff(cap, selectColor)).toBeLessThan(6)
+    await expect(await u.getGreatestPixDiff(cap, selectColor)).toBeLessThan(15)
   })
   test("Various pipe expressions should and shouldn't allow edit and or extrude", async ({
     page,
