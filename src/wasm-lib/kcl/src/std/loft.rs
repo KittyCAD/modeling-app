04a77efae3ecc045d3b97c3027b1b4bf3467ce12
Pierre Jacquier <pierrejacquier39@gmail.com>
2024-09-06 18:59:22 -0400

    The electron-builder updater on 0.25.0 is pointing to the wrong directory Fixes #3817

--- src/wasm-lib/kcl/src/std/loft.rs
+++ src/wasm-lib/kcl/src/std/loft.rs
@@ -170,5 +170,5 @@ async fn inner_loft(
     .await?;
 
     // Using the first sketch as the base curve, idk we might want to change this later.
-    do_post_extrude(sketch_groups[0].clone(), 0.0, id, args).await
+    do_post_extrude(sketch_groups[0].clone(), 0.0, args).await
 }
