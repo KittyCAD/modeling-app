ad9f23be0aeeaf260c793d44eaa2412d1738456a
Jonathan Tran <jonnytran@gmail.com>
2024-09-08 19:25:23 -0400

    Fix clippy errors

--- src/wasm-lib/kcl-to-core/src/conn_mock_core.rs
+++ src/wasm-lib/kcl-to-core/src/conn_mock_core.rs
@@ -314,8 +314,8 @@ fn generate_repl_uuids(count: usize) -> Vec<uuid::Uuid> {
 fn codegen_cpp_repl_uuid_setters(reps_id: &str, entity_ids: &[uuid::Uuid]) -> String {
     let mut codegen = String::new();
 
-    for (i, id) in entity_ids.into_iter().enumerate() {
-        let cpp_id = id_to_cpp(&id);
+    for (i, id) in entity_ids.iter().enumerate() {
+        let cpp_id = id_to_cpp(id);
         let iter = format!(
             r#"
             //change object id -> {id}
