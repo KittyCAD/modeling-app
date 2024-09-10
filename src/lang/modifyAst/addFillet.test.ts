542a882200e9cc7cd434363d5b0b3838220be026
max-mrgrsk <156543465+max-mrgrsk@users.noreply.github.com>
2024-09-10 22:11:28 +0200

    wipe as

--- src/lang/modifyAst/addFillet.test.ts
+++ src/lang/modifyAst/addFillet.test.ts
@@ -3,7 +3,6 @@ import {
   recast,
   initPromise,
   PathToNode,
-  Expr,
   Program,
   CallExpression,
   makeDefaultPlanes,
@@ -91,7 +90,7 @@ const runGetPathToExtrudeForSegmentSelectionTest = async (
   // ast
   const astOrError = parse(code)
   if (err(astOrError)) return new Error('AST not found')
-  const ast = astOrError as Program
+  const ast = astOrError
 
   // selection
   const segmentRange: [number, number] = [
@@ -226,7 +225,7 @@ const runFilletTest = async (
   code: string,
   segmentSnippet: string,
   extrudeSnippet: string,
-  radius = createLiteral(5) as Expr,
+  radius = createLiteral(5),
   expectedCode: string
 ) => {
   const astOrError = parse(code)
@@ -234,7 +233,7 @@ const runFilletTest = async (
     return new Error('AST not found')
   }
 
-  const ast = astOrError as Program
+  const ast = astOrError
 
   const segmentRange: [number, number] = [
     code.indexOf(segmentSnippet),
@@ -288,7 +287,7 @@ describe('Testing addFillet', () => {
     `
     const segmentSnippet = `line([60.04, -55.72], %)`
     const extrudeSnippet = `const extrude001 = extrude(50, sketch001)`
-    const radius = createLiteral(5) as Expr
+    const radius = createLiteral(5)
     const expectedCode = `const sketch001 = startSketchOn('XZ')
   |> startProfileAt([2.16, 49.67], %)
   |> line([101.49, 139.93], %)
@@ -331,7 +330,7 @@ const extrude001 = extrude(50, sketch001)
       `
     const segmentSnippet = `line([60.04, -55.72], %)`
     const extrudeSnippet = `const extrude001 = extrude(50, sketch001)`
-    const radius = createLiteral(5) as Expr
+    const radius = createLiteral(5)
     const expectedCode = `const sketch001 = startSketchOn('XZ')
   |> startProfileAt([2.16, 49.67], %)
   |> line([101.49, 139.93], %)
@@ -374,7 +373,7 @@ const extrude001 = extrude(50, sketch001)
       `
     const segmentSnippet = `line([-87.24, -47.08], %, $seg03)`
     const extrudeSnippet = `const extrude001 = extrude(50, sketch001)`
-    const radius = createLiteral(5) as Expr
+    const radius = createLiteral(5)
     const expectedCode = `const sketch001 = startSketchOn('XZ')
   |> startProfileAt([2.16, 49.67], %)
   |> line([101.49, 139.93], %)
@@ -416,7 +415,7 @@ const extrude001 = extrude(50, sketch001)
             |> fillet({ radius: 10, tags: [seg03] }, %)`
     const segmentSnippet = `line([60.04, -55.72], %)`
     const extrudeSnippet = `const extrude001 = extrude(50, sketch001)`
-    const radius = createLiteral(5) as Expr
+    const radius = createLiteral(5)
     const expectedCode = `const sketch001 = startSketchOn('XZ')
   |> startProfileAt([2.16, 49.67], %)
   |> line([101.49, 139.93], %)
@@ -452,7 +451,7 @@ const runModifyAstWithFilletAndTagTest = async (
   if (err(astOrError)) {
     return new Error('AST not found')
   }
-  const ast = astOrError as Program
+  const ast = astOrError
 
   // selection
   const segmentRanges: Array<[number, number]> = selectionSnippets.map(
@@ -471,7 +470,7 @@ const runModifyAstWithFilletAndTagTest = async (
 
   // radius
   const radius: KclCommandValue = {
-    valueAst: createLiteral(radiusValue) as Expr,
+    valueAst: createLiteral(radiusValue),
     valueText: radiusValue.toString(),
     valueCalculated: radiusValue.toString(),
   }
@@ -637,7 +636,7 @@ describe('Testing button states', () => {
     if (err(astOrError)) {
       return new Error('AST not found')
     }
-    const ast = astOrError as Program
+    const ast = astOrError
 
     // selectionRanges
     const range: [number, number] = segmentSnippet
