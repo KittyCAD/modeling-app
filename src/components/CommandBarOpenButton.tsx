563096fba41fa9ffd293617d8026d61890c1a8c9
49lf <ircsurfer33@gmail.com>
2024-09-18 11:16:58 -0400

    Close command bar and make open button disabled on pause

--- src/components/CommandBarOpenButton.tsx
+++ src/components/CommandBarOpenButton.tsx
@@ -1,14 +1,21 @@
+import { useEffect } from 'react'
 import { useCommandsContext } from 'hooks/useCommandsContext'
 import usePlatform from 'hooks/usePlatform'
 import { hotkeyDisplay } from 'lib/hotkeyWrapper'
 import { COMMAND_PALETTE_HOTKEY } from './CommandBar/CommandBar'
+import { useNetworkContext } from 'hooks/useNetworkContext'
+import { EngineConnectionStateType } from 'lang/std/engineConnection'
 
 export function CommandBarOpenButton() {
   const { commandBarSend } = useCommandsContext()
+  const { immediateState } = useNetworkContext()
   const platform = usePlatform()
+
+  const isDisabled = immediateState.type !== EngineConnectionStateType.ConnectionEstablished
 
   return (
     <button
+      disabled={isDisabled}
       className="group rounded-full flex items-center justify-center gap-2 px-2 py-1 bg-primary/10 dark:bg-chalkboard-90 dark:backdrop-blur-sm border-primary hover:border-primary dark:border-chalkboard-50 dark:hover:border-inherit text-primary dark:text-inherit"
       onClick={() => commandBarSend({ type: 'Open' })}
       data-testid="command-bar-open-button"
