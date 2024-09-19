abe8fa039edd2bb67263ec6805788896d4d0e3c8
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2024-09-19 14:13:19 +1000

    fix stupid test name

--- src/lang/executor.test.ts
+++ src/lang/executor.test.ts
@@ -410,7 +410,7 @@ describe('testing math operators', () => {
     const mem = await exe(code)
     expect(mem.get('myVar')?.value).toBe(5)
   })
-  it('it can do power of math', async () => {
+  it('can do power of math', async () => {
     const code = 'const myNeg2 = 4 ^ 2 - 3 ^ 2 * 2'
     const mem = await exe(code)
     expect(mem.get('myNeg2')?.value).toBe(-2)
