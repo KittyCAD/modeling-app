5d0e26088964c449f9b72a1760f54616d97e5b5a
Kevin Nadro <kevin@zoo.dev>
2024-09-19 12:04:01 -0500

    fix: fmt

--- src/wasm-lib/kcl/src/coredump/mod.rs
+++ src/wasm-lib/kcl/src/coredump/mod.rs
@@ -162,10 +162,9 @@ impl CoreDumpInfo {
             r#"[Add a title above and insert a description of the issue here]
 
 ![Screenshot]({screenshot_url})
-<!--
-  If you are capturing from a browser there is limited support for screenshots, only captures the modeling scene.
-  If you are on MacOS native screenshots may be disabled by default. To enable native screenshots add Zoo Modeling App to System Settings -> Screen & SystemAudio Recording for native screenshots.
--->
+
+> _Note: If you are capturing from a browser there is limited support for screenshots, only captures the modeling scene.
+  If you are on MacOS native screenshots may be disabled by default. To enable native screenshots add Zoo Modeling App to System Settings -> Screen & SystemAudio Recording for native screenshots._
 
 <details>
 <summary><b>Core Dump</b></summary>
