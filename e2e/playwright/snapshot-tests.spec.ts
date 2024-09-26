9d7fd0271036f076bfb6b0f3aab1770ef35e47d5
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2024-09-27 05:57:49 +1000

    Revert "disable failing step export test (#4000)"
    This reverts commit 27c6f75a4938f5eaaaf80da1f9b853f7f517e97c.

--- e2e/playwright/snapshot-tests.spec.ts
+++ e2e/playwright/snapshot-tests.spec.ts
@@ -129,17 +129,15 @@ const part001 = startSketchOn('-XZ')
     // NOTE it was easiest to leverage existing types and have doExport take Models['OutputFormat_type'] as in input
     // just note that only `type` and `storage` are used for selecting the drop downs is the app
     // the rest are only there to make typescript happy
-
-    // TODO - failing because of an exporter issue, ADD BACK IN WHEN ITS FIXED
-    // exportLocations.push(
-    //   await doExport(
-    //     {
-    //       type: 'step',
-    //       coords: sysType,
-    //     },
-    //     page
-    //   )
-    // )
+    exportLocations.push(
+      await doExport(
+        {
+          type: 'step',
+          coords: sysType,
+        },
+        page
+      )
+    )
     exportLocations.push(
       await doExport(
         {
