c2caa5ff786049f45595ae6c9180217d4529cd5c
Jonathan Tran <jonnytran@gmail.com>
2024-10-03 18:16:53 -0400

    Fix IdGenerator to use camel case in TS

--- src/wasm-lib/kcl/src/executor.rs
+++ src/wasm-lib/kcl/src/executor.rs
@@ -296,8 +296,8 @@ impl DynamicState {
 
 /// A generator for [ArtifactId]s that can be stable across executions.
 #[derive(Debug, Clone, Default, Deserialize, Serialize, PartialEq, ts_rs::TS, JsonSchema)]
-#[ts(export)]
 #[serde(rename_all = "camelCase")]
+#[ts(export)]
 pub struct IdGenerator {
     next_id: usize,
     ids: Vec<uuid::Uuid>,
