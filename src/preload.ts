787a677fbe55f38f403d2444b3fc52cd42dbeb3b
Jonathan Tran <jonnytran@gmail.com>
2024-10-04 16:19:23 -0400

    Fix to not use any

--- src/preload.ts
+++ src/preload.ts
@@ -5,6 +5,7 @@ import os from 'node:os'
 import fsSync from 'node:fs'
 import packageJson from '../package.json'
 import { MachinesListing } from 'lib/machineManager'
+import chokidar from 'chokidar'
 
 const open = (args: any) => ipcRenderer.invoke('dialog.showOpenDialog', args)
 const save = (args: any) => ipcRenderer.invoke('dialog.showSaveDialog', args)
@@ -23,28 +24,17 @@ const isMac = os.platform() === 'darwin'
 const isWindows = os.platform() === 'win32'
 const isLinux = os.platform() === 'linux'
 
-let fsWatchListeners = new Map<
-  string,
-  {
-    watcher: fsSync.FSWatcher
-    callback: (eventType: string, path: string) => void
-  }
->()
+let fsWatchListeners = new Map<string, ReturnType<typeof chokidar.watch>>()
 
-const watchFileOn = (
-  path: string,
-  callback: (eventType: string, path: string) => void
-) => {
-  const watcher = fsSync.watch(path)
-  watcher.on('change', callback)
-  fsWatchListeners.set(path, { watcher, callback })
+const watchFileOn = (path: string, callback: (path: string) => void) => {
+  const watcher = chokidar.watch(path)
+  watcher.on('change', callback).on('unlink', watchFileOff)
+  fsWatchListeners.set(path, watcher)
 }
 const watchFileOff = (path: string) => {
-  const entry = fsWatchListeners.get(path)
-  if (!entry) return
-  const { watcher, callback } = entry
-  watcher.off('change', callback)
-  watcher.close()
+  const watcher = fsWatchListeners.get(path)
+  if (!watcher) return
+  watcher.unwatch(path)
   fsWatchListeners.delete(path)
 }
 const readFile = (path: string) => fs.readFile(path, 'utf-8')
