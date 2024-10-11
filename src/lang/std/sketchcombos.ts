1d6fee415fe07d76a2fcc949b0310cc7bebc2129
Frank Noirot <frank@kittycad.io>
2024-10-11 11:44:49 -0400

    Don't mutate the user's `codeBasedSelections` when they apply a multi-selection constraint

--- src/lang/std/sketchcombos.ts
+++ src/lang/std/sketchcombos.ts
@@ -1559,9 +1559,14 @@ export function transformSecondarySketchLinesTagFirst({
     }
   | Error {
   // let node = structuredClone(ast)
-  const primarySelection = selectionRanges.codeBasedSelections.sort(
+
+  // We need to sort the selections by their start position
+  // so that we can process them in dependency order and not write invalid KCL.
+  const sortedCodeBasedSelections = selectionRanges.codeBasedSelections.sort(
     (a, b) => a.range[0] - b.range[0]
-  )[0].range
+  )
+  const primarySelection = sortedCodeBasedSelections[0].range
+  const secondarySelections = sortedCodeBasedSelections.slice(1)
 
   const _tag = giveSketchFnCallTag(ast, primarySelection, forceSegName)
   if (err(_tag)) return _tag
@@ -1571,7 +1576,7 @@ export function transformSecondarySketchLinesTagFirst({
     ast: modifiedAst,
     selectionRanges: {
       ...selectionRanges,
-      codeBasedSelections: selectionRanges.codeBasedSelections.slice(1),
+      codeBasedSelections: secondarySelections,
     },
     referencedSegmentRange: primarySelection,
     transformInfos,
