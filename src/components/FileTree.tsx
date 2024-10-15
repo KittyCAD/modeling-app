edd1ff11fe094d0e80425131ba01e52a09db5cab
49lf <ircsurfer33@gmail.com>
2024-10-15 15:45:40 -0400

    Cut off titles that are too long

--- src/components/FileTree.tsx
+++ src/components/FileTree.tsx
@@ -533,7 +533,10 @@ export const FileTreeRoot = () => {
   // project.path should never be empty here but I guess during initial loading
   // it can be.
   return (
-    <div className="cursor-pointer" title={project?.path ?? ''}>
+    <div
+      className="max-w-xs text-ellipsis overflow-hidden cursor-pointer"
+      title={project?.path ?? ''}
+    >
       {project?.name ?? ''}
     </div>
   )
