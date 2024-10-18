KittyCAD/modeling-app

commit a010743abb5292639ed2da1b18be94ba9ee54eac

KCL: Skip serializing default values for AST nodes (#4193)
This will make our snapshots and JSON representations easier to read (making our tests less verbose).

src/wasm-lib/kcl/src/ast/types.rs
@@ -53,8 +53,10 @@ pub struct Program {
     pub start: usize,
     pub end: usize,
     pub body: Vec<BodyItem>,
+    #[serde(default, skip_serializing_if = "NonCodeMeta::is_empty")]
     pub non_code_meta: NonCodeMeta,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -837,6 +839,7 @@ pub struct NonCodeNode {
     pub end: usize,
     pub value: NonCodeValue,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -981,6 +984,7 @@ pub struct NonCodeMeta {
     pub non_code_nodes: HashMap<usize, Vec<NonCodeNode>>,
     pub start: Vec<NonCodeNode>,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -1053,6 +1057,7 @@ pub struct ImportItem {
     pub start: usize,
     pub end: usize,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -1105,6 +1110,7 @@ pub struct ImportStatement {
     pub path: String,
     pub raw_path: String,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -1146,6 +1152,7 @@ pub struct ExpressionStatement {
     pub end: usize,
     pub expression: Expr,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -1162,6 +1169,7 @@ pub struct CallExpression {
     pub arguments: Vec<Expr>,
     pub optional: bool,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -1321,6 +1329,7 @@ pub struct VariableDeclaration {
     pub visibility: ItemVisibility,
     pub kind: VariableKind, // Change to enum if there are specific values
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -1575,6 +1584,7 @@ pub struct VariableDeclarator {
     /// The value of the variable.
     pub init: Expr,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -1606,6 +1616,7 @@ pub struct Literal {
     pub value: LiteralValue,
     pub raw: String,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -1662,6 +1673,7 @@ pub struct Identifier {
     pub end: usize,
     pub name: String,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -1703,6 +1715,7 @@ pub struct TagDeclarator {
     #[serde(rename = "value")]
     pub name: String,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -1821,6 +1834,7 @@ pub struct PipeSubstitution {
     pub start: usize,
     pub end: usize,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -1859,6 +1873,7 @@ pub struct ArrayExpression {
     #[serde(default, skip_serializing_if = "NonCodeMeta::is_empty")]
     pub non_code_meta: NonCodeMeta,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -1934,6 +1949,7 @@ pub struct ArrayRangeExpression {
     /// Is the `end_element` included in the range?
     pub end_inclusive: bool,
     // TODO (maybe) comments on range components?
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -2000,6 +2016,7 @@ pub struct ObjectExpression {
     #[serde(default, skip_serializing_if = "NonCodeMeta::is_empty")]
     pub non_code_meta: NonCodeMeta,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -2067,6 +2084,7 @@ pub struct ObjectProperty {
     pub key: Identifier,
     pub value: Expr,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -2197,6 +2215,7 @@ pub struct MemberExpression {
     pub property: LiteralIdentifier,
     pub computed: bool,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -2256,6 +2275,7 @@ pub struct BinaryExpression {
     pub left: BinaryPart,
     pub right: BinaryPart,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -2438,6 +2458,7 @@ pub struct UnaryExpression {
     pub operator: UnaryOperator,
     pub argument: BinaryPart,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -2513,8 +2534,10 @@ pub struct PipeExpression {
     // TODO: Only the first body expression can be any Value.
     // The rest will be CallExpression, and the AST type should reflect this.
     pub body: Vec<Expr>,
+    #[serde(default, skip_serializing_if = "NonCodeMeta::is_empty")]
     pub non_code_meta: NonCodeMeta,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -2650,6 +2673,7 @@ pub struct Parameter {
     /// Is the parameter optional?
     pub optional: bool,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -2665,6 +2689,7 @@ pub struct FunctionExpression {
     #[serde(skip)]
     pub return_type: Option<FnArgType>,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 
@@ -2751,6 +2776,7 @@ pub struct ReturnStatement {
     pub end: usize,
     pub argument: Expr,
 
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub digest: Option<Digest>,
 }
 

commit 9f6999829acbd0b68a43d53bbc30eaacf965b7b4

Fix broken digest code (#4206)
I broke the typescript bindings in https://github.com/KittyCAD/modeling-app/pull/4193 -- basically the `digest: Option<Digest>` fields previously allowed Typescript to not set a value for the `digest` key at all, but somehow my change made it required in the Typescript.

Fix is to apply `#[ts(optional)]`, see docs at https://docs.rs/ts-rs/latest/ts_rs/trait.TS.html#struct-field-attributes

src/wasm-lib/kcl/src/ast/types.rs
@@ -57,6 +57,7 @@ pub struct Program {
     pub non_code_meta: NonCodeMeta,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -840,6 +841,7 @@ pub struct NonCodeNode {
     pub value: NonCodeValue,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -985,6 +987,7 @@ pub struct NonCodeMeta {
     pub start: Vec<NonCodeNode>,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -1058,6 +1061,7 @@ pub struct ImportItem {
     pub end: usize,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -1111,6 +1115,7 @@ pub struct ImportStatement {
     pub raw_path: String,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -1153,6 +1158,7 @@ pub struct ExpressionStatement {
     pub expression: Expr,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -1170,6 +1176,7 @@ pub struct CallExpression {
     pub optional: bool,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -1330,6 +1337,7 @@ pub struct VariableDeclaration {
     pub kind: VariableKind, // Change to enum if there are specific values
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -1585,6 +1593,7 @@ pub struct VariableDeclarator {
     pub init: Expr,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -1617,6 +1626,7 @@ pub struct Literal {
     pub raw: String,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -1674,6 +1684,7 @@ pub struct Identifier {
     pub name: String,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -1716,6 +1727,7 @@ pub struct TagDeclarator {
     pub name: String,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -1835,6 +1847,7 @@ pub struct PipeSubstitution {
     pub end: usize,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -1874,6 +1887,7 @@ pub struct ArrayExpression {
     pub non_code_meta: NonCodeMeta,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -1950,6 +1964,7 @@ pub struct ArrayRangeExpression {
     pub end_inclusive: bool,
     // TODO (maybe) comments on range components?
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -2017,6 +2032,7 @@ pub struct ObjectExpression {
     pub non_code_meta: NonCodeMeta,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -2085,6 +2101,7 @@ pub struct ObjectProperty {
     pub value: Expr,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -2216,6 +2233,7 @@ pub struct MemberExpression {
     pub computed: bool,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -2276,6 +2294,7 @@ pub struct BinaryExpression {
     pub right: BinaryPart,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -2459,6 +2478,7 @@ pub struct UnaryExpression {
     pub argument: BinaryPart,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -2538,6 +2558,7 @@ pub struct PipeExpression {
     pub non_code_meta: NonCodeMeta,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -2674,6 +2695,7 @@ pub struct Parameter {
     pub optional: bool,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -2690,6 +2712,7 @@ pub struct FunctionExpression {
     pub return_type: Option<FnArgType>,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 
@@ -2777,6 +2800,7 @@ pub struct ReturnStatement {
     pub argument: Expr,
 
     #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[ts(optional)]
     pub digest: Option<Digest>,
 }
 