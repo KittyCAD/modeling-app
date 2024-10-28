8ccdf2286d2e502278e71c880196f450dff01f54
Jonathan Tran <jonnytran@gmail.com>
2024-10-28 16:51:47 -0400

    Add minimal failing test

---
+++ src/lang/wasm.test.ts
@@ -0,0 +1,13 @@
+import { err } from 'lib/trap'
+import { parse } from './wasm'
+import { enginelessExecutor } from 'lib/testHelpers'
+
+it('can execute parsed AST', async () => {
+  const code = `x = 1
+// A comment.`
+  const ast = parse(code)
+  expect(err(ast)).toEqual(false)
+  const execState = await enginelessExecutor(ast)
+  expect(err(ast)).toEqual(false)
+  expect(execState.memory.get('x')).toEqual(1)
+})
