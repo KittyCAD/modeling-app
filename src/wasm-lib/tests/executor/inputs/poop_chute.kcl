KittyCAD/modeling-app

commit 4a62862ca002d705d95b18eda94586346473ab58

Track tagged geometry with an enum, not options (#4333)
Tags can refer to either a surface, or a path. Tags track what they're
tagging with two fields: `Option<Path>` and `Option<Surface>`. This means
as more things can be tagged, we'll have a bunch of options and only one
can be Some at a given time. This should be an enum instead, so the
compiler can enforce that only one thing is being tagged.

Also I want to allow tags to refer to points, so I'd like to make this
enum before I add a new possible variant.

src/wasm-lib/kcl/src/ast/types/execute.rs
@@ -7,7 +7,7 @@ use crate::{
     errors::{KclError, KclErrorDetails},
     executor::{
         BodyType, ExecState, ExecutorContext, KclValue, Metadata, Sketch, SourceRange, StatementKind, TagEngineInfo,
-        TagIdentifier, UserVal,
+        TagIdentifier, Tagged, UserVal,
     },
     std::FunctionKind,
 };
@@ -346,8 +346,7 @@ impl CallExpression {
                                         value: tag.name.clone(),
                                         info: Some(TagEngineInfo {
                                             id: value.get_id(),
-                                            surface: Some(value.clone()),
-                                            path: None,
+                                            tagged: Tagged::Surface(value.clone()),
                                             sketch: solid.id,
                                         }),
                                         meta: vec![Metadata {
@@ -364,7 +363,7 @@ impl CallExpression {
                                 };
 
                                 let mut info = info.clone();
-                                info.surface = Some(value.clone());
+                                info.tagged = Tagged::Surface(value.clone());
                                 info.sketch = solid.id;
                                 t.info = Some(info);
 

src/wasm-lib/kcl/src/executor.rs
@@ -1126,10 +1126,49 @@ pub struct TagEngineInfo {
     pub id: uuid::Uuid,
     /// The sketch the tag is on.
     pub sketch: uuid::Uuid,
+    /// The thing which was tagged.
+    #[serde(flatten)]
+    pub tagged: Tagged,
+}
+
+impl TagEngineInfo {
+    /// If this is tagging a path, get it.
+    pub fn path(&self) -> Option<&Path> {
+        self.tagged.path()
+    }
+
+    /// If this is tagging a surface, get it.
+    pub fn surface(&self) -> Option<&ExtrudeSurface> {
+        self.tagged.surface()
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, ts_rs::TS, JsonSchema)]
+#[ts(export)]
+#[serde(rename_all = "camelCase")]
+pub enum Tagged {
     /// The path the tag is on.
-    pub path: Option<Path>,
+    Path(Path),
     /// The surface information for the tag.
-    pub surface: Option<ExtrudeSurface>,
+    Surface(ExtrudeSurface),
+}
+
+impl Tagged {
+    /// If this is a path, get it.
+    fn path(&self) -> Option<&Path> {
+        let Self::Path(x) = &self else {
+            return None;
+        };
+        Some(x)
+    }
+
+    /// If this is a path, get it.
+    fn surface(&self) -> Option<&ExtrudeSurface> {
+        let Self::Surface(x) = &self else {
+            return None;
+        };
+        Some(x)
+    }
 }
 
 /// A sketch is a collection of paths.
@@ -1206,8 +1245,7 @@ impl Sketch {
         tag_identifier.info = Some(TagEngineInfo {
             id: base.geo_meta.id,
             sketch: self.id,
-            path: Some(current_path.clone()),
-            surface: None,
+            tagged: Tagged::Path(current_path.clone()),
         });
 
         self.tags.insert(tag.name.to_string(), tag_identifier);

src/wasm-lib/kcl/src/std/args.rs
@@ -118,7 +118,7 @@ impl Args {
         'e: 'a,
     {
         if let Some(info) = &tag.info {
-            if info.surface.is_some() {
+            if info.surface().is_some() {
                 return Ok(info);
             }
         }
@@ -387,7 +387,7 @@ impl Args {
 
         let engine_info = self.get_tag_engine_info_check_surface(exec_state, tag)?;
 
-        let surface = engine_info.surface.as_ref().ok_or_else(|| {
+        let surface = engine_info.surface().ok_or_else(|| {
             KclError::Type(KclErrorDetails {
                 message: format!("Tag `{}` does not have a surface", tag.value),
                 source_ranges: vec![self.source_range],

src/wasm-lib/kcl/src/std/segment.rs
@@ -35,7 +35,7 @@ pub async fn segment_end_x(exec_state: &mut ExecState, args: Args) -> Result<Kcl
 }]
 fn inner_segment_end_x(tag: &TagIdentifier, exec_state: &mut ExecState, args: Args) -> Result<f64, KclError> {
     let line = args.get_tag_engine_info(exec_state, tag)?;
-    let path = line.path.clone().ok_or_else(|| {
+    let path = line.path().ok_or_else(|| {
         KclError::Type(KclErrorDetails {
             message: format!("Expected a line segment with a path, found `{:?}`", line),
             source_ranges: vec![args.source_range],
@@ -72,7 +72,7 @@ pub async fn segment_end_y(exec_state: &mut ExecState, args: Args) -> Result<Kcl
 }]
 fn inner_segment_end_y(tag: &TagIdentifier, exec_state: &mut ExecState, args: Args) -> Result<f64, KclError> {
     let line = args.get_tag_engine_info(exec_state, tag)?;
-    let path = line.path.clone().ok_or_else(|| {
+    let path = line.path().ok_or_else(|| {
         KclError::Type(KclErrorDetails {
             message: format!("Expected a line segment with a path, found `{:?}`", line),
             source_ranges: vec![args.source_range],
@@ -195,7 +195,7 @@ pub async fn segment_length(exec_state: &mut ExecState, args: Args) -> Result<Kc
 }]
 fn inner_segment_length(tag: &TagIdentifier, exec_state: &mut ExecState, args: Args) -> Result<f64, KclError> {
     let line = args.get_tag_engine_info(exec_state, tag)?;
-    let path = line.path.clone().ok_or_else(|| {
+    let path = line.path().ok_or_else(|| {
         KclError::Type(KclErrorDetails {
             message: format!("Expected a line segment with a path, found `{:?}`", line),
             source_ranges: vec![args.source_range],
@@ -235,7 +235,7 @@ pub async fn segment_angle(exec_state: &mut ExecState, args: Args) -> Result<Kcl
 }]
 fn inner_segment_angle(tag: &TagIdentifier, exec_state: &mut ExecState, args: Args) -> Result<f64, KclError> {
     let line = args.get_tag_engine_info(exec_state, tag)?;
-    let path = line.path.clone().ok_or_else(|| {
+    let path = line.path().ok_or_else(|| {
         KclError::Type(KclErrorDetails {
             message: format!("Expected a line segment with a path, found `{:?}`", line),
             source_ranges: vec![args.source_range],
@@ -279,7 +279,7 @@ fn inner_angle_to_match_length_x(
     args: Args,
 ) -> Result<f64, KclError> {
     let line = args.get_tag_engine_info(exec_state, tag)?;
-    let path = line.path.clone().ok_or_else(|| {
+    let path = line.path().ok_or_else(|| {
         KclError::Type(KclErrorDetails {
             message: format!("Expected a line segment with a path, found `{:?}`", line),
             source_ranges: vec![args.source_range],
@@ -343,7 +343,7 @@ fn inner_angle_to_match_length_y(
     args: Args,
 ) -> Result<f64, KclError> {
     let line = args.get_tag_engine_info(exec_state, tag)?;
-    let path = line.path.clone().ok_or_else(|| {
+    let path = line.path().ok_or_else(|| {
         KclError::Type(KclErrorDetails {
             message: format!("Expected a line segment with a path, found `{:?}`", line),
             source_ranges: vec![args.source_range],

src/wasm-lib/kcl/src/std/sketch.rs
@@ -12,6 +12,7 @@ use parse_display::{Display, FromStr};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+use crate::executor::Tagged;
 use crate::{
     ast::types::TagDeclarator,
     errors::{KclError, KclErrorDetails},
@@ -804,7 +805,7 @@ async fn inner_angled_line_that_intersects(
     args: Args,
 ) -> Result<Sketch, KclError> {
     let intersect_path = args.get_tag_engine_info(exec_state, &data.intersect_tag)?;
-    let path = intersect_path.path.clone().ok_or_else(|| {
+    let path = intersect_path.path().ok_or_else(|| {
         KclError::Type(KclErrorDetails {
             message: format!("Expected an intersect path with a path, found `{:?}`", intersect_path),
             source_ranges: vec![args.source_range],
@@ -1244,10 +1245,9 @@ pub(crate) async fn inner_start_profile_at(
             tag_identifier.info = Some(TagEngineInfo {
                 id: current_path.geo_meta.id,
                 sketch: path_id,
-                path: Some(Path::Base {
+                tagged: Tagged::Path(Path::Base {
                     base: current_path.clone(),
                 }),
-                surface: None,
             });
             HashMap::from([(tag.name.to_string(), tag_identifier)])
         } else {

commit f074f1f66394a0992faf7d8f1aa99293abc3f5c5

KCL test for subset of poop chute
This would have caught the regression in https://github.com/KittyCAD/modeling-app/pull/4333

which had to be reverted in https://github.com/KittyCAD/modeling-app/pull/4339

src/wasm-lib/tests/executor/inputs/poop_chute.kcl
@@ -1,0 +1,58 @@
+wall_thickness = 0.125
+back_walls_width = 2
+front_walls_width = 2.5
+height = 5.5
+filletRadius = 0.050
+back_length = 7
+exit_height = 1
+front_length = 6
+Fx = 0.5
+Fy = 0.5
+
+sketch001 = startSketchOn('-YZ')
+  |> startProfileAt([back_walls_width / 2, 0], %)
+  |> xLine(wall_thickness / 2, %)
+  |> angledLineToX({ angle: 45, to: back_walls_width }, %, $seg01)
+  |> yLineTo(height, %)
+  |> xLine(-wall_thickness, %)
+  |> yLineTo(segEndY(seg01), %)
+  |> angledLineToX({
+       angle: 45,
+       to: back_walls_width / 2 + wall_thickness / 2
+     }, %)
+  |> xLine(-wall_thickness, %)
+  |> angledLineToX({ angle: 180 - 45, to: wall_thickness }, %)
+  |> yLineTo(height, %)
+  |> xLineTo(0, %)
+  |> yLineTo(segEndY(seg01), %)
+  |> angledLineToY({ angle: 180 - 45, to: 0 }, %)
+  |> close(%)
+part001 = revolve({
+  angle: 90,
+  axis: {
+    custom: {
+      axis: [1.0, 0.0],
+      origin: [0.0, height + .0000001]
+    }
+  }
+}, sketch001)
+
+sketch002 = startSketchOn('-YZ')
+  |> startProfileAt([back_walls_width / 2, 0], %)
+  |> xLine(wall_thickness / 2, %)
+  |> angledLineToX({ angle: 45, to: back_walls_width }, %, $seg02)
+  |> yLineTo(height, %)
+  |> xLine(-wall_thickness, %)
+  |> yLineTo(segEndY(seg01), %)
+  |> angledLineToX({
+       angle: 45,
+       to: back_walls_width / 2 + wall_thickness / 2
+     }, %)
+  |> xLine(-wall_thickness, %)
+  |> angledLineToX({ angle: 180 - 45, to: wall_thickness }, %)
+  |> yLineTo(height, %)
+  |> xLineTo(0, %)
+  |> yLineTo(segEndY(seg02), %)
+  |> angledLineToY({ angle: 180 - 45, to: 0 }, %)
+  |> close(%)
+  |> extrude(back_length - height, %)

src/wasm-lib/tests/executor/visuals.rs
@@ -19,6 +19,7 @@ macro_rules! kcl_test {
 }
 
 kcl_test!("sketch_on_face", kcl_test_sketch_on_face);
+kcl_test!("poop_chute", kcl_test_poop_chute);
 kcl_test!("neg_xz_plane", kcl_test_neg_xz_plane);
 kcl_test!("xz_plane", kcl_test_xz_plane);
 kcl_test!(