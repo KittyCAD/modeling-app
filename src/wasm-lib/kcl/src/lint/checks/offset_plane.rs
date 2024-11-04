bb23e0cc9cfcad2b67b4ed54595bbb44591517af
Paul R. Tagliamonte <paul@zoo.dev>
2024-11-04 14:48:33 -0500

    add actual lint warning

--- src/wasm-lib/kcl/src/lint/checks/offset_plane.rs
+++ src/wasm-lib/kcl/src/lint/checks/offset_plane.rs
@@ -1,10 +1,11 @@
-use anyhow::Result;
-
 use crate::{
     ast::types::{BinaryPart, Expr, LiteralValue, ObjectExpression, UnaryOperator},
+    executor::SourceRange,
     lint::rule::{def_finding, Discovered, Finding},
     walk::Node,
 };
+use anyhow::Result;
+use std::collections::HashMap;
 
 def_finding!(
     Z0003,
@@ -47,19 +48,12 @@ pub fn lint_should_be_offset_plane(node: Node) -> Result<Vec<Discovered>> {
         return Ok(vec![]);
     };
 
+    let mut origin: Option<(f64, f64, f64)> = None;
+    let mut x_vec: Option<(f64, f64, f64)> = None;
+    let mut y_vec: Option<(f64, f64, f64)> = None;
+    // let mut z_vec: Option<(f64, f64, f64)> = None;
+
     for property in &plane.inner.properties {
-        let property_name = &property.inner.key.inner.name;
-
-        match property_name.as_str() {
-            "origin" => {}
-            "xAxis" => {}
-            "yAxis" => {}
-            "zAxis" => {}
-            _ => {
-                continue;
-            }
-        };
-
         let Expr::ObjectExpression(ref point) = property.inner.value else {
             return Ok(vec![]);
         };
@@ -68,10 +62,96 @@ pub fn lint_should_be_offset_plane(node: Node) -> Result<Vec<Discovered>> {
             return Ok(vec![]);
         };
 
-        eprintln!("{:?} {} {} {}", property_name, x, y, z);
+        let property_name = &property.inner.key.inner.name;
+
+        match property_name.as_str() {
+            "origin" => origin = Some((x, y, z)),
+            "xAxis" => x_vec = Some((x, y, z)),
+            "yAxis" => y_vec = Some((x, y, z)),
+            // "zAxis" => z_vec = Some((x, y, z)),
+            _ => {
+                continue;
+            }
+        };
     }
 
-    Ok(vec![])
+    let Some(origin) = origin else { return Ok(vec![]) };
+    let Some(x_vec) = x_vec else { return Ok(vec![]) };
+    let Some(y_vec) = y_vec else { return Ok(vec![]) };
+    // let Some(z_vec) = z_vec else { return Ok(vec![]) };
+
+    if [origin.0, origin.1, origin.2].iter().filter(|v| **v == 0.0).count() < 2 {
+        return Ok(vec![]);
+    }
+    // two of the origin values are 0, 0; let's work it out and check
+    // what's **up**
+
+    /// This will attempt to very poorly translate orientation to a letter
+    /// if it's possible to do so. The engine will norm these vectors, so
+    /// we'll just use logic off 0 for now, but this sucks, generally speaking.
+    fn vector_to_letter<'a>(x: f64, y: f64, z: f64) -> Option<&'a str> {
+        if x > 0.0 && y == 0.0 && z == 0.0 {
+            return Some("X");
+        }
+        if x < 0.0 && y == 0.0 && z == 0.0 {
+            return Some("-X");
+        }
+
+        if x == 0.0 && y > 0.0 && z == 0.0 {
+            return Some("Y");
+        }
+        if x == 0.0 && y < 0.0 && z == 0.0 {
+            return Some("-Y");
+        }
+
+        if x == 0.0 && y == 0.0 && z > 0.0 {
+            return Some("Z");
+        }
+        if x == 0.0 && y == 0.0 && z < 0.0 {
+            return Some("-Z");
+        }
+
+        None
+    }
+
+    let allowed_planes = HashMap::from([
+        // allowed built-in planes
+        ("XY".to_owned(), true),
+        ("-XY".to_owned(), true),
+        ("XZ".to_owned(), true),
+        ("-XZ".to_owned(), true),
+        ("YZ".to_owned(), true),
+        ("-YZ".to_owned(), true),
+    ]);
+    // Currently, the engine **ONLY** accepts[1] the following:
+    //
+    // XY
+    // -XY
+    // XZ
+    // -XZ
+    // YZ
+    // -YZ
+    //
+    // [1]: https://zoo.dev/docs/kcl/types/PlaneData
+
+    let plane_name = format!(
+        "{}{}",
+        vector_to_letter(x_vec.0, x_vec.1, x_vec.2).unwrap_or(""),
+        vector_to_letter(y_vec.0, y_vec.1, y_vec.2).unwrap_or(""),
+    );
+
+    if allowed_planes.get(&plane_name).is_none() {
+        return Ok(vec![]);
+    };
+
+    let call_source_range = SourceRange::new(call.start, call.end);
+    Ok(vec![Z0003.at(
+        format!(
+            "custom plane in startSketchOn; offsetPlane from {} would work here",
+            plane_name
+        ),
+        call_source_range,
+    )])
 }
 
 fn get_xyz(point: &ObjectExpression) -> Option<(f64, f64, f64)> {
@@ -122,7 +202,7 @@ fn get_xyz(point: &ObjectExpression) -> Option<(f64, f64, f64)> {
 #[cfg(test)]
 mod tests {
     use super::{lint_should_be_offset_plane, Z0003};
-    use crate::lint::rule::test_finding;
+    use crate::lint::rule::{test_finding, test_no_finding};
 
     test_finding!(
         z0003_bad_sketch_on,
@@ -132,6 +212,22 @@ mod tests {
 startSketchOn({
   plane: {
     origin: { x: 0, y: -14.3, z: 0 },
+    xAxis: { x: 1, y: 0, z: 0 },
+    yAxis: { x: 0, y: 0, z: 1 },
+    zAxis: { x: 0, y: -1, z: 0 }
+  }
+})
+"
+    );
+
+    test_no_finding!(
+        z0003_good_sketch_on,
+        lint_should_be_offset_plane,
+        Z0003,
+        "\
+startSketchOn({
+  plane: {
+    origin: { x: 10, y: -14.3, z: 0 },
     xAxis: { x: 1, y: 0, z: 0 },
     yAxis: { x: 0, y: 0, z: 1 },
     zAxis: { x: 0, y: -1, z: 0 }
