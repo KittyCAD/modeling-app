a4b7dd5182475ba012e9e940754775b29026fde4
49lf <ircsurfer33@gmail.com>
2024-11-11 13:23:51 -0500

    Reapply "Deflake project settings override on desktop (#4370)" (#4450)
    This reverts commit b11040c23c10688de8ef38807a9014d15db5c3e1.

--- e2e/playwright/testing-settings.spec.ts
+++ e2e/playwright/testing-settings.spec.ts
@@ -318,6 +318,7 @@ test.describe('Testing settings', () => {
             timeout: 5_000,
           })
           .toContain(`themeColor = "${userThemeColor}"`)
+        // Only close the button after we've confirmed
       })
 
       await test.step('Set project theme color', async () => {
@@ -344,14 +345,13 @@ test.describe('Testing settings', () => {
       await test.step('Refresh the application and see project setting applied', async () => {
         // Make sure we're done navigating before we reload
         await expect(settingsCloseButton).not.toBeVisible()
-        await page.reload({ waitUntil: 'domcontentloaded' })
 
+        await page.reload({ waitUntil: 'domcontentloaded' })
         await expect(logoLink).toHaveCSS('--primary-hue', projectThemeColor)
       })
 
       await test.step(`Navigate back to the home view and see user setting applied`, async () => {
         await logoLink.click()
-        await page.screenshot({ path: 'out.png' })
         await expect(logoLink).toHaveCSS('--primary-hue', userThemeColor)
       })
 
