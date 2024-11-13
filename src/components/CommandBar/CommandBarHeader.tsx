144a68366e7473a88a0d6c73eb011f714f4e5777
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2024-11-13 21:53:51 +1100

    lint

--- src/components/CommandBar/CommandBarHeader.tsx
+++ src/components/CommandBar/CommandBarHeader.tsx
@@ -4,7 +4,6 @@ import React, { useState } from 'react'
 import { ActionButton } from '../ActionButton'
 import {
   Selections,
-  convertSelectionsToOld,
   getSelectionTypeDisplayText,
 } from 'lib/selections'
 import { useHotkeys } from 'react-hotkeys-hook'
