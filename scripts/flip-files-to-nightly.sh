6f169ab2970c26554ed0334d4149847523e51d37
Pierre Jacquier <pierrejacquier39@gmail.com>
2024-11-22 15:29:54 -0500

    Tag-based release process (#4530)
    * WIP: Improve current nightly implementation
    
    * Add nightly icons
    
    * Clean up script
    
    * Clean up
    
    * Clean up
    
    * Clean up2
    
    * Clean up3
    
    * WIP: Make the release process non-blocking
    First commit to fix #4522
    
    * Quick fix
    
    * Test release and updater-test
    
    * Try nightly build
    
    * WIP nightly tag creation
    
    * Change of direction
    
    * Quick fix
    
    * Test nightly
    
    * Better release notes for nightly
    
    * Migrate to artifact@v4 and prepare publish release
    
    * Fix out-yml for artifact@v4
    
    * Progress towards publish-release
    
    * merge-multiple: true
    
    * Add dependency
    
    * Clean up. Change direction again on updater-test (now with RELEASE workflow)
    
    * Clean up and test release builds
    
    * Fix name
    
    * Clean up, add README
    
    * Add release notes override, another release test
    
    * Fake release version & better draft release
    
    * Test nightly
    
    * A snapshot a day keeps the bugs away! 📷🐛 (OS: windows-latest-8-cores)
    
    * Clean up and first attempt at publish-release-apps
    
    * Add GH_TOKEN
    
    * Add github-token to download-artifact step
    
    * Add github-token to all download-artifact steps
    
    * Add checkout and node steps
    
    * Clean up
    
    * Remove whitespace
    
    * A snapshot a day keeps the bugs away! 📷🐛 (OS: windows-latest-8-cores)
    
    * Quick typo
    
    * Add Kevin's semantic-release.sh script to replace make-release.sh
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- scripts/flip-files-to-nightly.sh
+++ scripts/flip-files-to-nightly.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+export VERSION=$(date +'%-y.%-m.%-d')
+export COMMIT=$(git rev-parse --short HEAD)
+
 # package.json
-VERSION=$(date +'%-y.%-m.%-d') yarn files:set-version
+yarn files:set-version
 echo "$(jq --arg name 'Zoo Modeling App (Nightly)' '.productName=$name' package.json --indent 2)" > package.json
 
 # electron-builder.yml
 yq -i '.publish[0].url = "https://dl.zoo.dev/releases/modeling-app/nightly"' electron-builder.yml
 yq -i '.appId = "dev.zoo.modeling-app-nightly"' electron-builder.yml
+
+# Release notes
+echo "Nightly build $VERSION (commit $COMMIT)" > release-notes.md
 
 # icons
 cp assets/icon-nightly.png assets/icon.png
