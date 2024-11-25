6cfefd86118bec871ea767443e6084991c5d68e6
Jonathan Tran <jonnytran@gmail.com>
2024-11-25 13:07:03 -0500

    Add tangentToEnd stdlib function (#4541)

--- src/wasm-lib/kcl/src/std/segment.rs
+++ src/wasm-lib/kcl/src/std/segment.rs
@@ -2,10 +2,11 @@
 
 use anyhow::Result;
 use derive_docs::stdlib;
+use kittycad_modeling_cmds::shared::Angle;
 
 use crate::{
     errors::{KclError, KclErrorDetails},
-    executor::{ExecState, KclValue, Sketch, TagIdentifier},
+    executor::{ExecState, KclValue, Point2d, Sketch, TagIdentifier},
     std::{utils::between, Args},
 };
 
@@ -409,6 +410,112 @@ fn inner_segment_angle(tag: &TagIdentifier, exec_state: &mut ExecState, args: Ar
     let result = between(path.get_from().into(), path.get_to().into());
 
     Ok(result.to_degrees())
+}
+
+/// Returns the angle coming out of the end of the segment in degrees.
+pub async fn tangent_to_end(exec_state: &mut ExecState, args: Args) -> Result<KclValue, KclError> {
+    let tag: TagIdentifier = args.get_data()?;
+
+    let result = inner_tangent_to_end(&tag, exec_state, args.clone()).await?;
+    Ok(args.make_user_val_from_f64(result))
+}
+
+/// Returns the angle coming out of the end of the segment in degrees.
+///
+/// ```no_run
+/// // Horizontal pill.
+/// pillSketch = startSketchOn('XZ')
+///   |> startProfileAt([0, 0], %)
+///   |> line([20, 0], %)
+///   |> tangentialArcToRelative([0, 10], %, $arc1)
+///   |> angledLine({
+///     angle: tangentToEnd(arc1),
+///     length: 20,
+///   }, %)
+///   |> tangentialArcToRelative([0, -10], %)
+///   |> close(%)
+///
+/// pillExtrude = extrude(10, pillSketch)
+/// ```
+///
+/// ```no_run
+/// // Vertical pill.  Use absolute coordinate for arc.
+/// pillSketch = startSketchOn('XZ')
+///   |> startProfileAt([0, 0], %)
+///   |> line([0, 20], %)
+///   |> tangentialArcTo([10, 20], %, $arc1)
+///   |> angledLine({
+///     angle: tangentToEnd(arc1),
+///     length: 20,
+///   }, %)
+///   |> tangentialArcToRelative([-10, 0], %)
+///   |> close(%)
+///
+/// pillExtrude = extrude(10, pillSketch)
+/// ```
+///
+/// ```no_run
+/// rectangleSketch = startSketchOn('XZ')
+///   |> startProfileAt([0, 0], %)
+///   |> line([10, 0], %, $seg1)
+///   |> angledLine({
+///     angle: tangentToEnd(seg1),
+///     length: 10,
+///   }, %)
+///   |> line([0, 10], %)
+///   |> line([-20, 0], %)
+///   |> close(%)
+///
+/// rectangleExtrude = extrude(10, rectangleSketch)
+/// ```
+///
+/// ```no_run
+/// bottom = startSketchOn("XY")
+///   |> startProfileAt([0, 0], %)
+///   |> arcTo({
+///        end: [10, 10],
+///        interior: [5, 1]
+///      }, %, $arc1)
+///   |> angledLine([tangentToEnd(arc1), 20], %)
+///   |> close(%)
+/// ```
+///
+/// ```no_run
+/// circSketch = startSketchOn("XY")
+///   |> circle({ center: [0, 0], radius: 3 }, %, $circ)
+///
+/// triangleSketch = startSketchOn("XY")
+///   |> startProfileAt([-5, 0], %)
+///   |> angledLine([tangentToEnd(circ), 10], %)
+///   |> line([-15, 0], %)
+///   |> close(%)
+/// ```
+#[stdlib {
+    name = "tangentToEnd",
+}]
+async fn inner_tangent_to_end(tag: &TagIdentifier, exec_state: &mut ExecState, args: Args) -> Result<f64, KclError> {
+    let line = args.get_tag_engine_info(exec_state, tag)?;
+    let path = line.path.clone().ok_or_else(|| {
+        KclError::Type(KclErrorDetails {
+            message: format!("Expected a line segment with a path, found `{:?}`", line),
+            source_ranges: vec![args.source_range],
+        })
+    })?;
+
+    let from = Point2d::from(path.get_to());
+
+    // Undocumented voodoo from get_tangential_arc_to_info
+    let tangent_info = path.get_tangential_info();
+    let tan_previous_point = tangent_info.tan_previous_point(from.into());
+
+    // Calculate the end point from the angle and radius.
+    // atan2 outputs radians.
+    let previous_end_tangent = Angle::from_radians(f64::atan2(
+        from.y - tan_previous_point[1],
+        from.x - tan_previous_point[0],
+    ));
+
+    Ok(previous_end_tangent.to_degrees())
 }
 
 /// Returns the angle to match the given length for x.
