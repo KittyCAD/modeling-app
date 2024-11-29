793b4dd3d9979995f3e525099f60c24914ddf8b6
49lf <ircsurfer33@gmail.com>
2024-11-29 17:44:41 -0500

    Pass app header tests

--- e2e/playwright/app-header-tests.spec.ts
+++ e2e/playwright/app-header-tests.spec.ts
@@ -1,22 +1,11 @@
-import { test, expect } from '@playwright/test'
-
-import { setupElectron, tearDown } from './test-utils'
-
-test.afterEach(async ({ page }, testInfo) => {
-  await tearDown(page, testInfo)
-})
+import { test, expect } from './zoo-test'
 
 test.describe('Electron app header tests', () => {
   test(
     'Open Command Palette button has correct shortcut',
     { tag: '@electron' },
-    async ({ browserName }, testInfo) => {
-      const { electronApp, page } = await setupElectron({
-        testInfo,
-        folderSetupFn: async () => {},
-      })
-
-      await page.setViewportSize({ width: 1200, height: 500 })
+    async ({ page, browserName }, testInfo) => {
+      await page.setBodyDimensions({ width: 1200, height: 500 })
 
       // No space before the shortcut since it checks textContent.
       let text
@@ -34,21 +23,14 @@ test.describe('Electron app header tests', () => {
       const commandsButton = page.getByRole('button', { name: 'Commands' })
       await expect(commandsButton).toBeVisible()
       await expect(commandsButton).toHaveText(text)
-
-      await electronApp.close()
     }
   )
 
   test(
     'User settings has correct shortcut',
     { tag: '@electron' },
-    async ({ browserName }, testInfo) => {
-      const { electronApp, page } = await setupElectron({
-        testInfo,
-        folderSetupFn: async () => {},
-      })
-
-      await page.setViewportSize({ width: 1200, height: 500 })
+    async ({ page, browserName }, testInfo) => {
+      await page.setBodyDimensions({ width: 1200, height: 500 })
 
       // Open the user sidebar menu.
       await page.getByTestId('user-sidebar-toggle').click()
@@ -59,8 +41,6 @@ test.describe('Electron app header tests', () => {
       const userSettingsButton = page.getByTestId('user-settings')
       await expect(userSettingsButton).toBeVisible()
       await expect(userSettingsButton).toHaveText(text)
-
-      await electronApp.close()
     }
   )
 })
