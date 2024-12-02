bed7ae3b8b6e617980177660e639ab2e95b5185a
max <margorskyi@gmail.com>
2024-12-02 21:43:59 +0100

    Refactor addFillet into addEdgeTreatment Function Supporting Chamfers (#4593)
    * refactor code mod and tests
    
    * tsc
    
    * make lint happy
    
    * remove dumby data
    
    Co-authored-by: Kurt Hutten <k.hutten@protonmail.ch>
    
    ---------
    
    Co-authored-by: Kurt Hutten <k.hutten@protonmail.ch>

--- src/components/ModelingMachineProvider.tsx
+++ src/components/ModelingMachineProvider.tsx
@@ -82,7 +82,7 @@ import { getVarNameModal } from 'hooks/useToolbarGuards'
 import { err, reportRejection, trap } from 'lib/trap'
 import { useCommandsContext } from 'hooks/useCommandsContext'
 import { modelingMachineEvent } from 'editor/manager'
-import { hasValidFilletSelection } from 'lang/modifyAst/addFillet'
+import { hasValidEdgeTreatmentSelection } from 'lang/modifyAst/addFillet'
 import {
   ExportIntent,
   EngineConnectionStateType,
@@ -576,8 +576,10 @@ export const ModelingMachineProvider = ({
           if (selectionRanges.graphSelections.length <= 0) return false
           return true
         },
-        'has valid fillet selection': ({ context: { selectionRanges } }) => {
-          return hasValidFilletSelection({
+        'has valid edge treatment selection': ({
+          context: { selectionRanges },
+        }) => {
+          return hasValidEdgeTreatmentSelection({
             selectionRanges,
             ast: kclManager.ast,
             code: codeManager.code,
