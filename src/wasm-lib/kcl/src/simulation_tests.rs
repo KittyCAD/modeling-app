951f65a90dddc2a26735882ea99354e5ff6b52a9
Jonathan Tran <jonnytran@gmail.com>
2024-12-06 18:40:49 -0500

    Add test with tag parameter

--- src/wasm-lib/kcl/src/simulation_tests.rs
+++ src/wasm-lib/kcl/src/simulation_tests.rs
@@ -1502,3 +1502,26 @@ mod kw_fn {
         super::execute(TEST_NAME, true).await
     }
 }
+mod tag_can_be_proxied_through_parameter {
+    const TEST_NAME: &str = "tag_can_be_proxied_through_parameter";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    // TODO: This fails.
+    #[ignore]
+    #[test]
+    fn unparse() {
+        super::unparse(TEST_NAME)
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, false).await
+    }
+}
