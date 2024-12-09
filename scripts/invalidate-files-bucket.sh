e941094f021d7e18cd15109315de64cf28b5eecf
Pierre Jacquier <pierrejacquier39@gmail.com>
2024-12-09 11:45:32 -0500

    Invalidate nightly bucket files after publish (#4627)
    * Invalidate nightly bucket files after publish
    
    * Fix conflict resolution

---
+++ scripts/invalidate-files-bucket.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+base_dir="/releases/modeling-app"
+if [[ $1 = "--nightly" ]]; then
+    base_dir="/releases/modeling-app/nightly"
+fi
+
+echo "Invalidating json and yml files at $base_dir in the download bucket"
+gcloud compute url-maps invalidate-cdn-cache dl-url-map --path="$base_dir/last_download.json" --async
+gcloud compute url-maps invalidate-cdn-cache dl-url-map --path="$base_dir/latest-linux-arm64.yml" --async
+gcloud compute url-maps invalidate-cdn-cache dl-url-map --path="$base_dir/latest-mac.yml" --async
+gcloud compute url-maps invalidate-cdn-cache dl-url-map --path="$base_dir/latest.yml" --async
