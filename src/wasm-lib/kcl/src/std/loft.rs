c8ba36d9abf85ecee5629fa7f94f60f4fc5fdb7d
Adam Chalmers <adam.chalmers@zoo.dev>
2024-12-11 14:55:41 -0600

    Remove LoftData struct

--- src/wasm-lib/kcl/src/std/loft.rs
+++ src/wasm-lib/kcl/src/std/loft.rs
@@ -1,11 +1,11 @@
 //! Standard library lofts.
 
+use std::num::NonZeroU32;
+
 use anyhow::Result;
 use derive_docs::stdlib;
 use kcmc::{each_cmd as mcmd, length_unit::LengthUnit, ModelingCmd};
 use kittycad_modeling_cmds as kcmc;
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
 
 use crate::{
     errors::{KclError, KclErrorDetails},
@@ -15,46 +15,34 @@ use crate::{
 
 const DEFAULT_V_DEGREE: u32 = 2;
 
-/// Data for a loft.
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, ts_rs::TS, JsonSchema)]
-#[ts(export)]
-#[serde(rename_all = "camelCase")]
-pub struct LoftData {
-    /// Degree of the interpolation. Must be greater than zero.
-    /// For example, use 2 for quadratic, or 3 for cubic interpolation in the V direction.
-    /// This defaults to 2, if not specified.
-    pub v_degree: Option<std::num::NonZeroU32>,
-    /// Attempt to approximate rational curves (such as arcs) using a bezier.
-    /// This will remove banding around interpolations between arcs and non-arcs.  It may produce errors in other scenarios
-    /// Over time, this field won't be necessary.
-    #[serde(default)]
-    pub bez_approximate_rational: Option<bool>,
-    /// This can be set to override the automatically determined topological base curve, which is usually the first section encountered.
-    #[serde(default)]
-    pub base_curve_index: Option<u32>,
-    /// Tolerance for the loft operation.
-    #[serde(default)]
-    pub tolerance: Option<f64>,
-}
-
-impl Default for LoftData {
-    fn default() -> Self {
-        Self {
-            // This unwrap is safe because the default value is always greater than zero.
-            v_degree: Some(std::num::NonZeroU32::new(DEFAULT_V_DEGREE).unwrap()),
-            bez_approximate_rational: None,
-            base_curve_index: None,
-            tolerance: None,
-        }
-    }
-}
-
 /// Create a 3D surface or solid by interpolating between two or more sketches.
 pub async fn loft(exec_state: &mut ExecState, args: Args) -> Result<KclValue, KclError> {
     let sketches = args.get_unlabeled_kw_arg("sketches")?;
-    let data: Option<LoftData> = args.get_kw_arg_opt("data");
+    // Degree of the interpolation. Must be greater than zero.
+    // For example, use 2 for quadratic, or 3 for cubic interpolation in the V direction.
+    // This defaults to 2, if not specified.
+    let v_degree: NonZeroU32 = args
+        .get_kw_arg_opt("vDegree")
+        .unwrap_or(NonZeroU32::new(DEFAULT_V_DEGREE).unwrap());
+    // Attempt to approximate rational curves (such as arcs) using a bezier.
+    // This will remove banding around interpolations between arcs and non-arcs.  It may produce errors in other scenarios
+    // Over time, this field won't be necessary.
+    let bez_approximate_rational = args.get_kw_arg_opt("bezApproximateRational").unwrap_or(false);
+    // This can be set to override the automatically determined topological base curve, which is usually the first section encountered.
+    let base_curve_index: Option<u32> = args.get_kw_arg_opt("baseCurveIndex");
+    // Tolerance for the loft operation.
+    let tolerance: Option<f64> = args.get_kw_arg_opt("tolerance");
 
-    let solid = inner_loft(sketches, data, exec_state, args).await?;
+    let solid = inner_loft(
+        sketches,
+        v_degree,
+        bez_approximate_rational,
+        base_curve_index,
+        tolerance,
+        exec_state,
+        args,
+    )
+    .await?;
     Ok(KclValue::Solid(solid))
 }
 
@@ -117,21 +105,12 @@ pub async fn loft(exec_state: &mut ExecState, args: Args) -> Result<KclValue, Kc
 /// circleSketch1 = startSketchOn(offsetPlane('XY', 150))
 ///     |> circle({ center = [0, 100], radius = 20 }, %)
 ///
-/// loft([squareSketch, circleSketch0, circleSketch1], data: {
-///     // This can be set to override the automatically determined
-///     // topological base curve, which is usually the first section encountered.
+/// loft([squareSketch, circleSketch0, circleSketch1],
 ///     baseCurveIndex = 0,
-///     // Attempt to approximate rational curves (such as arcs) using a bezier.
-///     // This will remove banding around interpolations between arcs and non-arcs.
-///     // It may produce errors in other scenarios Over time, this field won't be necessary.
 ///     bezApproximateRational = false,
-///     // Tolerance for the loft operation.
 ///     tolerance = 0.000001,
-///     // Degree of the interpolation. Must be greater than zero.
-///     // For example, use 2 for quadratic, or 3 for cubic interpolation in
-///     // the V direction. This defaults to 2, if not specified.
 ///     vDegree = 2,
-/// })
+/// )
 /// ```
 #[stdlib {
     name = "loft",
@@ -140,7 +119,10 @@ pub async fn loft(exec_state: &mut ExecState, args: Args) -> Result<KclValue, Kc
 }]
 async fn inner_loft(
     sketches: Vec<Sketch>,
-    data: Option<LoftData>,
+    v_degree: NonZeroU32,
+    bez_approximate_rational: bool,
+    base_curve_index: Option<u32>,
+    tolerance: Option<f64>,
     exec_state: &mut ExecState,
     args: Args,
 ) -> Result<Box<Solid>, KclError> {
@@ -155,20 +137,15 @@ async fn inner_loft(
         }));
     }
 
-    // Get the loft data.
-    let data = data.unwrap_or_default();
-
     let id = exec_state.id_generator.next_uuid();
     args.batch_modeling_cmd(
         id,
         ModelingCmd::from(mcmd::Loft {
             section_ids: sketches.iter().map(|group| group.id).collect(),
-            base_curve_index: data.base_curve_index,
-            bez_approximate_rational: data.bez_approximate_rational.unwrap_or(false),
-            tolerance: LengthUnit(data.tolerance.unwrap_or(default_tolerance(&args.ctx.settings.units))),
-            v_degree: data
-                .v_degree
-                .unwrap_or_else(|| std::num::NonZeroU32::new(DEFAULT_V_DEGREE).unwrap()),
+            base_curve_index,
+            bez_approximate_rational,
+            tolerance: LengthUnit(tolerance.unwrap_or(default_tolerance(&args.ctx.settings.units))),
+            v_degree,
         }),
     )
     .await?;
