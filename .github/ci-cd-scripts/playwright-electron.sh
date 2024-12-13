a06234a43f388a292a3ffb3415d6eab90251ee7d
Pierre Jacquier <pierre@zoo.dev>
2024-12-13 07:56:39 -0500

    Hopefully this time fix windows runner detection

--- .github/ci-cd-scripts/playwright-electron.sh
+++ .github/ci-cd-scripts/playwright-electron.sh
@@ -8,7 +8,7 @@ if [[ ! -f "test-results/.last-run.json" ]]; then
     echo "run playwright normally"
         if [[ "$3" == ubuntu-latest* ]]; then
             xvfb-run --auto-servernum --server-args="-screen 0 1280x960x24" -- yarn test:playwright:electron:ubuntu -- --shard=$1/$2 || true
-        elif [[ "$3" == windows-latest* ]]; then
+        elif [[ "$3" == windows* ]]; then
             yarn test:playwright:electron:windows -- --shard=$1/$2 || true
         elif [[ "$3" == macos-14* ]]; then
             yarn test:playwright:electron:macos  -- --shard=$1/$2 || true
