54740eb9e38eb01f7151e033815692e8fa059a37
Matt Mundell <matt@mundell.me>
2024-12-29 22:22:36 +0200

    Start CodeMirror grammar tests

---
+++ packages/codemirror-lang-kcl/test/all.test.ts
@@ -0,0 +1,19 @@
+import { KclLanguage } from '../src/index.ts'
+import { fileTests } from '@lezer/generator/dist/test'
+import { expect, test } from 'vitest'
+
+import * as fs from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+
+let caseDir = path.dirname(fileURLToPath(import.meta.url))
+
+for (let file of fs.readdirSync(caseDir)) {
+  if (!/\.txt$/.test(file)) continue
+
+  let fname = /^[^\.]*/.exec(file)[0]
+  let tests = fileTests(fs.readFileSync(path.join(caseDir, file), 'utf8'), file)
+  describe(fname, () => {
+    for (let { name, run } of tests) it(name, () => run(KclLanguage.parser))
+  })
+}
