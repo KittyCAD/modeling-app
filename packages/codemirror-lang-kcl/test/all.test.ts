650935d420e91f92786463c598f1c9849335718b
Matt Mundell <matt@mundell.me>
2024-12-30 20:34:00 +0200

    Check exec return before access

--- packages/codemirror-lang-kcl/test/all.test.ts
+++ packages/codemirror-lang-kcl/test/all.test.ts
@@ -9,9 +9,11 @@ let caseDir = path.dirname(__filename)
 for (let file of fs.readdirSync(caseDir)) {
   if (!/\.txt$/.test(file)) continue
 
-  let fname = /^[^\.]*/.exec(file)[0]
-  let tests = fileTests(fs.readFileSync(path.join(caseDir, file), 'utf8'), file)
-  describe(fname, () => {
-    for (let { name, run } of tests) it(name, () => run(KclLanguage.parser))
-  })
+  let fname = /^[^\.]*/.exec(file)?.at(0)
+  if (fname) {
+   let tests = fileTests(fs.readFileSync(path.join(caseDir, file), 'utf8'), file)
+   describe(fname, () => {
+     for (let { name, run } of tests) it(name, () => run(KclLanguage.parser))
+   })
+  }
 }
