77c5a3c7582f3322ad1e0f9cff7e0fb7e8dede9f
Matt Mundell <matt@mundell.me>
2024-12-30 23:40:30 +0200

    Build ES and CJS to dist

---
+++ packages/codemirror-lang-kcl/rollup.config.js
@@ -0,0 +1,25 @@
+import dts from 'rollup-plugin-dts'
+import { lezer } from '@lezer/generator/rollup'
+import typescript from '@rollup/plugin-typescript'
+
+export default [
+  {
+    input: 'src/index.ts',
+    // imports are considered internal if they start with './' or '/' or 'word:'
+    external: id => id != 'tslib' && !/^(\.?\/|\w:)/.test(id),
+    output: [
+      { file: 'dist/index.cjs', format: 'cjs' },
+      { file: 'dist/index.js', format: "es" },
+    ],
+    plugins: [ lezer(), typescript() ]
+  },
+  {
+    input: 'src/index.ts',
+    external: id => id != 'tslib' && !/^(\.?\/|\w:)/.test(id),
+    output: [
+      { file: 'dist/index.d.cts', format: "cjs" },
+      { file: 'dist/index.d.ts', format: "es" },
+    ],
+    plugins: [ lezer(), typescript(), dts() ]
+  }
+]
