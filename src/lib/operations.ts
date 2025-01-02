a6395631d7a85f4cb6f3b3dd47f1fd56f000db15
Frank Noirot <frank@kittycad.io>
2025-01-02 17:26:23 -0500

    Start implementing a "prepareToEdit" callback for extrude

--- src/lib/operations.ts
+++ src/lib/operations.ts
@@ -1,23 +1,106 @@
 import { CustomIconName } from 'components/CustomIcon'
+import { Artifact, getArtifactOfTypes } from 'lang/std/artifactGraph'
 import { Operation } from 'wasm-lib/kcl/bindings/Operation'
+import { engineCommandManager, kclManager } from './singletons'
+import { err } from './trap'
+import { getNodePathFromSourceRange } from 'lang/queryAst'
+import { sourceRangeFromRust } from 'lang/wasm'
+import { useCommandsContext } from 'hooks/useCommandsContext'
+import { CommandBarMachineEvent } from 'machines/commandBarMachine'
+
+type ExecuteCommandEvent = CommandBarMachineEvent & {
+  type: 'Find and select command'
+}
+type ExecuteCommandEventPayload = ExecuteCommandEvent['data']
 
 interface StdLibCallInfo {
   label: string
   icon: CustomIconName
+  /**
+   * There are items which are honored by the feature tree
+   * that do not yet have a corresponding modeling command.
+   */
+  prepareToEdit?:
+    | ExecuteCommandEventPayload
+    | ((
+        props: Omit<EnterEditFlowProps, 'onComplete'>
+      ) => ExecuteCommandEventPayload | Promise<ExecuteCommandEventPayload>)
 }
 
 const stdLibMap: Record<string, StdLibCallInfo> = {
   chamfer: {
     label: 'Chamfer',
     icon: 'chamfer3d',
+    // modelingEvent: 'Chamfer',
   },
   extrude: {
     label: 'Extrude',
     icon: 'extrude',
+    prepareToEdit: async ({ item, artifact }) => {
+      const baseCommand = {
+        name: 'Extrude',
+        groupId: 'modeling',
+      }
+      if (!artifact || !('pathId' in artifact) || item.type !== 'StdLibCall')
+        return baseCommand
+      const pathArtifact = getArtifactOfTypes(
+        {
+          key: artifact.pathId,
+          types: ['path'],
+        },
+        engineCommandManager.artifactGraph
+      )
+      if (
+        err(pathArtifact) ||
+        pathArtifact.type !== 'path' ||
+        !pathArtifact.solid2dId
+      )
+        return baseCommand
+      const solid2DArtifact = getArtifactOfTypes(
+        {
+          key: pathArtifact.solid2dId,
+          types: ['solid2D'],
+        },
+        engineCommandManager.artifactGraph
+      )
+      if (err(solid2DArtifact) || solid2DArtifact.type !== 'solid2D') {
+        return baseCommand
+      }
+      const argDefaultValues = {
+        selection: {
+          graphSelections: [
+            {
+              artifact: solid2DArtifact,
+              codeRef: pathArtifact.codeRef,
+            },
+          ],
+          otherSelections: [],
+        },
+        // distance: await useCalculateKclExpression(
+        //   codeManager.code.slice(
+        //     item.labeledArgs?.['length']?.sourceRange[0],
+        //     item.labeledArgs?.['length']?.sourceRange[1]
+        //   )
+        // ),
+        nodeToEdit: getNodePathFromSourceRange(
+          kclManager.ast,
+          sourceRangeFromRust(item.sourceRange)
+        ),
+      }
+      console.log({
+        item: item,
+        argDefaultValues,
+      })
+      return {
+        ...baseCommand,
+        argDefaultValues,
+      }
+    },
   },
   fillet: {
     label: 'Fillet',
     icon: 'fillet3d',
+    // prepareToEdit: 'Fillet',
   },
   hole: {
     label: 'Hole',
@@ -34,10 +117,13 @@ const stdLibMap: Record<string, StdLibCallInfo> = {
   loft: {
     label: 'Loft',
     icon: 'loft',
+    prepareToEdit: () => ({ name: 'Loft', groupId: 'modeling' }),
   },
   offsetPlane: {
     label: 'Offset Plane',
     icon: 'plane',
+    // TODO: Implement prepareToEdit
+    // prepareToEdit: () => ({ type: 'Offset plane', data: { distance: 1 } }),
   },
   patternCircular2d: {
     label: 'Circular Pattern',
@@ -58,14 +144,17 @@ const stdLibMap: Record<string, StdLibCallInfo> = {
   revolve: {
     label: 'Revolve',
     icon: 'revolve',
+    prepareToEdit: () => ({ name: 'Revolve', groupId: 'modeling' }),
   },
   shell: {
     label: 'Shell',
     icon: 'shell',
+    prepareToEdit: () => ({ name: 'Shell', groupId: 'modeling' }),
   },
   startSketchOn: {
     label: 'Sketch',
     icon: 'sketch',
+    prepareToEdit: { name: 'Enter sketch', groupId: 'modeling' },
   },
   sweep: {
     label: 'Sweep',
@@ -177,4 +266,43 @@ function isNotUserFunctionWithNoOperations(
  */
 function isNotUserFunctionReturn(ops: Operation[]): Operation[] {
   return ops.filter((op) => op.type !== 'UserDefinedFunctionReturn')
+}
+
+interface EnterEditFlowProps {
+  item: Operation
+  artifact?: Artifact
+  onComplete: ReturnType<typeof useCommandsContext>['commandBarSend']
+}
+
+export async function enterEditFlow({
+  item,
+  artifact,
+  onComplete,
+}: EnterEditFlowProps) {
+  if (item.type !== 'StdLibCall') return
+
+  const stdLibInfo = stdLibMap[item.name]
+
+  if (stdLibInfo && stdLibInfo.prepareToEdit) {
+    if (typeof stdLibInfo.prepareToEdit === 'function') {
+      const eventPayload = await stdLibInfo.prepareToEdit({
+        item,
+        artifact,
+      })
+      console.log('', {
+        item,
+        artifact,
+        eventPayload,
+      })
+      onComplete({
+        type: 'Find and select command',
+        data: eventPayload,
+      })
+    } else {
+      onComplete({
+        type: 'Find and select command',
+        data: stdLibInfo.prepareToEdit,
+      })
+    }
+  }
 }
