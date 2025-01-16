5f4e389be6c1c048f3cd603062c9e7454ef7b63d
Jonathan Tran <jonnytran@gmail.com>
2025-01-16 17:22:02 -0500

    Update doc comment for the website

--- src/wasm-lib/kcl/src/source_range.rs
+++ src/wasm-lib/kcl/src/source_range.rs
@@ -25,9 +25,11 @@ impl ModuleId {
 
 /// The first two items are the start and end points (byte offsets from the start of the file).
 /// The third item is whether the source range belongs to the 'main' file, i.e., the file currently
-/// being rendered/displayed in the editor (TODO we need to handle modules better in the frontend).
-///
-/// @see {isTopLevelModule}
+/// being rendered/displayed in the editor.
+//
+// Don't use a doc comment for the below since the above goes in the website docs.
+// @see isTopLevelModule() in wasm.ts.
+// TODO we need to handle modules better in the frontend.
 #[derive(Debug, Default, Deserialize, Serialize, PartialEq, Copy, Clone, ts_rs::TS, JsonSchema, Hash, Eq)]
 #[ts(export, type = "[number, number, number]")]
 pub struct SourceRange([usize; 3]);
