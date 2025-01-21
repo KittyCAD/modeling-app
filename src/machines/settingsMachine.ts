53b534ca676b739e617082819ae8866c2ea2a9c2
Frank Noirot <frank@kittycad.io>
2025-01-21 14:42:57 -0500

    Don't provide anything to settingsMachine from React

--- src/machines/settingsMachine.ts
+++ src/machines/settingsMachine.ts
@@ -1,5 +1,10 @@
-import { assign, setup } from 'xstate'
-import { Themes, getSystemTheme, setThemeClass } from 'lib/theme'
+import { assign, fromPromise, setup } from 'xstate'
+import {
+  Themes,
+  getOppositeTheme,
+  getSystemTheme,
+  setThemeClass,
+} from 'lib/theme'
 import { createSettings, settings } from 'lib/settings/initialSettings'
 import {
   BaseUnit,
@@ -11,14 +16,26 @@ import {
 import {
   configurationToSettingsPayload,
   projectConfigurationToSettingsPayload,
+  saveSettings,
   setSettingsAtLevel,
 } from 'lib/settings/settingsUtils'
-import { sceneInfra } from 'lib/singletons'
+import {
+  codeManager,
+  engineCommandManager,
+  kclManager,
+  sceneEntitiesManager,
+  sceneInfra,
+} from 'lib/singletons'
+import toast from 'react-hot-toast'
+import decamelize from 'decamelize'
+import { reportRejection } from 'lib/trap'
+
+type SettingsMachineContext = ReturnType<typeof createSettings>
 
 export const settingsMachine = setup({
   types: {
-    context: {} as ReturnType<typeof createSettings>,
-    input: {} as ReturnType<typeof createSettings>,
+    context: {} as SettingsMachineContext,
+    input: {} as SettingsMachineContext,
     events: {} as (
       | WildcardSetEvent<SettingsPaths>
       | SetEventTypes
@@ -37,14 +54,106 @@ export const settingsMachine = setup({
       | { type: 'Set all settings'; settings: typeof settings }
     ) & { doNotPersist?: boolean },
   },
+  actors: {
+    persistSettings: fromPromise<void, { doNotPersist: boolean, context: SettingsMachineContext }>(async ({ input, self }) => {
+      // Without this, when a user changes the file, it'd
+      // create a detection loop with the file-system watcher.
+      if (input.doNotPersist) return
+
+      codeManager.writeCausedByAppCheckedInFileTreeFileSystemWatcher = true
+
+      // If the machine is not spawned with an appMachine,
+      // it will only persist user settings.
+      const parentActor = self._parent?.getSnapshot()
+      const currentProjectFromParent = parentActor?.context?.currentProject
+
+      return saveSettings(input.context, currentProjectFromParent)
+    }),
+  },
   actions: {
-    setEngineTheme: () => {},
-    setClientTheme: () => {},
-    'Execute AST': () => {},
-    toastSuccess: () => {},
-    setClientSideSceneUnits: () => {},
-    setAllowOrbitInSketchMode: () => {},
-    persistSettings: () => {},
+    setClientSideSceneUnits: ({ context, event }) => {
+      const newBaseUnit =
+        event.type === 'set.modeling.defaultUnit'
+          ? (event.data.value as BaseUnit)
+          : context.modeling.defaultUnit.current
+      sceneInfra.baseUnit = newBaseUnit
+    },
+    setEngineTheme: ({ context }) => {
+      engineCommandManager
+        .setTheme(context.app.theme.current)
+        .catch(reportRejection)
+    },
+    setClientTheme: ({ context }) => {
+      const opposingTheme = getOppositeTheme(context.app.theme.current)
+      sceneInfra.theme = opposingTheme
+      sceneEntitiesManager.updateSegmentBaseColor(opposingTheme)
+    },
+    setAllowOrbitInSketchMode: ({ context }) => {
+      sceneInfra.camControls._setting_allowOrbitInSketchMode =
+        context.app.allowOrbitInSketchMode.current
+      // ModelingMachineProvider will do a use effect to trigger the camera engine sync
+    },
+    toastSuccess: ({ event }) => {
+      if (!('data' in event)) return
+      const eventParts = event.type.replace(/^set./, '').split('.') as [
+        keyof typeof settings,
+        string
+      ]
+      const truncatedNewValue = event.data.value?.toString().slice(0, 28)
+      const message =
+        `Set ${decamelize(eventParts[1], { separator: ' ' })}` +
+        (truncatedNewValue
+          ? ` to "${truncatedNewValue}${
+              truncatedNewValue.length === 28 ? '...' : ''
+            }"${
+              event.data.level === 'project'
+                ? ' for this project'
+                : ' as a user default'
+            }`
+          : '')
+      toast.success(message, {
+        duration: message.split(' ').length * 100 + 1500,
+        id: `${event.type}.success`,
+      })
+    },
+    'Execute AST': ({ context, event }) => {
+      try {
+        const relevantSetting = (s: typeof settings) => {
+          return (
+            s.modeling?.defaultUnit?.current !==
+              context.modeling.defaultUnit.current ||
+            s.modeling.showScaleGrid.current !==
+              context.modeling.showScaleGrid.current ||
+            s.modeling?.highlightEdges.current !==
+              context.modeling.highlightEdges.current
+          )
+        }
+
+        const allSettingsIncludesUnitChange =
+          event.type === 'Set all settings' && relevantSetting(event.settings)
+        const resetSettingsIncludesUnitChange =
+          event.type === 'Reset settings' && relevantSetting(settings)
+
+        if (
+          event.type === 'set.modeling.defaultUnit' ||
+          event.type === 'set.modeling.showScaleGrid' ||
+          event.type === 'set.modeling.highlightEdges' ||
+          allSettingsIncludesUnitChange ||
+          resetSettingsIncludesUnitChange
+        ) {
+          // Unit changes requires a re-exec of code
+          // eslint-disable-next-line @typescript-eslint/no-floating-promises
+          kclManager.executeCode(true)
+        } else {
+          // For any future logging we'd like to do
+          // console.log(
+          //   'Not re-executing AST because the settings change did not affect the code interpretation'
+          // )
+        }
+      } catch (e) {
+        console.error('Error executing AST after settings change', e)
+      }
+    },
     resetSettings: assign(({ context, event }) => {
       if (!('level' in event)) return {}
 
@@ -96,7 +205,7 @@ export const settingsMachine = setup({
     },
   },
 }).createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QGUwBc0EsB2VYDpMIAbMAYlnXwEMAHW-Ae2wCNHqAnCHKZNatAFdYAbQAMAXUShajWJizNpIAB6IALAFYAnPgBMARgDsBsQDY969QGYjmzQBoQAT0SnrADnwePY61r0PAwNtMyMAX3CnVAweAiJSCio6BjQACzAAWzAAYUZiRg5xKSQQWXlFbGU1BD1PfFtfE3UzTUNNaydXBCD1b209PTEPTTMtdQNNSOj0LFx4knJKNHxMxggwYh58DYAzakFiNABVbAVi5XKFTCVSmusxPXx7bRt1DzMxI3UjD3UutwhAz4MyeHxiV5+AYRKIgGJzPCERZJFYpfDpLJgC6lK6VaqIExPMwWGwdGxBPRmAE9PSafCPMQ-EzWbQ6ELTOGzOJIxLLVbrTbbNKYKBpLaitAAUWgcGxMjk11uoBqVmBH0ZLKCrVs-xciCCwLCvhCjyMFhGHPh3IS5AASnB0AACZYI0SSS4KvF3AlafADRl1YZ2IxiRx6hBtIzPb7abQ+DxGaxmYKWrnzHnkGKO6jEYjOtN4OVlT03KrehAtOnm7Qaup6Ixm6mR6OaR4dAwjM1mVOxdM2lH8jZbXD4WBpRgAd2QAGMc2AAOIcIhF3Gl-EIRPA6yGcyh4whSnU0xGJ5GAat0OfFowma9xH9gBUK5LStUiECdMmfx+mg8hmNTY-PgMYQpoZoxh41g9q6+C0GAHDyLACL5nesBkBAzBgIQ2AAG6MAA1lhcEIZgSFWvMz4VGu5YALTbtYwEnj8HhxnooT1mG3QhmY-TmJ82gGCyjzaJEsLYAK8ClOReAelRr41HRJiMZYvysexdjUuohh+poBiGDuXzGKy0HWossmKmWyqIDR3zAZWLSahM2jWJ04YjDxHbDMmmhaYE3wmemxGIchLpxOZXpWQgNEjMB1h6WEYHqK8ZgJk2EL6N8wR1Cy-gJqJ4RAA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QGUwBc0EsB2VYDpMIAbMAYlnXwEMAHW-Ae2wCNHqAnCHKZNatAFdYAbQAMAXUShajWJizNpIAB6IAzAA4x+AIyaAbJoCsAFl1njAJmOaANCACeiXQHZ1+a7bdWDATnUxawBfYIdUDB4CIlIKKjoGNAALMABbMABhRmJGDnEpJBBZeUVsZTUELR19IzMLUy97J0RfTXxDBr8DAxtdMSs-UPD0LFxoknJKNHxUxggwYh58eYAzakFiNABVbAV85WKFTCVCiqq9QxNzSxsm50qDU3wrMXV1V2M-bT8xV6GQCKjPCECZxaYJfDJNJgfaFQ6lcoabQXWrXBq3Bz3YzqPz4AyvL7qYw1TS6Az-QFREGxKY0ej4WBoDhgaipACSEwAsnMYZIDnIjidQGdkSS6jdbJjEK5zHi-PouqYiQZjBSRlSYpN4vTqMQcgB3ADyHBYCjZ2GQAGt0ABjJLc+awmQChGnJHVS7i9GS5oIQweVxueVWVz4mW6NWRMbUrXTWbzRa4fA21lgDjUAAKHEYACswDbSk6ii7jmU3ZVRZ60Y0pQg+rorPglQ2rKZ-FY3DLI0DxjSqPGFkskpgoElFqO0ABRaBwIvw0uIise1H1Gu+3Rk3R6Uydaz47qqsIA9XRzVkABKcHQAAIpj25yWhap3SirquMevAriTK4urYBhYViaN2GqghE166sQt4nngD4lAu5Zkni1yaKG2i6OoBgblYtY7sYniGNYmjqKYmjyropggaeoK0gOiZQAySSMPqyApqQADiHBEHBgplsKL5itWH73BYKr4OoLzmBhHahlRwJngAVDxrr8Uur5emuImBk8rzvKSZH9KYrjAUelLRrQabyIyPDQVGeBkBAzBgIQ2AAG6MNa+BmcCFkcFZQK2T2CA4O5KaFpIykIapRm4iRQQ7u48p+EZpi1vobz4EEfifEqv4kSZwx2QQvn+TZd5RGQabZhw+C0MQAgrLkqReTBxWWZg1m4IFUTBW5jBhaW+SRU+FSkToRhiFhBjvEEPr3KSFyuCGnxmHhJGhEe2A8vAhTeTtzrwSNiAALTqFuRKaOYVhnV802aPdtbHQYmW-L8xiuIGl1oTYcm9mA-KHXxz4IEZtaoc9fjyj+PSQ62Eama1tXtZ1UDdWMAO8YuZFPBRDRYTKxifFhYOQ3i9QHl8phBPDoRAA */
   id: 'Settings',
   initial: 'idle',
   context: ({ input }) => {
@@ -217,8 +326,24 @@ export const settingsMachine = setup({
     },
 
     'persisting settings': {
-      entry: ['persistSettings'],
-      always: 'idle',
-    },
+      invoke: {
+        src:'persistSettings',
+        onDone: {
+          target: 'idle',
+        },
+        onError: {
+          target: 'idle',
+          actions: () => {
+            console.error('Error persisting settings')
+          },
+        },
+        input: ({ context, event }) => {
+          return {
+            doNotPersist: event.doNotPersist ?? false,
+            context,
+          }
+        },
+      }
+    }
   },
 })
