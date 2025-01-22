3ad59f83aa77d0ea1972008936bdfd2ac046be11
Frank Noirot <frank@kittycad.io>
2025-01-22 12:26:32 -0500

    Remove unnecessary use of Jest settings wrappers

--- src/components/NetworkHealthIndicator.test.tsx
+++ src/components/NetworkHealthIndicator.test.tsx
@@ -1,6 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
-import { SettingsAuthProviderJest } from './SettingsAuthProvider'
 import {
   NETWORK_HEALTH_TEXT,
   NetworkHealthIndicator,
@@ -9,11 +8,7 @@ import { NetworkHealthState } from 'hooks/useNetworkStatus'
 
 function TestWrap({ children }: { children: React.ReactNode }) {
   // wrap in router and xState context
-  return (
-    <BrowserRouter>
-      <SettingsAuthProviderJest>{children}</SettingsAuthProviderJest>
-    </BrowserRouter>
-  )
+  return <BrowserRouter>{children}</BrowserRouter>
 }
 
 // Our Playwright tests for this are much more comprehensive.
