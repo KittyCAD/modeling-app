f36b027074c1de238f0cd905148a75616ca04d53
Jonathan Tran <jonnytran@gmail.com>
2025-01-29 20:11:00 -0500

    Add updating the artifact graph after mock execution

--- src/lang/KclSingleton.ts
+++ src/lang/KclSingleton.ts
@@ -484,6 +484,7 @@ export class KclManager {
       this.lastSuccessfulVariables = execState.variables
       this.lastSuccessfulOperations = execState.operations
     }
+    this.engineCommandManager.updateArtifactGraph(execState.artifactGraph)
   }
   cancelAllExecutions() {
     this._cancelTokens.forEach((_, key) => {
