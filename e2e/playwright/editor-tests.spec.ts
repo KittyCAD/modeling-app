d1417fd4a54ffbb2c49ee84054052f725542eff9
Pierre Jacquier <pierre@zoo.dev>
2025-02-04 14:23:20 -0500

    More skips

--- e2e/playwright/editor-tests.spec.ts
+++ e2e/playwright/editor-tests.spec.ts
@@ -515,6 +515,7 @@ test.describe('Editor tests', () => {
     page,
     homePage,
   }) => {
+    test.skip(process.platform === 'win32', 'Skip on windows')
     const u = await getUtils(page)
     await page.addInitScript(async () => {
       localStorage.setItem(
