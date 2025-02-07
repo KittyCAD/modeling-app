2e67b3bd0e23e388996f3a4f7c60c4e78e002f88
49lf <ircsurfer33@gmail.com>
2025-02-07 17:03:09 -0500

    Add back stream idle mode

--- src/components/ModelingMachineProvider.tsx
+++ src/components/ModelingMachineProvider.tsx
@@ -21,7 +21,6 @@ import {
   modelingMachine,
   modelingMachineDefaultContext,
 } from 'machines/modelingMachine'
-import { useSetupEngineManager } from 'hooks/useSetupEngineManager'
 import { useSettingsAuthContext } from 'hooks/useSettingsAuthContext'
 import {
   isCursorInSketchCommandRange,
@@ -113,13 +112,12 @@ export const ModelingMachineProvider = ({
   const {
     settings: {
       context: {
-        app: { theme, enableSSAO, allowOrbitInSketchMode },
+        app: { theme, allowOrbitInSketchMode },
         modeling: {
           defaultUnit,
-          cameraProjection,
           highlightEdges,
-          showScaleGrid,
           cameraOrbit,
+          cameraProjection,
         },
       },
     },
@@ -139,6 +137,7 @@ export const ModelingMachineProvider = ({
     commandBarActor,
     commandBarIsClosedSelector
   )
+
   // Settings machine setup
   // const retrievedSettings = useRef(
   // localStorage?.getItem(MODELING_PERSIST_KEY) || '{}'
@@ -1142,22 +1141,6 @@ export const ModelingMachineProvider = ({
       },
       // devTools: true,
     }
-  )
-
-  useSetupEngineManager(
-    streamRef,
-    modelingSend,
-    modelingState.context,
-    {
-      pool: pool,
-      theme: theme.current,
-      highlightEdges: highlightEdges.current,
-      enableSSAO: enableSSAO.current,
-      showScaleGrid: showScaleGrid.current,
-      cameraProjection: cameraProjection.current,
-      cameraOrbit: cameraOrbit.current,
-    },
-    token
   )
 
   useEffect(() => {
