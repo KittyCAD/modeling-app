1bb372b642214879f984eecec60680e980acfbd0
Jonathan Tran <jonnytran@gmail.com>
2025-02-10 14:24:00 -0500

    Add helix to the artifact graph (#5317)
    * Add helix simulation test
    
    * Update output for new test
    
    * Add helix to the artifact graph
    
    * Update output since adding helix to the graph
    
    * Fix helix selection-based deletion (#5323)
    
    * Fix helix selection-based deletion
    
    * Run e2e tests on all PRs
    
    * A snapshot a day keeps the bugs away! 📷🐛 (OS: namespace-profile-ubuntu-8-cores)
    
    * A snapshot a day keeps the bugs away! 📷🐛 (OS: namespace-profile-ubuntu-8-cores)
    
    * A snapshot a day keeps the bugs away! 📷🐛 (OS: namespace-profile-ubuntu-8-cores)
    
    * A snapshot a day keeps the bugs away! 📷🐛 (OS: namespace-profile-ubuntu-8-cores)
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Pierre Jacquier <pierrejacquier39@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/wasm-lib/kcl/src/simulation_tests.rs
+++ src/wasm-lib/kcl/src/simulation_tests.rs
@@ -1943,3 +1943,24 @@ mod array_elem_pop_fail {
         super::execute(TEST_NAME, false).await
     }
 }
+mod helix_simple {
+    const TEST_NAME: &str = "helix_simple";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[test]
+    fn unparse() {
+        super::unparse(TEST_NAME)
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
