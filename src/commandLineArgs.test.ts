ce585b7725b8d6ce6463272e9cc7e4b9883538bb
Pierre Jacquier <pierre@zoo.dev>
2025-02-19 06:27:18 -0500

    Add mac tests

--- src/commandLineArgs.test.ts
+++ src/commandLineArgs.test.ts
@@ -20,14 +20,33 @@ const linuxSecondInstanceDeepLinkArgv = [
   'zoo-studio://?create-file=true&name=bloobloo&code=c2tldGNoMDAxID0gc3RhcnRTa2V0Y2hPbignWFonKQogIHw%2BIHN0YXJ0UHJvZmlsZUF0KFs1MS4zNiwgMjA3LjQxXSwgJSkKICB8PiBsaW5lKGVuZCA9IFsxODguOTcsIC0xMTEuMjhdKQogIHw%2BIGxpbmUoZW5kID0gWy02Ny44MiwgLTIyNy4xNl0pCiAgfD4gbGluZShlbmQgPSBbLTEyNS4xLCA0Mi4xNF0pCiAgfD4gbGluZShlbmQgPSBbMjAuNDEsIDk4Ljc3XSkKICB8PiBsaW5lKGVuZCA9IFs3My4wOSwgLTY1Ljg1XSkKICB8PiBsaW5lKGVuZCA9IFs0NC43NywgNzYuMzhdKQogIHw%2BIGxpbmUoZW5kID0gWy05LjIyLCA1Ny45NV0pCiAgfD4geExpbmUoLTExMC42MSwgJSkKICB8PiBsaW5lKGVuZEFic29sdXRlID0gWzAsIDE0My41NF0pCiAgfD4gbGluZShlbmQgPSBbNDcuNDEsIDcuOV0pCiAgfD4gbGluZShlbmQgPSBbLTI1LjAyLCA0Mi44XSkKICB8PiBsaW5lKGVuZEFic29sdXRlID0gW3Byb2ZpbGVTdGFydFgoJSksIHByb2ZpbGVTdGFydFkoJSldKQogIHw%2BIGNsb3NlKCkKZXh0cnVkZTAwMSA9IGV4dHJ1ZGUoc2tldGNoMDAxLCBsZW5ndGggPSA1MCkKc2tldGNoMDAyID0gc3RhcnRTa2V0Y2hPbihleHRydWRlMDAxLCAnRU5EJykKICB8PiBjaXJjbGUoewogICAgICAgY2VudGVyID0gWzEwMS41MSwgMTM1LjY2XSwKICAgICAgIHJhZGl1cyA9IDI0LjM2CiAgICAgfSwgJSkKZXh0cnVkZTAwMiA9IGV4dHJ1ZGUoc2tldGNoMDAyLCBsZW5ndGggPSA1KQ%3D%3D'
 ]
 
+const macColdStartNoPathArgv = [
+  '/Applications/Zoo Modeling App.app/Contents/MacOS/Zoo Modeling App',
+]
+
+const macColdStartPathArgv = [
+  '/Applications/Zoo Modeling App.app/Contents/MacOS/Zoo Modeling App',
+  '/Users/pierremtb/Documents/zoo-modeling-app-projects/loft/main.kcl'
+]
+
 describe('getPathOrUrlFromArgs', () => {
   it('should parse linux second-instance path arg', () => {
     const args = parseCLIArgs(linuxSecondInstancePathArgv)
     expect(getPathOrUrlFromArgs(args)).toContain('main.kcl')
   })
 
+  it('should parse mac cold start path arg', () => {
+    const args = parseCLIArgs(macColdStartPathArgv)
+    expect(getPathOrUrlFromArgs(args)).toContain('main.kcl')
+  })
+
   it('should return undefined on linux cold start without path arg', () => {
     const args = parseCLIArgs(linuxColdStartNoPathArgv)
+    expect(getPathOrUrlFromArgs(args)).toBeUndefined()
+  })
+
+  it('should return undefined on mac cold start without path arg', () => {
+    const args = parseCLIArgs(macColdStartNoPathArgv)
     expect(getPathOrUrlFromArgs(args)).toBeUndefined()
   })
 
