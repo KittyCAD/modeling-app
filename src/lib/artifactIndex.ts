4b300cc24f1d5ce3707286b151c21bd040db2139
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2025-02-21 16:25:18 +1100

    better comments

--- src/lib/artifactIndex.ts
+++ src/lib/artifactIndex.ts
@@ -3,11 +3,14 @@ import { getFaceCodeRef } from 'lang/std/artifactGraph'
 
 // Index artifacts in an ordered list for binary search
 export type ArtifactEntry = { artifact: Artifact; id: ArtifactId }
+/** Index artifacts by their codeRef range, ordered by start position */
 export type ArtifactIndex = Array<{
   range: SourceRange
   entry: ArtifactEntry
 }>
 
+/** Creates an array of artifacts, only those with codeRefs, orders them by start range,
+ * to be used later by binary search */
 export function buildArtifactIndex(
   artifactGraph: ArtifactGraph
 ): ArtifactIndex {
