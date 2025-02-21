1a74aea879587a73665e778c6ab9e956b5e927b9
Pierre Jacquier <pierre@zoo.dev>
2025-02-21 10:09:07 -0500

    Fix flip-files-to-nightly after PS7 upgrade

--- scripts/flip-files-to-nightly.ps1
+++ scripts/flip-files-to-nightly.ps1
@@ -1,16 +1,16 @@
-$env:VERSION=(Get-Date -Format "yy.M.d")
-$env:COMMIT=$(git rev-parse --short HEAD)
-$env:PRODUCT_NAME="Zoo Modeling App (Nightly)"
+$VERSION=$(Get-Date -Format "yy.M.d")
+$COMMIT=$(git rev-parse --short HEAD)
+$PRODUCT_NAME="Zoo Modeling App (Nightly)"
 
 # package.json
 yq -i '.version = env(VERSION)' -p=json -o=json package.json
 yq -i '.productName = env(PRODUCT_NAME)' -p=json -o=json package.json
-yq -i '.name = ""zoo-modeling-app-nightly""' -p=json -o=json package.json
+yq -i '.name = "zoo-modeling-app-nightly"' -p=json -o=json package.json
 
 # electron-builder.yml
-yq -i '.publish[0].url = ""https://dl.zoo.dev/releases/modeling-app/nightly""' electron-builder.yml
-yq -i '.appId = ""dev.zoo.modeling-app-nightly""' electron-builder.yml
-yq -i '.nsis.include = ""./scripts/installer-nightly.nsh""' electron-builder.yml
+yq -i '.publish[0].url = "https://dl.zoo.dev/releases/modeling-app/nightly"' electron-builder.yml
+yq -i '.appId = "dev.zoo.modeling-app-nightly"' electron-builder.yml
+yq -i '.nsis.include = "./scripts/installer-nightly.nsh"' electron-builder.yml
 
 # Release notes
 echo "Nightly build $VERSION (commit $COMMIT)" > release-notes.md
