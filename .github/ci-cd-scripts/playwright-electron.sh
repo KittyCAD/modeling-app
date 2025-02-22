388371b05d37f9caf9b92c78a2c8d14d824adaf7
Pierre Jacquier <pierrejacquier39@gmail.com>
2025-02-22 12:59:19 -0500

    External retry of the playwright step instead of --last-failed (#5465)
    * External retry
    
    * Trigger CI
    
    * More external attemps, less internal retries
    
    * Trigger CI
    
    * Trigger CI
    
    * Clean up for PR
    
    * Add --retries=2 to cargo test

--- .github/ci-cd-scripts/playwright-electron.sh
+++ .github/ci-cd-scripts/playwright-electron.sh
@@ -21,7 +21,7 @@ if [[ ! -f "test-results/.last-run.json" ]]; then
 fi
 
 retry=1
-max_retrys=5
+max_retrys=1
 
 # retry failed tests, doing our own retries because using inbuilt playwright retries causes connection issues
 while [[ $retry -le $max_retrys ]]; do
