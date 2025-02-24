80c0e8b7315a8ce9c85d4c162a051fb92f9b8864
Pierre Jacquier <pierre@zoo.dev>
2025-02-24 13:30:08 -0500

    Update with @franknoirot's headers, project based now

--- scripts/get-closed-issues-since-last-meeting.sh
+++ scripts/get-closed-issues-since-last-meeting.sh
@@ -3,7 +3,9 @@
 # Usage: ./scripts/get-closed-issues-since-last-meeting.sh [LAST_MEETING_DATE_ISO_8601]
 
 last_meeting_date=${1:-'2025-02-10'}
-filter="milestone:\"v1 Modeling App Launch\""
+milestone="milestone:\"v1 Modeling App Launch\""
+extermination_project="project:KittyCAD/34"
+features_project="project:KittyCAD/35"
 repo="kittycad/modeling-app"
 limit=999
 
@@ -17,17 +19,20 @@ function list_from_ids() {
 echo "Closed issues on $repo since $last_meeting_date"
 echo
 
-closed_bug_ids=$(gh issue list --repo $repo --limit $limit --search "type:Bug sort:closed-asc closed:>$last_meeting_date $filter" --json number | jq -r '.[].number')
-echo "### Bugs"
+closed_bug_ids=$(gh issue list --repo $repo --limit $limit --search "sort:closed-asc closed:>$last_meeting_date $extermination_project" --json number | jq -r '.[].number')
+echo "### Tracked bugs completed"
+echo "Bugs closed that were on our Extermination Station project:"
 list_from_ids "$closed_bug_ids"
 echo
 
-closed_enhancement_ids=$(gh issue list --repo $repo --limit $limit --search "type:Enhancement sort:closed-asc closed:>$last_meeting_date $filter" --json number | jq -r '.[].number') 
-echo "### Enhancements"
-list_from_ids "$closed_enhancement_ids"
+closed_enchancement_ids=$(gh issue list --repo $repo --limit $limit --search "sort:closed-asc closed:>$last_meeting_date $features_project" --json number | jq -r '.[].number') 
+echo "### Tracked enhancements completed"
+echo "Feature issues closed that were on our V1 Battle Plan project:"
+list_from_ids "$closed_enchancement_ids"
 echo
 
-closed_other_ids=$(gh issue list --repo $repo --limit $limit --search "no:type sort:closed-asc closed:>$last_meeting_date $filter" --json number | jq -r '.[].number') 
-echo "### Other"
+closed_other_ids=$(gh issue list --repo $repo --limit $limit --search "sort:closed-asc closed:>$last_meeting_date no:project" --json number | jq -r '.[].number') 
+echo "### Other and untracked"
+echo "Other notable work that wasn't tracked in either of those places:"
 list_from_ids "$closed_other_ids"
 echo
