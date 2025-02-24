f8218d1bf12a59114ee5f14fbce5197fe7627b02
Pierre Jacquier <pierre@zoo.dev>
2025-02-24 12:19:48 -0500

    Fix script

--- scripts/get-closed-issues-since-last-meeting.sh
+++ scripts/get-closed-issues-since-last-meeting.sh
@@ -2,7 +2,7 @@
 
 # Usage: echo "$(scripts/get-changelog-from-releases.sh)" > CHANGELOG.md
 
-last_meeting_date=${1:-'2024-02-10'}
+last_meeting_date=${1:-'2025-02-10'}
 filter="milestone:\"v1 Modeling App Launch\""
 repo="kittycad/modeling-app"
 limit=999
@@ -17,12 +17,17 @@ function list_from_ids() {
 echo "Closed issues on $repo since $last_meeting_date"
 echo
 
-closed_bug_ids=$(gh issue list --repo $repo --limit $limit --search "type:bug sort:closed-asc closed:>$last_meeting_date $filter" --json number | jq -r '.[].number')
+closed_bug_ids=$(gh issue list --repo $repo --limit $limit --search "type:Bug sort:closed-asc closed:>$last_meeting_date $filter" --json number | jq -r '.[].number')
 echo "### Bugs"
 list_from_ids "$closed_bug_ids"
 echo
 
-closed_enchancement_ids=$(gh issue list --repo $repo --limit $limit --search "type:enhancement sort:closed-asc closed:>$last_meeting_date $filter" --json number | jq -r '.[].number') 
+closed_enchancement_ids=$(gh issue list --repo $repo --limit $limit --search "type:Enhancement sort:closed-asc closed:>$last_meeting_date $filter" --json number | jq -r '.[].number') 
 echo "### Enhancements"
 list_from_ids "$closed_enchancement_ids"
+echo
+
+closed_other_ids=$(gh issue list --repo $repo --limit $limit --search "no:type sort:closed-asc closed:>$last_meeting_date $filter" --json number | jq -r '.[].number') 
+echo "### Other"
+list_from_ids "$closed_other_ids"
 echo
