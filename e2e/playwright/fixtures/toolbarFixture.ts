1e8d61ac19ff421a7560e25efa930155823f61ec
49lf <ircsurfer33@gmail.com>
2025-02-25 16:09:00 -0500

    Fix flakey tests with new toolbar.exitSketch

--- e2e/playwright/fixtures/toolbarFixture.ts
+++ e2e/playwright/fixtures/toolbarFixture.ts
@@ -86,9 +86,7 @@ export class ToolbarFixture {
 
   exitSketch = async () => {
     await this.exitSketchBtn.click()
-    await expect(
-      this.page.getByRole('button', { name: 'Start Sketch' })
-    ).toBeVisible()
+    await expect(this.page.getByRole('button', { name: 'Start Sketch' })).toBeVisible()
     await expect(
       this.page.getByRole('button', { name: 'Start Sketch' })
     ).not.toBeDisabled()
