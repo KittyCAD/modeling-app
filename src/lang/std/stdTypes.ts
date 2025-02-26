b5efa5297c9d9fad2256331ad6015457292b7627
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2025-02-27 10:29:17 +1100

    bare minimum

--- src/lang/std/stdTypes.ts
+++ src/lang/std/stdTypes.ts
@@ -42,8 +42,10 @@ interface StraightSegmentInput {
 interface ArcSegmentInput {
   type: 'arc-segment'
   from: [number, number]
+  to: [number, number]
   center: [number, number]
   radius: number
+  ccw: boolean
 }
 /** Inputs for three point circle */
 interface CircleThreePointSegmentInput {
