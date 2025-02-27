2655d1447c3d86d7e41537abfeb23d89ecc22cbb
Kevin Nadro <kevin@zoo.dev>
2025-02-27 13:52:40 -0600

    fix: removing testing code

--- src/wasm-lib/src/toml.rs
+++ src/wasm-lib/src/toml.rs
@@ -8,13 +8,7 @@ use wasm_bindgen::prelude::wasm_bindgen;
 pub fn toml_stringify(json: &str) -> Result<String, String> {
     console_error_panic_hook::set_once();
 
-    #[cfg(target_arch = "wasm32")]
-    web_sys::console::log_1(&format!("json {:?}", json).into());
-
     let value: Configuration = serde_json::from_str(json).map_err(|e| e.to_string())?;
-
-    #[cfg(target_arch = "wasm32")]
-    web_sys::console::log_1(&format!("SERDE {:?}", value).into());
 
     toml::to_string_pretty(&value).map_err(|e| e.to_string())
 }
