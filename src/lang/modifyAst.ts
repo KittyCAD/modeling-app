07c87e8ec63e324f67d209c1fb581ed25f2ba240
Adam Chalmers <adam.chalmers@zoo.dev>
2025-03-07 14:56:38 -0600

    Update src/lang/modifyAst.ts
    Co-authored-by: Jonathan Tran <jonnytran@gmail.com>

--- src/lang/modifyAst.ts
+++ src/lang/modifyAst.ts
@@ -1004,11 +1004,6 @@ export function createCallExpressionStdLibKw(
   unlabeled: CallExpressionKw['unlabeled'],
   args: CallExpressionKw['arguments']
 ): Node<CallExpressionKw> {
-  console.error(
-    'ADAM: Creating call expression',
-    name,
-    args.map((a) => a.label.name)
-  )
   return {
     type: 'CallExpressionKw',
     start: 0,
