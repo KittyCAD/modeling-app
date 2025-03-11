c9e18e6e4b8c76dcb7a2a989c5bbbcc0503739ce
49lf <ircsurfer33@gmail.com>
2025-03-11 16:39:23 -0400

    yarn lint

--- e2e/playwright/fixtures/fixtureSetup.ts
+++ e2e/playwright/fixtures/fixtureSetup.ts
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
 import type {
   BrowserContext,
   ElectronApplication,
--- e2e/playwright/zoo-test.ts
+++ e2e/playwright/zoo-test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
 import {
   test as playwrightTestFn,
   ElectronApplication,
