1748f56ed14df874ff86e88ff45ae3bb3faa6932
Pierre Jacquier <pierre@zoo.dev>
2025-03-11 10:59:35 -0400

    Skip flaky test on macOS

--- e2e/playwright/testing-constraints.spec.ts
+++ e2e/playwright/testing-constraints.spec.ts
@@ -1117,7 +1117,7 @@ part002 = startSketchOn('XZ')
 test.describe('Electron constraint tests', () => {
   test(
     'Able to double click label to set constraint',
-    { tag: '@electron' },
+    { tag: ['@electron', '@skipMacos'] },
     async ({ page, context, homePage, scene, editor, toolbar }) => {
       await context.folderSetupFn(async (dir) => {
         const bracketDir = path.join(dir, 'test-sample')
