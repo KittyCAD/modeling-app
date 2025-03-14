bc5ddae54e6a8ecb03450315856b266079119c98
Frank Noirot <frankjohnson1993@gmail.com>
2025-03-14 14:47:47 -0400

    Add utilities for working with variable declarations

---
+++ src/lang/queryAst/getVariableDeclarationIndex.ts
@@ -0,0 +1,17 @@
+import { Node } from '@rust/kcl-lib/bindings/Node'
+import { Program } from 'lang/wasm'
+
+/**
+ * Given a program and a variable name, return the index of the variable declaration
+ * in the body, returning `-1` if it doesn't exist.
+ */
+export function getVariableDeclarationIndex(
+  program: Node<Program>,
+  variableName: string
+) {
+  return program.body.findIndex(
+    (item) =>
+      item.type === 'VariableDeclaration' &&
+      item.declaration.id.name === variableName
+  )
+}
