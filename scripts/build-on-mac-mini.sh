a93d580b05511aa55cf2bc255c7e000189c3fd91
Pierre Jacquier <pierre@zoo.dev>
2025-03-14 15:50:26 -0400

    Remove wasm-prep, rust flag only on wasm-pack build, add ps1 script

--- scripts/build-on-mac-mini.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export RUSTFLAGS='--cfg getrandom_backend="wasm_js"'
-rm -rf rust/kcl-wasm-lib/pkg && mkdir -p rust/kcl-wasm-lib/pkg && rm -rf rust/kcl-lib/bindings
-cd rust
-wasm-pack build kcl-wasm-lib --release --target web --out-dir pkg && cargo test -p kcl-lib export_bindings
-cd ../
-cp rust/kcl-wasm-lib/pkg/kcl_wasm_lib_bg.wasm public && yarn fmt
