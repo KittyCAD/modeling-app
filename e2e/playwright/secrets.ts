6784551bb3d4cb373fda29758532c03a8c6de0e1
Jonathan Tran <jonnytran@gmail.com>
2025-03-17 18:22:22 -0400

    Change playwright to always use env vars

--- e2e/playwright/secrets.ts
+++ e2e/playwright/secrets.ts
@@ -1,8 +1,9 @@
 import { readFileSync } from 'fs'
 
 const secrets: Record<string, string> = {}
+const secretsPath = './e2e/playwright/playwright-secrets.env'
 try {
-  const file = readFileSync('./e2e/playwright/playwright-secrets.env', 'utf8')
+  const file = readFileSync(secretsPath, 'utf8')
   file
     .split('\n')
     .filter((line) => line && line.length > 1)
@@ -15,9 +16,13 @@ try {
     })
 } catch (err) {
   // probably running in CI
-  secrets.token = process.env.token || ''
-  secrets.snapshottoken = process.env.snapshottoken || ''
-  // add more env vars here to make them available in CI
+  console.warn(
+    `Error reading ${secretsPath}; environment variables will be used`,
+    err
+  )
 }
+secrets.token = secrets.token || process.env.token || ''
+secrets.snapshottoken = secrets.snapshottoken || process.env.snapshottoken || ''
+// add more env vars here to make them available in CI
 
 export { secrets }
