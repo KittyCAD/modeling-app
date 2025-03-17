15c3f21acc51fb16cad64971c019ebe81272ce31
Jonathan Tran <jonnytran@gmail.com>
2025-03-17 14:08:40 -0400

    Add arrow down to try to fix tests

--- e2e/playwright/editor-tests.spec.ts
+++ e2e/playwright/editor-tests.spec.ts
@@ -21,6 +21,7 @@ test.describe('Editor tests', { tag: ['@skipWin'] }, () => {
     await expect(page.locator('.cm-lint-marker-error')).not.toBeVisible()
 
     await u.codeLocator.click()
+    await page.keyboard.press('ArrowDown')
     await page.keyboard.type(`sketch001 = startSketchOn('XY')
     |> startProfileAt([-10, -10], %)
     |> line(end = [20, 0])
@@ -171,6 +172,7 @@ sketch001 = startSketchOn('XY')
     await expect(page.locator('.cm-lint-marker-error')).not.toBeVisible()
 
     await u.codeLocator.click()
+    await page.keyboard.press('ArrowDown')
     await page.keyboard.type(`sketch001 = startSketchOn('XY')
     |> startProfileAt([-10, -10], %)
     |> line(end = [20, 0])
@@ -203,6 +205,7 @@ sketch001 = startSketchOn('XY')
     await expect(page.locator('.cm-lint-marker-error')).not.toBeVisible()
 
     await u.codeLocator.click()
+    await page.keyboard.press('ArrowDown')
     await page.keyboard.type(`sketch_001 = startSketchOn('XY')
     |> startProfileAt([-10, -10], %)
     |> line(end = [20, 0])
