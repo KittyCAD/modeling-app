09131722e35f7960751c27aeb717c0a5124b28b9
Jess Frazelle <jessfraz@users.noreply.github.com>
2025-03-17 13:35:49 -0700

    fix not giving miette error in bindings (#5844)
    * try python unit test
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * try python unit test
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * updates
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * updates
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * updates
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * updates
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * fix
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * updates
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * fixes;
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * fixes;
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    ---------
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -2245,3 +2245,24 @@ mod assembly_mixed_units_cubes {
         super::execute(TEST_NAME, true).await
     }
 }
+mod bad_units_in_annotation {
+    const TEST_NAME: &str = "bad_units_in_annotation";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[test]
+    fn unparse() {
+        super::unparse(TEST_NAME)
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
