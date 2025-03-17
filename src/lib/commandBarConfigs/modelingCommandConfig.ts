ebc16296eed5ee821246a11bddf03f1aeee040ef
Frank Noirot <frankjohnson1993@gmail.com>
2025-03-17 14:04:53 -0400

    Add edit flow config

--- src/lib/commandBarConfigs/modelingCommandConfig.ts
+++ src/lib/commandBarConfigs/modelingCommandConfig.ts
@@ -606,6 +606,50 @@ export const modelingMachineCommandConfig: StateMachineCommandSetConfig<
       },
     },
   },
+  'event.parameter.edit': {
+    displayName: 'Edit parameter',
+    description: 'Edit the value of a named constant',
+    icon: 'make-variable',
+    status: 'development',
+    needsReview: false,
+    args: {
+      name: {
+        inputType: 'options',
+        required: true,
+        options() {
+          return (
+            Object.keys(kclManager.execState.variables).map((name) => ({
+              name: name,
+              value: name,
+            })) || []
+          )
+        },
+      },
+      value: {
+        inputType: 'kcl',
+        required: true,
+        defaultValue(commandBarContext) {
+          const variableName = commandBarContext.argumentsToSubmit.name
+          if (typeof variableName !== 'string') return '5'
+          const variableNode = getVariableDeclaration(
+            kclManager.ast,
+            variableName
+          )
+          if (!variableNode) return '5'
+          const code = codeManager.code.slice(
+            variableNode.declaration.init.start,
+            variableNode.declaration.init.end
+          )
+          return code
+        },
+        createVariable: 'force',
+        variableName(commandBarContext) {
+          const variableName = commandBarContext.argumentsToSubmit.name
+          return typeof variableName === 'string' ? variableName : 'myParamater'
+        },
+      },
+    },
+  },
   'Constrain length': {
     description: 'Constrain the length of one or more segments.',
     icon: 'dimension',
