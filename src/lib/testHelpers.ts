69484215dc0223e11cad63b0e3e9b3f57ca816ae
Jess Frazelle <github@jessfraz.com>
2025-03-17 19:25:12 -0700

    fixes
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/lib/testHelpers.ts
+++ src/lib/testHelpers.ts
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import { Program, ExecState, jsAppSettings } from '../lang/wasm'
 import { Node } from '@rust/kcl-lib/bindings/Node'
 import { rustContext } from './singletons'
-=======
-import { Program, executeMock, ExecState } from '../lang/wasm'
-import { Node } from '@rust/kcl-lib/bindings/Node'
->>>>>>> 2a5bd269f (more)
 
 export async function enginelessExecutor(
   ast: Node<Program>,
