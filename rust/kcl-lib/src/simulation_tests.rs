b86d116d57910d9a93902566595065bc074427b7
Jess Frazelle <github@jessfraz.com>
2025-03-19 09:01:40 -0700

    rotate
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -2331,3 +2331,24 @@ mod scale_after_fillet {
         super::execute(TEST_NAME, true).await
     }
 }
+mod rotate_after_fillet {
+    const TEST_NAME: &str = "rotate_after_fillet";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
