94722f806055491d2c724d76295bb5dd7c5b61e6
Kevin Nadro <kevin@zoo.dev>
2025-03-19 16:14:07 -0500

    fix: trying to fix custom type?

--- src/menu/roles.ts
+++ src/menu/roles.ts
@@ -1,25 +1,4 @@
-// Does not matter what labels belong to what type. I only split these into some internal types to easily parse
-// what labels should belong to what grouping
 import { Menu, MenuItemConstructorOptions } from 'electron'
-
-type HelpRoleLabel =
-  | 'Report a bug'
-  | 'Request a feature'
-  | 'Ask the community discord'
-  | 'Ask the community discourse'
-  | 'KCL code samples'
-  | 'KCL docs'
-  | 'Reset onboarding'
-  | 'Release notes'
-
-type OptionsRoleLabel = 'Options' | 'Report an issue'
-
-type UtilityRoleLabel = 'Utility'
-
-type ViewRoleLabel = 'View' | 'Learn more'
-
-// Only export the union of all the internal types since they are all labels
-// The internal types are only for readability within the file
 export type ZooLabel =
   | 'Learn more'
   | 'New project'
@@ -35,11 +14,19 @@ export type ZooLabel =
   | 'Keybindings'
   | 'Rename project'
   | 'Delete project'
-  | HelpRoleLabel
-  | OptionsRoleLabel
-  | UtilityRoleLabel
-  | ViewRoleLabel
-
+  | 'View'
+  | 'Learn more'
+  | 'Utility'
+  | 'Options'
+  | 'Report an issue'
+  | 'Report a bug'
+  | 'Request a feature'
+  | 'Ask the community discord'
+  | 'Ask the community discourse'
+  | 'KCL code samples'
+  | 'KCL docs'
+  | 'Reset onboarding'
+  | 'Release notes'
 // Extend the interface with additional custom properties
 export interface ZooMenuItemConstructorOptions
   extends MenuItemConstructorOptions {
