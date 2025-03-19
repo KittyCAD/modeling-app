31075186f0d4398e08d4729d8f90c863ae61dbbb
Jess Frazelle <github@jessfraz.com>
2025-03-19 09:00:16 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -2310,3 +2310,24 @@ mod translate_after_fillet {
         super::execute(TEST_NAME, true).await
     }
 }
+mod scale_after_fillet {
+    const TEST_NAME: &str = "scale_after_fillet";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
