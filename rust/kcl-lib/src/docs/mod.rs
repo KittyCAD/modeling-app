b5c8793e994eb22654d34a621284c6fca379cdb3
Jess Frazelle <github@jessfraz.com>
2025-03-21 13:41:53 -0700

    fix
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- rust/kcl-lib/src/docs/mod.rs
+++ rust/kcl-lib/src/docs/mod.rs
@@ -133,7 +133,7 @@ impl StdLibFnArg {
             || self.type_ == "[Solid]"
             || self.type_ == "SketchSurface"
             || self.type_ == "SketchOrSurface"
-            || self.type_ == "SolidOrSketchOrImportedGeometry"
+            || self.type_ == "SolidOrSketchOrImportedGeometry" && self.required
         {
             return Ok(Some((index, format!("{label}${{{}:{}}}", index, "%"))));
         } else if (self.type_ == "TagDeclarator" || self.type_ == "TagNode") && self.required {
