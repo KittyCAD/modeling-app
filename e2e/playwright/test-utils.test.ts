55c428f6b4c8db7b551116b1c294284fcb262224
Jace Browning <jacebrowning@gmail.com>
2025-03-25 11:15:27 -0400

    Limit skip to just Windows

--- e2e/playwright/test-utils.test.ts
+++ e2e/playwright/test-utils.test.ts
@@ -1,12 +1,74 @@
-import { orRunWhenFullSuiteEnabled } from './test-utils'
+import {
+  runningOnLinux,
+  runningOnMac,
+  runningOnWindows,
+  orRunWhenFullSuiteEnabled,
+} from './test-utils'
 
-const originalEnv = { ...process.env }
+describe('platform detection utilities', () => {
+  const originalPlatform = process.platform
 
-afterAll(() => {
-  process.env = { ...originalEnv }
+  afterAll(() => {
+    Object.defineProperty(process, 'platform', {
+      value: originalPlatform,
+    })
+  })
+
+  describe('runningOnLinux', () => {
+    it('returns true on Linux', () => {
+      Object.defineProperty(process, 'platform', {
+        value: 'linux',
+      })
+      expect(runningOnLinux()).toBe(true)
+    })
+
+    it('returns false on other platforms', () => {
+      Object.defineProperty(process, 'platform', {
+        value: 'darwin',
+      })
+      expect(runningOnLinux()).toBe(false)
+    })
+  })
+
+  describe('runningOnMac', () => {
+    it('returns true on Mac', () => {
+      Object.defineProperty(process, 'platform', {
+        value: 'darwin',
+      })
+      expect(runningOnMac()).toBe(true)
+    })
+
+    it('returns false on other platforms', () => {
+      Object.defineProperty(process, 'platform', {
+        value: 'linux',
+      })
+      expect(runningOnMac()).toBe(false)
+    })
+  })
+
+  describe('runningOnWindows', () => {
+    it('returns true on Windows', () => {
+      Object.defineProperty(process, 'platform', {
+        value: 'win32',
+      })
+      expect(runningOnWindows()).toBe(true)
+    })
+
+    it('returns false on other platforms', () => {
+      Object.defineProperty(process, 'platform', {
+        value: 'linux',
+      })
+      expect(runningOnWindows()).toBe(false)
+    })
+  })
 })
 
 describe('utility to bypass unreliable tests', () => {
+  const originalEnv = { ...process.env }
+
+  afterAll(() => {
+    process.env = { ...originalEnv }
+  })
   it('always runs them on dedicated branch', () => {
     process.env.GITHUB_EVENT_NAME = 'push'
     process.env.GITHUB_REF = 'refs/heads/all-e2e'
