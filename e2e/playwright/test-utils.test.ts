736533a482d715a9c400ebc0809018966885c1c8
Jace Browning <jacebrowning@gmail.com>
2025-03-25 20:59:07 -0400

    Ignore unit tests when running Playwright Electron (#5996)
    * Ignore unit tests when running Playwright Electron
    
    * Skip a couple more Windows tests for now

--- e2e/playwright/test-utils.test.ts
+++ e2e/playwright/test-utils.test.ts
@@ -69,28 +69,31 @@ describe('utility to bypass unreliable tests', () => {
   afterAll(() => {
     process.env = { ...originalEnv }
   })
+
   it('always runs them on dedicated branch', () => {
     process.env.GITHUB_EVENT_NAME = 'push'
     process.env.GITHUB_REF = 'refs/heads/all-e2e'
     process.env.GITHUB_HEAD_REF = ''
     process.env.GITHUB_BASE_REF = ''
-    const condition = orRunWhenFullSuiteEnabled()
-    expect(condition).toBe(false)
+    const shouldSkip = orRunWhenFullSuiteEnabled()
+    expect(shouldSkip).toBe(false)
   })
+
   it('skips them on the main branch', () => {
     process.env.GITHUB_EVENT_NAME = 'push'
     process.env.GITHUB_REF = 'refs/heads/main'
     process.env.GITHUB_HEAD_REF = ''
     process.env.GITHUB_BASE_REF = ''
-    const condition = orRunWhenFullSuiteEnabled()
-    expect(condition).toBe(true)
+    const shouldSkip = orRunWhenFullSuiteEnabled()
+    expect(shouldSkip).toBe(true)
   })
+
   it('skips them on pull requests', () => {
     process.env.GITHUB_EVENT_NAME = 'pull_request'
     process.env.GITHUB_REF = 'refs/pull/5883/merge'
     process.env.GITHUB_HEAD_REF = 'my-branch'
     process.env.GITHUB_BASE_REF = 'main'
-    const condition = orRunWhenFullSuiteEnabled()
-    expect(condition).toBe(true)
+    const shouldSkip = orRunWhenFullSuiteEnabled()
+    expect(shouldSkip).toBe(true)
   })
 })
