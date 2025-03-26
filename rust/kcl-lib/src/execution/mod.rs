3741d9fc6d558b243967e7e4a6aef0dfb913c4a6
Frank Noirot <frankjohnson1993@gmail.com>
2025-03-26 17:36:11 -0400

    Remove test that is no longer true on Rust side
    The parser doesn't even recognize project-level unit settings in this
    PR, which is why this test started failing.

--- rust/kcl-lib/src/execution/mod.rs
+++ rust/kcl-lib/src/execution/mod.rs
@@ -1578,31 +1578,6 @@ const inInches = 2.0 * inch()"#;
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn test_unit_suggest() {
-        let src = "foo = 42";
-        let program = crate::Program::parse_no_errs(src).unwrap();
-        let ctx = ExecutorContext {
-            engine: Arc::new(Box::new(
-                crate::engine::conn_mock::EngineConnection::new().await.unwrap(),
-            )),
-            fs: Arc::new(crate::fs::FileManager::new()),
-            stdlib: Arc::new(crate::std::StdLib::new()),
-            settings: ExecutorSettings { ..Default::default() },
-            context_type: ContextType::Mock,
-        };
-        let mut exec_state = ExecState::new(&ctx);
-        ctx.run(&program, &mut exec_state).await.unwrap();
-        let errs = exec_state.errors();
-        assert_eq!(errs.len(), 1, "{errs:?}");
-        let warn = &errs[0];
-        assert_eq!(warn.severity, Severity::Warning);
-        assert_eq!(
-            warn.apply_suggestion(src).unwrap(),
-            "@settings(defaultLengthUnit = ft)\nfoo = 42"
-        )
-    }
-
-    #[tokio::test(flavor = "multi_thread")]
     async fn test_zero_param_fn() {
         let ast = r#"const sigmaAllow = 35000 // psi
 const leg1 = 5 // inches
