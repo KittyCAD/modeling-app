6ca8fd09f95bb8ec4606af75ad5ad7067600623c
Jonathan Tran <jonnytran@gmail.com>
2025-03-27 13:31:39 -0400

    Add intersect sim test

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -2421,3 +2421,24 @@ mod subtract_cylinder_from_cube {
         super::execute(TEST_NAME, true).await
     }
 }
+mod intersect_cubes {
+    const TEST_NAME: &str = "intersect_cubes";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
