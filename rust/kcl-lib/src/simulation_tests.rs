bb769a7da8d3b2b67b05fa37d21133bd65661eef
Jonathan Tran <jonnytran@gmail.com>
2025-03-27 13:27:48 -0400

    Add CSG subtract test

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -2400,3 +2400,24 @@ mod union_cubes {
         super::execute(TEST_NAME, true).await
     }
 }
+mod subtract_cylinder_from_cube {
+    const TEST_NAME: &str = "subtract_cylinder_from_cube";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
