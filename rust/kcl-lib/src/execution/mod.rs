d122d7a2244a05f5954c8abbc9ac8665329fd8db
Jess Frazelle <github@jessfraz.com>
2025-04-01 12:34:42 -0700

    fix rebase add feature flag
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- rust/kcl-lib/src/execution/mod.rs
+++ rust/kcl-lib/src/execution/mod.rs
@@ -401,22 +401,14 @@ impl ExecutorContext {
     }
 
     #[cfg(target_arch = "wasm32")]
-    pub async fn new(
-        engine_manager: crate::engine::conn_wasm::EngineCommandManager,
-        fs_manager: crate::fs::wasm::FileSystemManager,
-        settings: ExecutorSettings,
-    ) -> Result<Self, String> {
-        Ok(ExecutorContext {
-            engine: Arc::new(Box::new(
-                crate::engine::conn_wasm::EngineConnection::new(engine_manager)
-                    .await
-                    .map_err(|e| format!("{:?}", e))?,
-            )),
-            fs: Arc::new(FileManager::new(fs_manager)),
+    pub fn new(engine: Arc<Box<dyn EngineManager>>, fs: Arc<FileManager>, settings: ExecutorSettings) -> Self {
+        ExecutorContext {
+            engine,
+            fs,
             stdlib: Arc::new(StdLib::new()),
             settings,
             context_type: ContextType::Live,
-        })
+        }
     }
 
     #[cfg(not(target_arch = "wasm32"))]
@@ -433,21 +425,14 @@ impl ExecutorContext {
     }
 
     #[cfg(target_arch = "wasm32")]
-    pub async fn new_mock(
-        fs_manager: crate::fs::wasm::FileSystemManager,
-        settings: ExecutorSettings,
-    ) -> Result<Self, String> {
-        Ok(ExecutorContext {
-            engine: Arc::new(Box::new(
-                crate::engine::conn_mock::EngineConnection::new()
-                    .await
-                    .map_err(|e| format!("{:?}", e))?,
-            )),
-            fs: Arc::new(FileManager::new(fs_manager)),
+    pub fn new_mock(engine: Arc<Box<dyn EngineManager>>, fs: Arc<FileManager>, settings: ExecutorSettings) -> Self {
+        ExecutorContext {
+            engine,
+            fs,
             stdlib: Arc::new(StdLib::new()),
             settings,
             context_type: ContextType::Mock,
-        })
+        }
     }
 
     #[cfg(not(target_arch = "wasm32"))]
