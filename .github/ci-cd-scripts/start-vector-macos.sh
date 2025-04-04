415e6ed67e4a98e71a5f3b2153670ef25fdcda78
Jace Browning <jacebrowning@gmail.com>
2025-04-04 13:40:35 -0400

    wip: dedicated scripts

---
+++ .github/ci-cd-scripts/start-vector-macos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+
+# Install vector
+brew tap vectordotdev/brew && brew install vector
+
+# Configure vector
+mkdir -p /tmp/vector
+cp .github/workflows/vector.toml /tmp/vector.toml
+sed -i '' "s#GITHUB_WORKFLOW#${GITHUB_WORKFLOW}#g" /tmp/vector.toml
+sed -i '' "s#GITHUB_REPOSITORY#${GITHUB_REPOSITORY}#g" /tmp/vector.toml
+sed -i '' "s#GITHUB_SHA#${GITHUB_SHA}#g" /tmp/vector.toml
+sed -i '' "s#GITHUB_REF_NAME#${GITHUB_REF_NAME}#g" /tmp/vector.toml
+sed -i '' "s#GH_ACTIONS_AXIOM_TOKEN#${GH_ACTIONS_AXIOM_TOKEN}#g" /tmp/vector.toml
+
+# Display settings
+cat /tmp/vector.toml
+
+# Start in the background
+${HOME}/.vector/bin/vector --config /tmp/vector.toml &
