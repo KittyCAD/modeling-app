e98a95755381b4c99d9eade6e705fd311ef36641
lee-at-zoo-corp <lee@zoo.dev>
2025-04-04 02:01:00 -0400

    Remove a test race - poll window info.

--- e2e/playwright/native-file-menu.spec.ts
+++ e2e/playwright/native-file-menu.spec.ts
@@ -234,11 +234,10 @@ test.describe('Native file menu', { tag: ['@electron'] }, () => {
           menu.click()
         })
         // Check the placeholder project name exists
-        const actual = await cmdBar.cmdBarElement
-          .getByTestId('command-name')
-          .textContent()
+        const actual = async () =>
+          cmdBar.cmdBarElement.getByTestId('command-name').textContent()
         const expected = 'Delete project'
-        expect(actual).toBe(expected)
+        await expect.poll(async () => await actual()).toBe(expected)
       })
       test('Home.Edit.Change project directory', async ({
         tronApp,
