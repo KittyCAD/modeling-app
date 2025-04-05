3cbf7d23b641dbde147484137970f0b4cd11f908
Andrew Varga <grizzly33@gmail.com>
2025-04-05 18:15:57 +0200

    add unit tests to closestPointoOnRay

--- src/lib/utils2d.test.ts
+++ src/lib/utils2d.test.ts
@@ -1,5 +1,6 @@
 import type { Coords2d } from '@src/lang/std/sketch'
 import { initPromise, isPointsCCW } from '@src/lang/wasm'
+import { closestPointOnRay } from '@src/lib/utils2d'
 
 beforeAll(async () => {
   await initPromise
@@ -17,5 +18,74 @@ describe('test isPointsCW', () => {
     const CW = isPointsCCW(points)
     expect(CCW).toBe(1)
     expect(CW).toBe(-1)
+  })
+})
+
+describe('test closestPointOnRay', () => {
+  test('point lies on ray', () => {
+    const rayOrigin: Coords2d = [0, 0]
+    const rayDirection: Coords2d = [1, 0]
+    const pointToCheck: Coords2d = [7, 0]
+
+    const result = closestPointOnRay(rayOrigin, rayDirection, pointToCheck)
+    expect(result.closestPoint).toEqual([7, 0])
+    expect(result.t).toBe(7)
+  })
+
+  test('point is above ray', () => {
+    const rayOrigin: Coords2d = [1, 0]
+    const rayDirection: Coords2d = [1, 0]
+    const pointToCheck: Coords2d = [7, 7]
+
+    const result = closestPointOnRay(rayOrigin, rayDirection, pointToCheck)
+    expect(result.closestPoint).toEqual([7, 0])
+    expect(result.t).toBe(6)
+  })
+
+  test('point lies behind ray origin and allowNegative=false', () => {
+    const rayOrigin: Coords2d = [0, 0]
+    const rayDirection: Coords2d = [1, 0]
+    const pointToCheck: Coords2d = [-7, 7]
+
+    const result = closestPointOnRay(rayOrigin, rayDirection, pointToCheck)
+    expect(result.closestPoint).toEqual([0, 0])
+    expect(result.t).toBe(0)
+  })
+
+  test('point lies behind ray origin and allowNegative=true', () => {
+    const rayOrigin: Coords2d = [0, 0]
+    const rayDirection: Coords2d = [1, 0]
+    const pointToCheck: Coords2d = [-7, 7]
+
+    const result = closestPointOnRay(
+      rayOrigin,
+      rayDirection,
+      pointToCheck,
+      true
+    )
+    expect(result.closestPoint).toEqual([-7, 0])
+    expect(result.t).toBe(-7)
+  })
+
+  test('diagonal ray and point', () => {
+    const rayOrigin: Coords2d = [0, 0]
+    const rayDirection: Coords2d = [1, 1]
+    const pointToCheck: Coords2d = [3, 4]
+
+    const result = closestPointOnRay(rayOrigin, rayDirection, pointToCheck)
+    expect(result.closestPoint[0]).toBeCloseTo(3.5)
+    expect(result.closestPoint[1]).toBeCloseTo(3.5)
+    expect(result.t).toBeCloseTo(4.95, 1)
+  })
+
+  test('non-normalized direction vector', () => {
+    const rayOrigin: Coords2d = [0, 0]
+    const rayDirection: Coords2d = [2, 2]
+    const pointToCheck: Coords2d = [3, 4]
+
+    const result = closestPointOnRay(rayOrigin, rayDirection, pointToCheck)
+    expect(result.closestPoint[0]).toBeCloseTo(3.5)
+    expect(result.closestPoint[1]).toBeCloseTo(3.5)
+    expect(result.t).toBeCloseTo(4.95, 1)
   })
 })
