757466310d4ee63ca0e3de39986d655a61a81baf
Pierre Jacquier <pierre@zoo.dev>
2025-04-08 07:08:59 -0400

    Add step insert test

--- e2e/playwright/point-click-assemblies.spec.ts
+++ e2e/playwright/point-click-assemblies.spec.ts
@@ -1,13 +1,47 @@
 import * as fsp from 'fs/promises'
 import path from 'path'
 
-import { executorInputPath } from '@e2e/playwright/test-utils'
+import { CmdBarFixture } from '@e2e/playwright/fixtures/cmdBarFixture'
+import { ToolbarFixture } from '@e2e/playwright/fixtures/toolbarFixture'
+import {
+  executorInputPath,
+  getUtils,
+  testsInputPath,
+} from '@e2e/playwright/test-utils'
 import { expect, test } from '@e2e/playwright/zoo-test'
+import { Page } from '@playwright/test'
+
+async function insertPartIntoAssembly(
+  path: string,
+  alias: string,
+  toolbar: ToolbarFixture,
+  cmdBar: CmdBarFixture,
+  page: Page
+) {
+  await toolbar.insertButton.click()
+  await cmdBar.selectOption({ name: path }).click()
+  await cmdBar.expectState({
+    stage: 'arguments',
+    currentArgKey: 'localName',
+    currentArgValue: '',
+    headerArguments: { Path: path, LocalName: '' },
+    highlightedHeaderArg: 'localName',
+    commandName: 'Insert',
+  })
+  await page.keyboard.insertText(alias)
+  await cmdBar.progressCmdBar()
+  await cmdBar.expectState({
+    stage: 'review',
+    headerArguments: { Path: path, LocalName: alias },
+    commandName: 'Insert',
+  })
+  await cmdBar.progressCmdBar()
+}
 
 // test file is for testing point an click code gen functionality that's assemblies related
 test.describe('Point-and-click assemblies tests', () => {
   test(
-    `Insert kcl part into assembly as whole module import`,
+    `Insert kcl parts into assembly as whole module import`,
     { tag: ['@electron'] },
     async ({
       context,
@@ -23,10 +57,14 @@ test.describe('Point-and-click assemblies tests', () => {
         fail()
       }
 
-      // One dumb hardcoded screen pixel value
-      const testPoint = { x: 575, y: 200 }
-      const partColor: [number, number, number] = [150, 150, 150]
+      const midPoint = { x: 500, y: 250 }
+      const partPoint = { x: midPoint.x + 30, y: midPoint.y - 30 } // mid point, just off top right
+      const defaultPlanesColor: [number, number, number] = [180, 220, 180]
+      const partColor: [number, number, number] = [100, 100, 100]
       const tolerance = 50
+      const u = await getUtils(page)
+      const gizmo = page.locator('[aria-label*=gizmo]')
+      const resetCameraButton = page.getByRole('button', { name: 'Reset view' })
 
       await test.step('Setup parts and expect empty assembly scene', async () => {
         const projectName = 'assembly'
@@ -35,12 +73,16 @@ test.describe('Point-and-click assemblies tests', () => {
           await fsp.mkdir(bracketDir, { recursive: true })
           await Promise.all([
             fsp.copyFile(
-              executorInputPath('cylinder-inches.kcl'),
+              executorInputPath('cylinder.kcl'),
               path.join(bracketDir, 'cylinder.kcl')
             ),
             fsp.copyFile(
               executorInputPath('e2e-can-sketch-on-chamfer.kcl'),
               path.join(bracketDir, 'bracket.kcl')
+            ),
+            fsp.copyFile(
+              testsInputPath('cube.step'),
+              path.join(bracketDir, 'cube.step')
             ),
             fsp.writeFile(path.join(bracketDir, 'main.kcl'), ''),
           ])
@@ -48,31 +90,19 @@ test.describe('Point-and-click assemblies tests', () => {
         await page.setBodyDimensions({ width: 1000, height: 500 })
         await homePage.openProject(projectName)
         await scene.settled(cmdBar)
+        await toolbar.closePane('code')
+        await scene.expectPixelColor(defaultPlanesColor, midPoint, tolerance)
       })
 
-      async function insertPartIntoAssembly(path: string, alias: string) {
-        await toolbar.insertButton.click()
-        await cmdBar.selectOption({ name: path }).click()
-        await cmdBar.expectState({
-          stage: 'arguments',
-          currentArgKey: 'localName',
-          currentArgValue: '',
-          headerArguments: { Path: path, LocalName: '' },
-          highlightedHeaderArg: 'localName',
-          commandName: 'Insert',
-        })
-        await page.keyboard.insertText(alias)
-        await cmdBar.progressCmdBar()
-        await cmdBar.expectState({
-          stage: 'review',
-          headerArguments: { Path: path, LocalName: alias },
-          commandName: 'Insert',
-        })
-        await cmdBar.progressCmdBar()
-      }
-
-      await test.step('Insert first part into the assembly', async () => {
-        await insertPartIntoAssembly('cylinder.kcl', 'cylinder')
+      await test.step('Insert kcl as first part as module', async () => {
+        await insertPartIntoAssembly(
+          'cylinder.kcl',
+          'cylinder',
+          toolbar,
+          cmdBar,
+          page
+        )
+        await toolbar.openPane('code')
         await editor.expectEditor.toContain(
           `
         import "cylinder.kcl" as cylinder
@@ -81,11 +111,26 @@ test.describe('Point-and-click assemblies tests', () => {
           { shouldNormalise: true }
         )
         await scene.settled(cmdBar)
-        await scene.expectPixelColor(partColor, testPoint, tolerance)
+
+        // Check scene for changes
+        await toolbar.closePane('code')
+        await u.doAndWaitForCmd(async () => {
+          await gizmo.click({ button: 'right' })
+          await resetCameraButton.click()
+        }, 'zoom_to_fit')
+        await toolbar.closePane('debug')
+        await scene.expectPixelColor(partColor, partPoint, tolerance)
+        await toolbar.openPane('code')
       })
 
-      await test.step('Insert second part into the assembly', async () => {
-        await insertPartIntoAssembly('bracket.kcl', 'bracket')
+      await test.step('Insert kcl second part as module', async () => {
+        await insertPartIntoAssembly(
+          'bracket.kcl',
+          'bracket',
+          toolbar,
+          cmdBar,
+          page
+        )
         await editor.expectEditor.toContain(
           `
         import "cylinder.kcl" as cylinder
@@ -100,7 +145,13 @@ test.describe('Point-and-click assemblies tests', () => {
 
       await test.step('Insert a second time and expect error', async () => {
         // TODO: revisit once we have clone with #6209
-        await insertPartIntoAssembly('bracket.kcl', 'bracket')
+        await insertPartIntoAssembly(
+          'bracket.kcl',
+          'bracket',
+          toolbar,
+          cmdBar,
+          page
+        )
         await editor.expectEditor.toContain(
           `
         import "cylinder.kcl" as cylinder
@@ -114,6 +165,72 @@ test.describe('Point-and-click assemblies tests', () => {
         )
         await scene.settled(cmdBar)
         await expect(page.locator('.cm-lint-marker-error')).toBeVisible()
+      })
+    }
+  )
+
+  test(
+    `Insert foreign parts into assembly as whole module import`,
+    { tag: ['@electron'] },
+    async ({
+      context,
+      page,
+      homePage,
+      scene,
+      editor,
+      toolbar,
+      cmdBar,
+      tronApp,
+    }) => {
+      if (!tronApp) {
+        fail()
+      }
+
+      const midPoint = { x: 500, y: 250 }
+      const partPoint = { x: midPoint.x + 30, y: midPoint.y - 30 } // mid point, just off top right
+      const defaultPlanesColor: [number, number, number] = [180, 220, 180]
+      const partColor: [number, number, number] = [150, 150, 150]
+      const tolerance = 50
+
+      await test.step('Setup parts and expect empty assembly scene', async () => {
+        const projectName = 'assembly'
+        await context.folderSetupFn(async (dir) => {
+          const bracketDir = path.join(dir, projectName)
+          await fsp.mkdir(bracketDir, { recursive: true })
+          await Promise.all([
+            fsp.copyFile(
+              testsInputPath('cube.step'),
+              path.join(bracketDir, 'cube.step')
+            ),
+            fsp.writeFile(path.join(bracketDir, 'main.kcl'), ''),
+          ])
+        })
+        await page.setBodyDimensions({ width: 1000, height: 500 })
+        await homePage.openProject(projectName)
+        await scene.settled(cmdBar)
+        await toolbar.closePane('code')
+        await scene.expectPixelColor(defaultPlanesColor, midPoint, tolerance)
+      })
+
+      await test.step('Insert step part as module', async () => {
+        await insertPartIntoAssembly('cube.step', 'cube', toolbar, cmdBar, page)
+        await toolbar.openPane('code')
+        await editor.expectEditor.toContain(
+          `
+        import "cube.step" as cube
+        cube
+      `,
+          { shouldNormalise: true }
+        )
+        await scene.settled(cmdBar)
+
+        // TODO: remove this once #5780 is fixed
+        await page.reload()
+
+        await scene.settled(cmdBar)
+        await expect(page.locator('.cm-lint-marker-error')).not.toBeVisible()
+        await toolbar.closePane('code')
+        await scene.expectPixelColor(partColor, partPoint, tolerance)
       })
     }
   )
