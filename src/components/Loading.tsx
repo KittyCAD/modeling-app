f78bbcf199e07600d38bc1bd109da4dee5966d4b
Frank Noirot <frankjohnson1993@gmail.com>
2025-04-08 15:14:15 -0400

    Refactor Loading spinner to account for early errors

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 
+import { CustomIcon } from '@src/components/CustomIcon'
 import { Spinner } from '@src/components/Spinner'
+import type { ErrorType } from '@src/lang/std/engineConnection'
 import {
   CONNECTION_ERROR_TEXT,
   ConnectionError,
@@ -16,8 +18,16 @@ interface LoadingProps extends React.PropsWithChildren {
 }
 
 const Loading = ({ children, className }: LoadingProps) => {
-  const [error, setError] = useState<ConnectionError>(ConnectionError.Unset)
-
+  const [error, setError] = useState<ErrorType>({
+    error: ConnectionError.Unset,
+  })
+  const isUnrecoverableError = error.error > ConnectionError.VeryLongLoadingTime
+  const colorClass =
+    error.error === ConnectionError.Unset
+      ? 'text-primary'
+      : !isUnrecoverableError
+        ? 'text-warn-60'
+        : 'text-chalkboard-60 dark:text-chalkboard-40'
   useEffect(() => {
     const onConnectionStateChange = ({ detail: state }: CustomEvent) => {
       if (
@@ -26,7 +36,7 @@ const Loading = ({ children, className }: LoadingProps) => {
         state.value?.type !== DisconnectingType.Error
       )
         return
-      setError(state.value.value.error)
+      setError(state.value.value)
     }
 
     const onEngineAvailable = ({ detail: engineConnection }: CustomEvent) => {
@@ -36,10 +46,27 @@ const Loading = ({ children, className }: LoadingProps) => {
       )
     }
 
-    engineCommandManager.addEventListener(
-      EngineCommandManagerEvents.EngineAvailable,
-      onEngineAvailable as EventListener
-    )
+    if (engineCommandManager.engineConnection) {
+      // Do an initial state check in case there is an immediate issue
+      onConnectionStateChange(
+        new CustomEvent(EngineConnectionEvents.ConnectionStateChanged, {
+          detail: engineCommandManager.engineConnection.state,
+        })
+      )
+      // Set up a listener on the state for future updates
+      onEngineAvailable(
+        new CustomEvent(EngineCommandManagerEvents.EngineAvailable, {
+          detail: engineCommandManager.engineConnection,
+        })
+      )
+    } else {
+      // If there is no engine connection yet, listen for it to be there *then*
+      // attach the listener
+      engineCommandManager.addEventListener(
+        EngineCommandManagerEvents.EngineAvailable,
+        onEngineAvailable as EventListener
+      )
+    }
 
     return () => {
       engineCommandManager.removeEventListener(
@@ -57,9 +84,12 @@ const Loading = ({ children, className }: LoadingProps) => {
     // Don't set long loading time if there's a more severe error
     if (error > ConnectionError.LongLoadingTime) return
 
-    const timer = setTimeout(() => {
-      setError(ConnectionError.LongLoadingTime)
+    const shorterTimer = setTimeout(() => {
+      setError({ error: ConnectionError.LongLoadingTime })
     }, 4000)
+    const longerTimer = setTimeout(() => {
+      setError({ error: ConnectionError.VeryLongLoadingTime })
+    }, 7000)
 
     return () => clearTimeout(timer)
   }, [error, setError])
@@ -79,6 +109,28 @@ const Loading = ({ children, className }: LoadingProps) => {
       >
         {CONNECTION_ERROR_TEXT[error]}
       </p>
+      {CONNECTION_ERROR_TEXT[error.error] && (
+        <div
+          className={
+            'text-center text-sm mt-4 text-opacity-70 transition-opacity duration-500' +
+            (error.error !== ConnectionError.Unset
+              ? ' opacity-100'
+              : ' opacity-0')
+          }
+          dangerouslySetInnerHTML={{
+            __html: Marked.parse(
+              CONNECTION_ERROR_TEXT[error.error] +
+                (error.context
+                  ? '\n\nThe error details are: ' + error.context
+                  : ''),
+              {
+                renderer: new SafeRenderer(markedOptions),
+                ...markedOptions,
+              }
+            ),
+          }}
+        ></div>
+      )}
     </div>
   )
 }
