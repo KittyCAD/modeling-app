032c4c04ebcee73ab0d234ad83ecf3e7a5025808
Kevin Nadro <kevin@zoo.dev>
2025-04-09 22:03:22 -0600

    fix: increasing timeout to reduce failures..?

---
+++ e2e/playwright/stress-test.spec.ts
@@ -0,0 +1,16 @@
+import { createProject } from '@e2e/playwright/test-utils'
+import { test } from '@e2e/playwright/zoo-test'
+
+test.describe('Stress test', () => {
+  test('Create project and load stress test', async ({
+    cmdBar,
+    scene,
+    page,
+  }, testInfo) => {
+    page.on('console', console.log)
+    const projectName = 'stress-test-project'
+    // Create and load project
+    await createProject({ name: projectName, page })
+    await scene.settled(cmdBar)
+  })
+})
