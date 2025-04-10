43ab54202fb3d0a0f83c68d34f3e2b24cd008f9b
Jonathan Tran <jonnytran@gmail.com>
2025-04-10 17:36:04 -0400

    Fix formatting

--- src/lang/std/sketch.ts
+++ src/lang/std/sketch.ts
@@ -1097,12 +1097,16 @@ export const tangentialArc: SketchLineHelperKw = {
     const { node: callExpression } = nodeMeta
 
     if (callExpression.type !== 'CallExpressionKw') {
-      return new Error(`Expected CallExpressionKw, but found ${callExpression.type}`)
+      return new Error(
+        `Expected CallExpressionKw, but found ${callExpression.type}`
+      )
     }
 
     for (const arg of callExpression.arguments) {
       if (arg.label.name !== ARG_END_ABSOLUTE && arg.label.name !== ARG_TAG) {
-        console.debug('Trying to edit unsupported tangentialArc keyword arguments; skipping')
+        console.debug(
+          'Trying to edit unsupported tangentialArc keyword arguments; skipping'
+        )
         return {
           modifiedAst: _node,
           pathToNode,
