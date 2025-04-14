c9587fda073eaf5d175de7eae10d7c192d1209a6
Kevin Nadro <kevin@zoo.dev>
2025-04-14 16:54:04 -0600

    chore: skeleton for web create file provide

--- src/machines/systemIO/systemIOMachineWeb.ts
+++ src/machines/systemIO/systemIOMachineWeb.ts
@@ -1,3 +1,21 @@
 import { systemIOMachine } from '@src/machines/systemIO/systemIOMachine'
+import type { SystemIOContext } from '@src/machines/systemIO/utils'
+import { SystemIOMachineActors } from '@src/machines/systemIO/utils'
+import { fromPromise } from 'xstate'
 
-export const systemIOMachineWeb = systemIOMachine.provide({})
+export const systemIOMachineWeb = systemIOMachine.provide({
+  actors: {
+    [SystemIOMachineActors.createKCLFile]: fromPromise(
+      async ({
+        input,
+      }: {
+        input: {
+          context: SystemIOContext
+          requestedProjectName: string
+          requestedFileName: string
+          requestedCode: string
+        }
+      }) => {}
+    ),
+  },
+})
