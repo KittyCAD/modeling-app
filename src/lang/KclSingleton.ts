3cca4a30af91b6ed34164f8acdb43920d7ce4e77
Kurt Hutten <k.hutten@protonmail.ch>
2025-04-15 23:27:50 +1000

    [BUG] Grab bag of sketch mode bug fixes (#6229)
    * use artifact id for sketch mode entry plane
    
    * fix up re-eval as best as possible
    
    * remove some async dodgyness
    
    * fmt
    
    * fix old sycronous re-execute shit
    
    * add a test
    
    * fix existing test
    
    * add toast for error state
    
    * spelling
    
    * test stuff
    
    * fmt
    
    * fix toast
    
    * test fix
    
    * some other fix ups
    
    * fix test

--- src/lang/KclSingleton.ts
+++ src/lang/KclSingleton.ts
@@ -27,7 +27,6 @@ import type {
   ExecState,
   PathToNode,
   Program,
-  SourceRange,
   VariableMap,
 } from '@src/lang/wasm'
 import { emptyExecState, getKclVersion, parse, recast } from '@src/lang/wasm'
@@ -48,7 +47,7 @@ import type {
 import { jsAppSettings } from '@src/lib/settings/settingsUtils'
 
 import { err, reportRejection } from '@src/lib/trap'
-import { deferExecution, isOverlap, uuidv4 } from '@src/lib/utils'
+import { deferExecution, uuidv4 } from '@src/lib/utils'
 
 interface ExecuteArgs {
   ast?: Node<Program>
@@ -74,6 +73,21 @@ export class KclManager {
   defaultPlanesShown: boolean = false
 
   private _ast: Node<Program> = {
+    body: [],
+    shebang: null,
+    start: 0,
+    end: 0,
+    moduleId: 0,
+    nonCodeMeta: {
+      nonCodeNodes: {},
+      startNodes: [],
+    },
+    innerAttrs: [],
+    outerAttrs: [],
+    preComments: [],
+    commentStart: 0,
+  }
+  _lastAst: Node<Program> = {
     body: [],
     shebang: null,
     start: 0,
@@ -115,13 +129,16 @@ export class KclManager {
   private _kclErrorsCallBack: (errors: KCLError[]) => void = () => {}
   private _diagnosticsCallback: (errors: Diagnostic[]) => void = () => {}
   private _wasmInitFailedCallback: (arg: boolean) => void = () => {}
-  private _executeCallback: () => void = () => {}
   sceneInfraBaseUnitMultiplierSetter: (unit: BaseUnit) => void = () => {}
 
   get ast() {
     return this._ast
   }
   set ast(ast) {
+    if (this._ast.body.length !== 0) {
+      // last intact ast, if the user makes a typo with a syntax error, we want to keep the one before they made that mistake
+      this._lastAst = structuredClone(this._ast)
+    }
     this._ast = ast
     this._astCallBack(ast)
   }
@@ -242,6 +259,8 @@ export class KclManager {
       await this.safeParse(this.singletons.codeManager.code).then((ast) => {
         if (ast) {
           this.ast = ast
+          // on setup, set _lastAst so it's populated.
+          this._lastAst = ast
         }
       })
     })
@@ -271,9 +290,6 @@ export class KclManager {
     this._diagnosticsCallback = setDiagnostics
     this._isExecutingCallback = setIsExecuting
     this._wasmInitFailedCallback = setWasmInitFailed
-  }
-  registerExecuteCallback(callback: () => void) {
-    this._executeCallback = callback
   }
 
   clearAst() {
@@ -336,24 +352,6 @@ export class KclManager {
         })
       }, 200)(null)
     }
-  }
-
-  // Some "objects" have the same source range, such as sketch_mode_start and start_path.
-  // So when passing a range, we need to also specify the command type
-  private mapRangeToObjectId(
-    range: SourceRange,
-    commandTypeToTarget: string
-  ): string | undefined {
-    for (const [artifactId, artifact] of this.artifactGraph) {
-      if (
-        'codeRef' in artifact &&
-        artifact.codeRef &&
-        isOverlap(range, artifact.codeRef.range)
-      ) {
-        if (commandTypeToTarget === artifact.type) return artifactId
-      }
-    }
-    return undefined
   }
 
   async safeParse(code: string): Promise<Node<Program> | null> {
@@ -474,10 +472,9 @@ export class KclManager {
       this.lastSuccessfulVariables = execState.variables
       this.lastSuccessfulOperations = execState.operations
     }
-    this.ast = { ...ast }
+    this.ast = structuredClone(ast)
     // updateArtifactGraph relies on updated executeState/variables
     await this.updateArtifactGraph(execState.artifactGraph)
-    this._executeCallback()
     if (!isInterrupted) {
       this.singletons.sceneInfra.modelingSend({
         type: 'code edit during sketch',
@@ -557,8 +554,7 @@ export class KclManager {
       return
     }
 
-    this.ast = { ...ast }
-    return this.executeAst()
+    return this.executeAst({ ast })
   }
 
   async format() {
