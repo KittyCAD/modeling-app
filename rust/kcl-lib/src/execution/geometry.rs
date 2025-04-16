9cc731220de949f17d68452f04125a9972731a70
Jess Frazelle <github@jessfraz.com>
2025-04-16 14:48:42 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- rust/kcl-lib/src/execution/geometry.rs
+++ rust/kcl-lib/src/execution/geometry.rs
@@ -3,11 +3,14 @@ use std::ops::{Add, AddAssign, Mul};
 use anyhow::Result;
 use indexmap::IndexMap;
 use kittycad_modeling_cmds as kcmc;
+use kittycad_modeling_cmds::ok_response::OkModelingCmdResponse;
+use kittycad_modeling_cmds::websocket::OkWebSocketResponseData;
 use kittycad_modeling_cmds::{each_cmd as mcmd, length_unit::LengthUnit, websocket::ModelingCmdReq, ModelingCmd};
 use parse_display::{Display, FromStr};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+use crate::errors::KclErrorDetails;
 use crate::{
     errors::KclError,
     execution::{types::NumericType, ArtifactId, ExecState, Metadata, TagEngineInfo, TagIdentifier, UnitLen},
@@ -98,11 +101,24 @@ impl ImportedGeometry {
             return Ok(());
         }
 
-        ctx.engine
+        let resp = ctx
+            .engine
             .ensure_async_command_completed(self.id, self.meta.first().map(|m| m.source_range))
             .await?;
 
+        let OkWebSocketResponseData::Modeling {
+            modeling_response: OkModelingCmdResponse::ImportFiles(imported_files),
+        } = &resp
+        else {
+            return Err(KclError::Engine(KclErrorDetails {
+                message: format!("ImportFiles response was not as expected: {:?}", resp),
+                source_ranges: vec![self.meta.first().map(|m| m.source_range).unwrap_or_default()],
+            }));
+        };
+
         self.completed = true;
+        // TODO: we can remove this once katie uses the id we give in the cmd_id
+        self.id = imported_files.object_id;
 
         Ok(())
     }
