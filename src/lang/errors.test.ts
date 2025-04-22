beee661dee66f290b522943689a987b002b334ef
Jess Frazelle <github@jessfraz.com>
2025-04-22 10:16:17 -0700

    move test
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/lang/errors.test.ts
+++ src/lang/errors.test.ts
@@ -2,7 +2,6 @@ import type { KCLError } from '@src/lang/errors'
 import { kclErrorsToDiagnostics } from '@src/lang/errors'
 import { defaultArtifactGraph } from '@src/lang/std/artifactGraph'
 import { topLevelRange } from '@src/lang/util'
-import { errFromErrWithOutputs } from '@src/lang/wasm'
 
 describe('test kclErrToDiagnostic', () => {
   it('converts KCL errors to CodeMirror diagnostics', () => {
@@ -47,17 +46,5 @@ describe('test kclErrToDiagnostic', () => {
         severity: 'error',
       },
     ])
-  })
-})
-
-describe('test errFromErrWithOutputs', () => {
-  it('converts KclErrorWithOutputs to KclError', () => {
-    const blob =
-      '{"error":{"kind":"internal","sourceRanges":[],"msg":"Cache busted"},"operations":[],"artifactCommands":[],"artifactGraph":{"map":{}},"filenames":{},"sourceFiles":{},"defaultPlanes":null}'
-    const error = errFromErrWithOutputs(blob)
-    const errorStr = JSON.stringify(error)
-    expect(errorStr).toEqual(
-      '{"kind":"internal","sourceRange":[0,0,0],"msg":"Cache busted","operations":[],"artifactCommands":[],"artifactGraph":{},"filenames":{},"defaultPlanes":null}'
-    )
   })
 })
