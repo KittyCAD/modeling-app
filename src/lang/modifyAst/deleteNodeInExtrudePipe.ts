d82516007374b0fcb19f491625b4ece11b0d25bd
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2025-04-24 21:42:31 +1000

    fix new circular dependency

---
+++ src/lang/modifyAst/deleteNodeInExtrudePipe.ts
@@ -0,0 +1,27 @@
+import type { Node } from '@rust/kcl-lib/bindings/Node'
+
+import type { PathToNode, Program } from '@src/lang/wasm'
+import { locateExtrudeDeclarator } from '@src/lang/modifyAst/addEdgeTreatment'
+import { err } from '@src/lib/trap'
+
+export function deleteNodeInExtrudePipe(
+  node: PathToNode,
+  ast: Node<Program>
+): Error | void {
+  const pipeIndex = node.findIndex(([_, type]) => type === 'PipeExpression') + 1
+  if (!(node[pipeIndex][0] && typeof node[pipeIndex][0] === 'number')) {
+    return new Error("Couldn't find node to delete in ast")
+  }
+
+  // const lookup = locateExtrudeDeclarator(ast, node)
+  const lookup = locateExtrudeDeclarator(ast, node)
+  if (err(lookup)) {
+    return lookup
+  }
+
+  if (lookup.extrudeDeclarator.init.type !== 'PipeExpression') {
+    return new Error("Couldn't find node to delete in looked up extrusion")
+  }
+
+  lookup.extrudeDeclarator.init.body.splice(node[pipeIndex][0], 1)
+}
