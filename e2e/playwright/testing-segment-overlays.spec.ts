e26c02c17c6aca788128353f9d4f3b633a5a92cb
Jace Browning <jacebrowning@gmail.com>
2025-04-25 09:45:07 -0400

    Remove stale comment

--- e2e/playwright/testing-segment-overlays.spec.ts
+++ e2e/playwright/testing-segment-overlays.spec.ts
@@ -8,8 +8,6 @@ import { expect, test } from '@e2e/playwright/zoo-test'
 
 test.describe('Testing segment overlays', () => {
   test('Hover over a segment should show its overlay, hovering over the input overlays should show its popover, clicking the input overlay should constrain/unconstrain it:\nfor the following segments', () => {
-    // TODO: fix this test on mac after the electron migration
-
     /**
      * Clicks on an constrained element
      * @param {Page} page - The page to perform the action on
