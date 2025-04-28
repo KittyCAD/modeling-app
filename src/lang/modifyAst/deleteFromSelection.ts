9cab3d0033663e9ea33a6f28bac5fd0f0e238394
Adam Chalmers <adam.chalmers@zoo.dev>
2025-04-28 10:59:02 -0500

    Fix loft

--- src/lang/modifyAst/deleteFromSelection.ts
+++ src/lang/modifyAst/deleteFromSelection.ts
@@ -207,11 +207,11 @@ export async function deleteFromSelection(
               extrudeNameToDelete = dec.id.name
             }
             if (
-              // TODO: This is wrong, loft is now a CallExpressionKw.
-              dec.init.type === 'CallExpression' &&
+              dec.init.type === 'CallExpressionKw' &&
               dec.init.callee.name.name === 'loft' &&
-              dec.init.arguments?.[0].type === 'ArrayExpression' &&
-              dec.init.arguments?.[0].elements.some(
+              dec.init.unlabeled !== null &&
+              dec.init.unlabeled.type === 'ArrayExpression' &&
+              dec.init.unlabeled.elements.some(
                 (a) => a.type === 'Name' && a.name.name === varDecName
               )
             ) {
