cb8c8fc5bf90a56565a853d0583d26be37ef515a
Jonathan Tran <jonnytran@gmail.com>
2025-04-28 10:06:03 -0400

    Remove outdated sim test

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -498,27 +498,6 @@ mod double_map_fn {
         super::execute(TEST_NAME, false).await
     }
 }
-mod property_of_object {
-    const TEST_NAME: &str = "property_of_object";
-
-    /// Test parsing KCL.
-    #[test]
-    fn parse() {
-        super::parse(TEST_NAME)
-    }
-
-    /// Test that parsing and unparsing KCL produces the original KCL input.
-    #[tokio::test(flavor = "multi_thread")]
-    async fn unparse() {
-        super::unparse(TEST_NAME).await
-    }
-
-    /// Test that KCL is executed correctly.
-    #[tokio::test(flavor = "multi_thread")]
-    async fn kcl_test_execute() {
-        super::execute(TEST_NAME, false).await
-    }
-}
 mod index_of_array {
     const TEST_NAME: &str = "index_of_array";
 
