fa51b4bbbcb4cc9335bdc46bcfef27a8f2aafd26
Adam Chalmers <adam.chalmers@zoo.dev>
2025-04-29 16:05:45 -0500

    Post-kwargs cleanup (#6571)
    * Remove dead code
    * Stop creating CallExpression and instead create CallExpressionKw

--- src/lang/modifyAst.ts
+++ src/lang/modifyAst.ts
@@ -5,7 +5,6 @@ import type { NonCodeMeta } from '@rust/kcl-lib/bindings/NonCodeMeta'
 
 import {
   createArrayExpression,
-  createCallExpressionStdLib,
   createCallExpressionStdLibKw,
   createExpressionStatement,
   createIdentifier,
@@ -14,7 +13,6 @@ import {
   createLabeledArg,
   createLiteral,
   createLocalName,
-  createObjectExpression,
   createPipeExpression,
   createVariableDeclaration,
   findUniqueName,
@@ -439,9 +437,11 @@ export function loftSketches(
   const modifiedAst = structuredClone(node)
   const name = findUniqueName(node, KCL_DEFAULT_CONSTANT_PREFIXES.LOFT)
   const elements = declarators.map((d) => createLocalName(d.id.name))
-  const loft = createCallExpressionStdLib('loft', [
+  const loft = createCallExpressionStdLibKw(
+    'loft',
     createArrayExpression(elements),
-  ])
+    []
+  )
   const declaration = createVariableDeclaration(name, loft)
   modifiedAst.body.push(declaration)
   const pathToNode: PathToNode = [
@@ -571,101 +571,6 @@ export function addSweep({
   return {
     modifiedAst,
     pathToNode,
-  }
-}
-
-export function revolveSketch(
-  node: Node<Program>,
-  pathToNode: PathToNode,
-  shouldPipe = false,
-  angle: Expr = createLiteral(4)
-):
-  | {
-      modifiedAst: Node<Program>
-      pathToNode: PathToNode
-      pathToRevolveArg: PathToNode
-    }
-  | Error {
-  const _node = structuredClone(node)
-  const _node1 = getNodeFromPath(_node, pathToNode)
-  if (err(_node1)) return _node1
-  const { node: sketchExpression } = _node1
-
-  // determine if sketchExpression is in a pipeExpression or not
-  const _node2 = getNodeFromPath<PipeExpression>(
-    _node,
-    pathToNode,
-    'PipeExpression'
-  )
-  if (err(_node2)) return _node2
-  const { node: pipeExpression } = _node2
-
-  const isInPipeExpression = pipeExpression.type === 'PipeExpression'
-
-  const _node3 = getNodeFromPath<VariableDeclarator>(
-    _node,
-    pathToNode,
-    'VariableDeclarator'
-  )
-  if (err(_node3)) return _node3
-  const { node: variableDeclarator, shallowPath: pathToDecleration } = _node3
-
-  const revolveCall = createCallExpressionStdLib('revolve', [
-    createObjectExpression({
-      angle: angle,
-      // TODO: hard coded 'X' axis for revolve MVP, should be changed.
-      axis: createLocalName('X'),
-    }),
-    createLocalName(variableDeclarator.id.name),
-  ])
-
-  if (shouldPipe) {
-    const pipeChain = createPipeExpression(
-      isInPipeExpression
-        ? [...pipeExpression.body, revolveCall]
-        : [sketchExpression as any, revolveCall]
-    )
-
-    variableDeclarator.init = pipeChain
-    const pathToRevolveArg: PathToNode = [
-      ...pathToDecleration,
-      ['init', 'VariableDeclarator'],
-      ['body', ''],
-      [pipeChain.body.length - 1, 'index'],
-      ['arguments', 'CallExpression'],
-      [0, 'index'],
-    ]
-
-    return {
-      modifiedAst: _node,
-      pathToNode,
-      pathToRevolveArg,
-    }
-  }
-
-  // We're not creating a pipe expression,
-  // but rather a separate constant for the extrusion
-  const name = findUniqueName(node, KCL_DEFAULT_CONSTANT_PREFIXES.REVOLVE)
-  const VariableDeclaration = createVariableDeclaration(name, revolveCall)
-  const sketchIndexInPathToNode =
-    pathToDecleration.findIndex((a) => a[0] === 'body') + 1
-  const sketchIndexInBody = pathToDecleration[sketchIndexInPathToNode][0]
-  if (typeof sketchIndexInBody !== 'number')
-    return new Error('expected sketchIndexInBody to be a number')
-  _node.body.splice(sketchIndexInBody + 1, 0, VariableDeclaration)
-
-  const pathToRevolveArg: PathToNode = [
-    ['body', ''],
-    [sketchIndexInBody + 1, 'index'],
-    ['declaration', 'VariableDeclaration'],
-    ['init', 'VariableDeclarator'],
-    ['arguments', 'CallExpression'],
-    [0, 'index'],
-  ]
-  return {
-    modifiedAst: _node,
-    pathToNode: [...pathToNode.slice(0, -1), [-1, 'index']],
-    pathToRevolveArg,
   }
 }
 
