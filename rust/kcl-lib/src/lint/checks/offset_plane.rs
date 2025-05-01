eafb5f65cf18e25cbc2db95cf4fb2fac3b9de183
Adam Chalmers <adam.chalmers@zoo.dev>
2025-05-01 10:15:29 -0500

    Linter can find either callexpression or kw

--- rust/kcl-lib/src/lint/checks/offset_plane.rs
+++ rust/kcl-lib/src/lint/checks/offset_plane.rs
@@ -5,7 +5,10 @@ use crate::{
     errors::Suggestion,
     execution::{types::UnitLen, PlaneInfo, Point3d},
     lint::rule::{def_finding, Discovered, Finding},
-    parsing::ast::types::{BinaryPart, Expr, LiteralValue, Node as AstNode, ObjectExpression, Program, UnaryOperator},
+    parsing::ast::types::{
+        BinaryPart, CallExpression, CallExpressionKw, Expr, LiteralValue, Node as AstNode, ObjectExpression, Program,
+        UnaryOperator,
+    },
     walk::Node,
     SourceRange,
 };
@@ -124,10 +127,41 @@ fn get_offset(info: &PlaneInfo) -> Option<f64> {
 }
 
 pub fn start_sketch_on_check_specific_plane(node: Node) -> Result<Option<(SourceRange, PlaneName, f64)>> {
-    let Node::CallExpressionKw(call) = node else {
+    match node {
+        Node::CallExpression(node) => start_sketch_on_check_specific_plane_pos(node),
+        Node::CallExpressionKw(node) => start_sketch_on_check_specific_plane_kw(node),
+        _ => Ok(None),
+    }
+}
+
+pub fn start_sketch_on_check_specific_plane_pos(
+    call: &AstNode<CallExpression>,
+) -> Result<Option<(SourceRange, PlaneName, f64)>> {
+    if call.inner.callee.inner.name.name != "startSketchOn" {
+        return Ok(None);
+    }
+
+    if call.arguments.len() != 1 {
+        // we only look for single-argument object patterns, if there's more
+        // than that we don't have a plane decl
+        return Ok(None);
+    }
+
+    let call_source_range = SourceRange::new(
+        call.arguments[0].start(),
+        call.arguments[0].end(),
+        call.arguments[0].module_id(),
+    );
+
+    let Expr::ObjectExpression(arg) = &call.arguments[0] else {
         return Ok(None);
     };
+    common(arg, call_source_range)
+}
 
+pub fn start_sketch_on_check_specific_plane_kw(
+    call: &AstNode<CallExpressionKw>,
+) -> Result<Option<(SourceRange, PlaneName, f64)>> {
     if call.inner.callee.inner.name.name != "startSketchOn" {
         return Ok(None);
     }
@@ -143,7 +177,13 @@ pub fn start_sketch_on_check_specific_plane(node: Node) -> Result<Option<(Source
     let Expr::ObjectExpression(arg) = &unlabeled else {
         return Ok(None);
     };
+    common(arg, call_source_range)
+}
 
+pub fn common(
+    arg: &AstNode<ObjectExpression>,
+    call_source_range: SourceRange,
+) -> Result<Option<(SourceRange, PlaneName, f64)>> {
     let mut origin: Option<Point3d> = None;
     let mut x_vec: Option<Point3d> = None;
     let mut y_vec: Option<Point3d> = None;
