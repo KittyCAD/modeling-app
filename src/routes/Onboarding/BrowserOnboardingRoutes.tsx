e6c8c8574fae4bd7f88df701ca2e326b3609fb8d
Frank Noirot <frankjohnson1993@gmail.com>
2025-05-02 15:26:15 -0400

    Show proof of concept with custom content per route

--- src/routes/Onboarding/BrowserOnboardingRoutes.tsx
+++ src/routes/Onboarding/BrowserOnboardingRoutes.tsx
@@ -1,18 +1,103 @@
-import { browserOnboardingPaths } from '@src/lib/onboardingPaths'
+import {
+  type BrowserOnboardingPath,
+  browserOnboardingPaths,
+} from '@src/lib/onboardingPaths'
 import type { RouteObject } from 'react-router-dom'
 import { OnboardingButtons } from './utils'
 
 type BrowserOnboaringRoute = RouteObject & {
   path: keyof typeof browserOnboardingPaths
 }
+
+const browserOnboardingComponents: Record<BrowserOnboardingPath, JSX.Element> =
+  {
+    '/browser': (
+      <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+        <h1 className="text-xl">/browser</h1>
+        <OnboardingButtons currentSlug="/browser" platform="browser" />
+      </div>
+    ),
+    '/browser/scene': (
+      <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+        <h1 className="text-xl">/browser/scene</h1>
+        <p>BAHAHAHA CUSTOM CONTENT BOIII</p>
+        <OnboardingButtons currentSlug="/browser/scene" platform="browser" />
+      </div>
+    ),
+    '/browser/toolbar': (
+      <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+        <h1 className="text-xl">/browser/toolbar</h1>
+        <OnboardingButtons currentSlug="/browser/toolbar" platform="browser" />
+      </div>
+    ),
+    '/browser/text-to-cad': (
+      <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+        <h1 className="text-xl">/browser/text-to-cad</h1>
+        <OnboardingButtons
+          currentSlug="/browser/text-to-cad"
+          platform="browser"
+        />
+      </div>
+    ),
+    '/browser/text-to-cad-prompt': (
+      <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+        <h1 className="text-xl">/browser/text-to-cad-prompt</h1>
+        <OnboardingButtons
+          currentSlug="/browser/text-to-cad-prompt"
+          platform="browser"
+        />
+      </div>
+    ),
+    '/browser/feature-tree-pane': (
+      <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+        <h1 className="text-xl">/browser/feature-tree-pane</h1>
+        <OnboardingButtons
+          currentSlug="/browser/feature-tree-pane"
+          platform="browser"
+        />
+      </div>
+    ),
+    '/browser/prompt-to-edit': (
+      <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+        <h1 className="text-xl">/browser/prompt-to-edit</h1>
+        <OnboardingButtons
+          currentSlug="/browser/prompt-to-edit"
+          platform="browser"
+        />
+      </div>
+    ),
+    '/browser/prompt-to-edit-prompt': (
+      <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+        <h1 className="text-xl">/browser/prompt-to-edit-prompt</h1>
+        <OnboardingButtons
+          currentSlug="/browser/prompt-to-edit-prompt"
+          platform="browser"
+        />
+      </div>
+    ),
+    '/browser/prompt-to-edit-result': (
+      <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+        <h1 className="text-xl">/browser/prompt-to-edit-result</h1>
+        <OnboardingButtons
+          currentSlug="/browser/prompt-to-edit-result"
+          platform="browser"
+        />
+      </div>
+    ),
+    '/browser/conclusion': (
+      <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+        <h1 className="text-xl">/browser/conclusion</h1>
+        <OnboardingButtons
+          currentSlug="/browser/conclusion"
+          platform="browser"
+        />
+      </div>
+    ),
+  }
+
 export const browserOnboardingRoutes: BrowserOnboaringRoute[] = [
   ...Object.values(browserOnboardingPaths).map((path) => ({
     path,
-    element: (
-      <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
-        <h1 className="text-xl">{path}</h1>
-        <OnboardingButtons currentSlug={path} platform="browser" />
-      </div>
-    ),
+    element: browserOnboardingComponents[path],
   })),
 ]
--- src/routes/Onboarding/DesktopOnboardingRoutes.tsx
+++ src/routes/Onboarding/DesktopOnboardingRoutes.tsx
@@ -1,19 +1,136 @@
-import { desktopOnboardingPaths } from '@src/lib/onboardingPaths'
+import {
+  type DesktopOnboardingPath,
+  desktopOnboardingPaths,
+} from '@src/lib/onboardingPaths'
 import type { RouteObject } from 'react-router-dom'
 import { OnboardingButtons } from './utils'
 
 type DesktopOnboardingRoute = RouteObject & {
   path: keyof typeof desktopOnboardingPaths
 }
+
+const onboardingComponents: Record<DesktopOnboardingPath, JSX.Element> = {
+  '/desktop': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop</h1>
+      <OnboardingButtons currentSlug="/desktop" platform="desktop" />
+    </div>
+  ),
+  '/desktop/scene': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop/scene</h1>
+      <p>BLAHHH THIS IS SPECIAL CONTENT!!!</p>
+      <OnboardingButtons currentSlug="/desktop/scene" platform="desktop" />
+    </div>
+  ),
+  '/desktop/toolbar': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop/toolbar</h1>
+      <OnboardingButtons currentSlug="/desktop/toolbar" platform="desktop" />
+    </div>
+  ),
+  '/desktop/text-to-cad': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop/text-to-cad</h1>
+      <OnboardingButtons
+        currentSlug="/desktop/text-to-cad"
+        platform="desktop"
+      />
+    </div>
+  ),
+  '/desktop/text-to-cad-prompt': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop/text-to-cad-prompt</h1>
+      <OnboardingButtons
+        currentSlug="/desktop/text-to-cad-prompt"
+        platform="desktop"
+      />
+    </div>
+  ),
+  '/desktop/feature-tree-pane': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop/feature-tree-pane</h1>
+      <OnboardingButtons
+        currentSlug="/desktop/feature-tree-pane"
+        platform="desktop"
+      />
+    </div>
+  ),
+  '/desktop/code-pane': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop/code-pane</h1>
+      <OnboardingButtons currentSlug="/desktop/code-pane" platform="desktop" />
+    </div>
+  ),
+  '/desktop/project-pane': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop/project-pane</h1>
+      <OnboardingButtons
+        currentSlug="/desktop/project-pane"
+        platform="desktop"
+      />
+    </div>
+  ),
+  '/desktop/other-panes': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop/other-panes</h1>
+      <OnboardingButtons
+        currentSlug="/desktop/other-panes"
+        platform="desktop"
+      />
+    </div>
+  ),
+  '/desktop/prompt-to-edit': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop/prompt-to-edit</h1>
+      <OnboardingButtons
+        currentSlug="/desktop/prompt-to-edit"
+        platform="desktop"
+      />
+    </div>
+  ),
+  '/desktop/prompt-to-edit-prompt': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop/prompt-to-edit-prompt</h1>
+      <OnboardingButtons
+        currentSlug="/desktop/prompt-to-edit-prompt"
+        platform="desktop"
+      />
+    </div>
+  ),
+  '/desktop/prompt-to-edit-result': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop/prompt-to-edit-result</h1>
+      <OnboardingButtons
+        currentSlug="/desktop/prompt-to-edit-result"
+        platform="desktop"
+      />
+    </div>
+  ),
+  '/desktop/imports': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop/imports</h1>
+      <OnboardingButtons currentSlug="/desktop/imports" platform="desktop" />
+    </div>
+  ),
+  '/desktop/exports': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop/exports</h1>
+      <OnboardingButtons currentSlug="/desktop/exports" platform="desktop" />
+    </div>
+  ),
+  '/desktop/conclusion': (
+    <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
+      <h1 className="text-xl">/desktop/conclusion</h1>
+      <OnboardingButtons currentSlug="/desktop/conclusion" platform="desktop" />
+    </div>
+  ),
+}
+
 export const desktopOnboardingRoutes: DesktopOnboardingRoute[] = [
   ...Object.values(desktopOnboardingPaths).map((path) => ({
     path,
     index: true,
-    element: (
-      <div className="fixed inset-0 z-50 bg-red-300 grid place-content-center">
-        <h1 className="text-xl">{path}</h1>
-        <OnboardingButtons currentSlug={path} platform="desktop" />
-      </div>
-    ),
+    element: onboardingComponents[path],
   })),
 ]
