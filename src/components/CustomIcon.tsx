e03352d54e1df4c884465bbd69d84cb827be1664
Zookeeper Lee <lee@zoo.dev>
2025-05-06 09:06:49 -0400

    Update src/components/CustomIcon.tsx
    Co-authored-by: Frank Noirot <frank@zoo.dev>

--- src/components/CustomIcon.tsx
+++ src/components/CustomIcon.tsx
@@ -1388,8 +1388,6 @@ const CustomIconMap = {
   ),
   infinity: (
     <svg
-      width="13"
-      height="6"
       viewBox="0 0 13 6"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
