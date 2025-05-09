fad5f75fb52aad9b5fcfeff8c2c402c1d8ff9f8b
Pierre Jacquier <pierre@zoo.dev>
2025-05-09 10:52:21 -0400

    Add dotenv to secrets for local testing

--- e2e/playwright/secrets.ts
+++ e2e/playwright/secrets.ts
@@ -1,3 +1,9 @@
+import dotenv from 'dotenv'
+
+// dotenv override when present
+const NODE_ENV = process.env.NODE_ENV 
+dotenv.config({ path: [`.env.${NODE_ENV}.local`, `.env.${NODE_ENV}`] })
+
 const secrets: Record<string, string> = {}
 secrets.token = process.env.token || ''
 // add more env vars here to make them available in CI
