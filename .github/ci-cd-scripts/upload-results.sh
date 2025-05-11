48f73edc0a770a873e4004edec4dae43f735dfaf
Jace Browning <jacebrowning@gmail.com>
2025-05-11 13:57:56 -0400

    Include metadata environment variables with results

--- .github/ci-cd-scripts/upload-results.sh
+++ .github/ci-cd-scripts/upload-results.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 set -euo pipefail
 
-BRANCH="${GITHUB_HEAD_REF:-${GITHUB_REF_NAME:-}}"
-COMMIT="${CI_COMMIT_SHA:-${GITHUB_SHA:-}}"
+if [ -z "${TAB_API_URL:-}" ] || [ -z "${TAB_API_KEY:-}" ]; then
+    exit 0
+fi
+
+branch="${GITHUB_HEAD_REF:-${GITHUB_REF_NAME:-}}"
+commit="${CI_COMMIT_SHA:-${GITHUB_SHA:-}}"
 
 curl --request POST \
   --header "X-API-Key: ${TAB_API_KEY}" \
   --form "project=https://github.com/KittyCAD/modeling-app" \
-  --form "branch=${BRANCH}" \
-  --form "commit=${COMMIT}" \
+  --form "branch=${branch}" \
+  --form "commit=${commit}" \
   --form "tests=@test-results/junit.xml" \
+  --form "CI_COMMIT_SHA=${CI_COMMIT_SHA:-}" \
+  --form "CI_PR_NUMBER=${CI_PR_NUMBER:-}" \
+  --form "GITHUB_BASE_REF=${GITHUB_BASE_REF:-}" \
+  --form "GITHUB_EVENT_NAME=${GITHUB_EVENT_NAME:-}" \
+  --form "GITHUB_HEAD_REF=${GITHUB_HEAD_REF:-}" \
+  --form "GITHUB_REF_NAME=${GITHUB_REF_NAME:-}" \
+  --form "GITHUB_REF=${GITHUB_REF:-}" \
+  --form "GITHUB_SHA=${GITHUB_SHA:-}" \
+  --form "GITHUB_WORKFLOW=${GITHUB_WORKFLOW:-}" \
+  --form "RUNNER_ARCH=${RUNNER_ARCH:-}" \
   ${TAB_API_URL}/api/results/bulk
