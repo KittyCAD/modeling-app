60cd16e9cdd2094979267f0e7009edbed556ae8f
Jess Frazelle <github@jessfraz.com>
2025-05-13 16:58:37 -0700

    remove my sins
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- rust/kcl-lib/src/docs/kcl_doc.rs
+++ rust/kcl-lib/src/docs/kcl_doc.rs
@@ -1262,10 +1262,6 @@ mod test {
                     &result,
                     0.99,
                 );
-                // yield to allow other tests to run
-                tokio::task::yield_now().await;
-                // when running against a local engine give it time to start again.
-                tokio::time::sleep(std::time::Duration::from_secs(1)).await;
             }
         }
 
