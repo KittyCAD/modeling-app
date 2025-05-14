269de903617bc255f0e341b12d9376ae2a62fecd
Jess Frazelle <github@jessfraz.com>
2025-05-14 16:40:34 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -3092,3 +3092,45 @@ mod error_revolve_on_edge_get_edge {
         super::execute(TEST_NAME, true).await
     }
 }
+mod subtract_with_pattern {
+    const TEST_NAME: &str = "subtract_with_pattern";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
+mod subtract_with_pattern_cut_thru {
+    const TEST_NAME: &str = "subtract_with_pattern_cut_thru";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
