bea599e08d0eac3f88cab900e923cfe8aa1bdff0
Frank Noirot <frankjohnson1993@gmail.com>
2025-05-14 14:25:00 -0400

    Move duplicate code into shared `resetCameraPosition` function

--- src/components/ViewControlMenu.tsx
+++ src/components/ViewControlMenu.tsx
@@ -26,6 +26,7 @@ import {
   engineViewIsometricWithoutGeometryPresent,
   engineViewIsometricWithGeometryPresent,
 } from '@src/lib/utils'
+import { resetCameraPosition } from '@src/lib/resetCameraPosition'
 
 export function useViewControlMenuItems() {
   const { state: modelingState, send: modelingSend } = useModelingContext()
@@ -51,33 +52,7 @@ export function useViewControlMenuItems() {
       <ContextMenuDivider />,
       <ContextMenuItem
         onClick={() => {
-          // Gotcha: Playwright E2E tests will be zoom_to_fit, when you try to recreate the e2e test manually
-          // your localhost will do view_isometric. Turn this boolean on to have the same experience when manually
-          // debugging e2e tests
-
-          // We need a padding of 0.1 for zoom_to_fit for all E2E tests since they were originally
-          // written with zoom_to_fit with padding 0.1
-          const padding = 0.1
-          if (isPlaywright()) {
-            engineStreamZoomToFit({ engineCommandManager, padding }).catch(
-              reportRejection
-            )
-          } else {
-            // If the scene is empty you cannot use view_isometric, it will not move the camera
-            if (kclManager.isAstBodyEmpty(kclManager.ast)) {
-              engineViewIsometricWithoutGeometryPresent({
-                engineCommandManager,
-                unit:
-                  kclManager.fileSettings.defaultLengthUnit ||
-                  DEFAULT_DEFAULT_LENGTH_UNIT,
-              }).catch(reportRejection)
-            } else {
-              engineViewIsometricWithGeometryPresent({
-                engineCommandManager,
-                padding,
-              }).catch(reportRejection)
-            }
-          }
+          resetCameraPosition().catch(reportRejection)
         }}
         disabled={shouldLockView}
       >
