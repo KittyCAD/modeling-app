bb8c5f37dca91afac68d3131af2e0eeeecc6cedf
Andrew Varga <grizzly33@gmail.com>
2025-05-15 17:48:06 +0200

    add missing radius line for center circle

--- src/clientSideScene/segments.ts
+++ src/clientSideScene/segments.ts
@@ -64,6 +64,7 @@ import {
   THREE_POINT_ARC_SEGMENT_BODY,
   THREE_POINT_ARC_SEGMENT_DASH,
   getParentGroup,
+  CIRCLE_SEGMENT_RADIUS_BODY,
 } from '@src/clientSideScene/sceneConstants'
 import type { SceneInfra } from '@src/clientSideScene/sceneInfra'
 import {
@@ -350,6 +351,7 @@ class StraightSegment implements SegmentUtils {
         new Vector3(from[0], from[1], 0),
         new Vector3(to[0], to[1], 0)
       )
+      straightSegmentBody.geometry?.dispose()
       straightSegmentBody.geometry = new ExtrudeGeometry(shape, {
         steps: 2,
         bevelEnabled: false,
@@ -607,6 +609,21 @@ class CircleSegment implements SegmentUtils {
     const arcMesh = new Mesh(geometry, mat)
     const meshType = isDraftSegment ? CIRCLE_SEGMENT_DASH : CIRCLE_SEGMENT_BODY
     const arrowGroup = createArrowhead(scale, theme, color)
+
+    const shape = new Shape()
+    const line = new LineCurve3(
+      new Vector3(from[0], from[1], 0),
+      new Vector3(center[0], center[1], 0)
+    )
+    const arrowGeometry = new ExtrudeGeometry(shape, {
+      steps: 2,
+      bevelEnabled: false,
+      extrudePath: line,
+    })
+    const body = new MeshBasicMaterial({ color })
+    const arrowBody = new Mesh(arrowGeometry, body)
+    arrowBody.name = CIRCLE_SEGMENT_RADIUS_BODY
+
     const circleCenterGroup = createCircleCenterHandle(scale, theme, color)
     // A radius indicator that appears from the center to the perimeter
     const radiusIndicatorGroup = createLengthIndicator({
@@ -633,7 +650,13 @@ class CircleSegment implements SegmentUtils {
     }
     group.name = CIRCLE_SEGMENT
 
-    group.add(arcMesh, arrowGroup, circleCenterGroup, radiusIndicatorGroup)
+    group.add(
+      arcMesh,
+      arrowGroup,
+      arrowBody,
+      circleCenterGroup,
+      radiusIndicatorGroup
+    )
     const updateOverlaysCallback = this.update({
       prevSegment,
       input,
@@ -700,6 +723,25 @@ class CircleSegment implements SegmentUtils {
       )
       arrowGroup.scale.set(scale, scale, scale)
       arrowGroup.visible = isHandlesVisible
+
+      const straightSegmentBody = group.getObjectByName(
+        CIRCLE_SEGMENT_RADIUS_BODY
+      ) as Mesh
+      if (straightSegmentBody) {
+        const line = new LineCurve3(
+          new Vector3(center[0], center[1], 0),
+          new Vector3(arrowPoint.x, arrowPoint.y, 0)
+        )
+        const shape = new Shape()
+        shape.moveTo(0, (-SEGMENT_WIDTH_PX / 2) * scale) // The width of the line in px (2.4px in this case)
+        shape.lineTo(0, (SEGMENT_WIDTH_PX / 2) * scale)
+        straightSegmentBody.geometry?.dispose()
+        straightSegmentBody.geometry = new ExtrudeGeometry(shape, {
+          steps: 2,
+          bevelEnabled: false,
+          extrudePath: line,
+        })
+      }
     }
 
     if (radiusLengthIndicator) {
