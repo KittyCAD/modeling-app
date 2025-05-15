e6154e9a0c40cad8956c9e764851a34c37d81427
Kevin Nadro <kevin@zoo.dev>
2025-05-15 14:59:20 -0500

    fix: when creating a sample in a new project and there is only 1 file make the filename main.kcl

--- src/lib/commandBarConfigs/applicationCommandConfig.ts
+++ src/lib/commandBarConfigs/applicationCommandConfig.ts
@@ -10,7 +10,7 @@ import {
   kclSamplesManifestWithNoMultipleFiles,
 } from '@src/lib/kclSamples'
 import { getUniqueProjectName } from '@src/lib/desktopFS'
-import { IS_ML_EXPERIMENTAL, ML_EXPERIMENTAL_MESSAGE } from '@src/lib/constants'
+import { IS_ML_EXPERIMENTAL, ML_EXPERIMENTAL_MESSAGE, PROJECT_ENTRYPOINT } from '@src/lib/constants'
 import toast from 'react-hot-toast'
 import { reportRejection } from '@src/lib/trap'
 import { relevantFileExtensions } from '@src/lang/wasmUtils'
@@ -22,11 +22,13 @@ function onSubmitKCLSampleCreation({
   kclSample,
   uniqueNameIfNeeded,
   systemIOActor,
+  isProjectNew
 }: {
   sample: any
   kclSample: ReturnType<typeof findKclSample>
   uniqueNameIfNeeded: any
   systemIOActor: ActorRefFrom<typeof systemIOMachine>
+  isProjectNew: boolean
 }) {
   if (!kclSample) {
     toast.error('The command could not be submitted, unable to find Zoo sample')
@@ -72,11 +74,13 @@ function onSubmitKCLSampleCreation({
          * Navigates to the single file that could be renamed on disk for duplicates
          */
         const folderNameBecomesKCLFileName = projectPathPart + FILE_EXT
+        // If the project is new create the single file as main.kcl
+        const requestedFileNameWithExtension = isProjectNew ? PROJECT_ENTRYPOINT : folderNameBecomesKCLFileName
         systemIOActor.send({
           type: SystemIOMachineEvents.importFileFromURL,
           data: {
             requestedProjectName: requestedFiles[0].requestedProjectName,
-            requestedFileNameWithExtension: folderNameBecomesKCLFileName,
+            requestedFileNameWithExtension: requestedFileNameWithExtension,
             requestedCode: requestedFiles[0].requestedCode,
           },
         })
@@ -207,6 +211,7 @@ export function createApplicationCommands({
             kclSample,
             uniqueNameIfNeeded,
             systemIOActor,
+            isProjectNew
           })
         } else if (data.source === 'local' && data.path) {
           const clonePath = data.path
@@ -397,6 +402,7 @@ export function createApplicationCommands({
           kclSample,
           uniqueNameIfNeeded,
           systemIOActor,
+          isProjectNew: true
         })
       }
     },
