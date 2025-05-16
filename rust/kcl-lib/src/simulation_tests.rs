cffc06e19b99375d14fe8198d560b50c5cccf4fb
benjamaan476 <ben@zoo.dev>
2025-05-16 17:08:24 +0100

    add new sim test

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -3113,3 +3113,24 @@ mod sketch_on_face_union {
         super::execute(TEST_NAME, true).await
     }
 }
+mod revolve_colinear {
+    const TEST_NAME: &str = "revolve-colinear";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
