8343054c10fe3630c661ca15c30ffd8c03e1c353
Jace Browning <jacebrowning@gmail.com>
2025-05-20 09:58:42 -0400

    Match the styling of the rest of the app
    We don't use all apps monospace fonts anywhere.

--- src/components/CommandBar/CommandBarHeader.tsx
+++ src/components/CommandBar/CommandBarHeader.tsx
@@ -94,7 +94,7 @@ function CommandBarHeader({ children }: React.PropsWithChildren<object>) {
                 {selectedCommand.displayName || selectedCommand.name}
               </span>
               {selectedCommand.status === 'experimental' ? (
-                <span className="text-ml-green font-mono text-xs rounded-full ml-2 px-2 py-1 uppercase tracking-wider border border-ml-green">
+                <span className="text-xs rounded-full ml-2 px-2 py-1 border border-ml-green dark:text-ml-green">
                   experimental
                 </span>
               ) : (
