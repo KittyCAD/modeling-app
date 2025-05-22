32922bfa678c622dca02b33dbca0d3db4f17eadd
Kevin Nadro <kevin@zoo.dev>
2025-05-22 11:32:36 -0500

    fix: reverting testing code

--- src/components/EngineStream.tsx
+++ src/components/EngineStream.tsx
@@ -326,7 +326,7 @@ export const EngineStream = (props: {
      */
   }, [file])
 
-  const IDLE_TIME_MS = Number(3000)
+  const IDLE_TIME_MS = Number(streamIdleMode)
 
   // When streamIdleMode is changed, setup or teardown the timeouts
   const timeoutStart = useRef<number | null>(null)
