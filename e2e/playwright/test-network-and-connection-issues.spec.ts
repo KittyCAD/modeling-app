c105d7b00d7cdd79e2f24358cf7f18eb70914a43
lee-at-zoo-corp <lee@zoo.dev>
2025-05-26 16:40:49 -0400

    Change Connected to Strong

--- e2e/playwright/test-network-and-connection-issues.spec.ts
+++ e2e/playwright/test-network-and-connection-issues.spec.ts
@@ -14,7 +14,7 @@ test.describe('Test network related behaviors', () => {
     'simulate network down and network little widget',
     { tag: '@skipLocalEngine' },
     async ({ page, homePage }) => {
-      const networkToggleConnectedText = page.getByText('Connected')
+      const networkToggleConnectedText = page.getByText('Network health (Strong)')
       const networkToggleWeakText = page.getByText('Ok')
 
       const u = await getUtils(page)
@@ -98,7 +98,7 @@ test.describe('Test network related behaviors', () => {
     { tag: '@skipLocalEngine' },
     async ({ page, homePage, toolbar, scene, cmdBar }) => {
       const networkToggle = page.getByTestId('network-toggle')
-      const networkToggleConnectedText = page.getByText('Connected')
+      const networkToggleConnectedText = page.getByText('Network health (Strong)')
       const networkToggleWeakText = page.getByText('Ok')
 
       const u = await getUtils(page)
@@ -283,8 +283,8 @@ profile001 = startProfile(sketch001, at = [12.34, -12.34])
     { tag: ['@desktop', '@skipLocalEngine'] },
     async ({ page, homePage, scene, cmdBar, toolbar, tronApp }) => {
       const networkToggle = page.getByTestId('network-toggle')
-      const networkToggleConnectedText = page.getByText('Connected')
-      const networkToggleWeakText = page.getByText('Ok')
+      const networkToggleConnectedText = page.getByText('Network health (Strong)')
+      const networkToggleWeakText = page.getByText('Network health (Ok)')
 
       if (!tronApp) {
         fail()
