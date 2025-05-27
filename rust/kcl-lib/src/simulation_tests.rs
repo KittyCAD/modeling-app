083bfe6ec204bb430d29df643b9dee2057a5e95a
Ben Crabbe <ben@zoo.dev>
2025-05-27 14:44:32 +0100

    Only consider staight lines for colinear check when doing a full revolve (#7209)
    * Only consider staight lines for colinear check
    
    * Neaten up code and add test
    
    * Sir, a second sphere has hit the unit test
    
    * Update test snapshots
    
    ---------
    
    Co-authored-by: Adam Chalmers <adam.chalmers@zoo.dev>

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -3456,3 +3456,24 @@ mod subtract_regression12 {
         super::execute(TEST_NAME, true).await
     }
 }
+mod spheres {
+    const TEST_NAME: &str = "spheres";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
