8f6e62df3c86b8a4905202adb48b4913825b3818
Adam Chalmers <adam.chalmers@zoo.dev>
2025-06-04 15:45:23 -0500

    fix: mock should be true

--- rust/kcl-wasm-lib/src/context.rs
+++ rust/kcl-wasm-lib/src/context.rs
@@ -168,7 +168,7 @@ impl Context {
             let err = KclError::internal(format!("Could not deserialize KCL AST. {TRUE_BUG} Details: {e}"));
             KclErrorWithOutputs::no_outputs(err)
         })?;
-        let ctx = self.create_executor_ctx(settings, path, false).map_err(|e| {
+        let ctx = self.create_executor_ctx(settings, path, true).map_err(|e| {
             KclErrorWithOutputs::no_outputs(KclError::internal(format!(
                 "Could not create KCL executor context. {TRUE_BUG} Details: {e}"
             )))
