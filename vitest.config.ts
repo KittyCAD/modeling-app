6a18ee1f43f7e87c537d1890d139a2f2af486c20
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2025-06-04 12:37:24 +1000

    make jest happy

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,13 @@ export default defineConfig({
     globalSetup: './src/test-setup/global-setup.ts',
     environment: 'happy-dom',
     setupFiles: ['./src/setupTests.ts'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+    ],
     deps: {
       external: [/playwright/],
     },
