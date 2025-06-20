c4911eeb18376e167e71c30cd46bccbbc791f9ac
lee-at-zoo-corp <lee@zoo.dev>
2025-06-20 16:39:18 -0400

    wip

--- src/machines/mlEphantManagerMachine.ts
+++ src/machines/mlEphantManagerMachine.ts
@@ -1,6 +1,13 @@
-import { setup, fromPromise } from 'xstate'
+import { assign, setup, fromPromise } from 'xstate'
+import type { ActorRefFrom } from 'xstate'
 import type { Prompt } from '@src/lib/prompt'
 import { generateFakeSubmittedPrompt } from '@src/lib/prompt'
+
+export enum MlEphantManagerStates {
+  Setup = 'setup',
+  Idle = 'idle',
+  Pending = 'pending',
+}
 
 export enum MlEphantManagerTransitionStates {
   GetPromptsThatCreatedProjects = 'get-prompts-that-created-projects',
@@ -23,12 +30,14 @@ export type MlEphantManagerEvents =
     }
   | {
       type: MlEphantManagerTransitionStates.PromptCreateModel
-      // May or may not belong to a project.
-      projectId?: string
+      // For now we fake this, using project_name.
+      projectId: string
+      prompt: string
     }
   | {
       type: MlEphantManagerTransitionStates.PromptEditModel
       projectId: string
+      prompt: string
     }
   | {
       type: MlEphantManagerTransitionStates.PromptRate
@@ -43,10 +52,8 @@ export type MlEphantManagerEvents =
       promptId: string
     }
 
-export enum MlEphantManagerStates {
-  Idle = 'idle',
-  Pending = 'pending',
-}
+// Used to specify a specific event in input properties
+type XSEvent<T> = Extract<MlEphantManagerEvents, { type: T }>
 
 export interface MlEphantManagerContext {
   promptsThatCreatedProjects: Map<Prompt['id'], Prompt>
@@ -54,7 +61,7 @@ export interface MlEphantManagerContext {
   promptsBelongingToProject?: Map<Prompt['id'], Prompt>
 }
 
-export const mlEphantDefaultContext = Object.freeze({
+export const mlEphantDefaultContext = () => ({
   promptsThatCreatedProjects: new Map(),
   promptsBelongingToProject: undefined,
   hasPendingPrompts: false,
@@ -67,32 +74,139 @@ export const mlEphantManagerMachine = setup({
   },
   actors: {
     [MlEphantManagerTransitionStates.GetPromptsThatCreatedProjects]:
-      fromPromise(async function (args) {
-        console.log(arguments)
-        return {
-          promptsThatCreatedProjects: new Array(13)
-            .fill(undefined)
-            .map(generateFakeSubmittedPrompt),
+      fromPromise(async function (args: {
+        input: {
+          context: MlEphantManagerContext
         }
+      }): Promise<Partial<MlEphantManagerContext>> {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            const results = new Array(13)
+              .fill(undefined)
+              .map(generateFakeSubmittedPrompt)
+
+            const promptsThatCreatedProjects = new Map(
+              args.input.context.promptsThatCreatedProjects
+            )
+            results.forEach((result) => {
+              promptsThatCreatedProjects.set(result.id, result)
+            })
+            resolve({
+              promptsThatCreatedProjects,
+            })
+          }, 2000)
+        })
       }),
+    [MlEphantManagerTransitionStates.PromptCreateModel]: fromPromise(
+      async function (args: {
+        input: {
+          event: XSEvent<MlEphantManagerTransitionStates.PromptCreateModel>
+          context: MlEphantManagerContext
+        }
+      }): Promise<Partial<MlEphantManagerContext>> {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            const promptsThatCreatedProjects = new Map(
+              args.input.context.promptsThatCreatedProjects
+            )
+
+            const result = generateFakeSubmittedPrompt()
+            promptsThatCreatedProjects.set(result.id, result)
+
+            resolve({
+              promptsThatCreatedProjects,
+            })
+          }, 5000)
+        })
+      }
+    ),
+    [MlEphantManagerTransitionStates.PromptPollStatus]: fromPromise(
+      async function (args: {
+        input: {
+          context: MlEphantManagerContext
+        }
+      }): Promise<Partial<MlEphantManagerContext>> {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            const promptsThatCreatedProjects = new Map(
+              args.input.context.promptsThatCreatedProjects
+            )
+            // Do the same for prompts of a project
+            promptsThatCreatedProjects.values().forEach((prompt) => {
+              if (prompt.status !== 'pending') return
+              prompt.status = 'completed'
+            })
+            resolve({
+              promptsThatCreatedProjects,
+            })
+          }, 3000)
+        })
+      }
+    ),
   },
 }).createMachine({
-  initial: MlEphantManagerStates.Idle,
-  context: mlEphantDefaultContext,
+  initial: MlEphantManagerStates.Setup,
+  context: mlEphantDefaultContext(),
   states: {
+    [MlEphantManagerStates.Setup]: {
+      invoke: {
+        input: (args: { context: MlEphantManagerContext }) => args,
+        src: MlEphantManagerTransitionStates.GetPromptsThatCreatedProjects,
+        onDone: {
+          target: MlEphantManagerStates.Idle,
+          actions: assign(({ event }) => event.output),
+        },
+        onError: { target: MlEphantManagerStates.Idle },
+      },
+    },
     [MlEphantManagerStates.Idle]: {
       on: {
         [MlEphantManagerTransitionStates.GetPromptsThatCreatedProjects]: {
-          target: MlEphantManagerTransitionStates.GetPromptsThatCreatedProjects,
+          target:
+            MlEphantManagerStates.Pending +
+            '.' +
+            MlEphantManagerTransitionStates.GetPromptsThatCreatedProjects,
         },
       },
     },
     [MlEphantManagerStates.Pending]: {
+      initial: MlEphantManagerStates.Idle,
       states: {
+        // Pop back out to Idle.
+        [MlEphantManagerStates.Idle]: {
+          type: 'final',
+          target: MlEphantManagerStates.Idle,
+        },
         [MlEphantManagerTransitionStates.GetPromptsThatCreatedProjects]: {
           invoke: {
-            input: (args: any) => args,
+            input: (args) => ({
+              context: args.context,
+            }),
             src: MlEphantManagerTransitionStates.GetPromptsThatCreatedProjects,
+            onDone: { target: MlEphantManagerStates.Idle },
+            onError: { target: MlEphantManagerStates.Idle },
+          },
+        },
+        [MlEphantManagerTransitionStates.PromptCreateModel]: {
+          invoke: {
+            input: (args) => ({
+              event:
+                args.event as XSEvent<MlEphantManagerTransitionStates.PromptCreateModel>,
+              context: args.context,
+            }),
+            src: MlEphantManagerTransitionStates.PromptCreateModel,
+            onDone: { target: MlEphantManagerStates.PromptPollStatus },
+            onError: { target: MlEphantManagerStates.PromptPollStatus },
+          },
+        },
+        [MlEphantManagerTransitionStates.PromptPollStatus]: {
+          invoke: {
+            input: (args) => ({
+              event:
+                args.event as XSEvent<MlEphantManagerTransitionStates.PromptPollStatus>,
+              context: args.context,
+            }),
+            src: MlEphantManagerTransitionStates.PromptPollStatus,
             onDone: { target: MlEphantManagerStates.Idle },
             onError: { target: MlEphantManagerStates.Idle },
           },
@@ -101,3 +215,5 @@ export const mlEphantManagerMachine = setup({
     },
   },
 })
+
+export type MlEphantManagerActor = ActorRefFrom<typeof mlEphantManagerMachine>
