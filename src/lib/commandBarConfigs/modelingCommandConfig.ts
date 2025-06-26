eceb2956ccfe57b49c9fbb2a45c03f0cfd1316c7
Pierre Jacquier <pierre@zoo.dev>
2025-06-26 17:12:20 -0400

    Keep subtract solids selection to one until engine supports multiple Follow up to #7614

--- src/lib/commandBarConfigs/modelingCommandConfig.ts
+++ src/lib/commandBarConfigs/modelingCommandConfig.ts
@@ -599,7 +599,8 @@ export const modelingMachineCommandConfig: StateMachineCommandSetConfig<
         inputType: 'selection',
         selectionTypes: ['path'],
         selectionFilter: ['object'],
-        multiple: true,
+        // TODO: turn back to true once engine supports it, the codemod and KCL are ready
+        multiple: false,
         required: true,
         hidden: (context) => Boolean(context.argumentsToSubmit.nodeToEdit),
       },
