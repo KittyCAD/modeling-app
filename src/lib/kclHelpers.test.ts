28bfa8814b9dbfb2495e54f5d449a0c7661ea1cb
Jonathan Tran <jonnytran@gmail.com>
2025-06-26 18:08:41 -0400

    Add display of units in UI modals with calculated KCL values

--- src/lib/kclHelpers.test.ts
+++ src/lib/kclHelpers.test.ts
@@ -2,11 +2,18 @@ import type { ParseResult } from '@src/lang/wasm'
 import { getCalculatedKclExpressionValue } from '@src/lib/kclHelpers'
 
 describe('KCL expression calculations', () => {
-  it('calculates a simple expression', async () => {
+  it('calculates a simple expression without units', async () => {
     const actual = await getCalculatedKclExpressionValue('1 + 2')
     const coercedActual = actual as Exclude<typeof actual, Error | ParseResult>
     expect(coercedActual).not.toHaveProperty('errors')
     expect(coercedActual.valueAsString).toEqual('3')
+    expect(coercedActual?.astNode).toBeDefined()
+  })
+  it('calculates a simple expression with units', async () => {
+    const actual = await getCalculatedKclExpressionValue('1deg + 30deg')
+    const coercedActual = actual as Exclude<typeof actual, Error | ParseResult>
+    expect(coercedActual).not.toHaveProperty('errors')
+    expect(coercedActual.valueAsString).toEqual('31deg')
     expect(coercedActual?.astNode).toBeDefined()
   })
   it('returns NAN for an invalid expression', async () => {
