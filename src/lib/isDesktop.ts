f5938c71f0b8f989da5bc4df9cb0e94d019ecab4
Kevin <kevin@zoo.dev>
2025-06-30 11:59:30 -0500

    fix: restoring this?

--- src/lib/isDesktop.ts
+++ src/lib/isDesktop.ts
@@ -1,12 +1,5 @@
 // https://github.com/electron/electron/issues/2288#issuecomment-337858978
 // Thank you
-
-let canThisBeCached: boolean | null = null
 export function isDesktop(): boolean {
-  if (canThisBeCached === null) {
-    canThisBeCached = navigator.userAgent.toLowerCase().indexOf('electron') > -1
-    return canThisBeCached
-  } else {
-    return canThisBeCached
-  }
+  return navigator.userAgent.toLowerCase().indexOf('electron') > -1
 }
