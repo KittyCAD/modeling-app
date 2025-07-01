6ddbb7a31df05f2624e53db67014eac8f43dfc74
Jonathan Tran <jonnytran@gmail.com>
2025-07-01 14:37:01 -0400

    Fix unknown property error message on sketches and solids (#7632)
    * Fix unknown property error message on sketches and solids
    
    * Add suggestion for common case
    
    * Move test code in file to avoid conflict

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -887,6 +887,27 @@ mod invalid_index_fractional {
         super::execute(TEST_NAME, false).await
     }
 }
+mod property_access_not_found_on_solid {
+    const TEST_NAME: &str = "property_access_not_found_on_solid";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
 mod invalid_member_object {
     const TEST_NAME: &str = "invalid_member_object";
 
