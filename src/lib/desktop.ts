1f58e7e36b001294dbcb137f1529e15584c51f9f
Kevin <kevin@zoo.dev>
2025-07-01 13:42:39 -0500

    fix: I think we got it!

--- src/lib/desktop.ts
+++ src/lib/desktop.ts
@@ -360,7 +360,11 @@ function buildProjectsBasedOffFileEntries(
 }
 
 export async function getProjectInfo(projectPath: string): Promise<Project> {
-  const starMatchedPath = joinOSPaths(projectPath, '**', '*')
+  /**
+   * Gotcha: Glob patterns should always use / as a path separator, even on Windows systems, as \ is used to escape glob characters. 
+   * You can disable this but it comes with gotchas!
+   */
+  const starMatchedPath = projectPath + '/**/*'
   let results = await window.electron.globDir(starMatchedPath)
   const isRelevantFile = (filename: string): boolean =>
     cachedRelevantFileExtensions().some((ext) => filename.endsWith('.' + ext))
