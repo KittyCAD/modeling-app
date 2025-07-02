b97bcdefa8207d384acac09b5375b7c5b0dd440b
Kevin <kevin@zoo.dev>
2025-07-02 16:45:22 -0500

    chore: using the websocket url instead of the hard coded environment variable

--- src/lib/withBaseURL.ts
+++ src/lib/withBaseURL.ts
@@ -1,4 +1,4 @@
-import { VITE_KC_API_BASE_URL, VITE_KC_SITE_BASE_URL } from '@src/env'
+import { VITE_KC_API_BASE_URL, VITE_KC_SITE_BASE_URL, VITE_KC_API_WS_MODELING_URL } from '@src/env'
 
 export function withAPIBaseURL(path: string): string {
   return VITE_KC_API_BASE_URL + path
@@ -6,4 +6,8 @@ export function withAPIBaseURL(path: string): string {
 
 export function withSiteBaseURL(path: string): string {
   return VITE_KC_SITE_BASE_URL + path
+}
+
+export function withWebSocketURL(path: string) : string {
+  return VITE_KC_API_WS_MODELING_URL + path
 }
