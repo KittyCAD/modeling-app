729ce3fdca7d562d9ba0c2e86c170e02886b5611
Pierre Jacquier <pierre@zoo.dev>
2025-07-07 19:32:35 -0400

    Fix existing test

--- src/lang/modifyAst/faces.test.ts
+++ src/lang/modifyAst/faces.test.ts
@@ -1,3 +1,4 @@
+import env from '@src/env'
 import {
   type Artifact,
   assertParse,
@@ -9,12 +10,16 @@ import { enginelessExecutor } from '@src/lib/testHelpers'
 import { err } from '@src/lib/trap'
 import { addShell } from '@src/lang/modifyAst/faces'
 import { stringToKclExpression } from '@src/lib/kclHelpers'
+import { initPromise } from '@src/lang/wasmUtils'
+import { engineCommandManager, kclManager } from '@src/lib/singletons'
 
 async function getAstAndArtifactGraph(code: string) {
   const ast = assertParse(code)
   if (err(ast)) throw ast
+  await kclManager.executeAst({ ast })
+  const artifactGraph = kclManager.artifactGraph
 
-  const { artifactGraph } = await enginelessExecutor(ast)
+  expect(kclManager.errors).toEqual([])
   return { ast, artifactGraph }
 }
 
@@ -53,46 +58,75 @@ async function getKclCommandValue(value: string) {
   return result
 }
 
+// Unfortunately, we need the real engine here it seems to get sweep faces populated
+beforeAll(async () => {
+  await initPromise
+
+  await new Promise((resolve) => {
+    engineCommandManager.start({
+      token: env().VITE_KITTYCAD_API_TOKEN,
+      width: 256,
+      height: 256,
+      setMediaStream: () => {},
+      setIsStreamReady: () => {},
+      callbackOnEngineLiteConnect: () => {
+        resolve(true)
+      },
+    })
+  })
+}, 30_000)
+
+afterAll(() => {
+  engineCommandManager.tearDown()
+})
+
 describe('Testing addShell', () => {
   it('should push a call in pipe if selection was in variable-less pipe', async () => {
-    const code = `startSketchOn(XY)
-  |> circle(center = [0, 0], radius = 10)
-  |> extrude(length = 10)
+    const code = `sketch001 = startSketchOn(XY)
+profile001 = circle(sketch001, center = [0, 0], radius = 10)
+extrude001 = extrude(profile001, length = 10)
 `
     const {
       artifactGraph,
       ast,
       sketches: solids,
     } = await getAstAndSketchSelections(code)
-    console.log('artifactGraph', artifactGraph)
-    const thickness = await getKclCommandValue('1')
+    if (
+      solids.graphSelections.length === 0 ||
+      !solids.graphSelections[0].artifact
+    ) {
+      throw new Error('No solids found in the graph selections')
+    }
+
+    const endFace = [...artifactGraph.values()].find(
+      (a) => a.type === 'cap' && a.subType === 'end'
+    )
+    if (!endFace || endFace.type !== 'cap' || endFace.subType !== 'end') {
+      throw new Error('End face not found in the artifact graph')
+    }
     const faces: Selections = {
-      graphSelections: [],
+      graphSelections: [
+        {
+          codeRef: endFace.faceCodeRef,
+          artifact: endFace,
+        },
+      ],
       otherSelections: [],
     }
+    const thickness = await getKclCommandValue('1')
     const result = addShell({ ast, artifactGraph, solids, faces, thickness })
     if (err(result)) throw result
     const newCode = recast(result.modifiedAst)
     expect(newCode).toContain(code)
-    expect(newCode).toContain(`|> shell(faces = [END], thickness = 1)`)
+    expect(newCode).toContain(
+      `shell001 = shell(extrude001, faces = [END], thickness = 1)`
+    )
     await enginelessExecutor(ast)
   })
 
-  //   it('should push a call with variable if selection was in variable profile', async () => {
-  //     const code = `sketch001 = startSketchOn(XY)
-  // profile001 = circle(sketch001, center = [0, 0], radius = 1)
-  // `
-  //     const { ast, sketches } = await getAstAndSketchSelections(code)
-  //     const length = await getKclCommandValue('2')
-  //     const result = addExtrude({ ast, sketches, length })
-  //     if (err(result)) throw result
-  //     const newCode = recast(result.modifiedAst)
-  //     expect(newCode).toContain(code)
-  //     expect(newCode).toContain(`extrude001 = extrude(profile001, length = 2)`)
-  //     await runNewAstAndCheckForSweep(result.modifiedAst)
-  //   })
+  // TODO: missing multi wall test
+
+  // TODO: missing multi solid test
 
   // TODO: missing edit flow test
-
-  // TODO: missing multi-profile test
 })
