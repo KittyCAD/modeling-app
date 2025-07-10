9b17a2de4142793cf4e8c655691917bdb5776854
Jonathan Tran <jonnytran@gmail.com>
2025-07-10 12:57:25 -0400

    Add appearance to operations (#7748)
    * Add appearance to operations
    
    * Update output
    
    * Add to feature tree with temporary icon

--- src/lib/operations.ts
+++ src/lib/operations.ts
@@ -1108,6 +1108,10 @@ const prepareToEditRevolve: PrepareToEditCallback = async ({
  * for use in the feature tree UI.
  */
 export const stdLibMap: Record<string, StdLibCallInfo> = {
+  appearance: {
+    label: 'Appearance',
+    icon: 'text',
+  },
   chamfer: {
     label: 'Chamfer',
     icon: 'chamfer3d',
