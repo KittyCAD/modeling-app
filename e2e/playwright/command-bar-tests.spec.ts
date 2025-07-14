be925d41aa4f8128601b276bb24f8d18b10dc160
Pierre Jacquier <pierre@zoo.dev>
2025-07-14 11:29:36 -0400

    Add Clear button for non required args Fixes #7772

--- e2e/playwright/command-bar-tests.spec.ts
+++ e2e/playwright/command-bar-tests.spec.ts
@@ -659,6 +659,8 @@ theta = 45deg
     )
   })
 
+  // TODO: add pw test for clear non-required arg button
+
   test('Command palette can be opened via query parameter', async ({
     page,
     homePage,
