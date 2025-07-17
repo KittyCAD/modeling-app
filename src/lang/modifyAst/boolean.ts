6882f4cb2c924e3647f4709d847b18d143b91fc5
Pierre Jacquier <pierre@zoo.dev>
2025-07-17 14:04:51 -0400

    Clean up circ deps

--- src/lang/modifyAst/boolean.ts
+++ src/lang/modifyAst/boolean.ts
@@ -4,22 +4,10 @@ import {
   createCallExpressionStdLibKw,
   createLabeledArg,
 } from '@src/lang/create'
-import {
-  getNodeFromPath,
-  getVariableExprsFromSelection,
-} from '@src/lang/queryAst'
-import { getNodePathFromSourceRange } from '@src/lang/queryAstNodePathUtils'
-import { getFaceCodeRef } from '@src/lang/std/artifactGraph'
-import type {
-  Artifact,
-  ArtifactGraph,
-  PathToNode,
-  Program,
-  VariableDeclaration,
-} from '@src/lang/wasm'
+import { getVariableExprsFromSelection } from '@src/lang/queryAst'
+import type { ArtifactGraph, PathToNode, Program } from '@src/lang/wasm'
 import type { Selections } from '@src/lib/selections'
 import { err } from '@src/lib/trap'
-import { isArray } from '@src/lib/utils'
 import { KCL_DEFAULT_CONSTANT_PREFIXES } from '@src/lib/constants'
 import {
   createVariableExpressionsArray,
@@ -191,115 +179,4 @@ export function addSubtract({
     modifiedAst,
     pathToNode,
   }
-}
-
-/** returns all children of a given artifact, and sorts them DESC by start sourceRange
- * The usecase is we want the last declare relevant  child to use in the boolean operations
- * but might be useful else where.
- */
-export function findAllChildrenAndOrderByPlaceInCode(
-  artifact: Artifact,
-  artifactGraph: ArtifactGraph
-): Artifact[] {
-  const result: string[] = []
-  const stack: string[] = [artifact.id]
-
-  const getArtifacts = (stringIds: string[]): Artifact[] => {
-    const artifactsWithCodeRefs: Artifact[] = []
-    for (const id of stringIds) {
-      const artifact = artifactGraph.get(id)
-      if (artifact) {
-        const codeRef = getFaceCodeRef(artifact)
-        if (codeRef && codeRef.range[1] > 0) {
-          artifactsWithCodeRefs.push(artifact)
-        }
-      }
-    }
-    return artifactsWithCodeRefs
-  }
-
-  const pushToSomething = (
-    resultId: string,
-    childrenIdOrIds: string | string[] | null | undefined
-  ) => {
-    if (isArray(childrenIdOrIds)) {
-      if (childrenIdOrIds.length) {
-        stack.push(...childrenIdOrIds)
-      }
-      result.push(resultId)
-    } else {
-      if (childrenIdOrIds) {
-        stack.push(childrenIdOrIds)
-      }
-      result.push(resultId)
-    }
-  }
-
-  while (stack.length > 0) {
-    const currentId = stack.pop()!
-    const current = artifactGraph.get(currentId)
-    if (current?.type === 'path') {
-      pushToSomething(currentId, current?.sweepId)
-      pushToSomething(currentId, current?.segIds)
-    } else if (current?.type === 'sweep') {
-      pushToSomething(currentId, current?.surfaceIds)
-      const path = artifactGraph.get(current.pathId)
-      if (path && path.type === 'path') {
-        const compositeSolidId = path.compositeSolidId
-        if (compositeSolidId) {
-          result.push(compositeSolidId)
-        }
-      }
-    } else if (current?.type === 'wall' || current?.type === 'cap') {
-      pushToSomething(currentId, current?.pathIds)
-    } else if (current?.type === 'segment') {
-      pushToSomething(currentId, current?.edgeCutId)
-      pushToSomething(currentId, current?.surfaceId)
-    } else if (current?.type === 'edgeCut') {
-      pushToSomething(currentId, current?.surfaceId)
-    } else if (current?.type === 'startSketchOnPlane') {
-      pushToSomething(currentId, current?.planeId)
-    } else if (current?.type === 'plane') {
-      pushToSomething(currentId, current.pathIds)
-    }
-  }
-
-  const resultSet = new Set(result)
-  const codeRefArtifacts = getArtifacts(Array.from(resultSet))
-  const orderedByCodeRefDest = codeRefArtifacts.sort((a, b) => {
-    const aCodeRef = getFaceCodeRef(a)
-    const bCodeRef = getFaceCodeRef(b)
-    if (!aCodeRef || !bCodeRef) {
-      return 0
-    }
-    return bCodeRef.range[0] - aCodeRef.range[0]
-  })
-
-  return orderedByCodeRefDest
-}
-
-/** Returns the last declared in code, relevant child */
-export function getLastVariable(
-  orderedDescArtifacts: Artifact[],
-  ast: Node<Program>
-) {
-  for (const artifact of orderedDescArtifacts) {
-    const codeRef = getFaceCodeRef(artifact)
-    if (codeRef) {
-      const pathToNode = getNodePathFromSourceRange(ast, codeRef.range)
-      const varDec = getNodeFromPath<VariableDeclaration>(
-        ast,
-        pathToNode,
-        'VariableDeclaration'
-      )
-      if (!err(varDec)) {
-        return {
-          variableDeclaration: varDec,
-          pathToNode: pathToNode,
-          artifact,
-        }
-      }
-    }
-  }
-  return null
 }
