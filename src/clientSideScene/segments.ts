4c11577d37ea8584ef6675a96bfbd35b955f287e
Andrew Varga <grizzly33@gmail.com>
2025-07-24 11:33:17 +0200

    remove console.logs

--- src/clientSideScene/segments.ts
+++ src/clientSideScene/segments.ts
@@ -262,8 +262,6 @@ class StraightSegment implements SegmentUtils {
       Math.pow(to[0] - from[0], 2) + Math.pow(to[1] - from[1], 2)
     )
 
-    console.log('update>>')
-
     const pxLength = length / scale
     const shouldHideIdle = pxLength < HIDE_SEGMENT_LENGTH
     const shouldHideHover = pxLength < HIDE_HOVER_SEGMENT_LENGTH
