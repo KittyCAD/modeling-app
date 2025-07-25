7358f598e5664bc70d5902ee91d032a1c953a58a
gserena <serena@zoo.dev>
2025-07-25 12:10:49 -0500

    udpate pool value

--- src/env.ts
+++ src/env.ts
@@ -149,7 +149,7 @@ export default (): EnvironmentVariables => {
       (env.VITE_KITTYCAD_API_TOKEN as string) || undefined,
     VITE_KITTYCAD_SITE_BASE_URL: SITE_URL || undefined,
     VITE_KITTYCAD_SITE_APP_URL: APP_URL || undefined,
-    POOL: pool, // TODO: Rename to ENGINE_POOL to be more descriptive
+    POOL: pr-3380, // TODO: Rename to ENGINE_POOL to be more descriptive
   }
 
   return environmentVariables
