3e69a2f247506f389fca7f4ad5c5f2e8487e4a3f
Kevin <kevin@zoo.dev>
2025-08-12 11:45:19 -0500

    chore: nvm reverting this so I don't do too much in one PR

--- src/components/ProjectSidebarMenu.tsx
+++ src/components/ProjectSidebarMenu.tsx
@@ -19,7 +19,6 @@ import { PATHS } from '@src/lib/paths'
 import { engineCommandManager, kclManager } from '@src/lib/singletons'
 import type { IndexLoaderData } from '@src/lib/types'
 import { commandBarActor } from '@src/lib/singletons'
-import env from '@src/env'
 
 interface ProjectSidebarMenuProps extends React.PropsWithChildren {
   enableMenu?: boolean
@@ -63,14 +62,8 @@ function AppLogoLink({
   file?: IndexLoaderData['file']
 }) {
   const { onProjectClose } = useLspContext()
-  const baseDomain = env().VITE_KITTYCAD_BASE_DOMAIN
-  let backgroundLogoCSS = 'bg-gradient-to-b from-primary to-chalkboard-90'
-  if (baseDomain === 'zoo.dev') {
-    backgroundLogoCSS = ''
-  } else if (baseDomain === 'zoogov.dev') {
-    backgroundLogoCSS = `bg-[url('/flag.svg')] bg-cover bg-no-repeat bg-chalkboard-80`
-  }
-  const wrapperClassName = `relative h-full grid flex-none place-content-center group p-1.5 before:block before:content-[''] before:absolute before:inset-0 before:bottom-1 before:z-[-1] before:bg-primary before:rounded-b-sm ${backgroundLogoCSS}`
+  const wrapperClassName =
+    "relative h-full grid flex-none place-content-center group p-1.5 before:block before:content-[''] before:absolute before:inset-0 before:bottom-1 before:z-[-1] before:bg-primary before:rounded-b-sm"
   const logoClassName = 'w-auto h-4 text-chalkboard-10'
 
   return isDesktop() ? (
@@ -81,7 +74,7 @@ function AppLogoLink({
         kclManager.switchedFiles = true
       }}
       to={PATHS.HOME}
-      className={wrapperClassName}
+      className={wrapperClassName + ' hover:before:brightness-110'}
     >
       <Logo data-onboarding-id="app-logo" className={logoClassName} />
       <span className="sr-only">{APP_NAME}</span>
