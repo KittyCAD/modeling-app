b7f9c4606825eab166c0513c895f3f94b847f2ae
Adam Chalmers <adam.chalmers@zoo.dev>
2025-08-14 16:10:32 -0500

    Adding equations

--- rust/ezpz/src/equations.rs
+++ rust/ezpz/src/equations.rs
@@ -17,7 +17,7 @@ pub struct Eval {
 
 /// Symbolic equation that can be evaluated.
 pub struct Equation {
-    eval: Box<dyn Fn(Vars) -> Result<Eval>>,
+    eval: Box<dyn FnOnce(&Vars) -> Result<Eval>>,
 }
 
 /// Errors that could occur.
@@ -30,15 +30,13 @@ impl Equation {
     /// The simplest kind of equation, a single variable.
     /// E.g. `x`.
     pub fn single_variable(label: Label) -> Self {
-        let eval = move |vars: Vars| {
-            let Some(var_value) = vars.get(&label) else {
-                return Err(Error::MissingVariable(label.clone()));
+        let eval = move |vars: &Vars| {
+            let Some(var_value) = vars.get(&label).copied() else {
+                return Err(Error::MissingVariable(label));
             };
 
-            // Reuse the input variables map as the output derivatives map.
-            let var_value = *var_value;
-            let mut derivatives = vars;
-            derivatives[&label] = 1.0;
+            let mut derivatives = Vars::with_capacity(1);
+            derivatives.insert(label, 1.0);
 
             // All done.
             Ok(Eval {
@@ -49,8 +47,39 @@ impl Equation {
         Self { eval: Box::new(eval) }
     }
 
-    pub fn evaluate(&self, vars: Vars) -> Result<Eval> {
+    pub fn evaluate(self, vars: &Vars) -> Result<Eval> {
         (self.eval)(vars)
+    }
+}
+
+impl std::ops::Add for Equation {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let eval = |vars: &Vars| {
+            let a = self;
+            let b = rhs;
+            let Eval {
+                value: ra,
+                derivatives: das,
+            } = a.evaluate(vars)?;
+            let Eval {
+                value: rb,
+                derivatives: dbs,
+            } = b.evaluate(vars)?;
+            let mut derivatives = Vars::with_capacity(das.len() + dbs.len());
+            for d in das {
+                *derivatives.entry(d.0).or_insert(0.0) += d.1;
+            }
+            for d in dbs {
+                *derivatives.entry(d.0).or_insert(0.0) += d.1;
+            }
+            Ok(Eval {
+                value: ra + rb,
+                derivatives,
+            })
+        };
+        Self { eval: Box::new(eval) }
     }
 }
 
@@ -59,14 +88,38 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_basics() {
-        let actual = Equation::single_variable('a')
-            .evaluate(Vars::from([('a', 14.0)]))
-            .unwrap();
+    fn eval_single_var() {
+        let equation = Equation::single_variable('a');
+
+        let actual = equation.evaluate(&Vars::from([('a', 14.0)])).unwrap();
         let expected = Eval {
             value: 14.0,
             derivatives: IndexMap::from([('a', 1.0)]),
         };
         assert_eq!(actual, expected,);
+    }
+
+    #[test]
+    fn eval_two_vars() {
+        let equation = Equation::single_variable('a') + Equation::single_variable('b');
+
+        let actual = equation.evaluate(&Vars::from([('a', 14.0), ('b', 2.0)])).unwrap();
+        let expected = Eval {
+            value: 16.0,
+            derivatives: IndexMap::from([('a', 1.0), ('b', 1.0)]),
+        };
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn eval_same_var_added() {
+        let equation = Equation::single_variable('a') + Equation::single_variable('a') + Equation::single_variable('b');
+
+        let actual = equation.evaluate(&Vars::from([('a', 14.0), ('b', 3.0)])).unwrap();
+        let expected = Eval {
+            value: 31.0,
+            derivatives: IndexMap::from([('a', 2.0), ('b', 1.0)]),
+        };
+        assert_eq!(actual, expected);
     }
 }
