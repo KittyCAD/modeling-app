2c45200dafd90874d5b512a7bc210a0f4f249035
Kevin <kevin@zoo.dev>
2025-08-20 13:02:19 -0500

    chore: hooked up fs watch for code editor

--- src/components/RouteProvider.tsx
+++ src/components/RouteProvider.tsx
@@ -6,16 +6,17 @@ import {
   useNavigation,
   useRouteLoaderData,
 } from 'react-router-dom'
+import { normalizeLineEndings } from '@src/lib/codeEditor'
 
 import { useAuthNavigation } from '@src/hooks/useAuthNavigation'
 import { useFileSystemWatcher } from '@src/hooks/useFileSystemWatcher'
 import { getAppSettingsFilePath } from '@src/lib/desktop'
-import { PATHS } from '@src/lib/paths'
+import { getStringAfterLastSeparator, PATHS } from '@src/lib/paths'
 import { markOnce } from '@src/lib/performance'
 import { loadAndValidateSettings } from '@src/lib/settings/settingsUtils'
 import { trap } from '@src/lib/trap'
 import type { IndexLoaderData } from '@src/lib/types'
-import { settingsActor } from '@src/lib/singletons'
+import { codeManager, kclManager, settingsActor } from '@src/lib/singletons'
 import { fsManager } from '@src/lang/std/fileSystemManager'
 
 export const RouteProviderContext = createContext({})
@@ -53,7 +54,7 @@ export function RouteProvider({ children }: { children: ReactNode }) {
   }, [])
 
   useFileSystemWatcher(
-    async (eventType: string) => {
+    async (eventType: string, path: string) => {
       // If there is a projectPath but it no longer exists it means
       // it was externally removed. If we let the code past this condition
       // execute it will recreate the directory due to code in
@@ -69,13 +70,43 @@ export function RouteProvider({ children }: { children: ReactNode }) {
 
       // Only reload if there are changes. Ignore everything else.
       if (eventType !== 'change') return
-
       const data = await loadAndValidateSettings(loadedProject?.project?.path)
       settingsActor.send({
         type: 'Set all settings',
         settings: data.settings,
         doNotPersist: true,
       })
+
+      // Try to detect file changes and overwrite the editor
+      if (codeManager.writeCausedByAppCheckedInFileTreeFileSystemWatcher) {
+        codeManager.writeCausedByAppCheckedInFileTreeFileSystemWatcher = false
+        return
+      }
+
+      const fileNameWithExtension = getStringAfterLastSeparator(path)
+      // Is the file from the change event type imported into the currently opened file
+      const isImportedInCurrentFile = kclManager.ast.body.some(
+        (n) =>
+          n.type === 'ImportStatement' &&
+          ((n.path.type === 'Kcl' &&
+            n.path.filename.includes(fileNameWithExtension)) ||
+            (n.path.type === 'Foreign' &&
+              n.path.path.includes(fileNameWithExtension)))
+      )
+
+      const isCurrentFile = loadedProject?.file?.path === path
+      if (isCurrentFile && eventType === 'change') {
+        if (window.electron) {
+          // Your current file is changed, read it from disk and write it into the code manager and execute the AST
+          let code = await window.electron.readFile(path, { encoding: 'utf-8' })
+          code = normalizeLineEndings(code)
+          codeManager.updateCodeStateEditor(code)
+          await kclManager.executeCode()
+        }
+      } else if (isImportedInCurrentFile && eventType === 'change') {
+        // Re execute the file you are in because an imported file was changed
+        await kclManager.executeAst()
+      }
     },
     [settingsPath, loadedProject?.project?.path].filter(
       (x: string | undefined) => x !== undefined
--- src/hooks/useFileSystemWatcher.tsx
+++ src/hooks/useFileSystemWatcher.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react'
 
 import { isDesktop } from '@src/lib/isDesktop'
 import { reportRejection } from '@src/lib/trap'
+import { uuidv4 } from '@src/lib/utils'
 
 type Path = string
 
@@ -11,6 +12,7 @@ type Path = string
 // NodeJS side. Because EventEmitters come bundled with their listener
 // methods it complicates things because we can't just do
 // watcher.addListener(() => { ... }).
+const globalListeners = new Map()
 
 export const useFileSystemWatcher = (
   callback: (eventType: string, path: Path) => Promise<void>,
@@ -18,7 +20,8 @@ export const useFileSystemWatcher = (
 ): void => {
   // Used to track this instance of useFileSystemWatcher.
   // Assign to ref so it doesn't change between renders.
-  const key = useRef(Math.random().toString())
+  const key = useRef(uuidv4())
+  globalListeners.set(key.current, true)
 
   const [output, setOutput] = useState<
     { eventType: string; path: string } | undefined
