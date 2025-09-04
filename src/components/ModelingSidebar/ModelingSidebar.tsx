fb1d4c3fb88351fd88566263c0ecdac6e63c9401
Frank Noirot <frank@zoo.dev>
2025-09-04 13:01:43 -0400

    Make TTC pane button green, change keyboard shortcut to `Ctrl+T` (#8212)
    * Update keyboard shortcut to not type a character
    
    Kind of a cop-out for now, but it side-steps the issue of the shortcut
    typing into the field immediately
    
    * Allow pane configs to set css class overrides, use this for ML
    
    * Tweak size and padding of avatar to not be so close to big green block
    
    * Update snapshots
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/components/ModelingSidebar/ModelingSidebar.tsx
+++ src/components/ModelingSidebar/ModelingSidebar.tsx
@@ -20,6 +20,7 @@ import { MachineManagerContext } from '@src/components/MachineManagerProvider'
 import { ModelingPane } from '@src/components/ModelingSidebar/ModelingPane'
 import type {
   SidebarAction,
+  SidebarCssOverrides,
   SidebarPane,
   SidebarType,
 } from '@src/components/ModelingSidebar/ModelingPanes'
@@ -452,6 +453,7 @@ interface ModelingPaneButtonProps
     keybinding: string
     iconClassName?: string
     iconSize?: 'sm' | 'md' | 'lg'
+    cssClassOverrides?: SidebarCssOverrides
   }
   align: Alignment
   onClick: () => void
@@ -478,11 +480,12 @@ function ModelingPaneButton({
   return (
     <div
       id={paneConfig.id + '-button-holder'}
-      className="relative py-[1px]"
+      className="relative"
       data-onboarding-id={`${paneConfig.id}-pane-button`}
     >
       <button
-        className={`group pointer-events-auto flex items-center justify-center border-0 rounded-none border-transparent dark:border-transparent p-2 m-0 !outline-0 ${paneIsOpen ? ' !border-primary' : ''}`}
+        className={`group pointer-events-auto flex items-center justify-center border-0 rounded-none border-transparent dark:border-transparent p-2 m-0 !outline-0 ${paneIsOpen ? ' !border-primary' : ''} ${paneConfig.cssClassOverrides?.button ?? ''}`}
+        aria-pressed={paneIsOpen}
         style={{
           [tooltipPosition === 'left' ? 'borderLeft' : 'borderRight']:
             'solid 2px transparent',
