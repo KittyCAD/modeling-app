a5cb4627155d79ffd36f33a43371cd4c4af333cd
lee-at-zoo-corp <lee@zoo.dev>
2025-09-11 11:45:04 -0400

    Use React property naming scheme

--- src/components/CustomIcon.tsx
+++ src/components/CustomIcon.tsx
@@ -1701,11 +1701,11 @@ const CustomIconMap = {
       xmlns="http://www.w3.org/2000/svg"
       fill="currentColor"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         {' '}
