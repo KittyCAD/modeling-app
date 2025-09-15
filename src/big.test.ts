f8ddc28f0674e0bd5fff82c1220b936655699658
Kevin <kevin@zoo.dev>
2025-09-15 12:28:07 -0500

    fix: formatting of whitespace in unit test

--- src/big.test.ts
+++ src/big.test.ts
@@ -6287,7 +6287,6 @@ describe('getTagDeclaratorsInProgram.test.ts', () => {
       bodyIndex,
     }
   }
-
   describe(`getTagDeclaratorsInProgram`, () => {
     it(`finds no tag declarators in an empty program`, () => {
       const tagDeclarators = getTagDeclaratorsInProgram(assertParse(''))
@@ -6296,19 +6295,19 @@ describe('getTagDeclaratorsInProgram.test.ts', () => {
     it(`finds a single tag declarators in a small program`, () => {
       const tagDeclarators = getTagDeclaratorsInProgram(
         assertParse(`sketch001 = startSketchOn(XZ)
-  profile001 = startProfile(sketch001, at = [0, 0])
-    |> angledLine(angle = 0, length = 11, tag = $a)`)
+profile001 = startProfile(sketch001, at = [0, 0])
+  |> angledLine(angle = 0, length = 11, tag = $a)`)
       )
       expect(tagDeclarators).toEqual([tagDeclaratorWithIndex('a', 126, 128, 1)])
     })
     it(`finds multiple tag declarators in a small program`, () => {
       const program = `sketch001 = startSketchOn(XZ)
-  profile001 = startProfile(sketch001, at = [0.07, 0])
-    |> angledLine(angle = 0, length = 11, tag = $a)
-    |> angledLine(angle = segAng(a) + 90, length = 11.17, tag = $b)
-    |> angledLine(angle = segAng(a), length = -segLen(a), tag = $c)
-    |> line(endAbsolute = [profileStartX(%), profileStartY(%)])
-    |> close()`
+profile001 = startProfile(sketch001, at = [0.07, 0])
+  |> angledLine(angle = 0, length = 11, tag = $a)
+  |> angledLine(angle = segAng(a) + 90, length = 11.17, tag = $b)
+  |> angledLine(angle = segAng(a), length = -segLen(a), tag = $c)
+  |> line(endAbsolute = [profileStartX(%), profileStartY(%)])
+  |> close()`
       const tagDeclarators = getTagDeclaratorsInProgram(assertParse(program))
       expect(tagDeclarators).toEqual([
         tagDeclaratorWithIndex('a', 129, 131, 1),
@@ -6318,12 +6317,12 @@ describe('getTagDeclaratorsInProgram.test.ts', () => {
     })
     it(`finds tag declarators at different indices`, () => {
       const program = `sketch001 = startSketchOn(XZ)
-  profile001 = startProfile(sketch001, at = [0.07, 0])
-    |> angledLine(angle = 0, length = 11, tag = $a)
-  profile002 = angledLine(profile001, angle = segAng(a) + 90, length = 11.17, tag = $b)
-    |> angledLine(angle = segAng(a), length = -segLen(a), tag = $c)
-    |> line(endAbsolute = [profileStartX(%), profileStartY(%)])
-    |> close()`
+profile001 = startProfile(sketch001, at = [0.07, 0])
+  |> angledLine(angle = 0, length = 11, tag = $a)
+profile002 = angledLine(profile001, angle = segAng(a) + 90, length = 11.17, tag = $b)
+  |> angledLine(angle = segAng(a), length = -segLen(a), tag = $c)
+  |> line(endAbsolute = [profileStartX(%), profileStartY(%)])
+  |> close()`
       const tagDeclarators = getTagDeclaratorsInProgram(assertParse(program))
       expect(tagDeclarators).toEqual([
         tagDeclaratorWithIndex('a', 129, 131, 1),
