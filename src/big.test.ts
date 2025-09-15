3339474ed832ff8d879bdd357a39219bac801bf8
Kevin <kevin@zoo.dev>
2025-09-15 11:33:26 -0500

    fix: moving unit test that isn't in the correct spot

--- src/big.test.ts
+++ src/big.test.ts
@@ -110,6 +110,7 @@ import { vi } from 'vitest'
 import { getConstraintInfoKw } from '@src/lang/std/sketch'
 import { ARG_END_ABSOLUTE, ARG_INTERIOR_ABSOLUTE } from '@src/lang/constants'
 import { removeSingleConstraintInfo } from '@src/lang/modifyAst'
+import { getTagDeclaratorsInProgram } from '@src/lang/queryAst/getTagDeclaratorsInProgram'
 
 // Unfortunately, we need the real engine here it seems to get sweep faces populated
 beforeAll(async () => {
@@ -6263,6 +6264,72 @@ p3 = [342.51, 216.38],
           }, 10_000)
         }
       )
+    })
+  })
+})
+
+describe('getTagDeclaratorsInProgram.test.ts', () => {
+  function tagDeclaratorWithIndex(
+    value: string,
+    start: number,
+    end: number,
+    bodyIndex: number
+  ) {
+    return {
+      tag: {
+        type: 'TagDeclarator',
+        value,
+        start,
+        end,
+        moduleId: 0,
+        commentStart: start,
+      },
+      bodyIndex,
+    }
+  }
+
+  describe(`getTagDeclaratorsInProgram`, () => {
+    it(`finds no tag declarators in an empty program`, () => {
+      const tagDeclarators = getTagDeclaratorsInProgram(assertParse(''))
+      expect(tagDeclarators).toEqual([])
+    })
+    it(`finds a single tag declarators in a small program`, () => {
+      const tagDeclarators = getTagDeclaratorsInProgram(
+        assertParse(`sketch001 = startSketchOn(XZ)
+  profile001 = startProfile(sketch001, at = [0, 0])
+    |> angledLine(angle = 0, length = 11, tag = $a)`)
+      )
+      expect(tagDeclarators).toEqual([tagDeclaratorWithIndex('a', 126, 128, 1)])
+    })
+    it(`finds multiple tag declarators in a small program`, () => {
+      const program = `sketch001 = startSketchOn(XZ)
+  profile001 = startProfile(sketch001, at = [0.07, 0])
+    |> angledLine(angle = 0, length = 11, tag = $a)
+    |> angledLine(angle = segAng(a) + 90, length = 11.17, tag = $b)
+    |> angledLine(angle = segAng(a), length = -segLen(a), tag = $c)
+    |> line(endAbsolute = [profileStartX(%), profileStartY(%)])
+    |> close()`
+      const tagDeclarators = getTagDeclaratorsInProgram(assertParse(program))
+      expect(tagDeclarators).toEqual([
+        tagDeclaratorWithIndex('a', 129, 131, 1),
+        tagDeclaratorWithIndex('b', 195, 197, 1),
+        tagDeclaratorWithIndex('c', 261, 263, 1),
+      ])
+    })
+    it(`finds tag declarators at different indices`, () => {
+      const program = `sketch001 = startSketchOn(XZ)
+  profile001 = startProfile(sketch001, at = [0.07, 0])
+    |> angledLine(angle = 0, length = 11, tag = $a)
+  profile002 = angledLine(profile001, angle = segAng(a) + 90, length = 11.17, tag = $b)
+    |> angledLine(angle = segAng(a), length = -segLen(a), tag = $c)
+    |> line(endAbsolute = [profileStartX(%), profileStartY(%)])
+    |> close()`
+      const tagDeclarators = getTagDeclaratorsInProgram(assertParse(program))
+      expect(tagDeclarators).toEqual([
+        tagDeclaratorWithIndex('a', 129, 131, 1),
+        tagDeclaratorWithIndex('b', 215, 217, 2),
+        tagDeclaratorWithIndex('c', 281, 283, 2),
+      ])
     })
   })
 })
