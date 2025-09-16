744a3329b5daff1e21bad31d7dafab0625af3e54
Frank Noirot <frankjohnson1993@gmail.com>
2025-09-16 09:04:17 -0400

    More sensible names for stack parts

--- src/lib/history/index.ts
+++ src/lib/history/index.ts
@@ -5,65 +5,63 @@ interface HistoryItem {
   redo: () => Promise<boolean>
 }
 
-interface History {
+interface HistoryStack {
   queue: HistoryItem[]
-  point: number | null
+  cursor: number | null
   undo: () => Promise<boolean>
   redo: () => Promise<boolean>
   addItem: (item: HistoryItem) => void
   canUndo: () => boolean
 }
 
-export class HistoryService implements History {
+export class Stack implements HistoryStack {
   queue: HistoryItem[] = []
-  _point = 0
-  lastUpdate = Date.now()
+  _cursor = 0
 
   async undo() {
-    console.log('undoing', this.queue, this.point, this.canUndo())
+    console.log('undoing', this.queue, this.cursor, this.canUndo())
     if (this.canUndo()) {
-      const result = await this.queue[this.point].undo()
-      this.point--
+      const result = await this.queue[this.cursor].undo()
+      this.cursor--
       return result
     }
     return false
   }
 
-  get point() {
-    return this._point
+  get cursor() {
+    return this._cursor
   }
 
-  set point(newPoint: number) {
+  set cursor(newPoint: number) {
     console.log('setting point to ', newPoint)
-    this._point = newPoint
+    this._cursor = newPoint
   }
 
   async redo() {
-    console.log('redoing', this.queue, this.point, this.canRedo())
+    console.log('redoing', this.queue, this.cursor, this.canRedo())
     if (this.canRedo()) {
-      const result = await this.queue[this.point + 1].redo()
-      this.point++
+      const result = await this.queue[this.cursor + 1].redo()
+      this.cursor++
       return result
     }
     return false
   }
 
   canUndo() {
-    return this.queue.length > 0 && this.point > -1
+    return this.queue.length > 0 && this.cursor > -1
   }
   canRedo() {
-    return this.queue.length > 0 && this.point < this.queue.length - 1
+    return this.queue.length > 0 && this.cursor < this.queue.length - 1
   }
 
   addItem(item: HistoryItem) {
     console.log('adding item', this.canRedo())
     if (this.canRedo()) {
       // Clear the redo portion of the history stack
-      this.queue = this.point < 0 ? [] : this.queue.slice(0, this.point + 1)
+      this.queue = this.cursor < 0 ? [] : this.queue.slice(0, this.cursor + 1)
     }
     this.queue.push(item)
-    this.point = this.queue.length - 1
-    this.lastUpdate = Date.now()
-    console.log('adding item', this.queue, this.point)
+    this.cursor = this.queue.length - 1
+    console.log('adding item', this.queue, this.cursor)
   }
 }
