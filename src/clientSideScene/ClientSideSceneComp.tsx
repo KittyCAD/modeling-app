149d4649220b950236865f313d62d648aa548bc2
Frank Noirot <frankjohnson1993@gmail.com>
2025-09-16 09:48:03 -0400

    Make all debuggers use detail elements as a wrapper

--- src/clientSideScene/ClientSideSceneComp.tsx
+++ src/clientSideScene/ClientSideSceneComp.tsx
@@ -705,213 +705,218 @@ export const CamDebugSettings = () => {
   }, [(camSettings as any)?.fov])
 
   return (
-    <div>
-      <h3>cam settings</h3>
-      perspective cam
-      <input
-        type="checkbox"
-        checked={camSettings.type === 'perspective'}
-        onChange={() =>
-          commandBarActor.send({
-            type: 'Find and select command',
-            data: {
-              groupId: 'settings',
-              name: 'modeling.cameraProjection',
-            },
-          })
-        }
-      />
+    <details>
+      <summary>Camera Debugger</summary>
       <div>
-        <button
-          onClick={() => {
-            sceneInfra.camControls.resetCameraPosition().catch(reportRejection)
-          }}
-        >
-          Reset Camera Position
-        </button>
-      </div>
-      {camSettings.type === 'perspective' && (
+        <h3>cam settings</h3>
+        perspective cam
         <input
-          type="range"
-          min="4"
-          max="90"
-          step={0.5}
-          value={fov}
-          onChange={(e) => {
-            setFov(parseFloat(e.target.value))
-
-            throttled({
-              ...camSettings,
-              fov: parseFloat(e.target.value),
+          type="checkbox"
+          checked={camSettings.type === 'perspective'}
+          onChange={() =>
+            commandBarActor.send({
+              type: 'Find and select command',
+              data: {
+                groupId: 'settings',
+                name: 'modeling.cameraProjection',
+              },
             })
-          }}
-          className="w-full cursor-pointer pointer-events-auto"
+          }
         />
-      )}
-      {camSettings.type === 'perspective' && (
         <div>
-          <span>fov</span>
+          <button
+            onClick={() => {
+              sceneInfra.camControls
+                .resetCameraPosition()
+                .catch(reportRejection)
+            }}
+          >
+            Reset Camera Position
+          </button>
+        </div>
+        {camSettings.type === 'perspective' && (
           <input
-            type="number"
-            value={camSettings.fov}
-            className="text-black w-16"
+            type="range"
+            min="4"
+            max="90"
+            step={0.5}
+            value={fov}
             onChange={(e) => {
-              sceneInfra.camControls.setCam({
+              setFov(parseFloat(e.target.value))
+
+              throttled({
                 ...camSettings,
                 fov: parseFloat(e.target.value),
               })
             }}
+            className="w-full cursor-pointer pointer-events-auto"
           />
-        </div>
-      )}
-      {camSettings.type === 'orthographic' && (
-        <>
+        )}
+        {camSettings.type === 'perspective' && (
           <div>
             <span>fov</span>
             <input
               type="number"
-              value={camSettings.zoom}
+              value={camSettings.fov}
               className="text-black w-16"
               onChange={(e) => {
                 sceneInfra.camControls.setCam({
                   ...camSettings,
-                  zoom: parseFloat(e.target.value),
+                  fov: parseFloat(e.target.value),
                 })
               }}
             />
           </div>
-        </>
-      )}
-      <div>
-        Position
-        <ul className="flex">
-          <li>
-            <span className="pl-2 pr-1">x:</span>
-            <input
-              type="number"
-              step={5}
-              data-testid="cam-x-position"
-              value={camSettings.position[0]}
-              className="text-black w-16"
-              onChange={(e) => {
-                sceneInfra.camControls.setCam({
-                  ...camSettings,
-                  position: [
-                    parseFloat(e.target.value),
-                    camSettings.position[1],
-                    camSettings.position[2],
-                  ],
-                })
-              }}
-            />
-          </li>
-          <li>
-            <span className="pl-2 pr-1">y:</span>
-            <input
-              type="number"
-              step={5}
-              data-testid="cam-y-position"
-              value={camSettings.position[1]}
-              className="text-black w-16"
-              onChange={(e) => {
-                sceneInfra.camControls.setCam({
-                  ...camSettings,
-                  position: [
-                    camSettings.position[0],
-                    parseFloat(e.target.value),
-                    camSettings.position[2],
-                  ],
-                })
-              }}
-            />
-          </li>
-          <li>
-            <span className="pl-2 pr-1">z:</span>
-            <input
-              type="number"
-              step={5}
-              data-testid="cam-z-position"
-              value={camSettings.position[2]}
-              className="text-black w-16"
-              onChange={(e) => {
-                sceneInfra.camControls.setCam({
-                  ...camSettings,
-                  position: [
-                    camSettings.position[0],
-                    camSettings.position[1],
-                    parseFloat(e.target.value),
-                  ],
-                })
-              }}
-            />
-          </li>
-        </ul>
+        )}
+        {camSettings.type === 'orthographic' && (
+          <>
+            <div>
+              <span>fov</span>
+              <input
+                type="number"
+                value={camSettings.zoom}
+                className="text-black w-16"
+                onChange={(e) => {
+                  sceneInfra.camControls.setCam({
+                    ...camSettings,
+                    zoom: parseFloat(e.target.value),
+                  })
+                }}
+              />
+            </div>
+          </>
+        )}
+        <div>
+          Position
+          <ul className="flex">
+            <li>
+              <span className="pl-2 pr-1">x:</span>
+              <input
+                type="number"
+                step={5}
+                data-testid="cam-x-position"
+                value={camSettings.position[0]}
+                className="text-black w-16"
+                onChange={(e) => {
+                  sceneInfra.camControls.setCam({
+                    ...camSettings,
+                    position: [
+                      parseFloat(e.target.value),
+                      camSettings.position[1],
+                      camSettings.position[2],
+                    ],
+                  })
+                }}
+              />
+            </li>
+            <li>
+              <span className="pl-2 pr-1">y:</span>
+              <input
+                type="number"
+                step={5}
+                data-testid="cam-y-position"
+                value={camSettings.position[1]}
+                className="text-black w-16"
+                onChange={(e) => {
+                  sceneInfra.camControls.setCam({
+                    ...camSettings,
+                    position: [
+                      camSettings.position[0],
+                      parseFloat(e.target.value),
+                      camSettings.position[2],
+                    ],
+                  })
+                }}
+              />
+            </li>
+            <li>
+              <span className="pl-2 pr-1">z:</span>
+              <input
+                type="number"
+                step={5}
+                data-testid="cam-z-position"
+                value={camSettings.position[2]}
+                className="text-black w-16"
+                onChange={(e) => {
+                  sceneInfra.camControls.setCam({
+                    ...camSettings,
+                    position: [
+                      camSettings.position[0],
+                      camSettings.position[1],
+                      parseFloat(e.target.value),
+                    ],
+                  })
+                }}
+              />
+            </li>
+          </ul>
+        </div>
+        <div>
+          target
+          <ul className="flex">
+            <li>
+              <span className="pl-2 pr-1">x:</span>
+              <input
+                type="number"
+                step={5}
+                data-testid="cam-x-target"
+                value={camSettings.target[0]}
+                className="text-black w-16"
+                onChange={(e) => {
+                  sceneInfra.camControls.setCam({
+                    ...camSettings,
+                    target: [
+                      parseFloat(e.target.value),
+                      camSettings.target[1],
+                      camSettings.target[2],
+                    ],
+                  })
+                }}
+              />
+            </li>
+            <li>
+              <span className="pl-2 pr-1">y:</span>
+              <input
+                type="number"
+                step={5}
+                data-testid="cam-y-target"
+                value={camSettings.target[1]}
+                className="text-black w-16"
+                onChange={(e) => {
+                  sceneInfra.camControls.setCam({
+                    ...camSettings,
+                    target: [
+                      camSettings.target[0],
+                      parseFloat(e.target.value),
+                      camSettings.target[2],
+                    ],
+                  })
+                }}
+              />
+            </li>
+            <li>
+              <span className="pl-2 pr-1">z:</span>
+              <input
+                type="number"
+                step={5}
+                data-testid="cam-z-target"
+                value={camSettings.target[2]}
+                className="text-black w-16"
+                onChange={(e) => {
+                  sceneInfra.camControls.setCam({
+                    ...camSettings,
+                    target: [
+                      camSettings.target[0],
+                      camSettings.target[1],
+                      parseFloat(e.target.value),
+                    ],
+                  })
+                }}
+              />
+            </li>
+          </ul>
+        </div>
       </div>
-      <div>
-        target
-        <ul className="flex">
-          <li>
-            <span className="pl-2 pr-1">x:</span>
-            <input
-              type="number"
-              step={5}
-              data-testid="cam-x-target"
-              value={camSettings.target[0]}
-              className="text-black w-16"
-              onChange={(e) => {
-                sceneInfra.camControls.setCam({
-                  ...camSettings,
-                  target: [
-                    parseFloat(e.target.value),
-                    camSettings.target[1],
-                    camSettings.target[2],
-                  ],
-                })
-              }}
-            />
-          </li>
-          <li>
-            <span className="pl-2 pr-1">y:</span>
-            <input
-              type="number"
-              step={5}
-              data-testid="cam-y-target"
-              value={camSettings.target[1]}
-              className="text-black w-16"
-              onChange={(e) => {
-                sceneInfra.camControls.setCam({
-                  ...camSettings,
-                  target: [
-                    camSettings.target[0],
-                    parseFloat(e.target.value),
-                    camSettings.target[2],
-                  ],
-                })
-              }}
-            />
-          </li>
-          <li>
-            <span className="pl-2 pr-1">z:</span>
-            <input
-              type="number"
-              step={5}
-              data-testid="cam-z-target"
-              value={camSettings.target[2]}
-              className="text-black w-16"
-              onChange={(e) => {
-                sceneInfra.camControls.setCam({
-                  ...camSettings,
-                  target: [
-                    camSettings.target[0],
-                    camSettings.target[1],
-                    parseFloat(e.target.value),
-                  ],
-                })
-              }}
-            />
-          </li>
-        </ul>
-      </div>
-    </div>
+    </details>
   )
 }
--- src/components/EngineCommands.tsx
+++ src/components/EngineCommands.tsx
@@ -23,73 +23,77 @@ export const EngineCommands = () => {
   const [containsFilter, setContainsFilter] = useState('')
   const [customCmd, setCustomCmd] = useState('')
   return (
-    <div>
-      <input
-        className="text-gray-800 bg-slate-300 px-2"
-        data-testid="filter-input"
-        type="text"
-        value={containsFilter}
-        onChange={(e) => setContainsFilter(e.target.value)}
-        placeholder="Filter"
-      />
-      <div className="max-w-xl max-h-36 overflow-auto">
-        {engineCommands.map((command, index) => {
-          const stringer = JSON.stringify(command)
-          if (containsFilter && !stringer.includes(containsFilter)) return null
-          return (
-            <pre className="text-xs" key={index}>
-              <code
-                key={index}
-                data-message-type={command.type}
-                data-command-type={
-                  (command.type === 'send-modeling' ||
-                    command.type === 'send-scene') &&
-                  command.data.type === 'modeling_cmd_req'
-                    ? command?.data?.cmd?.type
-                    : ''
-                }
-                data-command-id={
-                  (command.type === 'send-modeling' ||
-                    command.type === 'send-scene') &&
-                  command.data.type === 'modeling_cmd_req'
-                    ? command.data.cmd_id
-                    : ''
-                }
-                data-receive-command-type={
-                  command.type === 'receive-reliable' ? command.cmd_type : ''
-                }
-              >
-                {JSON.stringify(command, null, 2)}
-              </code>
-            </pre>
-          )
-        })}
-      </div>
-      <button
-        data-testid="clear-commands"
-        onClick={() => clearEngineCommands()}
-      >
-        Clear
-      </button>
-      <br />
-      <input
-        className="text-gray-800 bg-slate-300 px-2"
-        type="text"
-        value={customCmd}
-        onChange={(e) => setCustomCmd(e.target.value)}
-        placeholder="JSON command"
-        data-testid="custom-cmd-input"
-      />
-      <button
-        data-testid="custom-cmd-send-button"
-        onClick={() => {
-          engineCommandManager
-            .sendSceneCommand(JSON.parse(customCmd))
-            .catch(reportRejection)
-        }}
-      >
-        Send custom command
-      </button>
-    </div>
+    <details>
+      <summary>Engine Commands</summary>
+      <div>
+        <input
+          className="text-gray-800 bg-slate-300 px-2"
+          data-testid="filter-input"
+          type="text"
+          value={containsFilter}
+          onChange={(e) => setContainsFilter(e.target.value)}
+          placeholder="Filter"
+        />
+        <div className="max-w-xl max-h-36 overflow-auto">
+          {engineCommands.map((command, index) => {
+            const stringer = JSON.stringify(command)
+            if (containsFilter && !stringer.includes(containsFilter))
+              return null
+            return (
+              <pre className="text-xs" key={index}>
+                <code
+                  key={index}
+                  data-message-type={command.type}
+                  data-command-type={
+                    (command.type === 'send-modeling' ||
+                      command.type === 'send-scene') &&
+                    command.data.type === 'modeling_cmd_req'
+                      ? command?.data?.cmd?.type
+                      : ''
+                  }
+                  data-command-id={
+                    (command.type === 'send-modeling' ||
+                      command.type === 'send-scene') &&
+                    command.data.type === 'modeling_cmd_req'
+                      ? command.data.cmd_id
+                      : ''
+                  }
+                  data-receive-command-type={
+                    command.type === 'receive-reliable' ? command.cmd_type : ''
+                  }
+                >
+                  {JSON.stringify(command, null, 2)}
+                </code>
+              </pre>
+            )
+          })}
+        </div>
+        <button
+          data-testid="clear-commands"
+          onClick={() => clearEngineCommands()}
+        >
+          Clear
+        </button>
+        <br />
+        <input
+          className="text-gray-800 bg-slate-300 px-2"
+          type="text"
+          value={customCmd}
+          onChange={(e) => setCustomCmd(e.target.value)}
+          placeholder="JSON command"
+          data-testid="custom-cmd-input"
+        />
+        <button
+          data-testid="custom-cmd-send-button"
+          onClick={() => {
+            engineCommandManager
+              .sendSceneCommand(JSON.parse(customCmd))
+              .catch(reportRejection)
+          }}
+        >
+          Send custom command
+        </button>
+      </div>{' '}
+    </details>
   )
 }
