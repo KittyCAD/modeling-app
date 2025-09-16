be59b292ee65d62e5f66f28f1eb4ec1d03e35e2e
Kevin <kevin@zoo.dev>
2025-09-16 12:52:39 -0500

    fix: always needs to be dark in testing environments anything else would need to fix it within a test

--- src/network/connectionManager.ts
+++ src/network/connectionManager.ts
@@ -57,7 +57,7 @@ import {
 export class ConnectionManager extends EventTarget {
   started: boolean
   inSequence = 1
-  _camControlsCameraChange = () => {}
+  _camControlsCameraChange = () => { }
   id: string
 
   /**
@@ -113,10 +113,10 @@ export class ConnectionManager extends EventTarget {
       [localUnsubscribeId: string]: (a: any) => void
     }
   } = {} as any
-  _commandLogCallBack: (command: CommandLog[]) => void = () => {}
+  _commandLogCallBack: (command: CommandLog[]) => void = () => { }
   // Rogue runtime dependency from the modeling machine. hope it is there!
   modelingSend: ReturnType<typeof useModelingContext>['send'] =
-    (() => {}) as any
+    (() => { }) as any
   // Any event listener into this map to be cleaned up later
   // helps avoids duplicates as well
   allEventListeners: Map<string, IEventListenerTracked>
@@ -134,10 +134,10 @@ export class ConnectionManager extends EventTarget {
       // completely overwrite the settings
       this.settings = settings
     } else {
+      // Gotcha: This runs in testing environments
       this.settings = {
         pool: null,
-        // Should this be light for E2E tests?
-        theme: Themes.Light,
+        theme: Themes.Dark,
         highlightEdges: true,
         enableSSAO: true,
         showScaleGrid: false,
@@ -486,7 +486,7 @@ export class ConnectionManager extends EventTarget {
       this.connection.unreliableDataChannel &&
       !forceWebsocket
     ) {
-      ;(cmd as any).sequence = this.outSequence
+      ; (cmd as any).sequence = this.outSequence
       this.outSequence++
       this.connection.unreliableSend(command)
       return Promise.resolve(null)
@@ -511,7 +511,7 @@ export class ConnectionManager extends EventTarget {
       command.cmd.type === 'default_camera_look_at' ||
       command.cmd.type === ('default_camera_perspective_settings' as any)
     ) {
-      ;(cmd as any).sequence = this.outSequence++
+      ; (cmd as any).sequence = this.outSequence++
     }
     // since it's not mouse drag or highlighting send over TCP and keep track of the command
     return this.sendCommand(
