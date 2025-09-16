d825f15f6402312b06d9bfd68609025681dc3579
Kevin <kevin@zoo.dev>
2025-09-16 07:32:22 -0500

    fix: fmt

--- src/hooks/network/useOnFileRoute.tsx
+++ src/hooks/network/useOnFileRoute.tsx
@@ -1,10 +1,7 @@
-
 /**
  * When the router switches from one /file route to another /file route
  * the entire DOM tree will not unmount and mount. This means <App/> will not remount
- * 
+ *
  * If a connection is established with the engine and a /file route happens we need to execute the code
  */
-export const useOnFileRoute = () => {
-
-}
+export const useOnFileRoute = () => {}
