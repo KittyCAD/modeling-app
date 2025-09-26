164569348a639ceae24fc2575e499e2e1fdd7658
Zookeeper Lee <lee@zoo.dev>
2025-09-26 17:25:06 -0400

    Add ability to force tools (#8374)
    * Add forced tools
    
    * fix make
    
    * saoeuhoantehuao
    
    * staoeuhseoant
    
    * Color fixes for theme-ability
    
    * Add overflow behavior
    
    * Depluralize "1 Tools"
    
    * Button styling tweaks
    
    * Support to show selections
    
    ---------
    
    Co-authored-by: Frank Noirot <frankjohnson1993@gmail.com>

--- src/machines/mlEphantManagerMachine2.ts
+++ src/machines/mlEphantManagerMachine2.ts
@@ -2,6 +2,7 @@ import { BSON } from 'bson'
 import type {
   MlCopilotClientMessage,
   MlCopilotServerMessage,
+  MlCopilotTool,
 } from '@kittycad/lib'
 import { assertEvent, assign, setup, fromPromise } from 'xstate'
 import { createActorContext } from '@xstate/react'
@@ -73,6 +74,7 @@ export type MlEphantManagerEvents2 =
       projectFiles: FileMeta[]
       selections: Selections
       artifactGraph: ArtifactGraph
+      forcedTools: Set<MlCopilotTool>
     }
   | {
       type: MlEphantManagerTransitions2.ResponseReceive
@@ -384,6 +386,7 @@ export const mlEphantManagerMachine2 = setup({
         project_name: requestData.body.project_name,
         source_ranges: requestData.body.source_ranges,
         current_files: filesAsByteArrays,
+        forced_tools: Array.from(event.forcedTools),
       }
 
       context.ws.send(JSON.stringify(request))
