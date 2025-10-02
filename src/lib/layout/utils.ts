f7f5bdff8b519f125173da3aa74bcce685a6779f
Frank Noirot <frankjohnson1993@gmail.com>
2025-10-02 00:16:58 -0400

    Get Pane layout working alright too

--- src/lib/layout/utils.ts
+++ src/lib/layout/utils.ts
@@ -1,4 +1,13 @@
-import type { Layout } from '@src/lib/layout/types'
+import type {
+  Direction,
+  Layout,
+  Orientation,
+  Side,
+  StartEnd,
+} from '@src/lib/layout/types'
+import type React from 'react'
+import { capitaliseFC } from '../utils'
+import { TooltipProps } from '@src/components/Tooltip'
 
 /**
  * A split area must have the same number of sizes as children.
@@ -10,4 +19,71 @@ export function hasValidSizes(area: Layout & { type: 'splits' }) {
     area.sizes.length === area.children.length &&
     area.sizes.every((p) => Number.isInteger(p) && p > 0 && p < 100)
   )
+}
+
+export function getOppositeSide(side: Side): Side {
+  switch (side) {
+    case 'inline-start':
+      return 'inline-end'
+    case 'inline-end':
+      return 'inline-start'
+    case 'block-start':
+      return 'block-end'
+    case 'block-end':
+      return 'block-start'
+  }
+}
+export function logicalSideToTooltipPosition(
+  side: Side
+): TooltipProps['position'] {
+  switch (side) {
+    case 'inline-start':
+      return 'left'
+    case 'inline-end':
+      return 'right'
+    case 'block-start':
+      return 'top'
+    case 'block-end':
+      return 'bottom'
+  }
+}
+export function sideToTailwindLayoutDirection(
+  side: Side
+): React.HTMLProps<HTMLElement>['className'] {
+  switch (side) {
+    case 'inline-start':
+      return 'flex-row'
+    case 'inline-end':
+      return 'flex-row-reverse'
+    case 'block-start':
+      return 'flex-col'
+    case 'block-end':
+      return 'flex-col-reverse'
+  }
+}
+
+export function sideToTailwindTabDirection(
+  side: Side
+): React.HTMLProps<HTMLElement>['className'] {
+  if (side.includes('inline')) {
+    return 'flex-col'
+  }
+
+  return 'flex-row'
+}
+
+export function sideToSplitDirection(side: Side): Direction {
+  if (side.includes('inline')) {
+    return 'horizontal'
+  }
+
+  return 'vertical'
+}
+
+export function getOppositionDirection(direction: Direction): Direction {
+  return direction === 'horizontal' ? 'vertical' : 'horizontal'
+}
+
+export function sideToReactCss(side: Side): string {
+  return side.split('-').map(capitaliseFC).join('')
 }
