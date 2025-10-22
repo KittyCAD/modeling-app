db32e329554d87b92cfe70fdbf4191a9303c2781
Jonathan Tran <jonnytran@gmail.com>
2025-10-22 12:47:00 -0400

    Revert building ts-rs bindings for kcl-api

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -4,14 +4,12 @@ set -euo pipefail
 rm -rf rust/kcl-wasm-lib/pkg
 mkdir -p rust/kcl-wasm-lib/pkg
 rm -rf rust/kcl-lib/bindings
-rm -rf rust/kcl-api/bindings
 
 cd rust
 export RUSTFLAGS='--cfg getrandom_backend="wasm_js"'
 wasm-pack build kcl-wasm-lib --release --target web --out-dir pkg
 export RUSTFLAGS=''
 cargo test -p kcl-lib --features artifact-graph export_bindings
-cargo test -p kcl-api export_bindings
 cd ..
 
 cp rust/kcl-wasm-lib/pkg/kcl_wasm_lib_bg.wasm public
