bd1ff08de19b08535ea53fd5a43288e0373fc3bb
Pierre Jacquier <pierre@zoo.dev>
2025-10-24 13:48:23 -0400

    Validate codemod output with in-review args directly in the palette (#8595)
    * Draft: Validate codemod output with in-review args in the command palette directly
    Fixes #8593
    
    * CSS cleanup
    
    * Extend to all sweeps
    
    * Add Shell and CSG
    
    * Fix test and add nodeToEdit clone gotcha caught by test
    
    * Touch up on error display
    
    * Fix offset plane test
    
    * Support helix
    
    * Support all transforms
    
    * Support patterns
    
    * Remove TODO
    
    * Apply suggestion from @graphite-app[bot]
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    * Fix import following graphite's suggestion
    
    * Fix string | ReactNode types, thanks @lee-at-zoo-corp
    
    * Better typing on context
    
    * Move the validation execution to 'Validate all arguments', change its name to reviewValidation to decouple from reviewMessage
    
    * Clean up types for reviewMessage while we're at it, and remove logs
    
    * Extra check
    
    * Removing the need for Review on error, hopefully less breaking the world!
    
    * CSS tweak on disabled submit button
    
    * Remove Error: prefix
    
    ---------
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    Co-authored-by: Frank Noirot <frankjohnson1993@gmail.com>

--- src/lib/commandTypes.ts
+++ src/lib/commandTypes.ts
@@ -87,11 +87,9 @@ export type Command<
   groupId: T['id']
   needsReview: boolean
   reviewMessage?:
-    | string
     | ReactNode
-    | ((
-        commandBarContext: { argumentsToSubmit: Record<string, unknown> } // Should be the commandbarMachine's context, but it creates a circular dependency
-      ) => string | ReactNode)
+    | ((commandBarContext: CommandBarContext) => ReactNode)
+  reviewValidation?: (context: CommandBarContext) => Promise<void | Error>
   machineActor?: Actor<T>
   onSubmit: (data?: CommandSchema) => void
   onCancel?: () => void
