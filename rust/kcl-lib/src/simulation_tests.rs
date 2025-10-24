d8d15913eb3c2a4d0733b7f01ba251927189160d
Jonathan Tran <jonnytran@gmail.com>
2025-10-24 18:58:32 -0400

    Add coincident() support for a known segment point and a var point2d, and two var point2ds

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -4096,3 +4096,24 @@ mod sketch_block_points_coincident_simple {
         super::execute(TEST_NAME, true).await
     }
 }
+mod sketch_block_lines_coincident_simple {
+    const TEST_NAME: &str = "sketch_block_lines_coincident_simple";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
