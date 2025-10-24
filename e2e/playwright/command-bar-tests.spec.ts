b1991934715c7081057af55179e17a9ed002225b
Frank Noirot <frankjohnson1993@gmail.com>
2025-10-24 15:45:45 -0400

    Add a web version of the query param cmd e2e test

--- e2e/playwright/command-bar-tests.spec.ts
+++ e2e/playwright/command-bar-tests.spec.ts
@@ -688,7 +688,7 @@ theta = 45deg
     )
   })
 
-  test('Command palette can be opened via query parameter', async ({
+  test('Command palette can be opened via query parameter - desktop', async ({
     page,
     homePage,
     cmdBar,
@@ -710,6 +710,25 @@ theta = 45deg
       highlightedHeaderArg: 'value',
     })
   })
+
+  test(
+    'Command palette can be opened via query parameter - web',
+    { tag: '@web' },
+    async ({ page, cmdBar }) => {
+      await page.goto(`${page.url()}/?cmd=app.theme&groupId=settings`)
+      await cmdBar.expectState({
+        stage: 'arguments',
+        commandName: 'Settings · app · theme',
+        currentArgKey: 'value',
+        currentArgValue: '',
+        headerArguments: {
+          Level: 'user',
+          Value: '',
+        },
+        highlightedHeaderArg: 'value',
+      })
+    }
+  )
 
   test('Text-to-CAD command can be closed with escape while in prompt', async ({
     page,
