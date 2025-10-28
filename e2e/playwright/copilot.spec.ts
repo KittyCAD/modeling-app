13c986578d09378d44f753eae5c75ddad2dde019
Pierre Jacquier <pierrejacquier39@gmail.com>
2025-10-28 09:48:47 -0400

    Working locally

--- e2e/playwright/copilot.spec.ts
+++ e2e/playwright/copilot.spec.ts
@@ -2,6 +2,7 @@ import { expect, test } from '@e2e/playwright/zoo-test'
 
 test.describe('Copilot tests', () => {
   test('Happy path: new project, easy prompt, good result', async ({
+    page,
     editor,
     homePage,
     scene,
@@ -9,20 +10,23 @@ test.describe('Copilot tests', () => {
     cmdBar,
     copilot,
   }) => {
+    await page.setBodyDimensions({ width: 1500, height: 1000 })
     await homePage.goToModelingScene()
     await scene.settled(cmdBar)
 
-    await test.step(`Submit prompt`, async () => {
+    await test.step(`Submit basic prompt`, async () => {
       await toolbar.closePane('code')
       await toolbar.openPane('ttc')
-      await copilot.conversationInput.fill(
-        'Design a water bottle, use defaults'
-      )
+      await copilot.conversationInput.fill('make a cube')
       await copilot.submitButton.click()
       await expect(copilot.placeHolderResponse).toBeVisible()
       await expect(copilot.placeHolderResponse).not.toBeVisible({
         timeout: 120_000,
       })
+      await toolbar.openPane('code')
+      await editor.expectEditor.toContain('cube')
+      await editor.expectEditor.toContain('extrude')
+      await scene.settled(cmdBar)
     })
   })
 })
