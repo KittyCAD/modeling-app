2c597b730093ec09e3f86ed0ad770b713cec75c8
Pierre Jacquier <pierre@zoo.dev>
2025-11-05 09:39:58 -0500

    More descriptive prompt

--- e2e/playwright/copilot.spec.ts
+++ e2e/playwright/copilot.spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '@e2e/playwright/zoo-test'
+import { SidebarId } from '@src/components/layout/areas'
 
 test.describe('Copilot tests', () => {
   test('Happy path: new project, easy prompt, good result', async ({
@@ -16,8 +17,11 @@ test.describe('Copilot tests', () => {
 
     await test.step(`Submit basic prompt`, async () => {
       await toolbar.closePane('code')
-      await toolbar.openPane('ttc')
-      await copilot.conversationInput.fill('make a cube')
+      // TODO: fix the 'as' this it's not making sense
+      await toolbar.openPane('ttc' as SidebarId)
+      await copilot.conversationInput.fill(
+        'make a 10x10x10cm cube centered on the origin'
+      )
       await copilot.submitButton.click()
       await expect(copilot.placeHolderResponse).toBeVisible()
       await expect(copilot.placeHolderResponse).not.toBeVisible({
