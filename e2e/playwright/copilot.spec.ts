c188e7bd649b89427526dd599f6d14529d607c40
Pierre Jacquier <pierre@zoo.dev>
2025-11-05 16:29:56 -0500

    Test both web and desktop

--- e2e/playwright/copilot.spec.ts
+++ e2e/playwright/copilot.spec.ts
@@ -1,8 +1,15 @@
 import { expect, test } from '@e2e/playwright/zoo-test'
 import { DefaultLayoutPaneID } from '@src/lib/layout/configs/default'
+import type { Page } from '@playwright/test'
+import type { EditorFixture } from '@e2e/playwright/fixtures/editorFixture'
+import type { HomePageFixture } from '@e2e/playwright/fixtures/homePageFixture'
+import type { SceneFixture } from '@e2e/playwright/fixtures/sceneFixture'
+import type { ToolbarFixture } from '@e2e/playwright/fixtures/toolbarFixture'
+import type { CmdBarFixture } from '@e2e/playwright/fixtures/cmdBarFixture'
+import type { CopilotFixture } from '@e2e/playwright/fixtures/copilotFixture'
 
 test.describe('Copilot tests', () => {
-  test('Happy path: new project, easy prompt, good result', async ({
+  async function runHappyPathTest({
     page,
     editor,
     homePage,
@@ -10,7 +17,15 @@ test.describe('Copilot tests', () => {
     toolbar,
     cmdBar,
     copilot,
-  }) => {
+  }: {
+    page: Page
+    editor: EditorFixture
+    homePage: HomePageFixture
+    scene: SceneFixture
+    toolbar: ToolbarFixture
+    cmdBar: CmdBarFixture
+    copilot: CopilotFixture
+  }) {
     await page.setBodyDimensions({ width: 1500, height: 1000 })
     await homePage.goToModelingScene()
     await scene.settled(cmdBar)
@@ -33,5 +48,37 @@ test.describe('Copilot tests', () => {
       await scene.settled(cmdBar)
       // TODO: maybe check for a sweep artifact in the debug pane?
     })
-  })
+  }
+
+  test(
+    'Web happy path: new project, easy prompt, good result',
+    { tag: ['@web'] },
+    async ({ page, editor, homePage, scene, toolbar, cmdBar, copilot }) => {
+      await runHappyPathTest({
+        page,
+        editor,
+        homePage,
+        scene,
+        toolbar,
+        cmdBar,
+        copilot,
+      })
+    }
+  )
+
+  test(
+    'Desktop happy path: new project, easy prompt, good result',
+    { tag: ['@desktop'] },
+    async ({ page, editor, homePage, scene, toolbar, cmdBar, copilot }) => {
+      await runHappyPathTest({
+        page,
+        editor,
+        homePage,
+        scene,
+        toolbar,
+        cmdBar,
+        copilot,
+      })
+    }
+  )
 })
