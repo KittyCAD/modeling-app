fc5c8de074d3de7aeb121fd28dfc50eb119fff19
Jonathan Tran <jonnytran@gmail.com>
2025-11-10 10:08:14 -0500

    Fix KCL lint to not define a variable that collides with a tag name
    Resolves #8843.

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -4099,3 +4099,24 @@ mod multi_body_multi_tool_subtract {
         super::execute(TEST_NAME, true).await
     }
 }
+mod lint_chained_profile_tag_collision {
+    const TEST_NAME: &str = "lint_chained_profile_tag_collision";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
