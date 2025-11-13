5d6598e7a572ae854fd38c4439d98be2bbfb12ba
Kevin <kevin@zoo.dev>
2025-11-13 16:17:35 -0600

    chore: added unit tests for use on page mounted hook

---
+++ src/hooks/network/useOnPageMounted.test.tsx
@@ -0,0 +1,56 @@
+import { renderHook } from '@testing-library/react'
+import { useOnPageMounted } from '@src/hooks/network/useOnPageMounted'
+import { vi } from 'vitest'
+
+describe('useOnPageMounted tests', () => {
+  describe('on mounted', () => {
+    test('should run once', () => {
+      const callback = vi.fn(() => 1)
+      const { unmount, result } = renderHook(() =>
+        useOnPageMounted({
+          callback,
+        })
+      )
+      unmount()
+      expect(callback).toHaveBeenCalledTimes(1)
+
+      // clean up test!
+      result.current.resetGlobalEngineCommandManager()
+    })
+    test('should reset with helper function', async () => {
+      const callback_1 = vi.fn(() => 1)
+      const callback_2 = vi.fn(() => 2)
+      const { unmount, result, rerender } = renderHook(
+        ({ callback }) =>
+          useOnPageMounted({
+            callback,
+          }),
+        { initialProps: { callback: callback_1 } }
+      )
+      result.current.resetGlobalEngineCommandManager()
+      rerender({ callback: callback_2 })
+      unmount()
+      expect(callback_1).toHaveBeenCalledTimes(1)
+      expect(callback_2).toHaveBeenCalledTimes(1)
+      // clean up test!
+      result.current.resetGlobalEngineCommandManager()
+    })
+    test('should fail to call the callback again, did not reset', async () => {
+      const callback_1 = vi.fn(() => 1)
+      const callback_2 = vi.fn(() => 2)
+      const { unmount, result, rerender } = renderHook(
+        ({ callback }) =>
+          useOnPageMounted({
+            callback,
+          }),
+        { initialProps: { callback: callback_1 } }
+      )
+      rerender({ callback: callback_2 })
+      unmount()
+      expect(callback_1).toHaveBeenCalledTimes(1)
+      expect(callback_2).toHaveBeenCalledTimes(0)
+      // clean up test!
+      result.current.resetGlobalEngineCommandManager()
+    })
+  })
+})
