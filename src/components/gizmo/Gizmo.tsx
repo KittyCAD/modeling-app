deb161e89f757e87605e0d1c35c39979fb5e2493
Andrew Varga <grizzly33@gmail.com>
2025-11-13 22:10:35 +0100

    separate CubeGizmo so old Gizmo can come back as a setting

--- src/components/gizmo/Gizmo.tsx
+++ src/components/gizmo/Gizmo.tsx
@@ -1,87 +1,14 @@
 import { Popover } from '@headlessui/react'
-import { useEffect, useRef, useState } from 'react'
 import { CustomIcon } from '@src/components/CustomIcon'
-import {
-  ViewControlContextMenu,
-  useViewControlMenuItems,
-} from '@src/components/ViewControlMenu'
-import { useModelingContext } from '@src/hooks/useModelingContext'
-import { useResolvedTheme } from '@src/hooks/useResolvedTheme'
-import { useSettings } from '@src/lib/singletons'
-import GizmoRenderer from '@src/components/gizmo/GizmoRenderer'
+import { useViewControlMenuItems } from '@src/components/ViewControlMenu'
+import CubeGizmo from '@src/components/gizmo/CubeGizmo'
 
 export default function Gizmo() {
-  const { state: modelingState } = useModelingContext()
-  const settings = useSettings()
-  const resolvedTheme = useResolvedTheme()
   const menuItems = useViewControlMenuItems()
-  const wrapperRef = useRef<HTMLDivElement>(null!)
-  const canvasRef = useRef<HTMLCanvasElement | null>(null)
-  const [disableOrbit, setDisabledOrbit] = useState(false)
-
-  const renderer = useRef<GizmoRenderer | null>(null)
-
-  const isPerspective =
-    settings.modeling.cameraProjection.current === 'perspective'
-  const initialIsPerspectiveRef = useRef(isPerspective)
-  const initialResolvedThemeRef = useRef(resolvedTheme)
-
-  // onMount
-  useEffect(() => {
-    if (canvasRef.current) {
-      renderer.current = new GizmoRenderer(
-        canvasRef.current,
-        initialIsPerspectiveRef.current,
-        initialResolvedThemeRef.current
-      )
-    }
-  }, [])
-
-  // perspective changed
-  useEffect(() => {
-    renderer.current?.setPerspective(isPerspective)
-  }, [isPerspective])
-
-  // theme changed
-  useEffect(() => {
-    renderer.current?.setTheme(resolvedTheme)
-  }, [resolvedTheme])
-
-  // Temporary fix for #4040:
-  // Disable gizmo orbiting in sketch mode
-  // This effect updates disableOrbitRef whenever the user
-  // toggles between Sketch mode and 3D mode
-  useEffect(() => {
-    const disabled =
-      modelingState.matches('Sketch') &&
-      !settings.app.allowOrbitInSketchMode.current
-
-    setDisabledOrbit(disabled)
-    renderer.current?.setDisabled(disabled)
-    // if (wrapperRef.current) {
-    //   // wrapperRef.current.style.filter = disableOrbitRef.current
-    //   //   ? 'grayscale(100%)'
-    //   //   : 'none'
-    //   wrapperRef.current.style.cursor = disabled ? 'not-allowed' : 'auto'
-    // }
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- TODO: blanket-ignored fix me!
-  }, [modelingState, settings.app.allowOrbitInSketchMode.current])
 
   return (
     <div className="relative">
-      <div
-        ref={wrapperRef}
-        style={{ cursor: disableOrbit ? 'not-allowed' : 'auto' }}
-        aria-label="View orientation gizmo"
-        data-testid={`gizmo${disableOrbit ? '-disabled' : ''}`}
-        className="grid place-content-center rounded-full overflow-hidden pointer-events-auto backdrop-blur-sm bg-black/10"
-      >
-        <canvas
-          ref={canvasRef}
-          style={{ pointerEvents: disableOrbit ? 'none' : 'auto' }}
-        />
-        <ViewControlContextMenu menuTargetElement={wrapperRef} />
-      </div>
+      <CubeGizmo />
       <GizmoDropdown items={menuItems} />
     </div>
   )
