d1fa7dcce50cea3dca69e68fa96836f41e1cacb9
Kevin <kevin@zoo.dev>
2025-11-14 14:20:04 -0600

    fix: improving copy

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -40,7 +40,7 @@ export const CONNECTION_ERROR_CALL_TO_ACTION_TEXT: Record<
 > = {
   [ConnectionError.Unset]: '',
   [ConnectionError.LongLoadingTime]:
-    'Connecting is taking longer than expected, checking your network connection.',
+  'Connecting is taking longer than expected, check your network connection.',
   [ConnectionError.VeryLongLoadingTime]:
     'Check the connection is being blocked by a firewall, or if your internet is disconnected.',
   [ConnectionError.ICENegotiate]:
@@ -184,8 +184,7 @@ const Loading = ({
       {CONNECTION_ERROR_TEXT[error.error] && (
         <div>
           <div className="inline-block max-w-3xl text-base gap-2 px-32 pt-2 mt-2 pb-6 mb-6 text-chalkboard-80 dark:text-chalkboard-20">
-            {CONNECTION_ERROR_CALL_TO_ACTION_TEXT[error.error]} If the issue
-            persists, please visit the community support thread on{' '}
+            {CONNECTION_ERROR_CALL_TO_ACTION_TEXT[error.error]} If it persists, please visit the community support thread on{' '}
             <a
               className="contents text-chalkboard-80 dark:text-chalkboard-10"
               href={diagnosingNetworkIssuesUrl}
