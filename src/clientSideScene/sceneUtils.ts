84d42c9560b010a83614bc4c5e6412a20e970d92
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2025-11-17 22:30:57 +1100

    remove bad vars

--- src/clientSideScene/sceneUtils.ts
+++ src/clientSideScene/sceneUtils.ts
@@ -31,8 +31,6 @@ export const ARROWHEAD = 'arrowhead'
 export const SEGMENT_LENGTH_LABEL = 'segment-length-label'
 export const SEGMENT_LENGTH_LABEL_TEXT = 'segment-length-label-text'
 export const SEGMENT_LENGTH_LABEL_OFFSET_PX = 30
-export const CIRCLE_3_POINT_DRAFT_POINT = 'circle-3-point-draft-point'
-export const CIRCLE_3_POINT_DRAFT_CIRCLE = 'circle-3-point-draft-circle'
 export const SKETCH_POINT_HANDLE = 'sketch-point-handle'
 
 export function getSceneScale(
