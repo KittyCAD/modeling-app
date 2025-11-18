abb3a5579cb8bbc834968e0b8c4f5d33f4da31fb
Kevin <kevin@zoo.dev>
2025-11-18 09:47:00 -0600

    fix: auto fix

--- src/hooks/network/useOnPageExit.test.tsx
+++ src/hooks/network/useOnPageExit.test.tsx
@@ -1,6 +1,6 @@
 import { renderHook } from '@testing-library/react'
 import { useOnPageExit } from '@src/hooks/network/useOnPageExit'
-import { expect, vi } from 'vitest'
+import { expect, vi, describe } from 'vitest'
 import { ConnectionManager } from '@src/network/connectionManager'
 import { SceneInfra } from '@src/clientSideScene/sceneInfra'
 
