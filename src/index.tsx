68237b0aea0528631d1a7ba9c7bc3c2664be3e81
Frank Noirot <frankjohnson1993@gmail.com>
2025-11-21 12:44:34 -0500

    Wrap app init in awaited initPromise

--- src/index.tsx
+++ src/index.tsx
@@ -25,8 +25,8 @@ import monkeyPatchForBrowserTranslation from '@src/lib/monkeyPatchBrowserTransla
 markOnce('code/willAuth')
 initializeWindowExceptionHandler()
 
-// Don't start the app machine until all these singletons
-// are initialized, and the wasm module is loaded.
+// Don't start the app until the WASM bundle is initialized,
+// as all subsystems rely on it.
 initPromise
   .then(() => {
     appActor.start()
@@ -40,93 +40,95 @@ initPromise
         ],
       },
     })
+
+    // Monkey patch to prevent issues in the web app with automated browser translation
+    // This mitigates https://github.com/KittyCAD/modeling-app/issues/8667, until
+    // we roll out our own i18n solution and can disable browser translation altogether.
+    // https://github.com/KittyCAD/modeling-app/issues/4959
+    if (!window.electron) {
+      monkeyPatchForBrowserTranslation()
+    }
+
+    const root = ReactDOM.createRoot(
+      document.getElementById('root') as HTMLElement
+    )
+
+    root.render(
+      <HotkeysProvider>
+        <AppStreamProvider>
+          <Router />
+          <Toaster
+            position="bottom-center"
+            toastOptions={{
+              style: {
+                borderRadius: '3px',
+                maxInlineSize: 'min(480px, 100%)',
+              },
+              className:
+                'bg-chalkboard-10 dark:bg-chalkboard-90 text-chalkboard-110 dark:text-chalkboard-10 rounded-sm border-chalkboard-20/50 dark:border-chalkboard-80/50',
+              success: {
+                iconTheme: {
+                  primary: 'oklch(89% 0.16 143.4deg)',
+                  secondary: 'oklch(48.62% 0.1654 142.5deg)',
+                },
+                // We shouldn't have a different duration in tests than prod, it might
+                // lead to issues.
+                duration: 1500,
+              },
+            }}
+          />
+          <ModalContainer />
+        </AppStreamProvider>
+      </HotkeysProvider>
+    )
+
+    // If you want to start measuring performance in your app, pass a function
+    // to log results (for example: reportWebVitals(console.log))
+    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    reportWebVitals()
+
+    if (window.electron) {
+      window.electron.onUpdateChecking(() => {
+        const message = `Checking for updates...`
+        console.log(message)
+        toast.loading(message, { id: AUTO_UPDATER_TOAST_ID })
+      })
+
+      window.electron.onUpdateNotAvailable(() => {
+        const message = `You're already using the latest version of the app.`
+        console.log(message)
+        toast.success(message, { id: AUTO_UPDATER_TOAST_ID })
+      })
+
+      window.electron.onUpdateDownloadStart(() => {
+        const message = `Downloading app update...`
+        console.log(message)
+        toast.loading(message, { id: AUTO_UPDATER_TOAST_ID })
+      })
+
+      window.electron.onUpdateError(({ error }) => {
+        console.error(error)
+        toast.error('An error occurred while downloading the update.', {
+          id: AUTO_UPDATER_TOAST_ID,
+        })
+      })
+
+      const electron = window.electron
+      window.electron.onUpdateDownloaded(({ version, releaseNotes }) => {
+        const message = `A new update (${version}) was downloaded and will be available next time you open the app.`
+        console.log(message)
+        toast.custom(
+          ToastUpdate({
+            version,
+            releaseNotes,
+            onRestart: () => {
+              electron.appRestart()
+            },
+            onDismiss: () => {},
+          }),
+          { duration: 30000, id: AUTO_UPDATER_TOAST_ID }
+        )
+      })
+    }
   })
   .catch(reportRejection)
-
-// Monkey patch to prevent issues in the web app with automated browser translation
-// This mitigates https://github.com/KittyCAD/modeling-app/issues/8667, until
-// we roll out our own i18n solution and can disable browser translation altogether.
-// https://github.com/KittyCAD/modeling-app/issues/4959
-if (!window.electron) {
-  monkeyPatchForBrowserTranslation()
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-
-root.render(
-  <HotkeysProvider>
-    <AppStreamProvider>
-      <Router />
-      <Toaster
-        position="bottom-center"
-        toastOptions={{
-          style: {
-            borderRadius: '3px',
-            maxInlineSize: 'min(480px, 100%)',
-          },
-          className:
-            'bg-chalkboard-10 dark:bg-chalkboard-90 text-chalkboard-110 dark:text-chalkboard-10 rounded-sm border-chalkboard-20/50 dark:border-chalkboard-80/50',
-          success: {
-            iconTheme: {
-              primary: 'oklch(89% 0.16 143.4deg)',
-              secondary: 'oklch(48.62% 0.1654 142.5deg)',
-            },
-            // We shouldn't have a different duration in tests than prod, it might
-            // lead to issues.
-            duration: 1500,
-          },
-        }}
-      />
-      <ModalContainer />
-    </AppStreamProvider>
-  </HotkeysProvider>
-)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
-
-if (window.electron) {
-  window.electron.onUpdateChecking(() => {
-    const message = `Checking for updates...`
-    console.log(message)
-    toast.loading(message, { id: AUTO_UPDATER_TOAST_ID })
-  })
-
-  window.electron.onUpdateNotAvailable(() => {
-    const message = `You're already using the latest version of the app.`
-    console.log(message)
-    toast.success(message, { id: AUTO_UPDATER_TOAST_ID })
-  })
-
-  window.electron.onUpdateDownloadStart(() => {
-    const message = `Downloading app update...`
-    console.log(message)
-    toast.loading(message, { id: AUTO_UPDATER_TOAST_ID })
-  })
-
-  window.electron.onUpdateError(({ error }) => {
-    console.error(error)
-    toast.error('An error occurred while downloading the update.', {
-      id: AUTO_UPDATER_TOAST_ID,
-    })
-  })
-
-  const electron = window.electron
-  window.electron.onUpdateDownloaded(({ version, releaseNotes }) => {
-    const message = `A new update (${version}) was downloaded and will be available next time you open the app.`
-    console.log(message)
-    toast.custom(
-      ToastUpdate({
-        version,
-        releaseNotes,
-        onRestart: () => {
-          electron.appRestart()
-        },
-        onDismiss: () => {},
-      }),
-      { duration: 30000, id: AUTO_UPDATER_TOAST_ID }
-    )
-  })
-}
