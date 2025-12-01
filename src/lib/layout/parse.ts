8ba87460361f5acd4f5b4ebbc6bade0b98297743
Kevin <kevin@zoo.dev>
2025-12-01 11:04:25 -0600

    fix: latest

--- src/lib/layout/parse.ts
+++ src/lib/layout/parse.ts
@@ -135,7 +135,7 @@ function isOrientation(o: unknown): o is Orientation {
  * Fatal: `areaType`
  */
 export function parseSplitLayout(
-  layout: Omit<BaseLayout,'type'> & Partial<Omit<SplitLayout, 'type'>>
+  layout: Omit<BaseLayout, 'type'> & Partial<Omit<SplitLayout, 'type'>>
 ): SplitLayout | Error {
   // No children is fatal
   if (!isArray(layout.children)) {
