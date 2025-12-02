afdbb6b8d7b9dfbfc48bd5c704e14094c162f395
Frank Noirot <frankjohnson1993@gmail.com>
2025-12-02 13:22:22 -0500

    Dep inject kclManager into routeLoaders

--- src/Router.tsx
+++ src/Router.tsx
@@ -71,7 +71,7 @@ const router = createRouter([
         },
       },
       {
-        loader: fileLoader,
+        loader: () => fileLoader(kclManager),
         id: PATHS.FILE,
         path: PATHS.FILE + '/:id',
         errorElement: <ErrorPage />,
