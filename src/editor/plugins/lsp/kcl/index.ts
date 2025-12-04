d35b0749e42ca1a95a5efb8ff29611c96d7be221
Pierre Jacquier <pierre@zoo.dev>
2025-12-04 07:56:57 -0800

    Revert "Clear selections on KclPlugin.updateDoc"
    This reverts commit 9b2ec4bc244de5bb2c79a736440d3e1ad88ae768.

--- src/editor/plugins/lsp/kcl/index.ts
+++ src/editor/plugins/lsp/kcl/index.ts
@@ -153,10 +153,8 @@ export class KclPlugin implements PluginValue {
     }
 
     if (!this.client.ready) return
-
-    const clearSelections = true // no reason to keep them after a manual edit
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    this.kclManager.executeCode(clearSelections)
+    this.kclManager.executeCode()
   }
 
   ensureDocUpdated() {
