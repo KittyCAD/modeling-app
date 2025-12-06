370bf47c60e0cfe4c3123523d6ed88c52b8a5a4f
Pierre Jacquier <pierre@zoo.dev>
2025-12-06 15:29:05 -0800

    Keep track of markdown-rendered elements in Zookeeper to prevent selection clearing Fixes #9183

--- src/components/ExchangeCard.tsx
+++ src/components/ExchangeCard.tsx
@@ -12,6 +12,7 @@ import {
   useState,
   type ReactNode,
   type ComponentProps,
+  useMemo,
 } from 'react'
 import Tooltip from '@src/components/Tooltip'
 import toast from 'react-hot-toast'
@@ -322,8 +323,8 @@ export const ResponsesCard = (props: ResponsesCardProp) => {
 
   const itemsFilteredNulls = items.filter((x: ReactNode | null) => x !== null)
 
-  const deltasAggregatedMarkdown =
-    props.deltasAggregated !== '' ? (
+  const deltasAggregatedMarkdown = useMemo(() => {
+    return props.deltasAggregated !== '' ? (
       <span
         className="parsed-markdown whitespace-normal"
         dangerouslySetInnerHTML={{
@@ -334,6 +335,7 @@ export const ResponsesCard = (props: ResponsesCardProp) => {
         }}
       ></span>
     ) : null
+  }, [props.deltasAggregated])
 
   return (
     <ChatBubble
