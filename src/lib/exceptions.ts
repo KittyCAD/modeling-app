949910a814e4e1128ceb44d398b79fa47efc628d
Jonathan Tran <jonnytran@gmail.com>
2025-12-08 17:45:59 -0500

    Handle the stack overflow panic in TS

--- src/lib/exceptions.ts
+++ src/lib/exceptions.ts
@@ -24,6 +24,7 @@ export const initializeWindowExceptionHandler = (
         if (
           matchImportExportErrorCrash(event.message) ||
           matchUnreachableErrorCrash(event.message) ||
+          matchStackSizeExceededErrorCrash(event.message) ||
           matchGenericWasmRuntimeHeuristicErrorCrash(event)
         ) {
           // do global singleton cleanup
@@ -78,6 +79,11 @@ const matchImportExportErrorCrash = (message: string): boolean => {
 const matchUnreachableErrorCrash = (message: string): boolean => {
   const substringError = `Uncaught RuntimeError: unreachable`
   return message.indexOf(substringError) !== -1 ? true : false
+}
+
+function matchStackSizeExceededErrorCrash(message: string): boolean {
+  const substringError = `Uncaught RangeError: Maximum call stack size exceeded`
+  return message.indexOf(substringError) !== -1
 }
 
 const matchGenericWasmRuntimeHeuristicErrorCrash = (
