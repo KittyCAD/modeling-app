986fc011f5caa492e3acec90debff933f7f19267
Frank Noirot <frankjohnson1993@gmail.com>
2025-12-09 11:22:47 -0500

    Make updatelistener callback more readable

--- src/editor/index.ts
+++ src/editor/index.ts
@@ -4,13 +4,15 @@ import { EditorView } from 'codemirror'
 export const wasUpdatedFromDisk = Annotation.define<boolean>()
 
 /** Proof-of-concept editor extension to watch and act on updates from disk */
-const updateFromDiskAlert = EditorView.updateListener.of((update) =>
-  update.transactions.some(
-    (tr) =>
-      tr.annotation(wasUpdatedFromDisk) &&
-      globalThis.alert('WOAH WE UPDATED FROM DISK')
+const updateFromDiskAlert = EditorView.updateListener.of((update) => {
+  const weCare = update.transactions.some((tr) =>
+    tr.annotation(wasUpdatedFromDisk)
   )
-)
+
+  if (weCare) {
+    globalThis.alert('WOAH WE UPDATED FROM DISK')
+  }
+})
 
 // Extensions are just arrays (or nested arrays) of Extensions
 export const diskWatcher = () => [updateFromDiskAlert]
