5e9f3c967e126701498060542a088a1861a5992b
lee-at-zoo-corp <lee@zoo.dev>
2025-12-09 10:44:44 -0500

    Revert "Fix ZK conversation history loss after manually reconnecting (#9205)"
    This reverts commit 1d26e7a2fbbd9e6af0654c6cb80fe3ddec975a35.

--- src/components/layout/areas/MlEphantConversationPane2.tsx
+++ src/components/layout/areas/MlEphantConversationPane2.tsx
@@ -127,27 +127,10 @@ export const MlEphantConversationPane2 = (props: {
 
   const needsReconnect = abruptlyClosed
 
-  const getConversationId = () => {
-    const mlEphantConversations =
-      props.systemIOActor.getSnapshot().context.mlEphantConversations
-
-    // Not ready yet.
-    if (mlEphantConversations === undefined) {
-      return undefined
-    }
-    if (props.settings.meta.id.current === uuidNIL) {
-      return undefined
-    }
-
-    return mlEphantConversations.get(props.settings.meta.id.current)
-  }
-
   const onReconnect = () => {
-    const conversationId = getConversationId()
     props.mlEphantManagerActor.send({
       type: MlEphantManagerTransitions2.CacheSetupAndConnect,
       refParentSend: props.mlEphantManagerActor.send,
-      conversationId: conversationId !== uuidNIL ? conversationId : undefined,
     })
   }
 
@@ -170,7 +153,21 @@ export const MlEphantConversationPane2 = (props: {
   }
 
   const tryToGetExchanges = () => {
-    const conversationId = getConversationId()
+    const mlEphantConversations =
+      props.systemIOActor.getSnapshot().context.mlEphantConversations
+
+    // Not ready yet.
+    if (mlEphantConversations === undefined) {
+      return
+    }
+    if (props.settings.meta.id.current === uuidNIL) {
+      return
+    }
+
+    const conversationId = mlEphantConversations.get(
+      props.settings.meta.id.current
+    )
+
     if (conversationId === uuidNIL) {
       return
     }
