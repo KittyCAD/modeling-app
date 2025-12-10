ddfd9331cf750c52a183d3500cd2128a9583497f
Pierre Jacquier <pierre@zoo.dev>
2025-12-10 07:35:29 -0800

    Improve loading state UI in Zookeeper (#9292)
    * Improve loading state UI in Zookeeper
    Fixes #9264
    
    * Fix for dark mode

--- src/components/MlEphantConversation2.tsx
+++ src/components/MlEphantConversation2.tsx
@@ -387,8 +387,11 @@ export const MlEphantConversation2 = (props: MlEphantConversationProps) => {
                   <StarterCard />
                 )
               ) : (
-                <div className="text-center p-4 text-3 text-md animate-pulse">
-                  <Loading></Loading>
+                <div className="text-center p-4">
+                  <Loading
+                    isDummy={true}
+                    className="text-ml-green dark:text-ml-green"
+                  ></Loading>
                 </div>
               )}
             </div>
