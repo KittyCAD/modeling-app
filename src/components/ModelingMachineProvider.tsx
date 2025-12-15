48221fd3341605030869c3b488cc59d1d567ec34
Frank Noirot <frankjohnson1993@gmail.com>
2025-12-15 00:13:24 -0500

    Remove dynamic singletons import from settingsUtils

--- src/components/ModelingMachineProvider.tsx
+++ src/components/ModelingMachineProvider.tsx
@@ -51,6 +51,7 @@ import {
   useCommandBarState,
   setLayout,
   settingsActor,
+  getSettings,
 } from '@src/lib/singletons'
 import { useSettings } from '@src/lib/singletons'
 import { getDeleteKeys, uuidv4 } from '@src/lib/utils'
