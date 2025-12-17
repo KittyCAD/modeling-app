15713abb73019bfabc0fb8566c73967854850a75
Adam Chalmers <adam.chalmers@zoo.dev>
2025-12-17 10:31:19 -0600

    KCL: Error when solid extruding an open profile
    Previously caught by engine, now is caught earlier by KCL.
    
    This is helpful for catching erros in the frontend's command palette,
    which uses mock execution, so it never gets to the engine.

--- rust/kcl-lib/src/std/extrude.rs
+++ rust/kcl-lib/src/std/extrude.rs
@@ -103,6 +103,15 @@ async fn inner_extrude(
 ) -> Result<Vec<Solid>, KclError> {
     let body_type = body_type.unwrap_or_default();
 
+    if matches!(body_type, BodyType::Solid)
+        && let Some(open_profile) = sketches.iter().find(|sk| matches!(sk.is_closed, ProfileClosed::No))
+    {
+        return Err(KclError::new_semantic(KclErrorDetails::new(
+            "Cannot solid extrude an open profile. Either close the profile, or use a a surface extrude.".to_owned(),
+            open_profile.meta.iter().map(|m| m.source_range).collect(),
+        )));
+    }
+
     // Extrude the element(s).
     let mut solids = Vec::new();
     let tolerance = LengthUnit(tolerance.as_ref().map(|t| t.to_mm()).unwrap_or(DEFAULT_TOLERANCE_MM));
