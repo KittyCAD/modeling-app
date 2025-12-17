119a180ae0029b8699c72ec940e9a72b9be3dd1e
Adam Chalmers <adam.chalmers@zoo.dev>
2025-12-16 22:58:35 -0600

    Add a test

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -4373,3 +4373,24 @@ mod extrude_closes {
         super::execute(TEST_NAME, true).await
     }
 }
+mod implicit_close {
+    const TEST_NAME: &str = "implicit_close";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
