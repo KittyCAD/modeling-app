c921e614b8178967ac04faa3c56455105239fd59
Kurt Hutten <k.hutten@protonmail.ch>
2025-12-19 10:34:41 +1100

    [SOLVE]: Add tool equipped cursor (#9404)
    * fix wasm build
    
    * add cursor when tool is equipped
    
    * andrew's comment

--- src/machines/sketchSolve/tools/moveTool/moveTool.ts
+++ src/machines/sketchSolve/tools/moveTool/moveTool.ts
@@ -618,6 +618,7 @@ export function createOnDragCallback({
       // Notify about new sketch outcome if edit was successful
       if (result) {
         onNewSketchOutcome({ ...result, writeToDisk: false })
+        await new Promise((resolve) => requestAnimationFrame(resolve))
       }
     } finally {
       setIsSolveInProgress(false)
