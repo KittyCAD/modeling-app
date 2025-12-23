43ee8b0c49138328a461ffbc081bb232f2956b1a
Kevin <kevin@zoo.dev>
2025-12-23 10:15:38 -0600

    feat: filtering based on your project

--- src/components/DiffView/HistoryView.tsx
+++ src/components/DiffView/HistoryView.tsx
@@ -14,6 +14,7 @@ import { getSettings, kclManager } from '@src/lib/singletons'
 import { useSignals } from '@preact/signals-react/runtime'
 import { useSettings } from '@src/lib/singletons'
 import {
+  getProjectDirectoryFromKCLFilePath,
   parentPathRelativeToApplicationDirectory,
   parentPathRelativeToProject,
 } from '@src/lib/paths'
@@ -30,6 +31,8 @@ import { lineHighlightField } from '@src/editor/highlightextension'
 import { historyCompartment } from '@src/editor/compartments'
 import { history } from '@codemirror/commands'
 import { CustomIcon } from '@src/components/CustomIcon'
+import { useLoaderData } from 'react-router-dom'
+import type { IndexLoaderData } from '@src/lib/types'
 
 function timeSince(date) {
   const seconds = Math.floor((new Date() - date) / 1000)
@@ -62,6 +65,8 @@ export const HistoryView = (props: AreaTypeComponentProps) => {
   const settings = useSettings()
   const applicationProjectDirectory = settings.app.projectDirectory.current
   const { kclLSP } = useLspContext()
+  const loaderData = useLoaderData() as IndexLoaderData
+  const { project } = loaderData
 
   return (
     <LayoutPanel
@@ -78,47 +83,57 @@ export const HistoryView = (props: AreaTypeComponentProps) => {
       <div className="w-full h-full relative overflow-y-auto overflow-x-hidden">
         <div className="w-full h-full flex flex-col">
           <div className="overflow-auto pb-12 inset-0">
-            {theValue.map((e) => {
-              const path = parentPathRelativeToProject(
-                e.absoluteFilePath,
-                applicationProjectDirectory
-              )
-              const projectPath = parentPathRelativeToApplicationDirectory(
-                e.absoluteFilePath,
-                applicationProjectDirectory
-              )
-              const outlineCSS = false
-                ? 'outline outline-1 outline-primary'
-                : 'outline-0 outline-none'
-              const isSelected = false
-              const month = e.date.toLocaleString('default', { month: 'long' })
-              const dayOfMonth = e.date.getDate()
-              const year = e.date.getFullYear()
-              return (
-                <div
-                  className={`px-2 h-5 flex flex-row items-center justify-between text-xs cursor-pointer -outline-offset-1 ${outlineCSS} hover:outline hover:outline-1 hover:bg-gray-300/50 hover:bg-gray-300/50 ${isSelected ? 'bg-primary/10' : ''}`}
-                  onClick={() =>
-                    (kclManager.history.lastEntrySelected.value = e)
-                  }
-                >
-                  <div className="flex flex-row items-center">
-                    <CustomIcon
-                      name="kcl"
-                      className="inline-block w-4 text-current mr-1"
-                    />
-                    <span className="pr-2">{path}</span>
-                    <span className="text-xs text-chalkboard-80 dark:text-chalkboard-30 ">
-                      {projectPath}
+            {theValue
+              .filter((e) => {
+                const projectDirectoryName = getProjectDirectoryFromKCLFilePath(
+                  e.absoluteFilePath,
+                  applicationProjectDirectory
+                )
+                return projectDirectoryName === project.name
+              })
+              .map((e) => {
+                const path = parentPathRelativeToProject(
+                  e.absoluteFilePath,
+                  applicationProjectDirectory
+                )
+                const projectPath = parentPathRelativeToApplicationDirectory(
+                  e.absoluteFilePath,
+                  applicationProjectDirectory
+                )
+                const outlineCSS = false
+                  ? 'outline outline-1 outline-primary'
+                  : 'outline-0 outline-none'
+                const isSelected = false
+                const month = e.date.toLocaleString('default', {
+                  month: 'long',
+                })
+                const dayOfMonth = e.date.getDate()
+                const year = e.date.getFullYear()
+                return (
+                  <div
+                    className={`px-2 h-5 flex flex-row items-center justify-between text-xs cursor-pointer -outline-offset-1 ${outlineCSS} hover:outline hover:outline-1 hover:bg-gray-300/50 hover:bg-gray-300/50 ${isSelected ? 'bg-primary/10' : ''}`}
+                    onClick={() =>
+                      (kclManager.history.lastEntrySelected.value = e)
+                    }
+                  >
+                    <div className="flex flex-row items-center">
+                      <CustomIcon
+                        name="kcl"
+                        className="inline-block w-4 text-current mr-1"
+                      />
+                      <span className="pr-2">{path}</span>
+                      <span className="text-xs text-chalkboard-80 dark:text-chalkboard-30 ">
+                        {projectPath}
+                      </span>
+                    </div>
+                    {e.wroteToDisk && <span>saved to disk</span>}
+                    <span>
+                      {timeSince(e.date)} ago ({month} {dayOfMonth}, {year} at{' '}
+                      {e.date.toLocaleTimeString()})
                     </span>
                   </div>
-                  {e.wroteToDisk && <span>saved to disk</span>}
-                  <span>
-                    {timeSince(e.date)} ago ({month} {dayOfMonth}, {year} at{' '}
-                    {e.date.toLocaleTimeString()})
-                  </span>
-                </div>
-              )
-            })}
+                )
+              })}
           </div>
         </div>
       </div>
