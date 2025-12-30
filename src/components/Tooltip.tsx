82c6867582a43482076e135990b2aa4f5696adc3
Frank Noirot <frankjohnson1993@gmail.com>
2025-12-30 03:38:39 -0500

    Make tooltips use the browser's Popover API
    With a polyfill for anchor positioning in CSS for Firefox et al

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,6 +1,7 @@
 // We do use all the classes in this file currently, but we
 // index into them with styles[position], which CSS Modules doesn't pick up.
 // eslint-disable-next-line css-modules/no-unused-class
+import { useRef, useEffect } from 'react'
 import styles from './Tooltip.module.css'
 
 type TopOrBottom = 'top' | 'bottom'
@@ -18,6 +19,8 @@ export interface TooltipProps extends React.HTMLProps<HTMLDivElement> {
   inert?: boolean
 }
 
+let tooltipIndex = 0
+
 export default function Tooltip({
   children,
   position = 'top',
@@ -29,8 +32,37 @@ export default function Tooltip({
   inert = true,
   ...rest
 }: TooltipProps) {
+  const tooltip = useRef<HTMLDivElement>(null)
+  const tooltipID = useRef(`tooltip-${tooltipIndex++}`)
+  useEffect(() => {
+    if (tooltip.current === null) {
+      return
+    }
+    const parent = tooltip.current.parentElement
+    if (!parent) {
+      return
+    }
+
+    // @ts-ignore-next-line -- React is not up to date about the options that can be passed
+    const show = () => tooltip.current?.showPopover({ source: parent })
+    const hide = () => tooltip.current?.hidePopover()
+
+    parent.addEventListener('mouseenter', show)
+    parent.addEventListener('mouseleave', hide)
+    parent.addEventListener('focus', show)
+    parent.addEventListener('blur', hide)
+
+    return () => {
+      parent.removeEventListener('mouseenter', show)
+      parent.removeEventListener('mouseleave', hide)
+      parent.removeEventListener('focus', show)
+      parent.removeEventListener('blur', hide)
+    }
+  }, [])
   return (
     <div
+      popover="hint"
+      ref={tooltip}
       inert={inert}
       role="tooltip"
       className={`p-3 ${
