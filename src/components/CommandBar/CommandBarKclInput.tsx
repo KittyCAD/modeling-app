KittyCAD/modeling-app

commit 7673d392750823172589840c159eb5981be5f2fd

Double revert #9465 with fix for #9514, cursor jumping and "loaded file from disk" toast (#9516)
* Revert "Revert "Decouple CodeMirror from React" (#9515)"

This reverts commit 10bbff95e28ad157fdf3fc53d39a44dc0710462e.

* Forward fix for #9469 behavior: don't debounce kclManager.lastWrite

* Make the bot's bugfix recommendation

src/components/CommandBar/CommandBarKclInput.tsx
@@ -16,7 +16,6 @@ import type { AnyStateMachine, SnapshotFrom } from 'xstate'
 import type { Node } from '@rust/kcl-lib/bindings/Node'
 
 import { CustomIcon } from '@src/components/CustomIcon'
-import { useCodeMirror } from '@src/components/layout/areas/CodeEditor'
 import { Spinner } from '@src/components/Spinner'
 import { createLocalName, createVariableDeclaration } from '@src/lang/create'
 import { getNodeFromPath } from '@src/lang/queryAst'
@@ -34,11 +33,31 @@ import { varMentions } from '@src/lib/varCompletionExtension'
 
 import { useModelingContext } from '@src/hooks/useModelingContext'
 import styles from './CommandBarKclInput.module.css'
-import { editorTheme } from '@src/lib/codeEditor'
+import { editorTheme, themeCompartment } from '@src/editor/plugins/theme'
+import { Compartment, EditorState } from '@codemirror/state'
+import { kclLspCompartment, kclAutocompleteCompartment } from '@src/editor'
 
 // TODO: remove the need for this selector once we decouple all actors from React
 const machineContextSelector = (snapshot?: SnapshotFrom<AnyStateMachine>) =>
   snapshot?.context
+
+const varMentionsCompartment = new Compartment()
+const setValueCompartment = new Compartment()
+const keymapCompartment = new Compartment()
+const miniEditor = new EditorView({
+  state: EditorState.create({
+    extensions: [
+      themeCompartment.of([]),
+      varMentionsCompartment.of([]),
+      setValueCompartment.of([]),
+      kclLspCompartment.of([]),
+      kclAutocompleteCompartment.of([]),
+      closeBrackets(),
+      keymap.of([...closeBracketsKeymap, ...completionKeymap]),
+      keymapCompartment.of([]),
+    ],
+  }),
+})
 
 function CommandBarKclInput({
   arg,
@@ -183,65 +202,53 @@ function CommandBarKclInput({
   })
   const varMentionsExtension = varMentions(varMentionData)
 
-  const { setContainer, view } = useCodeMirror({
-    container: editorRef.current,
-    initialDocValue: value,
-    autoFocus: true,
-    selection: {
-      anchor: 0,
-      head:
-        typeof previouslySetValue === 'object' &&
-        'valueText' in previouslySetValue
-          ? previouslySetValue.valueText.length
-          : defaultValue.length,
-    },
-    extensions: [
-      // Typically we prefer to update CodeMirror outside of React, but this "micro-editor" doesn't exist outside of React.
-      editorTheme[getResolvedTheme(settings.app.theme.current)],
-      varMentionsExtension,
-      EditorView.updateListener.of((vu: ViewUpdate) => {
-        if (vu.docChanged) {
-          setValue(vu.state.doc.toString())
-        }
-      }),
-      closeBrackets(),
-      keymap.of([
-        ...closeBracketsKeymap,
-        ...completionKeymap,
-        {
-          key: 'Enter',
-          run: (editor) => {
-            // Only submit if there is no completion active
-            if (completionStatus(editor.state) === null) {
-              handleSubmit()
-              return true
-            } else {
-              return false
-            }
-          },
-        },
-        {
-          key: 'Meta-Backspace',
-          run: () => {
-            stepBack()
-            return true
-          },
-        },
-      ]),
-    ],
+  useEffect(() => {
+    miniEditor.dispatch({
+      effects: [
+        keymapCompartment.reconfigure(
+          keymap.of([
+            {
+              key: 'Enter',
+              run: (editor) => {
+                // Only submit if there is no completion active
+                if (completionStatus(editor.state) !== null) return false
+                handleSubmit()
+                return true
+              },
+            },
+            {
+              key: 'Meta-Backspace',
+              run: () => {
+                stepBack()
+                return true
+              },
+            },
+          ])
+        ),
+      ],
+    })
   })
 
   useEffect(() => {
+    miniEditor.dispatch({
+      effects: [varMentionsCompartment.reconfigure(varMentionsExtension)],
+    })
+  }, [varMentionsExtension])
+
+  useEffect(() => {
+    miniEditor.dispatch({
+      effects: themeCompartment.reconfigure(
+        editorTheme[getResolvedTheme(settings.app.theme.current)]
+      ),
+    })
+  }, [settings.app.theme])
+
+  useEffect(() => {
     if (editorRef.current) {
-      setContainer(editorRef.current)
-      // Reset the value when the arg changes and
-      // the new arg is also a KCL type, since the component
-      // sticks around.
-      view?.focus()
-      view?.dispatch({
+      miniEditor.dispatch({
         changes: {
           from: 0,
-          to: view.state.doc.length,
+          to: miniEditor.state.doc.length,
           insert: initialValue,
         },
         selection: {
@@ -249,9 +256,19 @@ function CommandBarKclInput({
           head: initialValue.length,
         },
       })
+      editorRef.current.appendChild(miniEditor.dom)
+      miniEditor.focus()
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- TODO: blanket-ignored fix me!
-  }, [arg, editorRef])
+    miniEditor.dispatch({
+      effects: setValueCompartment.reconfigure(
+        EditorView.updateListener.of((vu: ViewUpdate) => {
+          if (vu.docChanged) {
+            setValue(vu.state.doc.toString())
+          }
+        })
+      ),
+    })
+  }, [arg, editorRef, initialValue])
 
   useEffect(() => {
     setCanSubmit(

src/components/layout/areas/KclEditorPane.tsx
@@ -3,73 +3,17 @@ import type { PropsWithChildren } from 'react'
 import { ActionIcon } from '@src/components/ActionIcon'
 import { useConvertToVariable } from '@src/hooks/useToolbarGuards'
 import { openExternalBrowserIfDesktop } from '@src/lib/openWindow'
-import {
-  commandBarActor,
-  getSettings,
-  settingsActor,
-} from '@src/lib/singletons'
+import { commandBarActor, settingsActor } from '@src/lib/singletons'
 import { withSiteBaseURL } from '@src/lib/withBaseURL'
 import toast from 'react-hot-toast'
 import styles from './KclEditorMenu.module.css'
-import {
-  closeBrackets,
-  closeBracketsKeymap,
-  completionKeymap,
-} from '@codemirror/autocomplete'
-import {
-  defaultKeymap,
-  history,
-  historyField,
-  historyKeymap,
-  indentWithTab,
-} from '@codemirror/commands'
-import {
-  bracketMatching,
-  codeFolding,
-  foldGutter,
-  foldKeymap,
-  indentOnInput,
-} from '@codemirror/language'
-import { diagnosticCount, lintGutter, lintKeymap } from '@codemirror/lint'
-import { highlightSelectionMatches, searchKeymap } from '@codemirror/search'
-import type { Extension } from '@codemirror/state'
-import { EditorState, Prec, Transaction } from '@codemirror/state'
-import {
-  EditorView,
-  drawSelection,
-  dropCursor,
-  highlightActiveLine,
-  highlightActiveLineGutter,
-  highlightSpecialChars,
-  keymap,
-  lineNumbers,
-  rectangularSelection,
-} from '@codemirror/view'
-import interact from '@replit/codemirror-interact'
-import { useSelector } from '@xstate/react'
-import { useEffect, useMemo, useRef } from 'react'
-import { useLspContext } from '@src/components/LspProvider'
-import CodeEditor from '@src/components/layout/areas/CodeEditor'
-import { historyCompartment } from '@src/editor/compartments'
-import { lineHighlightField } from '@src/editor/highlightextension'
-import { modelingMachineEvent } from '@src/lang/KclManager'
+import { useEffect, useRef } from 'react'
 import { kclManager } from '@src/lib/singletons'
-import { useSettings } from '@src/lib/singletons'
 import { reportRejection, trap } from '@src/lib/trap'
-import { onMouseDragMakeANewNumber, onMouseDragRegex } from '@src/lib/utils'
-import { artifactAnnotationsExtension } from '@src/editor/plugins/artifacts'
-import { getArtifactAnnotationsAtCursor } from '@src/lib/getArtifactAnnotationsAtCursor'
-import {
-  editorIsMountedSelector,
-  kclEditorActor,
-  selectionEventSelector,
-} from '@src/machines/kclEditorMachine'
 import type { AreaTypeComponentProps } from '@src/lib/layout'
 import { LayoutPanel, LayoutPanelHeader } from '@src/components/layout/Panel'
-import { editorTheme, themeCompartment } from '@src/lib/codeEditor'
 import { CustomIcon } from '@src/components/CustomIcon'
-import { getResolvedTheme } from '@src/lib/theme'
-import { kclAstExtension } from '@src/editor/plugins/ast'
+import { kclEditorActor } from '@src/machines/kclEditorMachine'
 
 export const editorShortcutMeta = {
   formatCode: {
@@ -101,182 +45,22 @@ export const KclEditorPane = (props: AreaTypeComponentProps) => {
 }
 
 export const KclEditorPaneContents = () => {
-  const context = useSettings()
-  const lastSelectionEvent = useSelector(kclEditorActor, selectionEventSelector)
-  const editorIsMounted = useSelector(kclEditorActor, editorIsMountedSelector)
-  const { copilotLSP, kclLSP } = useLspContext()
-
-  // When this component unmounts, we need to tell the machine that the editor
+  const editorParent = useRef<HTMLDivElement>(null)
   useEffect(() => {
-    return () => {
+    kclEditorActor.send({ type: 'setKclEditorMounted', data: true })
+    editorParent.current?.appendChild(kclManager.editorView.dom)
+
+    return () =>
       kclEditorActor.send({ type: 'setKclEditorMounted', data: false })
-      kclEditorActor.send({ type: 'setLastSelectionEvent', data: undefined })
-      kclManager.diagnostics = []
-    }
   }, [])
-
-  useEffect(() => {
-    const editorView = kclManager.getEditorView()
-    if (!editorIsMounted || !lastSelectionEvent || !editorView) {
-      return
-    }
-
-    try {
-      editorView.dispatch({
-        selection: lastSelectionEvent.codeMirrorSelection,
-        annotations: [modelingMachineEvent, Transaction.addToHistory.of(false)],
-        scrollIntoView: lastSelectionEvent.scrollIntoView,
-      })
-    } catch (e) {
-      console.error('Error setting selection', e)
-    }
-  }, [editorIsMounted, lastSelectionEvent])
-
-  const textWrapping = context.textEditor.textWrapping
-  const cursorBlinking = context.textEditor.blinkingCursor
-  // DO NOT ADD THE CODEMIRROR HOTKEYS HERE TO THE DEPENDENCY ARRAY
-  // It reloads the editor every time we do _anything_ in the editor
-  // I have no idea why.
-  // Instead, hot load hotkeys via code mirror native.
-  const codeMirrorHotkeys = kclManager.getCodemirrorHotkeys()
-
-  // When opening the editor, use the existing history in kclManager.
-  // This is needed to ensure users can undo beyond when the editor has been openeed.
-  // (Another solution would be to reuse the same state instead of creating a new one in CodeEditor.)
-  const existingHistory = kclManager.editorState.field(historyField)
-  const initialHistory = existingHistory
-    ? historyField.init(() => existingHistory)
-    : history()
-
-  const editorExtensions = useMemo(() => {
-    const extensions = [
-      themeCompartment.of(
-        editorTheme[getResolvedTheme(getSettings().app.theme.current)]
-      ),
-      drawSelection({
-        cursorBlinkRate: cursorBlinking.current ? 1200 : 0,
-      }),
-      lineHighlightField,
-      artifactAnnotationsExtension(),
-      kclAstExtension(),
-      historyCompartment.of(initialHistory),
-      closeBrackets(),
-      codeFolding(),
-      keymap.of([
-        ...closeBracketsKeymap,
-        ...defaultKeymap,
-        ...searchKeymap,
-        ...historyKeymap,
-        ...foldKeymap,
-        ...completionKeymap,
-        ...lintKeymap,
-        indentWithTab,
-        ...codeMirrorHotkeys,
-        {
-          key: editorShortcutMeta.convertToVariable.codeMirror,
-          run: () => {
-            return kclManager.convertToVariable()
-          },
-        },
-      ]),
-    ] as Extension[]
-
-    if (kclLSP) extensions.push(Prec.highest(kclLSP))
-    if (copilotLSP) extensions.push(copilotLSP)
-
-    extensions.push(
-      lintGutter(),
-      lineNumbers(),
-      highlightActiveLineGutter(),
-      highlightSpecialChars(),
-      foldGutter(),
-      EditorState.allowMultipleSelections.of(true),
-      indentOnInput(),
-      bracketMatching(),
-      closeBrackets(),
-      highlightActiveLine(),
-      highlightSelectionMatches(),
-      rectangularSelection(),
-      dropCursor(),
-      interact({
-        rules: [
-          // a rule for a number dragger
-          {
-            // the regexp matching the value
-            regexp: onMouseDragRegex,
-            // set cursor to "ew-resize" on hover
-            cursor: 'ew-resize',
-            // change number value based on mouse X movement on drag
-            onDrag: (text, setText, e) => {
-              onMouseDragMakeANewNumber(text, setText, e)
-            },
-          },
-        ],
-      })
-    )
-    if (textWrapping.current) extensions.push(EditorView.lineWrapping)
-
-    if (context.app.showDebugPanel.current) {
-      extensions.push(
-        keymap.of([
-          {
-            key: 'Mod-Shift-d',
-            preventDefault: true,
-            run: () => {
-              const view = kclManager.getEditorView()
-              if (!view) return false
-              const data = getArtifactAnnotationsAtCursor(view.state)
-              // eslint-disable-next-line no-console
-              console.log('Artifact annotations at cursor:', data)
-              return true
-            },
-          },
-        ])
-      )
-    }
-
-    return extensions
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- TODO: blanket-ignored fix me!
-  }, [kclLSP, copilotLSP, textWrapping.current, cursorBlinking.current])
-
-  const initialCode = useRef(kclManager.code)
 
   return (
     <div className="relative">
       <div
         id="code-mirror-override"
-        className={
-          'absolute inset-0 pr-1 ' + (cursorBlinking.current ? 'blink' : '')
-        }
-      >
-        <CodeEditor
-          initialDocValue={initialCode.current}
-          extensions={editorExtensions}
-          onCreateEditor={(_editorView) => {
-            kclManager.setEditorView(_editorView)
-
-            if (!_editorView) return
-
-            // Update diagnostics as they are cleared when the editor is unmounted.
-            // Without this, errors would not be shown when closing and reopening the editor.
-            kclManager.wasmInstancePromise
-              .then((wasmInstance) =>
-                kclManager
-                  .safeParse(kclManager.code, wasmInstance)
-                  .then(() => {
-                    // On first load of this component, ensure we show the current errors
-                    // in the editor.
-                    // Make sure we don't add them twice.
-                    if (diagnosticCount(_editorView.state) === 0) {
-                      kclManager.setDiagnosticsForCurrentErrors()
-                    }
-                  })
-                  .catch(reportRejection)
-              )
-              .catch(reportRejection)
-          }}
-        />
-      </div>
+        className="absolute inset-0 pr-1"
+        ref={editorParent}
+      />
     </div>
   )
 }

commit f5bb1eb9625f3ac0ecec7bd389109a73cbbef801

Chore: move theme CodeMirror extension into /src/editor/plugins/theme
Moves this extension that was in a stray file called
`/src/lib/codeEditor.ts` to make this CodeMirror extension follow the
convention of our others. Broken out from
https://github.com/KittyCAD/modeling-app/pull/9516 because it is just
diff noise.

src/components/CommandBar/CommandBarKclInput.tsx
@@ -34,7 +34,7 @@ import { varMentions } from '@src/lib/varCompletionExtension'
 
 import { useModelingContext } from '@src/hooks/useModelingContext'
 import styles from './CommandBarKclInput.module.css'
-import { editorTheme } from '@src/lib/codeEditor'
+import { editorTheme } from '@src/editor/plugins/theme'
 
 // TODO: remove the need for this selector once we decouple all actors from React
 const machineContextSelector = (snapshot?: SnapshotFrom<AnyStateMachine>) =>

src/components/layout/areas/KclEditorPane.tsx
@@ -66,7 +66,7 @@ import {
 } from '@src/machines/kclEditorMachine'
 import type { AreaTypeComponentProps } from '@src/lib/layout'
 import { LayoutPanel, LayoutPanelHeader } from '@src/components/layout/Panel'
-import { editorTheme, themeCompartment } from '@src/lib/codeEditor'
+import { editorTheme, themeCompartment } from '@src/editor/plugins/theme'
 import { CustomIcon } from '@src/components/CustomIcon'
 import { getResolvedTheme } from '@src/lib/theme'
 import { kclAstExtension } from '@src/editor/plugins/ast'

src/editor/plugins/theme/index.ts
@@ -1,0 +1,153 @@
+import { HighlightStyle, syntaxHighlighting } from '@codemirror/language'
+import { Compartment, Annotation, StateEffect } from '@codemirror/state'
+import { tags } from '@lezer/highlight'
+import { EditorView } from 'codemirror'
+
+const green = {
+  light: 'oklch(from var(--primary) calc(l - 0.1) calc(c) calc(h - 90))',
+  dark: 'oklch(from var(--primary) calc(l + 0.1) c calc(h - 90))',
+}
+const orange = {
+  light: 'oklch(from var(--primary) calc(l + 0.05) calc(c + .1) calc(h - 180))',
+  dark: 'oklch(from var(--primary) calc(l + 0.25) calc(c + .1) calc(h - 180))',
+}
+const textDefault = {
+  light: 'var(--chalkboard-100)',
+  dark: 'var(--chalkboard-20)',
+}
+const textFaded = {
+  light: 'var(--chalkboard-70)',
+  dark: 'var(--chalkboard-30)',
+}
+const magenta = {
+  light: 'oklch(from var(--primary) calc(l + 0.05) c calc(h + 90))',
+}
+const primary = {
+  light: 'var(--primary)',
+  dark: 'oklch(from var(--primary) calc(l + 0.15) c h)',
+}
+const colors = {
+  green,
+  magenta,
+  orange,
+  primary,
+  textDefault,
+  textFaded,
+}
+const baseKclHighlights = HighlightStyle.define([
+  {
+    tag: [tags.keyword, tags.annotation],
+    color: colors.orange.light,
+  },
+  {
+    tag: [tags.number, tags.string, tags.tagName],
+    color: colors.green.light,
+    fontWeight: 'normal',
+  },
+  {
+    tag: [tags.attributeName, tags.definition(tags.propertyName)],
+    color: colors.magenta.light,
+    fontWeight: 'normal',
+  },
+  { tag: tags.function(tags.variableName), color: colors.textDefault.light },
+  {
+    tag: tags.definitionKeyword,
+    backgroundColor: 'oklch(from var(--primary) calc(l + 0.05) c h / .1)',
+    color: colors.primary.light,
+    borderRadius: '2px',
+  },
+  { tag: [tags.variableName], color: colors.primary.light },
+  { tag: tags.comment, color: colors.textFaded.light, fontStyle: 'italic' },
+  {
+    tag: tags.definition(tags.variableName),
+    color: colors.textFaded.light,
+    fontWeight: 'bold',
+  },
+  { tag: tags.controlOperator, color: colors.textFaded.light },
+  {
+    tag: [tags.paren, tags.brace, tags.bracket],
+    color: colors.textFaded.light,
+    fontWeight: 'bold',
+  },
+])
+
+const darkKclHighlights = HighlightStyle.define(
+  [
+    ...baseKclHighlights.specs,
+    {
+      tag: [tags.keyword, tags.annotation],
+      color: colors.orange.dark,
+    },
+    {
+      tag: tags.definitionKeyword,
+      backgroundColor: 'oklch(from var(--primary) calc(l + 0.25) c h / .2)',
+      color: colors.primary.dark,
+    },
+    { tag: tags.function(tags.variableName), color: colors.textDefault.dark },
+    {
+      tag: [tags.variableName],
+      color: colors.primary.dark,
+    },
+    {
+      tag: [tags.number, tags.string, tags.tagName],
+      color: colors.green.dark,
+      fontWeight: 'normal',
+    },
+    { tag: tags.comment, color: 'var(--chalkboard-30)', fontStyle: 'italic' },
+    {
+      tag: tags.definition(tags.variableName),
+      color: 'var(--chalkboard-30)',
+      fontWeight: 'bold',
+    },
+    { tag: tags.atom, color: 'var(--chalkboard-40)' },
+    { tag: tags.controlOperator, color: 'var(--chalkboard-30)' },
+    {
+      tag: [tags.paren, tags.brace, tags.bracket],
+      color: 'var(--chalkboard-30)',
+      fontWeight: 'bold',
+    },
+  ],
+  {
+    themeType: 'dark',
+  }
+)
+
+const lightTheme = EditorView.theme(
+  {
+    '&': {
+      backgroundColor: '#fff',
+    },
+  },
+  {
+    dark: false,
+  }
+)
+const darkTheme = EditorView.theme(
+  {
+    '&': {
+      backgroundColor: 'transparent',
+    },
+  },
+  {
+    dark: true,
+  }
+)
+
+/**
+ * Nearly-empty themes that just mark themselves as light or dark
+ * so our {@link syntaxHighlightingExtension} can apply its styling.
+ */
+export const editorTheme = {
+  light: [lightTheme, syntaxHighlighting(baseKclHighlights)],
+  dark: [darkTheme, syntaxHighlighting(darkKclHighlights)],
+}
+/** Compartment to allow us to reconfigure CodeMirror's theme dynamically outside of React */
+export const themeCompartment = new Compartment()
+
+/** Annotation to flag CodeMirror transactions as coming from an app settings update */
+export const settingsUpdateAnnotation = Annotation.define<null>()
+
+/** StateEffect to dispatch to CodeMirror when the app theme setting changes */
+export const appSettingsThemeEffect = StateEffect.define<'light' | 'dark'>()
+/** StateEffect to dispatch to CodeMirror when the app blinkingCursor setting changes */
+export const appSettingsBlinkingCursorEffect = StateEffect.define<boolean>()

src/lang/KclManager.ts
@@ -99,7 +99,7 @@ import {
   themeCompartment,
   appSettingsThemeEffect,
   settingsUpdateAnnotation,
-} from '@src/lib/codeEditor'
+} from '@src/editor/plugins/theme'
 import type { SceneEntities } from '@src/clientSideScene/sceneEntities'
 import {
   createEmptyAst,

src/lib/codeEditor.ts
@@ -1,14 +1,3 @@
-import { HighlightStyle, syntaxHighlighting } from '@codemirror/language'
-import {
-  Annotation,
-  Compartment,
-  type Extension,
-  StateEffect,
-} from '@codemirror/state'
-import { tags } from '@lezer/highlight'
-import { EditorView } from 'codemirror'
-import { Themes } from '@src/lib/theme'
-
 export const normalizeLineEndings = (str: string, normalized = '\n') => {
   return str.replace(/\r?\n/g, normalized)
 }
@@ -26,152 +15,3 @@ export function isCodeTheSame(left: string, right: string) {
   // any other future logic that we failed to implement that causes a bug
   return leftBasis === rightBasis
 }
-
-const green = {
-  light: 'oklch(from var(--primary) calc(l - 0.1) calc(c) calc(h - 90))',
-  dark: 'oklch(from var(--primary) calc(l + 0.1) c calc(h - 90))',
-}
-const orange = {
-  light: 'oklch(from var(--primary) calc(l + 0.05) calc(c + .1) calc(h - 180))',
-  dark: 'oklch(from var(--primary) calc(l + 0.25) calc(c + .1) calc(h - 180))',
-}
-const textDefault = {
-  light: 'var(--chalkboard-100)',
-  dark: 'var(--chalkboard-20)',
-}
-const textFaded = {
-  light: 'var(--chalkboard-70)',
-  dark: 'var(--chalkboard-30)',
-}
-const magenta = {
-  light: 'oklch(from var(--primary) calc(l + 0.05) c calc(h + 90))',
-}
-const primary = {
-  light: 'var(--primary)',
-  dark: 'oklch(from var(--primary) calc(l + 0.15) c h)',
-}
-const colors = {
-  green,
-  magenta,
-  orange,
-  primary,
-  textDefault,
-  textFaded,
-}
-const baseKclHighlights = HighlightStyle.define([
-  {
-    tag: [tags.keyword, tags.annotation],
-    color: colors.orange.light,
-  },
-  {
-    tag: [tags.number, tags.string, tags.tagName],
-    color: colors.green.light,
-    fontWeight: 'normal',
-  },
-  {
-    tag: [tags.attributeName, tags.definition(tags.propertyName)],
-    color: colors.magenta.light,
-    fontWeight: 'normal',
-  },
-  { tag: tags.function(tags.variableName), color: colors.textDefault.light },
-  {
-    tag: tags.definitionKeyword,
-    backgroundColor: 'oklch(from var(--primary) calc(l + 0.05) c h / .1)',
-    color: colors.primary.light,
-    borderRadius: '2px',
-  },
-  { tag: [tags.variableName], color: colors.primary.light },
-  { tag: tags.comment, color: colors.textFaded.light, fontStyle: 'italic' },
-  {
-    tag: tags.definition(tags.variableName),
-    color: colors.textFaded.light,
-    fontWeight: 'bold',
-  },
-  { tag: tags.controlOperator, color: colors.textFaded.light },
-  {
-    tag: [tags.paren, tags.brace, tags.bracket],
-    color: colors.textFaded.light,
-    fontWeight: 'bold',
-  },
-])
-
-const darkKclHighlights = HighlightStyle.define(
-  [
-    ...baseKclHighlights.specs,
-    {
-      tag: [tags.keyword, tags.annotation],
-      color: colors.orange.dark,
-    },
-    {
-      tag: tags.definitionKeyword,
-      backgroundColor: 'oklch(from var(--primary) calc(l + 0.25) c h / .2)',
-      color: colors.primary.dark,
-    },
-    { tag: tags.function(tags.variableName), color: colors.textDefault.dark },
-    {
-      tag: [tags.variableName],
-      color: colors.primary.dark,
-    },
-    {
-      tag: [tags.number, tags.string, tags.tagName],
-      color: colors.green.dark,
-      fontWeight: 'normal',
-    },
-    { tag: tags.comment, color: 'var(--chalkboard-30)', fontStyle: 'italic' },
-    {
-      tag: tags.definition(tags.variableName),
-      color: 'var(--chalkboard-30)',
-      fontWeight: 'bold',
-    },
-    { tag: tags.atom, color: 'var(--chalkboard-40)' },
-    { tag: tags.controlOperator, color: 'var(--chalkboard-30)' },
-    {
-      tag: [tags.paren, tags.brace, tags.bracket],
-      color: 'var(--chalkboard-30)',
-      fontWeight: 'bold',
-    },
-  ],
-  {
-    themeType: 'dark',
-  }
-)
-
-const lightTheme = EditorView.theme(
-  {
-    '&': {
-      backgroundColor: '#fff',
-    },
-  },
-  {
-    dark: false,
-  }
-)
-const darkTheme = EditorView.theme(
-  {
-    '&': {
-      backgroundColor: 'transparent',
-    },
-  },
-  {
-    dark: true,
-  }
-)
-
-/**
- * Nearly-empty themes that just mark themselves as light or dark
- * so our {@link syntaxHighlightingExtension} can apply its styling.
- */
-export const editorTheme = {
-  [Themes.Light]: [lightTheme, syntaxHighlighting(baseKclHighlights)],
-  [Themes.Dark]: [darkTheme, syntaxHighlighting(darkKclHighlights)],
-} satisfies Record<Exclude<Themes, 'system'>, Extension>
-/** Compartment to allow us to reconfigure CodeMirror's theme dynamically outside of React */
-export const themeCompartment = new Compartment()
-
-/** Annotation to flag CodeMirror transactions as coming from an app settings update */
-export const settingsUpdateAnnotation = Annotation.define<null>()
-
-/** StateEffect to dispatch to CodeMirror when the app theme setting changes */
-export const appSettingsThemeEffect = StateEffect.define<'light' | 'dark'>()
-/** StateEffect to dispatch to CodeMirror when the app blinkingCursor setting changes */
-export const appSettingsBlinkingCursorEffect = StateEffect.define<boolean>()