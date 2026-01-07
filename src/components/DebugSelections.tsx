947f088833728ac014c5461dbadd3eac1669ab14
Kevin <kevin@zoo.dev>
2026-01-07 10:32:14 -0600

    fix: highlight ranges

--- src/components/DebugSelections.tsx
+++ src/components/DebugSelections.tsx
@@ -8,6 +8,8 @@ import { uuidv4 } from '@src/lib/utils'
 import { processCodeMirrorRanges } from '@src/lib/selections'
 import { use } from 'react'
 import { EditorSelection } from '@codemirror/state'
+import { defaultSourceRange } from '@src/lang/sourceRange'
+import { getCodeRefsByArtifactId } from '@src/lang/std/artifactGraph'
 
 async function clearSelection() {
   await engineCommandManager.sendSceneCommand({
@@ -50,7 +52,9 @@ function compute(artifactGraph, wasmInstance) {
     const id = obj.id
     const codeRef = obj.codeRef
     const codeRefToIds = codeRangeToEventInfo(codeRef, wasmInstance)
-    return { id, type, codeRef, codeRefToIds}
+    const artifactIdtoCodeRefs = artifactToCodeRange(id) || []
+    console.log(codeRef)
+    return { id, type, codeRef, codeRefToIds, artifactIdtoCodeRefs }
   })
   return idsWithTypes
 }
@@ -79,7 +83,6 @@ async function selectionFromRange(codeRef) {
 }
 
 function codeRangeToEventInfo(codeRef, wasmInstance) {
-
   if (!codeRef) {
     // no code refs
     return []
@@ -103,14 +106,17 @@ function codeRangeToEventInfo(codeRef, wasmInstance) {
   }
 
   const asIfItWasSelected = EditorSelection.create([
-    EditorSelection.range(selections.graphSelections[0].codeRef.range[0], selections.graphSelections[0].codeRef.range[1])
+    EditorSelection.range(
+      selections.graphSelections[0].codeRef.range[0],
+      selections.graphSelections[0].codeRef.range[1]
+    ),
   ])
   const eventInfo = processCodeMirrorRanges({
     // codeMirrorRanges: editorView.state.selection.ranges,
     codeMirrorRanges: asIfItWasSelected.ranges,
     selectionRanges: {
       graphSelections: [],
-      otherSelections: []
+      otherSelections: [],
     },
     isShiftDown: kclManager.isShiftDown,
     ast: kclManager.ast,
@@ -126,28 +132,49 @@ function codeRangeToEventInfo(codeRef, wasmInstance) {
   return arrayOfEntityIds
 }
 
-function parseOutAllSelectAddEntities (eventInfo) {
+function parseOutAllSelectAddEntities(eventInfo) {
   if (!eventInfo || !eventInfo.engineEvents) {
     return []
   }
   const engineEvents = eventInfo.engineEvents
-  const selectAdds = engineEvents.filter((event)=>{return event.cmd.type === 'select_add'})
+  const selectAdds = engineEvents.filter((event) => {
+    return event.cmd.type === 'select_add'
+  })
   let ids = []
-  selectAdds.forEach((event)=>{
+  selectAdds.forEach((event) => {
     const entities = event.cmd.entities
     ids = ids.concat(entities)
   })
   return ids
 }
 
+function artifactToCodeRange (id) {
+  if (id) {
+    const codeRefs = getCodeRefsByArtifactId(
+      id,
+      kclManager.artifactGraph
+    )
+    if (codeRefs) {
+      return codeRefs.map(({ range }) => range)
+    }
+  } else if (
+    !kclManager.highlightRange ||
+    (kclManager.highlightRange[0] &&
+      kclManager.highlightRange[0][0] !== 0 &&
+      kclManager.highlightRange[0][1] !== 0)
+  ) {
+    return [defaultSourceRange()]
+  }
+}
 
 export function DebugSelections() {
   const [selectedId, setSelectedId] = useState('')
+  const [selectedRange, setSelectedRange] = useState('')
   const wasmInstance = use(kclManager.wasmInstancePromise)
   const highlightMinor = 'bg-red-950'
   const highlightMajor = 'bg-red-800'
   const artifactGraphTree = useMemo(() => {
-    const result =  compute(kclManager.artifactGraph, wasmInstance)
+    const result = compute(kclManager.artifactGraph, wasmInstance)
     return result
 
     // eslint-disable-next-line react-hooks/exhaustive-deps -- TODO: blanket-ignored fix me!
@@ -156,11 +183,12 @@ export function DebugSelections() {
     <div>
       {artifactGraphTree.map((a) => {
         const hightlightMyId = a.id === selectedId
+        const highlightMyRange = a?.codeRef?.range?.join(',') === selectedRange
         return (
           <div className="text-xs flex flex-col justify-between">
             <div className="text-xs hover:bg-cyan-600 flex flex-row justify-between bg-chalkboard-80">
               <div
-          className={`cursor-pointer ${hightlightMyId ? highlightMajor : ''}`}
+                className={`cursor-pointer ${hightlightMyId ? highlightMajor : ''}`}
                 onClick={() => {
                   selectionFromId(a.id)
                   setSelectedId(a.id)
@@ -170,30 +198,59 @@ export function DebugSelections() {
               </div>
               <div>{a.type}</div>
               <div
-                className="cursor-pointer"
+                className={`cursor-pointer ${highlightMyRange ? highlightMajor : ''}`}
                 onClick={() => {
                   selectionFromRange(a.codeRef)
+                  const rangeString = a.codeRef.range.join(',')
+                  setSelectedRange(rangeString)
                 }}
               >
                 {`[${a.codeRef?.range}]` || ''}
               </div>
             </div>
             <div className="text-xs flex flex-col justify-between">
-              <div className="ml-2 cursor-pointer"
+              <div
+                className="ml-2"
+              >
+                Ranges
+              </div>
+              {a.artifactIdtoCodeRefs.map((range) => {
+                const highlightMyRange = range.join(',') === selectedRange
+                return (
+                  <div
+                className={`ml-4 cursor-pointer ${highlightMyRange ? highlightMinor : ''}`}
+                    onClick={() => {
+
+                    }}
+                  >
+                    {`[${range}]` || ''}
+                  </div>
+                )
+              })}
+            </div>
+            <div className="text-xs flex flex-col justify-between">
+              <div
+                className={`ml-2 cursor-pointer ${highlightMyRange ? highlightMinor : ''}`}
                 onClick={() => {
                   selectionFromRange(a.codeRef)
+                  const rangeString = a.codeRef.range.join(',')
+                  setSelectedRange(rangeString)
                 }}
-            >
+              >
                 {`[${a.codeRef?.range}]` || ''}
               </div>
               {a.codeRefToIds.map((id) => {
                 const highlightMyId = id === selectedId
-                return (<div className={`ml-4 cursor-pointer ${highlightMyId ? highlightMinor : ''}`}
-                  onClick={() => {
-                    selectionFromId(id)
-                    setSelectedId(id)
-                  }}
-                  >{id}</div>
+                return (
+                  <div
+                    className={`ml-4 cursor-pointer ${highlightMyId ? highlightMinor : ''}`}
+                    onClick={() => {
+                      selectionFromId(id)
+                      setSelectedId(id)
+                    }}
+                  >
+                    {id}
+                  </div>
                 )
               })}
             </div>
