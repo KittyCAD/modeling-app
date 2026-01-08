3d4ebb7c0bcc5367c95e1f0aadacf093be220992
Pierre Jacquier <pierre@zoo.dev>
2026-01-08 14:30:57 -0500

    Clean up reasoning autoscroll, no more bleeding into parent scroll

--- src/components/Thinking.tsx
+++ src/components/Thinking.tsx
@@ -578,27 +578,16 @@ export const Thinking = (props: {
     if (props.onlyShowImmediateThought === true) {
       return
     }
-    if (refViewFull.current === null) {
-      return
-    }
-    const c = refViewFull.current.children
-    if (c.length === 0) {
-      return
-    }
 
-    setTimeout(() => {
-      c[c.length - 1].scrollIntoView({
+    // Always autoscroll to the bottom of the reasoning view
+    requestAnimationFrame(() => {
+      if (refViewFull.current === null) {
+        return
+      }
+
+      refViewFull.current.scrollTo({
+        top: refViewFull.current.scrollHeight,
         behavior: 'smooth',
-        block: 'nearest',
-        inline: 'nearest',
-      })
-      setTimeout(() => {
-        if (refViewFull.current === null) return
-        refViewFull.current.scrollIntoView({
-          behavior: 'smooth',
-          block: 'nearest',
-          inline: 'nearest',
-        })
       })
     })
   }, [reasoningThoughts.length, props.onlyShowImmediateThought])
