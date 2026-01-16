8550f74fa0c188fb68f92030aeb35a7d25c4f5a0
Frank Noirot <frankjohnson1993@gmail.com>
2026-01-16 13:59:30 -0500

    Fix tsc errors in AppHeader.tsx by using useSingletons hook instead of direct imports

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -2,7 +2,7 @@ import { CommandBarOpenButton } from '@src/components/CommandBarOpenButton'
 import ProjectSidebarMenu from '@src/components/ProjectSidebarMenu'
 import UserSidebarMenu from '@src/components/UserSidebarMenu'
 import { isDesktop } from '@src/lib/isDesktop'
-import { useUser } from '@src/lib/singletons'
+import { useSingletons } from '@src/lib/singletons'
 import type { IndexLoaderData } from '@src/lib/types'
 
 import type { ReactNode } from 'react'
@@ -26,6 +26,7 @@ export const AppHeader = ({
   nativeFileMenuCreated,
   projectMenuChildren,
 }: AppHeaderProps) => {
+  const { useUser } = useSingletons()
   const user = useUser()
 
   return (
