a9ce16946a6fa220ae873d3bcb1b3382289acee5
Frank Noirot <frankjohnson1993@gmail.com>
2026-01-17 01:40:45 -0500

    Fix tsc errors in usePreviousVarMentions.ts

--- src/lib/usePreviousVarMentions.ts
+++ src/lib/usePreviousVarMentions.ts
@@ -3,7 +3,7 @@ import type { CompletionContext } from '@codemirror/autocomplete'
 import { usePreviousVariables } from '@src/lib/usePreviousVariables'
 import type { Program, VariableMap } from '@src/lang/wasm'
 import { use } from 'react'
-import { kclManager } from '@src/lib/singletons'
+import { useSingletons } from '@src/lib/singletons'
 
 /// Basically a fork of the `mentions` extension https://github.com/uiwjs/react-codemirror/blob/master/extensions/mentions/src/index.ts
 /// But it matches on any word, not just the `@` symbol
@@ -12,6 +12,7 @@ export function usePreviousVarMentions(
   ast: Program,
   variables: VariableMap
 ) {
+  const { kclManager } = useSingletons()
   const wasmInstance = use(kclManager.wasmInstancePromise)
   const previousVariables = usePreviousVariables({
     code: context.view?.state.doc.toString() || '',
