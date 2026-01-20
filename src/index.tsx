910f3c4b7748051d8814f57c6e270eb7c2fa5746
Frank Noirot <frankjohnson1993@gmail.com>
2026-01-20 17:07:41 -0500

    Refactor index.tsx to have its init sequence wrapped in a function
    @nadr0 feedback

--- src/index.tsx
+++ src/index.tsx
@@ -18,123 +18,140 @@ import reportWebVitals from '@src/reportWebVitals'
 import monkeyPatchForBrowserTranslation from '@src/lib/monkeyPatchBrowserTranslate'
 
 const app = new App()
+/** Hook to gain access to the global app instance's singletons */
 export const useSingletons = () => React.useContext(app.ReactContext)
+init(app)
 
-markOnce('code/willAuth')
-initializeWindowExceptionHandler(
-  app.singletons.kclManager,
-  app.singletons.rustContext
-)
+/** The initialization sequence for this app */
+function init(app: App) {
+  initSingletonBehavior()
+  if (window.electron) {
+    initElectronBehavior(window.electron)
+  }
+  initReact()
 
-// Don't start the app machine until all these singletons
-// are initialized, and the wasm module is loaded.
-app.singletons.kclManager.wasmInstancePromise
-  .then((wasmInstance) => {
-    app.singletons.appActor.start()
-    // Application commands must be created after the initPromise because
-    // it calls WASM functions to file extensions, this dependency is not available during initialization, it is an async dependency
-    app.singletons.commandBarActor.send({
-      type: 'Add commands',
-      data: {
-        commands: [
-          ...createApplicationCommands({
-            systemIOActor: app.singletons.systemIOActor,
-            wasmInstance,
-            appActor: app.singletons.appActor,
-            setLayout: app.singletons.setLayout,
-          }),
-        ],
-      },
-    })
-  })
-  .catch(reportRejection)
-
-// Monkey patch to prevent issues in the web app with automated browser translation
-// This mitigates https://github.com/KittyCAD/modeling-app/issues/8667, until
-// we roll out our own i18n solution and can disable browser translation altogether.
-// https://github.com/KittyCAD/modeling-app/issues/4959
-if (!window.electron) {
-  monkeyPatchForBrowserTranslation()
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-
-root.render(
-  <app.ReactContext.Provider value={app.singletons}>
-    <HotkeysProvider>
-      <AppStreamProvider>
-        <Router />
-        <Toaster
-          position="bottom-center"
-          toastOptions={{
-            style: {
-              borderRadius: '3px',
-              maxInlineSize: 'min(480px, 100%)',
-            },
-            className:
-              'bg-chalkboard-10 dark:bg-chalkboard-90 text-chalkboard-110 dark:text-chalkboard-10 rounded-sm border-chalkboard-20/50 dark:border-chalkboard-80/50',
-            success: {
-              iconTheme: {
-                primary: 'oklch(89% 0.16 143.4deg)',
-                secondary: 'oklch(48.62% 0.1654 142.5deg)',
-              },
-              // We shouldn't have a different duration in tests than prod, it might
-              // lead to issues.
-              duration: 1500,
-            },
-          }}
-        />
-        <ModalContainer />
-      </AppStreamProvider>
-    </HotkeysProvider>
-  </app.ReactContext.Provider>
-)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
-
-if (window.electron) {
-  window.electron.onUpdateChecking(() => {
-    const message = `Checking for updates...`
-    console.log(message)
-    toast.loading(message, { id: AUTO_UPDATER_TOAST_ID })
-  })
-
-  window.electron.onUpdateNotAvailable(() => {
-    const message = `You're already using the latest version of the app.`
-    console.log(message)
-    toast.success(message, { id: AUTO_UPDATER_TOAST_ID })
-  })
-
-  window.electron.onUpdateDownloadStart(() => {
-    const message = `Downloading app update...`
-    console.log(message)
-    toast.loading(message, { id: AUTO_UPDATER_TOAST_ID })
-  })
-
-  window.electron.onUpdateError(({ error }) => {
-    console.error(error)
-    toast.error('An error occurred while downloading the update.', {
-      id: AUTO_UPDATER_TOAST_ID,
-    })
-  })
-
-  const electron = window.electron
-  window.electron.onUpdateDownloaded(({ version, releaseNotes }) => {
-    const message = `A new update (${version}) was downloaded and will be available next time you open the app.`
-    console.log(message)
-    toast.custom(
-      ToastUpdate({
-        version,
-        releaseNotes,
-        onRestart: () => {
-          electron.appRestart()
-        },
-        onDismiss: () => {},
-      }),
-      { duration: 30000, id: AUTO_UPDATER_TOAST_ID }
+  /** initialize behaviors that rely on singletons */
+  function initSingletonBehavior() {
+    markOnce('code/willAuth')
+    initializeWindowExceptionHandler(
+      app.singletons.kclManager,
+      app.singletons.rustContext
     )
-  })
+
+    // Don't start the app machine until all these singletons
+    // are initialized, and the wasm module is loaded.
+    app.singletons.kclManager.wasmInstancePromise
+      .then((wasmInstance) => {
+        app.singletons.appActor.start()
+        // Application commands must be created after the initPromise because
+        // it calls WASM functions to file extensions, this dependency is not available during initialization, it is an async dependency
+        app.singletons.commandBarActor.send({
+          type: 'Add commands',
+          data: {
+            commands: [
+              ...createApplicationCommands({
+                systemIOActor: app.singletons.systemIOActor,
+                wasmInstance,
+                appActor: app.singletons.appActor,
+                setLayout: app.singletons.setLayout,
+              }),
+            ],
+          },
+        })
+      })
+      .catch(reportRejection)
+  }
+
+  /** initialize behaviors that rely on electron (this is only available on desktop) */
+  function initElectronBehavior(electron: NonNullable<typeof window.electron>) {
+    // Monkey patch to prevent issues in the web app with automated browser translation
+    // This mitigates https://github.com/KittyCAD/modeling-app/issues/8667, until
+    // we roll out our own i18n solution and can disable browser translation altogether.
+    // https://github.com/KittyCAD/modeling-app/issues/4959
+    monkeyPatchForBrowserTranslation()
+
+    electron.onUpdateChecking(() => {
+      const message = `Checking for updates...`
+      console.log(message)
+      toast.loading(message, { id: AUTO_UPDATER_TOAST_ID })
+    })
+
+    electron.onUpdateNotAvailable(() => {
+      const message = `You're already using the latest version of the app.`
+      console.log(message)
+      toast.success(message, { id: AUTO_UPDATER_TOAST_ID })
+    })
+
+    electron.onUpdateDownloadStart(() => {
+      const message = `Downloading app update...`
+      console.log(message)
+      toast.loading(message, { id: AUTO_UPDATER_TOAST_ID })
+    })
+
+    electron.onUpdateError(({ error }) => {
+      console.error(error)
+      toast.error('An error occurred while downloading the update.', {
+        id: AUTO_UPDATER_TOAST_ID,
+      })
+    })
+
+    electron.onUpdateDownloaded(({ version, releaseNotes }) => {
+      const message = `A new update (${version}) was downloaded and will be available next time you open the app.`
+      console.log(message)
+      toast.custom(
+        ToastUpdate({
+          version,
+          releaseNotes,
+          onRestart: () => {
+            electron.appRestart()
+          },
+          onDismiss: () => {},
+        }),
+        { duration: 30000, id: AUTO_UPDATER_TOAST_ID }
+      )
+    })
+  }
+
+  /** mount the app as a React node and begin rendering its components */
+  function initReact() {
+    const root = ReactDOM.createRoot(
+      document.getElementById('root') as HTMLElement
+    )
+
+    root.render(
+      <app.ReactContext.Provider value={app.singletons}>
+        <HotkeysProvider>
+          <AppStreamProvider>
+            <Router />
+            <Toaster
+              position="bottom-center"
+              toastOptions={{
+                style: {
+                  borderRadius: '3px',
+                  maxInlineSize: 'min(480px, 100%)',
+                },
+                className:
+                  'bg-chalkboard-10 dark:bg-chalkboard-90 text-chalkboard-110 dark:text-chalkboard-10 rounded-sm border-chalkboard-20/50 dark:border-chalkboard-80/50',
+                success: {
+                  iconTheme: {
+                    primary: 'oklch(89% 0.16 143.4deg)',
+                    secondary: 'oklch(48.62% 0.1654 142.5deg)',
+                  },
+                  // We shouldn't have a different duration in tests than prod, it might
+                  // lead to issues.
+                  duration: 1500,
+                },
+              }}
+            />
+            <ModalContainer />
+          </AppStreamProvider>
+        </HotkeysProvider>
+      </app.ReactContext.Provider>
+    )
+
+    // If you want to start measuring performance in your app, pass a function
+    // to log results (for example: reportWebVitals(console.log))
+    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    reportWebVitals()
+  }
 }
