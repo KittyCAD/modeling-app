af76ff7a87091465c2ae737d81e302ec10e2d91f
Adam Chalmers <adam.chalmers@zoo.dev>
2026-01-26 18:54:25 -0600

    Emit error if scaling by 0

--- rust/kcl-lib/src/std/transform.rs
+++ rust/kcl-lib/src/std/transform.rs
@@ -42,6 +42,16 @@ pub async fn scale(exec_state: &mut ExecState, args: Args) -> Result<KclValue, K
     let scale_x: Option<TyF64> = args.get_kw_arg_opt("x", &RuntimeType::count(), exec_state)?;
     let scale_y: Option<TyF64> = args.get_kw_arg_opt("y", &RuntimeType::count(), exec_state)?;
     let scale_z: Option<TyF64> = args.get_kw_arg_opt("z", &RuntimeType::count(), exec_state)?;
+    for scale_dim in [&scale_x, &scale_y, &scale_z] {
+        if let Some(num) = scale_dim
+            && num.n == 0.0
+        {
+            return Err(KclError::new_semantic(KclErrorDetails::new(
+                "Cannot scale by 0".to_string(),
+                vec![args.source_range],
+            )));
+        }
+    }
     let factor: Option<TyF64> = args.get_kw_arg_opt("factor", &RuntimeType::count(), exec_state)?;
     let (scale_x, scale_y, scale_z) = match (scale_x, scale_y, scale_z, factor) {
         (None, None, None, Some(factor)) => (Some(factor.clone()), Some(factor.clone()), Some(factor)),
