0eb0e8b6bfcc7843a3c91a69064c83ab75429437
Frank Noirot <frankjohnson1993@gmail.com>
2026-01-30 12:31:01 -0500

    Feature: add ability for Pane > Split layouts to be closeable

--- src/lib/layout/types.ts
+++ src/lib/layout/types.ts
@@ -112,7 +112,7 @@ export type PaneLayout = BaseLayout &
     onExpandSize?: number
   }
 export interface Closeable {
-  onClose: () => void
+  onClose: (idOverride?: string) => void
 }
 export type SimpleLayout = BaseLayout & {
   type: LayoutType.Simple
