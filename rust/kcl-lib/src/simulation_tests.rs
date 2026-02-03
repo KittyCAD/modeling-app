37f7df5d2e44a3e75894cd6c97beefca3d31d868
Jonathan Tran <jonnytran@gmail.com>
2026-02-03 16:08:24 -0500

    Add failing test

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -4395,6 +4395,27 @@ mod sketch_block_on_offset_plane {
         super::execute(TEST_NAME, true).await
     }
 }
+mod sketch_block_tag_face_and_sketch {
+    const TEST_NAME: &str = "sketch_block_tag_face_and_sketch";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
 mod sketch_on_face_normal {
     const TEST_NAME: &str = "sketch_on_face_normal";
 
