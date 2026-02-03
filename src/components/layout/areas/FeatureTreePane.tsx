05a5fafd17d0babece87e62b3282f0994612d0f9
Frank Noirot <frankjohnson1993@gmail.com>
2026-02-02 20:23:42 -0500

    typo

--- src/components/layout/areas/FeatureTreePane.tsx
+++ src/components/layout/areas/FeatureTreePane.tsx
@@ -82,7 +82,7 @@ export function FeatureTreePane(props: AreaTypeComponentProps) {
         Menu={FeatureTreeMenu}
         onClose={() => {
           // Gotcha: because our layout system is a goofy first draft,
-          // it doesn't know how to handle Splits as childs of Panes very well,
+          // it doesn't know how to handle Splits as children of Panes very well,
           // so the onClose here needs to explicitly state the Split to close,
           // since this layout will be used in a Simple layout now.
           props.onClose?.('feature-tree')
