398b6deba067383a387e5775015b68b8e327ecdd
Ryan Barton <ryan@zoo.dev>
2026-02-20 10:55:09 -0500

    fix pyi name

--- rust/kcl-python-bindings/src/bin/stub_gen.rs
+++ rust/kcl-python-bindings/src/bin/stub_gen.rs
@@ -8,7 +8,7 @@ fn main() -> Result<()> {
     // Append PanicException to the generated stub file.
     // pyo3_stub_gen can't auto-generate this because PanicException comes from
     // pyo3 itself, not from our code annotated with gen_stub_pyclass.
-    let stub_path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("kcl.pyi");
+    let stub_path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("zoo_kcl.pyi");
     let mut contents = std::fs::read_to_string(&stub_path)?;
     contents.push_str(
         "\nclass PanicException(BaseException):\n    r\"\"\"\n    The exception raised when Rust code called from Python panics.\n\n    Like SystemExit, this exception is derived from BaseException so that\n    it will typically propagate all the way through the stack and cause the\n    Python interpreter to exit.\n    \"\"\"\n    ...\n",
