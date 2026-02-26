f7d19a691a892fa136c3f5ac88f63e8d2080c465
Adam Chalmers <adam.chalmers@zoo.dev>
2026-02-26 15:50:29 -0600

    Add simtest

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -4672,3 +4672,24 @@ mod delete_face_by_id {
         super::execute(TEST_NAME, true).await
     }
 }
+mod sketch_block_angle_constraint {
+    const TEST_NAME: &str = "sketch_block_angle_constraint";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
