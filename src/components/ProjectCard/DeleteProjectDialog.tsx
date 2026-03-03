99610e51ddb92926c83410ae9c9542947ce3a144
Andrew Varga <grizzly33@gmail.com>
2026-03-03 11:16:27 +0000

    SetVarNameModal button order, fix cancel button submitting the form

--- src/components/ProjectCard/DeleteProjectDialog.tsx
+++ src/components/ProjectCard/DeleteProjectDialog.tsx
@@ -16,7 +16,6 @@ export function DeleteConfirmationDialog({
   onDismiss,
   children,
 }: DeleteConfirmationDialogProps) {
-
   useEffect(() => {
     const onWindowKeyDown = (event: KeyboardEvent) => {
       if (event.key === 'Escape') {
