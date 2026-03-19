22b0ebf099a0ab31f74de7a32f1028a2e4660e8e
Jonathan Tran <jonnytran@gmail.com>
2026-03-19 18:48:52 -0400

    Add sim test

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -4550,6 +4550,27 @@ mod sketch_on_face_normal_inches {
         super::execute(TEST_NAME, true).await
     }
 }
+mod sketch_on_face_of_region_extrude_one_to_one {
+    const TEST_NAME: &str = "sketch_on_face_of_region_extrude_one_to_one";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
 mod pos_literals {
     const TEST_NAME: &str = "pos_literals";
 
