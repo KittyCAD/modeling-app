7a1fe5ab518bab6f56e5628f49e032605fa830cf
Jace Browning <jacebrowning@gmail.com>
2026-03-18 20:08:06 -0400

    Include the URL in the payload for tracking

--- e2e/playwright/lib/api-reporter.ts
+++ e2e/playwright/lib/api-reporter.ts
@@ -91,6 +91,7 @@ class APIReporter implements Reporter {
       message: result.error?.stack,
       target: process.env.TARGET || null,
       platform: process.env.RUNNER_OS || process.platform,
+      url: process.env.VERCEL_BASE_URL || null,
       // Extra test and result data
       annotations: test.annotations.map((a) => a.type), // e.g. 'fail' or 'fixme'
       id: test.id, // computed file/test/project ID used for reruns
