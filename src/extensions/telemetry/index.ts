675696cbf2641cbb826ebca40e23f63fe603f8e0
Frank Noirot <frankjohnson1993@gmail.com>
2026-03-22 13:58:44 -0400

    Update the telemetry extension to be a plugin

--- src/extensions/telemetry/index.ts
+++ src/extensions/telemetry/index.ts
@@ -1,11 +1,6 @@
-import { routesFacet } from '@src/facets'
-import {
-  Compartment,
-  type ExtensionNode,
-  createTogglableExtension,
-  defineExtension,
-  provide,
-} from '@src/lib/extensions'
+import { routesFacet, statusBarFacet } from '@src/facets'
+import { defineExtension, provide, createPlugin } from '@src/lib/extensions'
+import { PATHS } from '@src/lib/paths'
 import { telemetryFileRoute, telemetryHomeRoute } from '@src/routes/Telemetry'
 
 const telemetryRouteExt = defineExtension({
@@ -15,16 +10,31 @@ const telemetryRouteExt = defineExtension({
   ],
 })
 
-const compartment = new Compartment()
-const telemetryBase = [telemetryRouteExt]
-export const telemetryToggle = createTogglableExtension({
-  name: 'telemetry',
-  extensions: telemetryBase,
-  compartment,
+const telemetryStatusBarExt = defineExtension({
+  provides: [
+    provide(statusBarFacet, {
+      global: [
+        {
+          id: 'telemetry',
+          element: 'link',
+          icon: 'stopwatch',
+          href: `${PATHS.TELEMETRY}?tab=project`,
+          'data-testid': 'telemetry-link',
+          label: 'Telemetry',
+          hideLabel: true,
+          toolTip: {
+            children: 'Telemetry',
+          },
+        },
+      ],
+    }),
+  ],
 })
-window.telemetryToggle = telemetryToggle
 
-export const telemetry: ExtensionNode[] = [
-  compartment.of(...telemetryBase),
-  telemetryToggle.extension,
-]
+export const telemetry = createPlugin({
+  id: 'telemetry',
+  title: 'Telemetry',
+  description:
+    'Telemetry tracks the performance of the app, allowing other extensions to mark timers and events. It adds a `/telemetry` route and a status bar item.',
+  extensions: [telemetryRouteExt, telemetryStatusBarExt],
+})
