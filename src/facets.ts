90cd1ed16ba05233fd7dc5c2146e4982fcf1ae61
Frank Noirot <frankjohnson1993@gmail.com>
2026-03-22 02:24:28 -0400

    Add basic facets

---
+++ src/facets.ts
@@ -0,0 +1,13 @@
+import type { StatusBarItemType } from '@src/components/StatusBar/statusBarTypes'
+import type { RouteObject } from 'react-router-dom'
+import { appendFacet, mergeObjectsFacet } from '@src/lib/extensions'
+
+export const routesFacet = appendFacet<RouteObject>('routes')
+export const statusBarFacet = mergeObjectsFacet<{
+  global: StatusBarItemType[]
+  local: StatusBarItemType[]
+}>('statusbar', {
+  global: [],
+  local: [],
+})
+window.routesFacet = routesFacet
