59703d10dc180f670652ff24b58c66b9dc95e95b
Adam Chalmers <adam.chalmers@zoo.dev>
2026-03-30 17:30:46 -0500

    Add revolve example

--- rust/kcl-derive-docs/src/example_tests.rs
+++ rust/kcl-derive-docs/src/example_tests.rs
@@ -133,6 +133,7 @@ pub const TEST_NAMES: &[&str] = &[
     "std-sketch-revolve-12",
     "std-sketch-revolve-13",
     "std-sketch-revolve-14",
+    "std-sketch-revolve-15",
     "std-sketch-revolve-2",
     "std-sketch-revolve-3",
     "std-sketch-revolve-4",
