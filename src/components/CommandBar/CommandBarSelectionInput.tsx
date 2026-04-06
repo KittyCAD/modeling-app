d1e2c688a2148e51b2c05f5554fcfeaadb1be88c
Jonathan Tran <jonnytran@gmail.com>
2026-04-05 23:40:43 -0400

    Revert semantic change since this was actually dead code

--- src/components/CommandBar/CommandBarSelectionInput.tsx
+++ src/components/CommandBar/CommandBarSelectionInput.tsx
@@ -124,7 +124,6 @@ function CommandBarSelectionInput({
           selectionType: 'singleCodeCursor',
         },
       })
-      setHasClearedSelection(true)
     }
   }, [arg, engineCommandManager])
 
