9e540af0aaa2e7a327cfa5bb2859346655011641
Frank Noirot <frankjohnson1993@gmail.com>
2026-04-09 11:57:40 -0400

    @jtran feedback

--- src/machines/modelingMachine.ts
+++ src/machines/modelingMachine.ts
@@ -1295,7 +1295,9 @@ export const modelingMachine = setup({
     'disable rotate for sketch solve mode': ({ context }) => {
       // Sketch solve currently has sync issues with engine and trouble translating world space to sketch space,
       // so block orbit input until those controls are synchronized.
-      context.kclManager.sceneInfra.camControls.enableRotate = false
+      // When that is the case, the hidden setting "allow orbit in sketch mode" will be shown to users.
+      context.kclManager.sceneInfra.camControls.enableRotate =
+        context.kclManager.sceneInfra.camControls._setting_allowOrbitInSketchMode
     },
     /** TODO: this action is hiding unawaited asynchronous code */
     'set selection filter to faces only': ({ context }) => {
