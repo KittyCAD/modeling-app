5172e5fd775ee05740c4990b8629f66f37d26069
Andrew Varga <grizzly33@gmail.com>
2026-04-13 19:26:05 +0200

    #11018 Support Equal Radius constraint (#11021)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/lib/toolbar.ts
+++ src/lib/toolbar.ts
@@ -1273,7 +1273,7 @@ export const useToolbarConfig = () => {
                 onClick: ({ modelingSend }) =>
                   modelingSend({ type: 'Constrain equal length' }),
                 status: 'available',
-                title: 'Equal Length',
+                title: 'Equal',
                 showTitle: false,
                 description:
                   'Constrain two or more segments to have equal length.',
@@ -1743,13 +1743,14 @@ export const useToolbarConfig = () => {
             id: 'equalLength',
             onClick: ({ modelingSend, isActive }) =>
               modelingSend({
-                type: 'LinesEqualLength',
+                type: 'EqualLength',
               }),
             icon: 'equal',
             status: 'available',
-            title: 'Equal Length',
+            title: 'Equal',
             hotkey: 'E',
-            description: 'Constrain lines to have equal length.',
+            description:
+              'Constrain lines to have equal length, or arcs and circles to have equal radius.',
             links: [],
             isActive: (state) => false,
           },
