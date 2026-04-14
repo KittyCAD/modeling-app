492e8be29d8c2b41946b328af22acbb7900e134d
Pierre Jacquier <pierre@zoo.dev>
2026-04-14 16:13:35 -0400

    Experiment: default to useSketchSolveMode in playwright

--- src/lib/settings/settingsUtils.ts
+++ src/lib/settings/settingsUtils.ts
@@ -21,7 +21,6 @@ import {
 } from '@src/lib/desktop'
 import { isDesktop } from '@src/lib/isDesktop'
 import { createKCClient, kcCall } from '@src/lib/kcClient'
-import { isPlaywright } from '@src/lib/isPlaywright'
 import {
   createSettings,
   type Setting,
@@ -399,8 +398,10 @@ export async function loadAndValidateSettings(
     configurationToSettingsPayload(appSettingsPayload)
   )
 
-  settingsNext.modeling.useSketchSolveMode.default =
-    !isPlaywright() && !(await userHasFeature('classic_sketch_mode', false))
+  settingsNext.modeling.useSketchSolveMode.default = !(await userHasFeature(
+    'classic_sketch_mode',
+    false
+  ))
 
   // Load the project settings if they exist
   if (projectPath) {
