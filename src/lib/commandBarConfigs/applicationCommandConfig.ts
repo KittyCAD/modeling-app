9ad28f7c3ae48b115a13e67591f440c72ab8773b
Kurt Hutten Irev-Dev <k.hutten@protonmail.ch>
2026-04-14 14:29:45 +1000

    add overrides to web app

--- src/lib/commandBarConfigs/applicationCommandConfig.ts
+++ src/lib/commandBarConfigs/applicationCommandConfig.ts
@@ -409,10 +409,6 @@ export function createApplicationCommands({
     icon: 'gear',
     groupId: 'application',
     onSubmit: (data) => {
-      if (!window.electron) {
-        console.error(new Error('No file system present'))
-        return
-      }
       if (data) {
         const requestedEnvironmentFormatted = returnSelfOrGetHostNameFromURL(
           data.environment
@@ -453,10 +449,6 @@ export function createApplicationCommands({
       }
     },
     onSubmit: (data) => {
-      if (!window.electron) {
-        console.error(new Error('No file system present'))
-        return
-      }
       const environmentName = env().VITE_ZOO_BASE_DOMAIN
       if (environmentName)
         writeEnvironmentConfigurationKittycadWebSocketUrl(
@@ -501,10 +493,6 @@ export function createApplicationCommands({
       }
     },
     onSubmit: (data) => {
-      if (!window.electron) {
-        console.error(new Error('No file system present'))
-        return
-      }
       const environmentName = env().VITE_ZOO_BASE_DOMAIN
       if (environmentName)
         writeEnvironmentConfigurationMlephantWebSocketUrl(
@@ -594,6 +582,9 @@ export function createApplicationCommands({
         resetLayoutCommand,
         setLayoutCommand,
         createASampleDesktopOnly,
+        switchEnvironmentsCommand,
+        overrideEngineCommand,
+        overrideZookeeperCommand,
       ]
 }
 
