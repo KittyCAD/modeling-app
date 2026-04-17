KittyCAD/modeling-app

commit 3ce42dbe55032aec64e93759eba0f8d7c5433ede

Decouple CodeMirror from React (#9448)
* Pare down CodeMirror EditorView to live outside of React

* Get blinking cursor setting working without react too

* Wire up autocompletions again

* Get registered hotkeys working again, delete unused code

* Convert CommandBarKclInput to not use useCodeMirror

* Don't count hotkey registrations as relevant updates

* Rename `deferExecution` to `deferredCallback`, it's way too confusing

* KILL KCL VIEW PLUGIN LFG

* Fix up desktop integration tests

* I don't think we were updating the CM selection via dispatch, but rerender

* Don't make setting code auto-execute (in this PR at least)

* replace auto-execution flow (for now) with updateCodeStateEditor method

* Fix up E2E test that relied on lazy-loaded KCL code

* Remove sketch-specific branch from execution extension

* Fix up E2E test that didn't expect selections to persist so well!

* This test passes locally, what gives?

* Fix feature tree selection, which relied on this roundabout code in KclEditorPane

* Fix undo/redo button reactivity

* Try awaiting the diagnostic errors, not syntax highlighting

* Fix lints

* Move cursor blinking to singletons like all others that depend on kclManager

* Update CommandBarKclInput.tsx

Co-authored-by: Zookeeper Lee <lee@zoo.dev>

---------

Co-authored-by: Zookeeper Lee <lee@zoo.dev>

src/components/layout/areas/KclEditorPane.tsx
@@ -3,73 +3,17 @@ import type { PropsWithChildren } from 'react'
 import { ActionIcon } from '@src/components/ActionIcon'
 import { useConvertToVariable } from '@src/hooks/useToolbarGuards'
 import { openExternalBrowserIfDesktop } from '@src/lib/openWindow'
-import {
-  commandBarActor,
-  getSettings,
-  settingsActor,
-} from '@src/lib/singletons'
+import { commandBarActor, settingsActor } from '@src/lib/singletons'
 import { withSiteBaseURL } from '@src/lib/withBaseURL'
 import toast from 'react-hot-toast'
 import styles from './KclEditorMenu.module.css'
-import {
-  closeBrackets,
-  closeBracketsKeymap,
-  completionKeymap,
-} from '@codemirror/autocomplete'
-import {
-  defaultKeymap,
-  history,
-  historyField,
-  historyKeymap,
-  indentWithTab,
-} from '@codemirror/commands'
-import {
-  bracketMatching,
-  codeFolding,
-  foldGutter,
-  foldKeymap,
-  indentOnInput,
-} from '@codemirror/language'
-import { diagnosticCount, lintGutter, lintKeymap } from '@codemirror/lint'
-import { highlightSelectionMatches, searchKeymap } from '@codemirror/search'
-import type { Extension } from '@codemirror/state'
-import { EditorState, Prec, Transaction } from '@codemirror/state'
-import {
-  EditorView,
-  drawSelection,
-  dropCursor,
-  highlightActiveLine,
-  highlightActiveLineGutter,
-  highlightSpecialChars,
-  keymap,
-  lineNumbers,
-  rectangularSelection,
-} from '@codemirror/view'
-import interact from '@replit/codemirror-interact'
-import { useSelector } from '@xstate/react'
-import { useEffect, useMemo, useRef } from 'react'
-import { useLspContext } from '@src/components/LspProvider'
-import CodeEditor from '@src/components/layout/areas/CodeEditor'
-import { historyCompartment } from '@src/editor/compartments'
-import { lineHighlightField } from '@src/editor/highlightextension'
-import { modelingMachineEvent } from '@src/lang/KclManager'
+import { useEffect, useRef } from 'react'
 import { kclManager } from '@src/lib/singletons'
-import { useSettings } from '@src/lib/singletons'
 import { reportRejection, trap } from '@src/lib/trap'
-import { onMouseDragMakeANewNumber, onMouseDragRegex } from '@src/lib/utils'
-import { artifactAnnotationsExtension } from '@src/editor/plugins/artifacts'
-import { getArtifactAnnotationsAtCursor } from '@src/lib/getArtifactAnnotationsAtCursor'
-import {
-  editorIsMountedSelector,
-  kclEditorActor,
-  selectionEventSelector,
-} from '@src/machines/kclEditorMachine'
 import type { AreaTypeComponentProps } from '@src/lib/layout'
 import { LayoutPanel, LayoutPanelHeader } from '@src/components/layout/Panel'
-import { editorTheme, themeCompartment } from '@src/lib/codeEditor'
 import { CustomIcon } from '@src/components/CustomIcon'
-import { getResolvedTheme } from '@src/lib/theme'
-import { kclAstExtension } from '@src/editor/plugins/ast'
+import { kclEditorActor } from '@src/machines/kclEditorMachine'
 
 export const editorShortcutMeta = {
   formatCode: {
@@ -101,182 +45,22 @@ export const KclEditorPane = (props: AreaTypeComponentProps) => {
 }
 
 export const KclEditorPaneContents = () => {
-  const context = useSettings()
-  const lastSelectionEvent = useSelector(kclEditorActor, selectionEventSelector)
-  const editorIsMounted = useSelector(kclEditorActor, editorIsMountedSelector)
-  const { copilotLSP, kclLSP } = useLspContext()
-
-  // When this component unmounts, we need to tell the machine that the editor
+  const editorParent = useRef<HTMLDivElement>(null)
   useEffect(() => {
-    return () => {
+    kclEditorActor.send({ type: 'setKclEditorMounted', data: true })
+    editorParent.current?.appendChild(kclManager.editorView.dom)
+
+    return () =>
       kclEditorActor.send({ type: 'setKclEditorMounted', data: false })
-      kclEditorActor.send({ type: 'setLastSelectionEvent', data: undefined })
-      kclManager.diagnostics = []
-    }
   }, [])
-
-  useEffect(() => {
-    const editorView = kclManager.getEditorView()
-    if (!editorIsMounted || !lastSelectionEvent || !editorView) {
-      return
-    }
-
-    try {
-      editorView.dispatch({
-        selection: lastSelectionEvent.codeMirrorSelection,
-        annotations: [modelingMachineEvent, Transaction.addToHistory.of(false)],
-        scrollIntoView: lastSelectionEvent.scrollIntoView,
-      })
-    } catch (e) {
-      console.error('Error setting selection', e)
-    }
-  }, [editorIsMounted, lastSelectionEvent])
-
-  const textWrapping = context.textEditor.textWrapping
-  const cursorBlinking = context.textEditor.blinkingCursor
-  // DO NOT ADD THE CODEMIRROR HOTKEYS HERE TO THE DEPENDENCY ARRAY
-  // It reloads the editor every time we do _anything_ in the editor
-  // I have no idea why.
-  // Instead, hot load hotkeys via code mirror native.
-  const codeMirrorHotkeys = kclManager.getCodemirrorHotkeys()
-
-  // When opening the editor, use the existing history in kclManager.
-  // This is needed to ensure users can undo beyond when the editor has been openeed.
-  // (Another solution would be to reuse the same state instead of creating a new one in CodeEditor.)
-  const existingHistory = kclManager.editorState.field(historyField)
-  const initialHistory = existingHistory
-    ? historyField.init(() => existingHistory)
-    : history()
-
-  const editorExtensions = useMemo(() => {
-    const extensions = [
-      themeCompartment.of(
-        editorTheme[getResolvedTheme(getSettings().app.theme.current)]
-      ),
-      drawSelection({
-        cursorBlinkRate: cursorBlinking.current ? 1200 : 0,
-      }),
-      lineHighlightField,
-      artifactAnnotationsExtension(),
-      kclAstExtension(),
-      historyCompartment.of(initialHistory),
-      closeBrackets(),
-      codeFolding(),
-      keymap.of([
-        ...closeBracketsKeymap,
-        ...defaultKeymap,
-        ...searchKeymap,
-        ...historyKeymap,
-        ...foldKeymap,
-        ...completionKeymap,
-        ...lintKeymap,
-        indentWithTab,
-        ...codeMirrorHotkeys,
-        {
-          key: editorShortcutMeta.convertToVariable.codeMirror,
-          run: () => {
-            return kclManager.convertToVariable()
-          },
-        },
-      ]),
-    ] as Extension[]
-
-    if (kclLSP) extensions.push(Prec.highest(kclLSP))
-    if (copilotLSP) extensions.push(copilotLSP)
-
-    extensions.push(
-      lintGutter(),
-      lineNumbers(),
-      highlightActiveLineGutter(),
-      highlightSpecialChars(),
-      foldGutter(),
-      EditorState.allowMultipleSelections.of(true),
-      indentOnInput(),
-      bracketMatching(),
-      closeBrackets(),
-      highlightActiveLine(),
-      highlightSelectionMatches(),
-      rectangularSelection(),
-      dropCursor(),
-      interact({
-        rules: [
-          // a rule for a number dragger
-          {
-            // the regexp matching the value
-            regexp: onMouseDragRegex,
-            // set cursor to "ew-resize" on hover
-            cursor: 'ew-resize',
-            // change number value based on mouse X movement on drag
-            onDrag: (text, setText, e) => {
-              onMouseDragMakeANewNumber(text, setText, e)
-            },
-          },
-        ],
-      })
-    )
-    if (textWrapping.current) extensions.push(EditorView.lineWrapping)
-
-    if (context.app.showDebugPanel.current) {
-      extensions.push(
-        keymap.of([
-          {
-            key: 'Mod-Shift-d',
-            preventDefault: true,
-            run: () => {
-              const view = kclManager.getEditorView()
-              if (!view) return false
-              const data = getArtifactAnnotationsAtCursor(view.state)
-              // eslint-disable-next-line no-console
-              console.log('Artifact annotations at cursor:', data)
-              return true
-            },
-          },
-        ])
-      )
-    }
-
-    return extensions
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- TODO: blanket-ignored fix me!
-  }, [kclLSP, copilotLSP, textWrapping.current, cursorBlinking.current])
-
-  const initialCode = useRef(kclManager.code)
 
   return (
     <div className="relative">
       <div
         id="code-mirror-override"
-        className={
-          'absolute inset-0 pr-1 ' + (cursorBlinking.current ? 'blink' : '')
-        }
-      >
-        <CodeEditor
-          initialDocValue={initialCode.current}
-          extensions={editorExtensions}
-          onCreateEditor={(_editorView) => {
-            kclManager.setEditorView(_editorView)
-
-            if (!_editorView) return
-
-            // Update diagnostics as they are cleared when the editor is unmounted.
-            // Without this, errors would not be shown when closing and reopening the editor.
-            kclManager.wasmInstancePromise
-              .then((wasmInstance) =>
-                kclManager
-                  .safeParse(kclManager.code, wasmInstance)
-                  .then(() => {
-                    // On first load of this component, ensure we show the current errors
-                    // in the editor.
-                    // Make sure we don't add them twice.
-                    if (diagnosticCount(_editorView.state) === 0) {
-                      kclManager.setDiagnosticsForCurrentErrors()
-                    }
-                  })
-                  .catch(reportRejection)
-              )
-              .catch(reportRejection)
-          }}
-        />
-      </div>
+        className="absolute inset-0 pr-1"
+        ref={editorParent}
+      />
     </div>
   )
 }

e2e/playwright/code-pane-and-errors.spec.ts
@@ -69,7 +69,7 @@ extrude001 = extrude(sketch001, length = 5)`
     })
 
     // Ensure we have no errors in the gutter, since error out of view.
-    await expect(page.locator('.cm-lint-marker-error')).not.toBeVisible()
+    await expect(page.locator('.cm-lint-marker-error')).not.toBeInViewport()
 
     // Click the badge.
     const badge = page.locator('#code-badge')
@@ -77,7 +77,7 @@ extrude001 = extrude(sketch001, length = 5)`
     await badge.click()
 
     // Ensure we have an error diagnostic.
-    await expect(page.locator('.cm-lint-marker-error').first()).toBeVisible()
+    await expect(page.locator('.cm-lint-marker-error').first()).toBeInViewport()
 
     // Hover over the error to see the error message
     await page.hover('.cm-lint-marker-error')

e2e/playwright/editor-tests.spec.ts
@@ -670,14 +670,8 @@ a1 = startSketchOn(offsetPlane(XY, offset = 10))
 
     await scene.connectionEstablished()
 
-    // Wait for highlighting to kick in, a good proxy that the LSP is ready.
-    await expect
-      .poll(() =>
-        page.evaluate(
-          () => document.querySelector('.cm-line > span[class*="ͼ"]') !== null
-        )
-      )
-      .toBe(true)
+    // Wait for the linter diagnostics to populate, a good proxy that the LSP is ready.
+    await expect(page.locator('.cm-lint-marker-error')).toBeVisible()
 
     // Expect the signature help to NOT be visible
     await expect(page.locator('.cm-signature-tooltip')).not.toBeVisible()
@@ -699,9 +693,9 @@ a1 = startSketchOn(offsetPlane(XY, offset = 10))
     )
 
     // Make sure the tooltip goes away after a timeout.
-    await page.waitForTimeout(12000)
-
-    await expect(page.locator('.cm-signature-tooltip')).not.toBeVisible()
+    await expect(page.locator('.cm-signature-tooltip')).not.toBeVisible({
+      timeout: 15_000,
+    })
   })
 
   test('if you write kcl with lint errors you get lints', async ({

e2e/playwright/fixtures/editorFixture.ts
@@ -211,11 +211,11 @@ export class EditorFixture {
   scrollToText(text: string, placeCursor?: boolean) {
     return this.page.evaluate(
       (args: { text: string; placeCursor?: boolean }) => {
-        const editorView = window.kclManager.getEditorView()
+        const editorView = window.kclManager.editorView
         // error TS2339: Property 'docView' does not exist on type 'EditorView'.
         // Except it does so :shrug:
         // @ts-ignore
-        const index = editorView?.docView.view.state.doc
+        const index = editorView.docView.view.state.doc
           .toString()
           .indexOf(args.text)
         editorView?.focus()

e2e/playwright/point-click.spec.ts
@@ -474,7 +474,7 @@ openSketch = startSketchOn(XY)
       await editor.openPane()
       await editor.expectState({
         activeLines: [`|>circle(center=[8,5],radius=2)`],
-        highlightedCode: '',
+        highlightedCode: 'circle(center=[8,5],radius=2)',
         diagnostics: [],
       })
     })
@@ -511,7 +511,7 @@ openSketch = startSketchOn(XY)
       await editor.openPane()
       await editor.expectState({
         activeLines: [`|>tangentialArc(endAbsolute=[10,0])`],
-        highlightedCode: '',
+        highlightedCode: 'tangentialArc(endAbsolute=[10,0])',
         diagnostics: [],
       })
     })

e2e/playwright/test-utils.ts
@@ -163,10 +163,10 @@ async function openKclCodePanel(page: Page) {
   await page.evaluate(() => {
     // kclManager is available on the window object.
     //@ts-ignore this is in an entirely different context that tsc can't see.
-    kclManager.getEditorView().dispatch({
+    kclManager.editorView.dispatch({
       selection: {
         //@ts-ignore this is in an entirely different context that tsc can't see.
-        anchor: kclManager.getEditorView().docView.length,
+        anchor: kclManager.editorView.docView.length,
       },
       scrollIntoView: true,
     })

e2e/playwright/testing-selections.spec.ts
@@ -370,7 +370,7 @@ test.describe('Testing selections', () => {
     })
 
     await test.step('Right click on bracket sample leads to the right place in code', async () => {
-      await expect(line).not.toBeVisible()
+      await expect(line).not.toBeInViewport()
       await clickCenter()
       await expect(page.getByText('1 face')).toBeVisible()
       await clickCenter({ shouldRightClick: true })

src/Toolbar.tsx
@@ -96,7 +96,7 @@ const Toolbar_ = memo(
         modelingState: props.state,
         modelingSend: props.send,
         sketchPathId,
-        editorHasFocus: kclManager.getEditorView()?.hasFocus,
+        editorHasFocus: kclManager.editorView.hasFocus,
         isActive: false, // Default value - individual items will override this
       }),
       // eslint-disable-next-line react-hooks/exhaustive-deps -- TODO: blanket-ignored fix me!
@@ -107,7 +107,7 @@ const Toolbar_ = memo(
         commandBarActor.send,
         sketchPathId,
         // eslint-disable-next-line react-hooks/exhaustive-deps
-        kclManager.getEditorView()?.hasFocus,
+        kclManager.editorView.hasFocus,
       ]
     )
 

commit f7d8a335dd9d517f694342a579e98cf9b3f4983a

Make format code keyboard shortcut work again
Fixes #10268, which has probably been broken since #9448. Ultimately a
hack, just adding back a raw `useHotkeysWrapper`, but good enough for
now until we get #2300.

src/components/OpenedProject.tsx
@@ -210,6 +210,18 @@ export function OpenedProject() {
     kclManager
   )
 
+  useHotkeyWrapper(
+    ['alt + shift + f'],
+    () => {
+      void kclManager.format()
+    },
+    kclManager,
+    {
+      enableOnContentEditable: true,
+      enableOnFormTags: true,
+    }
+  )
+
   useEngineConnectionSubscriptions()
 
   useEffect(() => {

src/components/layout/areas/KclEditorPane.tsx
@@ -19,7 +19,6 @@ export const editorShortcutMeta = {
     display: 'Alt + Shift + F',
   },
   convertToVariable: {
-    codeMirror: 'Ctrl-Shift-c',
     display: 'Ctrl + Shift + C',
   },
 }