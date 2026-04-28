6548fa85ffa09db0cdfbd00bf24f06baae27d142
Frank Noirot <frankjohnson1993@gmail.com>
2026-04-28 15:35:13 -0400

    Missed one other regression in `sketchSolveDiagram`

--- src/machines/sketchSolve/sketchSolveDiagram.ts
+++ src/machines/sketchSolve/sketchSolveDiagram.ts
@@ -879,15 +879,6 @@ export const sketchSolveMachine = setup({
             ],
           },
         ],
-        escape: {
-          target: '#Sketch Solve Mode.exiting',
-          actions: [
-            'send tool unequipped to parent',
-            'cleanup sketch solve group',
-          ],
-          description:
-            'ESC in move and select (no tool equipped) exits sketch mode',
-        },
       },
       invoke: {
         id: 'moveTool',
