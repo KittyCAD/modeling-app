9f2b1f8ca37b04c1782597b92b47d39413f6a466
Adam Chalmers <adam.chalmers@zoo.dev>
2026-04-28 12:10:53 -0500

    Failing test

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -5282,3 +5282,24 @@ mod endless_impeller {
         super::execute(TEST_NAME, true).await
     }
 }
+mod seglen_with_segments {
+    const TEST_NAME: &str = "seglen_with_segments";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
