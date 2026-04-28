d27901a56b8e1dd170039a2482c10905a35c80e7
Jace Browning <jacebrowning@gmail.com>
2026-04-28 15:23:04 -0400

    Include light and dark theme

--- e2e/playwright/snapshot-tests.spec.ts
+++ e2e/playwright/snapshot-tests.spec.ts
@@ -1,7 +1,9 @@
 import type { Page } from '@playwright/test'
+import type { Fixtures } from '@e2e/playwright/fixtures/fixtureSetup'
 import { lowerRightMasks, settingsToToml } from '@e2e/playwright/test-utils'
 import { expect, test } from '@e2e/playwright/zoo-test'
 import { SETTINGS_FILE_NAME } from '@src/lib/constants'
+import { Themes } from '@src/lib/theme'
 
 const VIEWPORT = { width: 1200, height: 900 } as const
 
@@ -10,7 +12,34 @@ const snapshotOpts = (page: Page) => ({
   mask: lowerRightMasks(page),
 })
 
-test.beforeEach(async ({ page, fs, folderSetupFn }) => {
+type SnapshotTestContext = Pick<
+  Fixtures,
+  'cmdBar' | 'editor' | 'toolbar' | 'scene' | 'fs' | 'folderSetupFn'
+> & { page: Page }
+
+function testBodyForTheme(mode: Themes) {
+  return async ({
+    page,
+    cmdBar,
+    scene,
+    toolbar,
+    editor,
+    fs,
+    folderSetupFn,
+  }: SnapshotTestContext) => {
+    await runTestForTheme(mode, {
+      page,
+      cmdBar,
+      scene,
+      toolbar,
+      editor,
+      fs,
+      folderSetupFn,
+    })
+  }
+}
+
+test.beforeEach(async ({ page }) => {
   // Make the user avatar image always 404
   // so we see the fallback menu icon for all snapshot tests
   await page.route('https://lh3.googleusercontent.com/**', async (route) => {
@@ -20,11 +49,29 @@ test.beforeEach(async ({ page, fs, folderSetupFn }) => {
       body: 'Not Found!',
     })
   })
+})
 
+test(
+  'Create a sketch in a new project: light theme',
+  { tag: '@snapshot' },
+  testBodyForTheme(Themes.Light)
+)
+
+test(
+  'Create a sketch in a new project: dark theme',
+  { tag: '@snapshot' },
+  testBodyForTheme(Themes.Dark)
+)
+
+async function runTestForTheme(mode: Themes, ctx: SnapshotTestContext) {
+  const { page, cmdBar, scene, toolbar, editor, fs, folderSetupFn } = ctx
   const tomlStr = settingsToToml({
     settings: {
       app: {
         onboarding_status: 'dismissed',
+        appearance: {
+          theme: mode,
+        },
       },
     },
   })
@@ -41,61 +88,57 @@ test.beforeEach(async ({ page, fs, folderSetupFn }) => {
     const projectDir = await fs.join(dir, 'demo-project')
     await fs.mkdir(projectDir, { recursive: true })
   })
-})
 
-test(
-  'Create a sketch in a new project',
-  { tag: '@snapshot' },
-  async ({ page, cmdBar, scene, toolbar, editor }) => {
-    const [rectCorner1] = scene.makeMouseHelpers(0.24, 0.28, {
-      format: 'ratio',
-    })
-    const [rectCorner2] = scene.makeMouseHelpers(0.82, 0.52, {
-      format: 'ratio',
-    })
+  const buildName = (name: string) => `${name}-${mode}.png`
 
-    await test.step('Create a project', async () => {
-      await page.setViewportSize(VIEWPORT)
-      await scene.settled(cmdBar)
+  const [rectCorner1] = scene.makeMouseHelpers(0.24, 0.28, {
+    format: 'ratio',
+  })
+  const [rectCorner2] = scene.makeMouseHelpers(0.82, 0.52, {
+    format: 'ratio',
+  })
 
-      await toolbar.openFeatureTreePane()
-      await editor.openPane()
+  await test.step('Create a project', async () => {
+    await page.setViewportSize(VIEWPORT)
+    await scene.settled(cmdBar)
 
-      await expect(page).toHaveScreenshot(
-        '01-project-created.png',
-        snapshotOpts(page)
-      )
-    })
+    await toolbar.openFeatureTreePane()
+    await editor.openPane()
 
-    await test.step('Start a sketch', async () => {
-      await toolbar.startSketchOnDefaultPlane('Front plane')
+    await expect(page).toHaveScreenshot(
+      buildName('01-project-created'),
+      snapshotOpts(page)
+    )
+  })
 
-      await expect(page).toHaveScreenshot(
-        '02-sketch-started.png',
-        snapshotOpts(page)
-      )
-    })
+  await test.step('Start a sketch', async () => {
+    await toolbar.startSketchOnDefaultPlane('Front plane')
 
-    await test.step('Draw a rectangle', async () => {
-      await toolbar.rectangleBtn.click()
-      await rectCorner1()
-      await rectCorner2()
+    await expect(page).toHaveScreenshot(
+      buildName('02-sketch-started'),
+      snapshotOpts(page)
+    )
+  })
 
-      await expect(page).toHaveScreenshot(
-        '03-sketch-drawn.png',
-        snapshotOpts(page)
-      )
-    })
+  await test.step('Draw a rectangle', async () => {
+    await toolbar.rectangleBtn.click()
+    await rectCorner1()
+    await rectCorner2()
 
-    await test.step('Exit the sketch', async () => {
-      await toolbar.exitSketchBtn.click()
-      await expect(toolbar.startSketchBtn).not.toBeDisabled()
-      await scene.settled(cmdBar)
+    await expect(page).toHaveScreenshot(
+      buildName('03-sketch-drawn'),
+      snapshotOpts(page)
+    )
+  })
 
-      await expect(page).toHaveScreenshot(
-        '04-sketch-exited.png',
-        snapshotOpts(page)
-      )
-    })
-  }
-)
+  await test.step('Exit the sketch', async () => {
+    await toolbar.exitSketchBtn.click()
+    await expect(toolbar.startSketchBtn).not.toBeDisabled()
+    await scene.settled(cmdBar)
+
+    await expect(page).toHaveScreenshot(
+      buildName('04-sketch-exited'),
+      snapshotOpts(page)
+    )
+  })
+}
