e0e311dae0487398548d16d5d68d234c6608ba30
Frank Noirot <frankjohnson1993@gmail.com>
2026-05-07 10:39:03 -0700

    Codex: gate annotations view list behind `bodies_pane` feature flag (HACKY)

--- src/registry/plugins/annotationViews/index.ts
+++ src/registry/plugins/annotationViews/index.ts
@@ -1,7 +1,16 @@
-import { defineRegistryItem, provide } from '@kittycad/registry'
+import { computed, signal } from '@preact/signals-core'
+import {
+  defineRegistryItemFactory,
+  defineRuntimeRegistryItem,
+  provide,
+} from '@kittycad/registry'
+import { userHasFeature } from '@src/lib/settings/settingsUtils'
+import { reportRejection } from '@src/lib/trap'
 import { featureTreeSectionsValueSpec } from '@src/registry/contracts/featureTree'
 import { createZdsPlugin } from '@src/registry/createZdsPlugin'
 import { createElement, lazy, Suspense } from 'react'
+
+const BODIES_PANE_FEATURE_FLAG = 'bodies_pane'
 
 const AnnotationViewsFeatureTreeSection = lazy(() =>
   import('@src/components/layout/areas/AnnotationViewsFeatureTreeSection').then(
@@ -19,20 +28,36 @@ function LazyAnnotationViewsFeatureTreeSection() {
   )
 }
 
-const annotationViewsFeatureTreeSectionItem = defineRegistryItem({
-  id: 'annotation-views.feature-tree-section',
-  provides: [
-    provide(
-      featureTreeSectionsValueSpec,
-      {
-        id: 'annotationViews',
-        order: 110,
-        Component: LazyAnnotationViewsFeatureTreeSection,
-      },
-      { key: 'annotation-views.feature-tree-section' }
-    ),
-  ],
-})
+const annotationViewsFeatureTreeSectionItem = defineRegistryItemFactory(() => {
+  const enabled = signal(false)
+
+  userHasFeature(BODIES_PANE_FEATURE_FLAG, false)
+    .then((hasFeature) => {
+      enabled.value = hasFeature
+    })
+    .catch(reportRejection)
+
+  return {
+    item: defineRuntimeRegistryItem({
+      id: 'annotation-views.feature-tree-section',
+      provides: [
+        provide(
+          featureTreeSectionsValueSpec,
+          computed(() =>
+            enabled.value
+              ? {
+                  id: 'annotationViews',
+                  order: 110,
+                  Component: LazyAnnotationViewsFeatureTreeSection,
+                }
+              : undefined
+          ),
+          { key: 'annotation-views.feature-tree-section' }
+        ),
+      ],
+    }),
+  }
+}, 'annotation-views.feature-tree-section')
 
 const annotationViews = createZdsPlugin({
   id: 'annotationViews',
