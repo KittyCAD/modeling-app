9e8d4dfb753a612e6a3ac194801efc1618ce3770
Frank Noirot <frankjohnson1993@gmail.com>
2026-05-09 01:37:59 -0700

    Scope status bar items by route

--- src/components/OpenedProject.tsx
+++ src/components/OpenedProject.tsx
@@ -14,7 +14,6 @@ import { UndoRedoButtons } from '@src/components/UndoRedoButtons'
 import { WasmErrToast } from '@src/components/WasmErrToast'
 import { ZookeeperCreditsMenu } from '@src/components/ZookeeperCreditsMenu'
 import { getMlEphantProjectReloadBehavior } from '@src/components/openedProjectUtils'
-import { useAbsoluteFilePath } from '@src/hooks/useAbsoluteFilePath'
 import { useEngineConnectionSubscriptions } from '@src/hooks/useEngineConnectionSubscriptions'
 import { useHotKeyListener } from '@src/hooks/useHotKeyListener'
 import { useModelingContext } from '@src/hooks/useModelingContext'
@@ -53,6 +52,7 @@ import {
 import { useFolders, useLastOperation } from '@src/machines/systemIO/hooks'
 import { SystemIOMachineStates } from '@src/machines/systemIO/utils'
 import {
+  filterStatusBarItemsForScopes,
   statusBarGlobalItemsValueSpec,
   statusBarLocalItemsValueSpec,
 } from '@src/registry/contracts/statusBar'
@@ -90,7 +90,6 @@ export function OpenedProject() {
 
   const location = useLocation()
   const navigate = useNavigate()
-  const filePath = useAbsoluteFilePath()
   const autoUpdateDownloadProgress = autoUpdateDownloadProgressSignal.value
   const autoUpdateReady = autoUpdateReadySignal.value
   const lastOperation = useLastOperation()
@@ -185,8 +184,15 @@ export function OpenedProject() {
 
   const settingsValues = settings.useSettings()
   const machineApiEnabled = settingsValues.app.machineApi.current
+  const registryGlobalStatusBarItems = filterStatusBarItemsForScopes(
+    registry.signal(statusBarGlobalItemsValueSpec).value,
+    ['file']
+  )
   const registryLocalStatusBarItems = filterEngineSceneStatusBarItems(
-    registry.signal(statusBarLocalItemsValueSpec).value,
+    filterStatusBarItemsForScopes(
+      registry.signal(statusBarLocalItemsValueSpec).value,
+      ['file']
+    ),
     settingsValues
   )
   const authToken = auth.useToken()
@@ -209,21 +215,6 @@ export function OpenedProject() {
     e.preventDefault()
     kclManager.redo()
   })
-  useHotkeyWrapper(
-    [isDesktop() ? 'mod + ,' : 'shift + mod + ,'],
-    () => {
-      if (!filePath) {
-        console.warn('bug: filePath is undefined')
-        return
-      }
-
-      void navigate(filePath + PATHS.SETTINGS)
-    },
-    kclManager,
-    {
-      splitKey: '|',
-    }
-  )
 
   useHotkeyWrapper(
     ['alt + shift + f'],
@@ -418,7 +409,7 @@ export function OpenedProject() {
                 window.electron?.appRestart()
               },
             }),
-            ...registry.signal(statusBarGlobalItemsValueSpec).value,
+            ...registryGlobalStatusBarItems,
           ]}
           localItems={[
             ...(settingsValues.debug.showModelingMachineState.current
