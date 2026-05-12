b790c4ba7b63433e6f58e9d4fa74ce01b4e793b4
Kevin Nadro <kevin@zoo.dev>
2026-05-12 15:27:16 -0500

    fix: fmt

--- src/lang/KclManager.ts
+++ src/lang/KclManager.ts
@@ -244,7 +244,7 @@ function buildMinimalDocumentChange(
     currentSuffixStart > prefixLength &&
     nextSuffixStart > prefixLength &&
     currentCode.charCodeAt(currentSuffixStart - 1) ===
-    nextCode.charCodeAt(nextSuffixStart - 1)
+      nextCode.charCodeAt(nextSuffixStart - 1)
   ) {
     currentSuffixStart--
     nextSuffixStart--
@@ -661,8 +661,8 @@ export class File extends EventTarget {
     },
     write: (path: string, content: string) =>
       fsZds.writeFile(path, File.encoder.encode(content)),
-    watch: window.electron?.watchFileOn || (() => { }),
-    unwatch: window.electron?.watchFileOff || (() => { }),
+    watch: window.electron?.watchFileOn || (() => {}),
+    unwatch: window.electron?.watchFileOff || (() => {}),
   }
   static encoder = new TextEncoder()
 }
@@ -696,7 +696,7 @@ export class KclManager extends File {
     return this._wasmInstance
   }
   readonly systemDeps: SystemDeps
-  private _modelingSend: (eventInfo: ModelingMachineEvent) => void = () => { }
+  private _modelingSend: (eventInfo: ModelingMachineEvent) => void = () => {}
   private _modelingState: StateFrom<typeof modelingMachine> | null = null
 
   // CORE STATE
@@ -963,10 +963,10 @@ export class KclManager extends File {
     If this value isn't `null`, don't watch for file system writes it was probably us!
    */
   public writingPromise = signal<Promise<unknown> | null>(null)
-  sceneInfraBaseUnitMultiplierSetter: (unit: BaseUnit) => void = () => { }
+  sceneInfraBaseUnitMultiplierSetter: (unit: BaseUnit) => void = () => {}
   /** Values merged in from former EditorManager and CodeManager classes */
   private _convertToVariableEnabled: boolean = false
-  private _convertToVariableCallback: () => void = () => { }
+  private _convertToVariableCallback: () => void = () => {}
 
   // CONFIGURATION
 
@@ -1388,8 +1388,8 @@ export class KclManager extends File {
             this.lastCommittedAdditionalSpec =
               directEditCheckpointId != null
                 ? {
-                  sketchCheckpointId: directEditCheckpointId,
-                }
+                    sketchCheckpointId: directEditCheckpointId,
+                  }
                 : undefined
             this.lastCommittedSketchCheckpointId = directEditCheckpointId
 
@@ -1564,9 +1564,9 @@ export class KclManager extends File {
           const replayAdditionalSpec =
             isHistoryReplay && checkpointId != null
               ? {
-                annotations: effect.value.redoAdditionalSpec?.annotations,
-                sketchCheckpointId: checkpointId,
-              }
+                  annotations: effect.value.redoAdditionalSpec?.annotations,
+                  sketchCheckpointId: checkpointId,
+                }
               : effect.value.redoAdditionalSpec
           const docAlreadyMatchesReplay =
             vu.state.doc.toString() === effect.value.redoCode
@@ -3154,7 +3154,7 @@ export class KclManager extends File {
         updateOutsideEditorEvent,
         Transaction.addToHistory.of(
           resolvedOptions.shouldAddToHistory &&
-          !resolvedOptions.shouldClearHistory
+            !resolvedOptions.shouldClearHistory
         ),
         ...(additionalSpec?.annotations || []),
       ],
