c64f126f2318050443934463551d13b68d96e09f
Frank Noirot <frankjohnson1993@gmail.com>
2026-05-14 12:15:38 -0700

    Remove registry route contract

--- src/registry/contracts/routes.ts
+++
@@ -1,8 +0,0 @@
-import type { RouteObject } from 'react-router-dom'
-import { appendValueSpec, defineContract } from '@kittycad/registry'
-
-export const routesContract = defineContract({
-  routesValueSpec: appendValueSpec<RouteObject>('routes'),
-})
-
-export const { routesValueSpec } = routesContract
