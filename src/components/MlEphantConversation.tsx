55d85081c15bc4200448a307182da3aebac2a5b6
max <margorskyi@gmail.com>
2026-05-29 22:21:06 +0200

    Revert Zoodle rename (#11905)

--- src/components/MlEphantConversation.tsx
+++ src/components/MlEphantConversation.tsx
@@ -178,15 +178,15 @@ export const MlEphantExtraInputs = (props: MlEphantExtraInputsProps) => {
         </button>
         <button
           type="button"
-          data-testid="ml-ephant-zoodle-button"
+          data-testid="ml-ephant-annotate-screenshot-button"
           onClick={props.onAnnotateScreenshot}
           disabled={props.attachmentsDisabled}
           className="h-7 w-7 bg-default flex items-center justify-center rounded-sm m-0 p-0 flex-none disabled:opacity-60"
-          aria-label="Zoodle"
+          aria-label="Annotate viewport screenshot"
         >
           <CustomIcon name="sketch" className="w-5 h-5" />
           <Tooltip position="top" hoverOnly={true}>
-            <span>Zoodle</span>
+            <span>Annotate viewport screenshot</span>
           </Tooltip>
         </button>
       </div>
@@ -557,7 +557,10 @@ export const MlEphantConversationInput = (
           imageDataUrl={annotationImageDataUrl}
           onCancel={() => setAnnotationImageDataUrl(null)}
           onSend={(annotatedDataUrl) => {
-            appendDataUrlAttachment(annotatedDataUrl, 'zoodle')
+            appendDataUrlAttachment(
+              annotatedDataUrl,
+              'annotated-viewport-screenshot.png'
+            )
             setAnnotationImageDataUrl(null)
           }}
         />
