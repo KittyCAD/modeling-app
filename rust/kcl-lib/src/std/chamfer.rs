90d1fb6453ceaec9cc3a0f7ffc1fa1bf5357d170
Caleb Goates <caleb@zoo.dev>
2026-06-03 11:38:07 -0600

    DO NOT MERGE: trying turning off the delay

--- rust/kcl-lib/src/std/chamfer.rs
+++ rust/kcl-lib/src/std/chamfer.rs
@@ -237,7 +237,7 @@ async fn inner_chamfer(
         for edge_id in edge_ids {
             let id = exec_state.next_uuid();
             exec_state
-                .batch_end_cmd(
+                .batch_modeling_cmd(
                     ModelingCmdMeta::from_args_id(exec_state, &args, id),
                     ModelingCmd::from(
                         mcmd::Solid3dCutEdges::builder()
@@ -397,7 +397,7 @@ async fn inner_chamfer_v2(
         extra_face_ids.push(exec_state.next_uuid());
     }
     exec_state
-        .batch_end_cmd(
+        .batch_modeling_cmd(
             ModelingCmdMeta::from_args_id(exec_state, &args, id),
             ModelingCmd::from(
                 mcmd::Solid3dCutEdges::builder()
@@ -497,7 +497,7 @@ async fn inner_chamfer_with_engine_refs(
 
     let mut solid = solid.clone();
     exec_state
-        .batch_end_cmd(
+        .batch_modeling_cmd(
             ModelingCmdMeta::from_args_id(exec_state, &args, id),
             ModelingCmd::from(
                 mcmd::Solid3dCutEdgeReferences::builder()
