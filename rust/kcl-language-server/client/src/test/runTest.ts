b2c747d27c52112283601572875e6cdff9cfa2c2
Frank Noirot <frankjohnson1993@gmail.com>
2026-06-04 17:46:10 -0400

    fmt

--- rust/kcl-language-server/client/src/test/runTest.ts
+++ rust/kcl-language-server/client/src/test/runTest.ts
@@ -1,6 +1,6 @@
-import * as path from 'path'
 import * as fs from 'fs'
 import * as os from 'os'
+import * as path from 'path'
 import { runTests } from '@vscode/test-electron'
 
 function createShortVSCodeProfileDir() {
