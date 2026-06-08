f38870e1398b500603dce5dbdadcf5bef6cb3e53
Andrew Varga <grizzly33@gmail.com>
2026-06-08 23:13:58 +0200

    introduce pending_edge_cut_ids to fix missing edge_cuts (#12011)
    Co-authored-by: Jonathan Tran <jonnytran@gmail.com>

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -5907,3 +5907,24 @@ mod delete_body {
         super::execute(TEST_NAME, true).await
     }
 }
+mod solid_edge_cut_using_edge_ref_csg {
+    const TEST_NAME: &str = "solid_edge_cut_using_edge_ref_csg";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
