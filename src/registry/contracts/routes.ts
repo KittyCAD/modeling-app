96fb65af3e8dfbbddddd99d4f25cecba167f785f
Frank Noirot <frank@zoo.dev>
2026-06-10 10:44:32 -0400

    Remove telemetry status bar item, restore route, fix command on home page (#12058)

--- src/registry/contracts/routes.ts
+++
@@ -1,8 +0,0 @@
-import { appendValueSpec, defineContract } from '@kittycad/registry'
-import type { RouteObject } from 'react-router-dom'
-
-export const routesContract = defineContract({
-  routesValueSpec: appendValueSpec<RouteObject>('routes'),
-})
-
-export const { routesValueSpec } = routesContract
