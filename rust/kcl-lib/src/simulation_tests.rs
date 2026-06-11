44e7a43205527d08d5889c40add354c8fd87d870
Adam Chalmers <adam.chalmers@zoo.dev>
2026-06-11 17:27:28 -0500

    Demonstrate the bug

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -6012,3 +6012,24 @@ mod clone_a_mirror3d {
         super::execute(TEST_NAME, true).await
     }
 }
+mod sweep_mirror {
+    const TEST_NAME: &str = "sweep_mirror";
+
+    /// Test parsing KCL.
+    #[test]
+    fn parse() {
+        super::parse(TEST_NAME)
+    }
+
+    /// Test that parsing and unparsing KCL produces the original KCL input.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn unparse() {
+        super::unparse(TEST_NAME).await
+    }
+
+    /// Test that KCL is executed correctly.
+    #[tokio::test(flavor = "multi_thread")]
+    async fn kcl_test_execute() {
+        super::execute(TEST_NAME, true).await
+    }
+}
