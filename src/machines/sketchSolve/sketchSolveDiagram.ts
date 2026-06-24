e2ccd6675d80cd182e32426b8c7db3c0203ab599
Andrew Varga <grizzly33@gmail.com>
2026-06-24 18:10:19 +0200

    add horizontal/vertical constraint for 2 points instead of verticalDistance/horizontalDistance

--- src/machines/sketchSolve/sketchSolveDiagram.ts
+++ src/machines/sketchSolve/sketchSolveDiagram.ts
@@ -356,16 +356,7 @@ function getPreparedApplyForConstraintTool(
   const objects =
     context.sketchExecOutcome?.sceneGraphDelta.new_graph.objects || []
 
-  return getConstraintToolPreparedApply(
-    toolName,
-    context.selectedIds,
-    objects,
-    {
-      defaultLengthUnit: baseUnitToNumericSuffix(
-        context.kclManager.fileSettings.defaultLengthUnit ?? 'mm'
-      ),
-    }
-  )
+  return getConstraintToolPreparedApply(toolName, context.selectedIds, objects)
 }
 
 function isConstraintToolName(
@@ -428,9 +419,6 @@ export const sketchSolveMachine = setup({
           selectedIds: context.selectedIds,
           objects:
             context.sketchExecOutcome?.sceneGraphDelta.new_graph.objects || [],
-          defaultLengthUnit: baseUnitToNumericSuffix(
-            context.kclManager.fileSettings.defaultLengthUnit ?? 'mm'
-          ),
           rustContext: context.rustContext,
           sketchId: context.sketchId,
           settings: jsAppSettings(context.kclManager.systemDeps.settings),
