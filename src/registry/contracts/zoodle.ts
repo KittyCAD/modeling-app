b5d6a5e28a884650ef10dc28b53e0600a9a694ec
Frank Noirot <frankjohnson1993@gmail.com>
2026-07-01 13:46:30 -0400

    Add Zoodle brush size control

--- src/registry/contracts/zoodle.ts
+++ src/registry/contracts/zoodle.ts
@@ -2,6 +2,10 @@ import { defineContract, defineService } from '@kittycad/registry'
 import type { ReadonlySignal } from '@preact/signals-core'
 import { TRIM_PREVIEW_LINE_COLOR } from '@src/lib/constants'
 
+export const ZOODLE_BRUSH_SIZE_MIN_PX = 1
+export const ZOODLE_BRUSH_SIZE_DEFAULT_PX = 2
+export const ZOODLE_BRUSH_SIZE_MAX_PX = 12
+
 export type ZoodleDrawToolDefinition = {
   type: 'draw'
   label: string
@@ -61,7 +65,9 @@ export const zoodleToolKeys = Object.keys(
 export interface ZoodleService {
   readonly toolDefinitions: typeof zoodleToolDefinitions
   readonly activeToolKey: ReadonlySignal<ZoodleToolKey>
+  readonly brushSize: ReadonlySignal<number>
   equipTool(toolKey: ZoodleToolKey): void
+  setBrushSize(brushSize: number): void
 }
 
 export const zoodleContract = defineContract({
