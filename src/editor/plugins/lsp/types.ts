9c405b63f0785098c870c5307f2a8bf0703d3f2b
Pierre Jacquier <pierre@zoo.dev>
2026-07-06 17:32:54 -0400

    Revert "Reset more"
    This reverts commit 2cca3d68fa1c8512f6f968826f922e44c7f549b1.

--- src/editor/plugins/lsp/types.ts
+++ src/editor/plugins/lsp/types.ts
@@ -2,7 +2,6 @@ import type { LspWorkerEventType } from '@kittycad/codemirror-lsp-client'
 
 export enum LspWorker {
   Kcl = 'kcl',
-  Copilot = 'copilot',
 }
 export interface KclWorkerOptions {
   wasmUrl: string
@@ -10,14 +9,8 @@ export interface KclWorkerOptions {
   apiBaseUrl: string
 }
 
-export interface CopilotWorkerOptions {
-  wasmUrl: string
-  token: string
-  apiBaseUrl: string
-}
-
 export interface LspWorkerEvent {
   eventType: LspWorkerEventType
-  eventData: Uint8Array | KclWorkerOptions | CopilotWorkerOptions
+  eventData: Uint8Array | KclWorkerOptions
   worker: LspWorker
 }
