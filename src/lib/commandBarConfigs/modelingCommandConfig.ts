6b4d51a56018894e3acd03cb9c00ad2d85c974a8
Pierre Jacquier <pierre@zoo.dev>
2026-07-14 16:34:44 -0400

    No longer hide Revolve.axis on edit Context https://kittycadworkspace.slack.com/archives/C06L2RHP9SL/p1783850585712399

--- src/lib/commandBarConfigs/modelingCommandConfig.ts
+++ src/lib/commandBarConfigs/modelingCommandConfig.ts
@@ -814,11 +814,6 @@ export const modelingMachineCommandConfig: StateMachineCommandSetConfig<
               { name: 'X Axis', isCurrent: true, value: 'X' },
               { name: 'Y Axis', isCurrent: false, value: 'Y' },
             ],
-            hidden: (context) =>
-              isEditingNode(context) ||
-              !['Axis'].includes(
-                context.argumentsToSubmit.axisOrEdge as string
-              ),
           },
           edge: {
             required: (context) =>
