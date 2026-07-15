0b94c24c7f80ecd3ad0f9660d4c2311abbf03d19
Adam Chalmers <adam.chalmers@zoo.dev>
2026-07-15 15:12:08 -0500

    Rename type from Axis3dOrPoint3dOrEdgeReference to DirectionType3d
    Name types after their purpose, not their variants

--- rust/kcl-lib/src/std/sweep.rs
+++ rust/kcl-lib/src/std/sweep.rs
@@ -32,7 +32,7 @@ use crate::execution::types::PrimitiveType;
 use crate::execution::types::RuntimeType;
 use crate::parsing::ast::types::TagNode;
 use crate::std::Args;
-use crate::std::axis_or_reference::Axis3dOrPoint3dOrEdgeReference;
+use crate::std::axis_or_reference::DirectionType3d;
 use crate::std::extrude::BeingExtruded;
 use crate::std::extrude::build_segment_surface_sketch;
 use crate::std::extrude::coerce_extrude_targets;
@@ -93,7 +93,7 @@ pub async fn sweep(exec_state: &mut ExecState, args: Args) -> Result<KclValue, K
         args.get_kw_arg_opt("translateProfileToPath", &RuntimeType::bool(), exec_state)?;
     let orient_profile_perpendicular: Option<bool> =
         args.get_kw_arg_opt("orientProfilePerpendicular", &RuntimeType::bool(), exec_state)?;
-    let projected_axis: Option<Axis3dOrPoint3dOrEdgeReference> = args.get_kw_arg_opt(
+    let projected_axis: Option<DirectionType3d> = args.get_kw_arg_opt(
         "projectedAxis",
         &RuntimeType::Union(vec![
             RuntimeType::point3d(),
@@ -191,7 +191,7 @@ async fn inner_sweep(
     tag_start: Option<TagNode>,
     tag_end: Option<TagNode>,
     body_type: Option<BodyType>,
-    projected_axis: Option<Axis3dOrPoint3dOrEdgeReference>,
+    projected_axis: Option<DirectionType3d>,
     version: Option<u32>,
     exec_state: &mut ExecState,
     args: Args,
@@ -269,21 +269,21 @@ async fn inner_sweep(
     let projected_axis = match projected_axis {
         None => None,
         Some(axis) => match axis {
-            Axis3dOrPoint3dOrEdgeReference::Axis { direction, .. } => Some(DirectionType::Axis {
+            DirectionType3d::Axis { direction, .. } => Some(DirectionType::Axis {
                 direction: KPoint3d {
                     x: direction[0].to_mm(),
                     y: direction[1].to_mm(),
                     z: direction[2].to_mm(),
                 },
             }),
-            Axis3dOrPoint3dOrEdgeReference::Point(point) => Some(DirectionType::Axis {
+            DirectionType3d::Point(point) => Some(DirectionType::Axis {
                 direction: KPoint3d {
                     x: point[0].to_mm(),
                     y: point[1].to_mm(),
                     z: point[2].to_mm(),
                 },
             }),
-            Axis3dOrPoint3dOrEdgeReference::Edge(edge) => match edge {
+            DirectionType3d::Edge(edge) => match edge {
                 crate::std::fillet::EdgeReference::Uuid(uuid) => Some(DirectionType::Edge { id: uuid }),
                 crate::std::fillet::EdgeReference::Tag(tag) => Some(DirectionType::Edge {
                     id: match tag.get_cur_info() {
