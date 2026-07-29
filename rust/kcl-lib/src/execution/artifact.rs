2a39aa578602aff72b17cd030524d61a58770ee9
Kurt Hutten <k.hutten@protonmail.ch>
2026-07-29 21:40:41 +1000

    Adapt kcl-lib to optional extrude reference target (#12615)

--- rust/kcl-lib/src/execution/artifact.rs
+++ rust/kcl-lib/src/execution/artifact.rs
@@ -1575,9 +1575,14 @@ fn artifacts_to_update(
                 ModelingCmd::Extrude(kcmc::Extrude { target: None, .. }) => return Ok(Vec::new()),
                 ModelingCmd::TwistExtrude(kcmc::TwistExtrude { target, .. })
                 | ModelingCmd::Revolve(kcmc::Revolve { target, .. })
-                | ModelingCmd::RevolveAboutEdge(kcmc::RevolveAboutEdge { target, .. })
-                | ModelingCmd::ExtrudeToReference(kcmc::ExtrudeToReference { target, .. }) => {
+                | ModelingCmd::RevolveAboutEdge(kcmc::RevolveAboutEdge { target, .. }) => {
                     cmd_id_ref_to_artifact_id(target)
+                }
+                ModelingCmd::ExtrudeToReference(kcmc::ExtrudeToReference {
+                    target: Some(target), ..
+                }) => cmd_id_ref_to_artifact_id(target),
+                ModelingCmd::ExtrudeToReference(kcmc::ExtrudeToReference { target: None, .. }) => {
+                    return Ok(Vec::new());
                 }
                 _ => internal_error!(range, "Sweep-like command variant not handled: id={id:?}, cmd={cmd:?}"),
             };
