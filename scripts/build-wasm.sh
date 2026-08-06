33c5e44c9b3c64fed377f6164dc38f3c93de5680
Jace Browning <jacebrowning@gmail.com>
2026-08-06 11:25:31 -0400

    Simplify Wasm rebuilds

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -1,23 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-rm -rf rust/kcl-wasm-lib/pkg
-mkdir -p rust/kcl-wasm-lib/pkg
-rm -rf rust/kcl-lib/bindings
-
 cd rust
+
 wasm_pack_args=(build kcl-wasm-lib --release --target web --out-dir pkg --scope kittycad)
 if [ "${VERCEL_ENV:-}" = "preview" ]; then
   wasm_pack_args+=(--no-opt)
 fi
 wasm-pack "${wasm_pack_args[@]}"
+
 if [ -n "${VERCEL:-}" ]; then
   cp -R kcl-lib/expected-bindings/ts-rs kcl-lib/bindings
 else
   cargo test -p kcl-lib --features artifact-graph export_bindings
 fi
-cd ..
 
-cp rust/kcl-wasm-lib/README.md rust/kcl-wasm-lib/pkg/README.md
-cp rust/kcl-wasm-lib/pkg/kcl_wasm_lib_bg.wasm public
-npm run fmt
+cp kcl-wasm-lib/pkg/kcl_wasm_lib_bg.wasm ../public
+cp kcl-wasm-lib/README.md kcl-wasm-lib/pkg/README.md
