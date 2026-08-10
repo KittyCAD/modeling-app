49833733d3e13af2410cf0a3ef3239fcea2ceaea
Frank Noirot <frankjohnson1993@gmail.com>
2026-08-09 22:13:32 -0400

    Preserve explorer file creation toast

--- src/components/Explorer/ProjectExplorer.tsx
+++ src/components/Explorer/ProjectExplorer.tsx
@@ -1266,7 +1266,7 @@ export const ProjectExplorer = ({
                     projectSession.createFile({
                       path: requestedAbsolutePath,
                     }),
-                  successMessage: `File ${fileName} written successfully`,
+                  successMessage: 'Successfully created 1 file',
                   onSuccess: () => {
                     systemIOActor.send({
                       type: SystemIOMachineEvents.navigateToFile,
