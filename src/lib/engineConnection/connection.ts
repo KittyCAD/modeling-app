f1ec70d399ab05024d2d05dad9c80da4f904ef0c
Pierre Jacquier <pierre@zoo.dev>
2026-08-11 13:20:38 -0400

    Cancel pending disconnect timeout during teardown

--- src/lib/engineConnection/connection.ts
+++ src/lib/engineConnection/connection.ts
@@ -58,6 +58,7 @@ export class Connection extends EventTarget {
     pong: number | undefined
   }
   private _pingIntervalId: ReturnType<typeof setInterval> | undefined
+  private clearDisconnectedTimeout: (() => void) | undefined
   timeoutToForceConnectId: ReturnType<typeof setTimeout> | undefined
 
   peerConnection: RTCPeerConnection | undefined
@@ -472,11 +473,13 @@ export class Connection extends EventTarget {
     const onNegotiationNeeded = createOnNegotiationNeeded()
     const onSignalingStateChange = createOnSignalingStateChange()
     const onIceCandidateError = createOnIceCandidateError()
-    const onConnectionStateChange = createOnConnectionStateChange({
-      dispatchEvent: this.dispatchEvent.bind(this),
-      connection: this,
-      tearDownManager: this.tearDownManager.bind(this),
-    })
+    const { onConnectionStateChange, clearDisconnectedTimeout } =
+      createOnConnectionStateChange({
+        dispatchEvent: this.dispatchEvent.bind(this),
+        connection: this,
+        tearDownManager: this.tearDownManager.bind(this),
+      })
+    this.clearDisconnectedTimeout = clearDisconnectedTimeout
     const onTrack = createOnTrack({
       setMediaStream: this.setMediaStream.bind(this),
       setWebrtcStatsCollector: this.setWebrtcStatsCollector.bind(this),
@@ -865,6 +868,8 @@ export class Connection extends EventTarget {
   }
 
   cleanUpTimeouts() {
+    this.clearDisconnectedTimeout?.()
+    this.clearDisconnectedTimeout = undefined
     clearTimeout(this.timeoutToForceConnectId)
     this.timeoutToForceConnectId = undefined
   }
