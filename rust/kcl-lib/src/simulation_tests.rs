ac8f6301305ad2663adf02c20185021c4524eda8
Adam Chalmers <adam.chalmers@zoo.dev>
2026-08-20 14:00:07 -0500

    Remove sim test

--- rust/kcl-lib/src/simulation_tests.rs
+++ rust/kcl-lib/src/simulation_tests.rs
@@ -7520,24 +7520,3 @@ mod use_point_from_other_sketch {
         super::execute(TEST_NAME, true).await
     }
 }
-mod clone_rectangle_partial_revolve {
-    const TEST_NAME: &str = "clone_rectangle_partial_revolve";
-
-    /// Test parsing KCL.
-    #[test]
-    fn parse() {
-        super::parse(TEST_NAME)
-    }
-
-    /// Test that parsing and unparsing KCL produces the original KCL input.
-    #[tokio::test(flavor = "multi_thread")]
-    async fn unparse() {
-        super::unparse(TEST_NAME).await
-    }
-
-    /// Test that KCL is executed correctly.
-    #[tokio::test(flavor = "multi_thread")]
-    async fn kcl_test_execute() {
-        super::execute(TEST_NAME, true).await
-    }
-}
