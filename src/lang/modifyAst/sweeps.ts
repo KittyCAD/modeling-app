6fbbf897df50abf6a71000b8dcfc3ee9a5607cdc
Pierre Jacquier <pierre@zoo.dev>
2026-08-26 14:37:52 +0200

    Make tsc complain for P&C v. KCL drift

--- src/lang/modifyAst/sweeps.ts
+++ src/lang/modifyAst/sweeps.ts
@@ -800,6 +800,7 @@ export function addRevolve({
   sketches: Selections
   angle: KclCommandValue
   wasmInstance: ModuleType
+  axisOrEdge?: 'Axis' | 'Edge'
   axis?: string
   edge?: Selections
   tolerance?: KclCommandValue
