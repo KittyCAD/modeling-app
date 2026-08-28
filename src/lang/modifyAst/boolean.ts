75caf50f4dcd43c6f201343009e234df575b822c
Jordan Noone <jordan@kittycad.io>
2026-08-28 14:32:14 -0700

    fix(codemod): preserve split inputs on edit

--- src/lang/modifyAst/boolean.ts
+++ src/lang/modifyAst/boolean.ts
@@ -12,11 +12,17 @@ import {
   setCallInAst,
 } from '@src/lang/modifyAst'
 import {
+  getNodeFromPath,
   getVariableExprsFromSelection,
   stringifyPathToNode,
   valueOrVariable,
 } from '@src/lang/queryAst'
-import type { ArtifactGraph, PathToNode, Program } from '@src/lang/wasm'
+import type {
+  ArtifactGraph,
+  CallExpressionKw,
+  PathToNode,
+  Program,
+} from '@src/lang/wasm'
 import { modelingStdLibCommandName } from '@src/lib/commandBarConfigs/modelingCommandStdLib'
 import type { KclCommandValue } from '@src/lib/commandTypes'
 import { KCL_DEFAULT_CONSTANT_PREFIXES } from '@src/lib/constants'
@@ -73,6 +79,50 @@ function validateBooleanSelections(
       inputKeys.add(key)
     }
   }
+}
+
+function preserveSplitInputsOnEdit({
+  ast,
+  call,
+  nodeToEdit,
+  wasmInstance,
+}: {
+  ast: Node<Program>
+  call: Node<CallExpressionKw>
+  nodeToEdit?: PathToNode
+  wasmInstance: ModuleType
+}) {
+  if (!nodeToEdit) {
+    return
+  }
+
+  call.unlabeled = null
+  const existingCall = getNodeFromPath<CallExpressionKw>(
+    ast,
+    nodeToEdit,
+    wasmInstance,
+    'CallExpressionKw'
+  )
+  if (err(existingCall)) {
+    return
+  }
+
+  const existingToolsArg = existingCall.node.arguments.find(
+    (arg) => arg.label.name === 'tools'
+  )
+  if (!existingToolsArg) {
+    return
+  }
+
+  const replacementToolsIndex = call.arguments.findIndex(
+    (arg) => arg.label.name === 'tools'
+  )
+  if (replacementToolsIndex === -1) {
+    call.arguments.unshift(structuredClone(existingToolsArg))
+    return
+  }
+
+  call.arguments[replacementToolsIndex] = structuredClone(existingToolsArg)
 }
 
 export function addUnion({
@@ -432,6 +482,12 @@ export function addSplit({
     objectsExpr,
     labeledArgs
   )
+  preserveSplitInputsOnEdit({
+    ast: modifiedAst,
+    call,
+    nodeToEdit: mNodeToEdit,
+    wasmInstance,
+  })
 
   // 3. If edit, we assign the new function call declaration to the existing node,
   // otherwise just push to the end
