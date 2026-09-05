bf00a5c83e85a26e3ffff43b476222aaa2c5fa10
Jordan Noone <jordan@kittycad.io>
2026-09-05 12:47:23 -0700

    Test unsupported Firefox codec preflight

--- playwright.config.ts
+++ playwright.config.ts
@@ -60,6 +60,7 @@ export default defineConfig({
   projects: [
     {
       name: 'Google Chrome',
+      testIgnore: '**/firefox-unsupported-codec.spec.ts',
       use: {
         ...devices['Desktop Chrome'],
         channel: 'chrome',
@@ -75,6 +76,20 @@ export default defineConfig({
             : {}),
         },
       }, // or 'chrome-beta'
+    },
+    {
+      name: 'Firefox without H.264',
+      testMatch: '**/firefox-unsupported-codec.spec.ts',
+      use: {
+        ...devices['Desktop Firefox'],
+        launchOptions: {
+          firefoxUserPrefs: {
+            'media.gmp-gmpopenh264.autoupdate': false,
+            'media.gmp-gmpopenh264.enabled': false,
+            'media.peerconnection.video.h264_enabled': false,
+          },
+        },
+      },
     },
     // {
     //   name: 'webkit',
