e0ecb80cb18a59811851713055042001881533e2
Pierre Jacquier <pierre@zoo.dev>
2026-09-07 16:24:52 -0400

    Consume typed terminal engine errors

--- src/lib/engineConnection/utils.ts
+++ src/lib/engineConnection/utils.ts
@@ -339,7 +339,11 @@ export enum EngineConnectionManagerEvents {
 }
 
 export enum EngineConnectionErrorKind {
+  AccessDenied = 'access-denied',
+  AuthTokenInvalid = 'auth-token-invalid',
   BackendDisconnect = 'backend-disconnect',
+  InsufficientScope = 'insufficient-scope',
+  TooManyConnections = 'too-many-connections',
 }
 
 export type EngineConnectionError = {
