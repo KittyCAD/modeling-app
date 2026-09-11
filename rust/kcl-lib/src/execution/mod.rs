ea115d995bd64034a5ed86b82bf9c8731732e961
Benjamin Corey <benjamin@zoo.dev>
2026-09-11 16:31:43 -0400

    ask for cpu pool when geometry_only=true

--- rust/kcl-lib/src/execution/mod.rs
+++ rust/kcl-lib/src/execution/mod.rs
@@ -1142,7 +1142,7 @@ impl ExecutorContext {
                 },
                 replay: settings.replay.clone(),
                 show_grid: if settings.show_grid { Some(true) } else { None },
-                pool: None,
+                pool: settings.geometry_only.then_some("cpu".to_string()),
                 pr,
                 unlocked_framerate: None,
                 webrtc: Some(false),
