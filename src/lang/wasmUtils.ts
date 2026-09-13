251ba795ebad880584a2a30ddbd2c66da686eeb3
Jordan Noone <jordan@kittycad.io>
2026-09-13 00:41:34 -0700

    Fingerprint KCL WASM alongside its JavaScript

--- src/lang/wasmUtils.ts
+++ src/lang/wasmUtils.ts
@@ -1,4 +1,3 @@
-import { webSafeJoin, webSafePathSplit } from '@src/lib/paths'
 import type { ModuleType } from '@src/lib/wasm_lib_wrapper'
 import { getModule, init, reloadModule } from '@src/lib/wasm_lib_wrapper'
 import { notifyActiveWasmInstance } from '@src/lib/wasmLifecycle'
@@ -10,17 +9,11 @@ export const wasmUrl = () => {
     return process.env.VITE_WASM_BASE_URL + wasmFile
   }
 
-  // For when we're in electron (file based) or web server (network based)
-  // For some reason relative paths don't work as expected. Otherwise we would
-  // just do /wasm_lib_bg.wasm. In particular, the issue arises when the path
-  // is used from within worker.ts.
-  const fullUrl = document.location.protocol.includes('http')
-    ? document.location.origin + '/kcl_wasm_lib_bg.wasm'
-    : document.location.protocol +
-      webSafeJoin(webSafePathSplit(document.location.pathname).slice(0, -1)) +
-      wasmFile
-
-  return fullUrl
+  // Workers need an absolute URL to the WASM emitted with this JavaScript build.
+  return new URL(
+    '../../rust/kcl-wasm-lib/pkg/kcl_wasm_lib_bg.wasm',
+    import.meta.url
+  ).href
 }
 
 // Renderer-side WASM start entry point. New startup paths should go through
