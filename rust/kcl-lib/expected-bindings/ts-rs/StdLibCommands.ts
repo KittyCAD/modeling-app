720bcd866f63bcef0ffe7a1cc86e8d7385db2ca2
Caleb Goates <53098881+cgoates@users.noreply.github.com>
2026-09-14 16:14:18 -0600

    Add tangent chains argument to KCL 3.0 and above (#13790)

--- rust/kcl-lib/expected-bindings/ts-rs/StdLibCommands.ts
+++ rust/kcl-lib/expected-bindings/ts-rs/StdLibCommands.ts
@@ -963,6 +963,18 @@ export default {
         "deprecated": false,
         "deprecatedSince": null,
         "removedIn": "3.0"
+      },
+      {
+        "name": "tangentChain",
+        "ty": "bool",
+        "docs": "If true, also chamfer edges that are tangent to the selected edges. Defaults to true.",
+        "required": false,
+        "special": false,
+        "experimental": false,
+        "addedIn": "3.0",
+        "deprecated": false,
+        "deprecatedSince": null,
+        "removedIn": null
       }
     ]
   },
@@ -2170,6 +2182,18 @@ export default {
         "deprecated": false,
         "deprecatedSince": null,
         "removedIn": "3.0"
+      },
+      {
+        "name": "tangentChain",
+        "ty": "bool",
+        "docs": "If true, also fillet edges that are tangent to the selected edges. Defaults to true.",
+        "required": false,
+        "special": false,
+        "experimental": false,
+        "addedIn": "3.0",
+        "deprecated": false,
+        "deprecatedSince": null,
+        "removedIn": null
       }
     ]
   },
