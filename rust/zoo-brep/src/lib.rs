c970b94bbb4a45138579724cb94c2fa7ebac1432
Benjamin Corey <benjamin@zoo.dev>
2026-09-14 12:35:11 -0400

    delete zoo-brep in favor of gltf-rs fork

--- rust/zoo-brep/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod brep;
-mod raw_extension;
-
-pub use brep::BrepRenderData;
-pub use brep::BrepRenderError;
-pub use raw_extension::RawZooExtension;
-pub use raw_extension::ZOO_BREP_EXTENSION;
-pub use raw_extension::ZooGltfError;
-pub use raw_extension::extract_zoo_extension;
