32e59dc3748849142b1050fc8aaaa58beef0d46c
Pierre Jacquier <pierre@zoo.dev>
2026-09-15 11:20:53 -0400

    Remove install desktop app status-bar prompt

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -805,7 +805,6 @@ const Home = () => {
           ...defaultGlobalStatusBarItems({
             autoUpdateDownloadProgress,
             autoUpdateReady,
-            hasCloudSyncFeature,
             onRestartToUpdate: () => {
               window.electron?.appRestart()
             },
