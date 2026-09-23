91b81a16e33bab9e34ff9ff334d4873f1ed3cf1f
Jordan Noone <jordan@kittycad.io>
2026-09-23 15:37:09 -0700

    Avoid resending unchanged scene settings (#14132)

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -940,23 +940,24 @@ export class App implements AppSubsystems {
     const newTheme = context.app.theme.current
     const themeChanged = this.lastSettings.app.theme !== newTheme
     const newBackfaceColor = context.modeling.backfaceColor.current
-    const themeUpdate = this.singletons.kclManager
-      .updateTheme(newTheme)
-      .then(() => {
-        if (themeChanged) {
+    const backfaceColorChanged =
+      this.lastSettings.modeling.backfaceColor !== newBackfaceColor
+    if (themeChanged) {
+      this.singletons.kclManager
+        .updateTheme(newTheme)
+        .then(() =>
           this.singletons.kclManager.sceneEntitiesManager.updateSketchGrid()
-        }
-      })
-    Promise.all([
-      themeUpdate,
-      ...(this.singletons.kclManager.engineCommandManager.connection?.connected
-        ? [
-            this.singletons.kclManager.engineCommandManager.setDefaultSystemProperties(
-              newBackfaceColor
-            ),
-          ]
-        : []),
-    ]).catch(reportRejection)
+        )
+        .catch(reportRejection)
+    }
+    if (
+      backfaceColorChanged &&
+      this.singletons.kclManager.engineCommandManager.connection?.connected
+    ) {
+      this.singletons.kclManager.engineCommandManager
+        .setDefaultSystemProperties(newBackfaceColor)
+        .catch(reportRejection)
+    }
 
     // Reapply settings to the engine
     try {
@@ -967,9 +968,6 @@ export class App implements AppSubsystems {
           context.modeling.fixedSizeGrid.current ||
         this.lastSettings.modeling.highlightEdges !==
           context.modeling.highlightEdges.current
-      const backfaceColorChanged =
-        this.lastSettings.modeling.backfaceColor !==
-        context.modeling.backfaceColor.current
       const engineConnection =
         this.singletons.kclManager.engineCommandManager.connection
 
